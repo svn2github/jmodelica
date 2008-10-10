f177a33d1ac0d85a1213ddf333701d5b4bfd9232
jakesson <jakesson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2008-10-10 04:02:33 +0000

    Added make files.
    git-svn-id: https://svn.jmodelica.org@118 7ae51c65-13d5-4921-9d80-6511ddd1419c

---
+++ trunk/IpoptInterface/objects.mk
@@ -0,0 +1,7 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lipopt -lm -ldl -lfrtbegin -lg2c -lcygwin -luser32 -lkernel32 -ladvapi32 -lshell32
