640e7fce8c074f6bdaec25e2b094d3004a2edac5
claird <claird@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2008-10-10 04:18:14 +0000

    checkin in something that I want to remove
    git-svn-id: https://svn.jmodelica.org@119 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/IpoptInterface/objects.mk
+++ trunk/IpoptInterface/objects.mk
@@ -4,4 +4,4 @@
 
 USER_OBJS :=
 
-LIBS := -lipopt -lm -ldl -lfrtbegin -lg2c -lcygwin -luser32 -lkernel32 -ladvapi32 -lshell32
+LIBS := -lipopt -lm -ldl -lgfortran
--- trunk/IpoptInterface/sources.mk
+++ trunk/IpoptInterface/sources.mk
@@ -23,7 +23,6 @@ C_UPPER_DEPS :=
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
-src/examples/VDPExample \
 src/examples/SimpleExample \
 src/OptimicaInterface \
 src/ModelInterface \
