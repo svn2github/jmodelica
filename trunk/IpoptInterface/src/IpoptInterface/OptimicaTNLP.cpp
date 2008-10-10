3779a4cdc964c6824e7bb2d68c0947412a9817c7
claird <claird@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2008-10-10 14:01:31 +0000

    First Working version of OptimicaTNLP
    git-svn-id: https://svn.jmodelica.org@126 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/IpoptInterface/src/IpoptInterface/OptimicaTNLP.cpp
+++ trunk/IpoptInterface/src/IpoptInterface/OptimicaTNLP.cpp
@@ -25,14 +25,19 @@ OptimicaTNLP::~OptimicaTNLP()
 bool OptimicaTNLP::get_nlp_info(Index& n, Index& m, Index& nnz_jac_g,
 				Index& nnz_h_lag, IndexStyleEnum& index_style)
 {
+  std::cout << "In get_nlp_info\n";
   if (!problem_->getDimensions(n, n_eq_, n_ineq_, nnz_jac_eq_, nnz_jac_ineq_)) {
       return false;
   } 
   
+  
   m = n_eq_ + n_ineq_;
   nnz_jac_g = nnz_jac_eq_ + nnz_jac_ineq_;
+  std::cout << "NNZ: " << nnz_jac_g << std::endl;
   nnz_h_lag = 0;
   index_style = FORTRAN_STYLE;
+
+  std::cout << "n: " << n << " m: " << m << " nnz_jac_g: " << nnz_jac_g << " nnz_h_lag: " << nnz_h_lag << std::endl;
   
   return true;
 }
@@ -40,19 +45,30 @@ bool OptimicaTNLP::get_nlp_info(Index& n, Index& m, Index& nnz_jac_g,
 bool OptimicaTNLP::get_bounds_info(Index n, Number* x_l, Number* x_u,
 				   Index m, Number* g_l, Number* g_u)
 {
+  std::cout << "In Get Bounds Info\n";
   DBG_ASSERT(n_eq_ + n_ineq_ == m);
   DBG_ASSERT(sizeof(Number) == sizeof(double));
   DBG_ASSERT(sizeof(Index) == sizeof(int));
   
+  std::cout << "first";
   for (int i=0; i<m; i++) {
 	  g_l[i] = -1e20;
   }
 
-  for (int i=0; i<n; i++) {
+  std::cout << "second\n";
+  for (int i=0; i<m; i++) {
 	 g_u[i] = 0;
   }
   
-  return problem_->getBounds((double*)x_u, (double*)x_l);
+  bool retval = problem_->getBounds((double*)x_u, (double*)x_l);
+  
+  std::cout << "third\n";
+
+  for (int i=0; i<n; i++) {
+    std::cout << x_l[i] << " <= x <= " << x_u[i] << std::endl;
+  }
+
+  return retval;
 }
 
 bool OptimicaTNLP::get_starting_point(Index n, bool init_x, Number* x,
@@ -91,10 +107,21 @@ bool OptimicaTNLP::eval_jac_g(Index n, const Number* x, bool new_x,
 			      Index m, Index nele_jac, Index* iRow,
 			      Index *jCol, Number* values)
 {
-  bool retval = problem_->evalJacEqConstraint((const double*)x, (double*)values);
-  if (retval) {
-    double* values_ineq = values + nnz_jac_eq_;
-    retval = problem_->evalJacIneqConstraint((const double*)x, values_ineq);
+  bool retval = false;
+  if (values == NULL) {
+    retval = problem_->getJacEqConstraintNzElements(iRow,jCol);
+    if (retval) {
+      iRow += nnz_jac_eq_;
+      jCol += nnz_jac_eq_;
+      retval = problem_->getJacIneqConstraintNzElements(iRow, jCol);
+    }
+  }
+  else {
+    retval = problem_->evalJacEqConstraint((const double*)x, (double*)values);
+    if (retval) {
+      values += nnz_jac_eq_;
+      retval = problem_->evalJacIneqConstraint((const double*)x, values);
+    }
   }
   return retval;
 }
