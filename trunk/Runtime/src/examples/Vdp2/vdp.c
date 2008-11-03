600c6f939631eb69ae7f8acb8a9ddd94213bae2a
jakesson <jakesson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2008-11-03 07:42:25 +0000

    Updated the build system and added a skeleton for another example in the Runtime system.
    git-svn-id: https://svn.jmodelica.org@171 7ae51c65-13d5-4921-9d80-6511ddd1419c

---
+++ trunk/Runtime/src/examples/Vdp2/vdp.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../../Jmi2/jmi.h"
+#include "../../Jmi2/jmi_runtime.h"
+
---
+++ trunk/Runtime/src/examples/Vdp2/vdp_main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../../Jmi2/jmi.h"
+#include "../../Jmi2/jmi_runtime.h"
+
+int main(int argv, char* argc[])
+{
+
+  printf("vdp_main\n");
+
+  return 0;
+
+}
