b92ef9183a7d8c291d983643463098413251a647
jakesson <jakesson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2009-03-09 14:00:52 +0000

    Improvements on build system, with reference to #36.
    git-svn-id: https://svn.jmodelica.org@293 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/JMI/src/jmi/jmi.c
+++ trunk/JMI/src/jmi/jmi.c
@@ -1,4 +1,18 @@
-y
+ /*
+    Copyright (C) 2009 Modelon AB
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the Common Public License as published by
+    IBM, version 1.0 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY. See the Common Public License for more details.
+
+    You should have received a copy of the Common Public License
+    along with this program.  If not, see
+     <http://www.ibm.com/developerworks/library/os-cpl.html/>.
+*/
+
 
 /*
  * jmi.c contains pure C functions and does not support any AD functions.
