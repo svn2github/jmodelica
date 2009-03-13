c915f1b84d474969fb8ac52405688c54a67f6319
jens_rantil <jens_rantil@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2009-03-13 13:54:01 +0000

    Update of JMI Python interface:  * Added module and UnitTest docstrings.  * Formatted the code.  * Implemented loadDLL(...), which should be able to load any DLL file platform independently. This means the tests (hopefully) should run independently of platoform.  * Current tests now run without any problems.  * Refactored and created a parent class JMIModelTestCase for general test framework for JMI models.
    git-svn-id: https://svn.jmodelica.org@308 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Python/src/jmodelica/pyjmi/jmi.py
+++ trunk/Python/src/jmodelica/pyjmi/jmi.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""@todo: Add description.
+"""Module containing the JMI interface Python wrappers.
 """
 #    Copyright (C) 2009 Modelon AB
 #
@@ -19,3 +19,66 @@
 #     http://www.python.org/doc/2.5.2/lib/module-ctypes.html
 #     http://starship.python.net/crew/theller/ctypes/tutorial.html
 #     http://www.scipy.org/Cookbook/Ctypes
+
+import ctypes
+import os.path
+
+
+class JMIException(Exception):
+    """ A JMI exception. """
+    pass
+
+
+def loadDLL(filepath):
+    """Loads a model from a DLL file and returns it.
+    
+    The filepath can be be both with or without file suffixes (as long
+    as standard file suffixes are used, that is).
+    
+    Example inputs that should work:
+      >> lib = loadDLL('model')
+      >> lib = loadDLL('model.dll')
+      >> lib = loadDLL('model.so')
+    . All of the above should work on the JModelica supported platforms.
+    However, the first one is recommended as it is the least platform
+    dependent syntax.
+    
+    @param filename File path without suffix.
+    
+    @see http://docs.python.org/library/ctypes.html
+    
+    """
+    
+    SUFFIXES_TO_APPEND = ['.so'] # Suffixes that might have to be added
+    
+    try:
+        """Trying Windows DLL."""
+        if os.path.isfile(filepath+'.dll'):
+            lib = ctypes.CDLL(filepath)
+            return lib
+        
+        filename = os.path.basename(filepath)
+        (name, delimiter, suffix) = filename.rpartition('.')
+        if suffix.lower() is 'dll' and os.path.isfile(filename):
+            lib = ctypes.CDLL(filename)
+            return lib
+        
+        """Trying different suffixes."""
+        suffixed_files = map(lambda x: filepath+x, SUFFIXES_TO_APPEND)
+        for suffixed_file in suffixed_files:
+            if os.path.isfile(suffixed_file):
+                lib = ctypes.CDLL(suffixed_file)
+                return lib
+        
+        """Trying without suffix."""
+        lib = ctypes.CDLL(filepath)
+        return lib
+                                          
+    except OSError, e:
+        raise JMIException("%s\nCould not load model." % e + \
+                           " Please check that the" \
+                           " file '%s'" % filename + \
+                           " (with varying prefix) exists and is not" +
+                           " corrupt.")
+        
+    
--- trunk/Python/src/jmodelica/pyjmi/jmitest.py
+++ trunk/Python/src/jmodelica/pyjmi/jmitest.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""@todo: Add description.
+"""Tests of the JMI interface Python wrappers.
+
+Some of the tests require the JMI examples to be compiled. See README
+for more information.
 """
 #    Copyright (C) 2009 Modelon AB
 #
@@ -17,41 +20,64 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import unittest
-import jmodelica.jmi
 import ctypes
 import os.path
 
+from jmi import loadDLL, JMIException
+
 # C++ demangling: http://www.kegel.com/mangle.html
 
-class CTypesVDPTestCase(unittest.TestCase):
-    """ Test loading jmi model dll directly with ctypes """
+class JMIModelTestCase(unittest.TestCase):
+    """A general test framework for JMI examples."""
+    
+    def loadDLL(self, relpath):
+        """Load a test model in the JMI model example folder."""
+        try:
+            dll = loadDLL('../../../../build/JMI/examples/' \
+                          + relpath)
+        except JMIException, e:
+            raise JMIException("%s\nUnable to load test models." \
+                               " You have probably not compiled the" \
+                               " examples. Please refer to the"
+                               " JModelica for more information." % e)
+                               
+        return dll
+
+
+class CTypesVDPTestCase(JMIModelTestCase):
+    """Test loading jmi model dll directly with ctypes."""
+    
     def setUp(self):
-        self.lib = ctypes.CDLL(os.path.join(os.path.split(__file__)[0],
-                                            u'examples/vdp.dll'))
-    def tearDown(self):
-        pass
+        self.lib = self.loadDLL('Vdp/vdp')
+        
     def testLoaded(self):
-        assert isinstance(self.lib,ctypes.CDLL), "lib is not a CDLL instance"
-        assert isinstance(self.lib.jmi_new,ctypes._CFuncPtr), \
-            "lib.jmi_new is not a ctypes._CFuncPtr instance"
+        assert(isinstance(self.lib,ctypes.CDLL), \
+               "lib is not a CDLL instance")
+        assert(isinstance(self.lib.jmi_new,ctypes._CFuncPtr), \
+               "lib.jmi_new is not a ctypes._CFuncPtr instance")
+            
     def testJMINew(self):
         jmip = ctypes.c_voidp()
-        assert(self.lib.jmi_new(ctypes.byref(jmip)) == 0,"jmi_new returned non-zero")
-        assert(jmip.value != None,"jmi struct not returned correctly")
+        assert(self.lib.jmi_new(ctypes.byref(jmip)) == 0, \
+               "jmi_new returned non-zero")
+        assert(jmip.value != None, \
+               "jmi struct not returned correctly")
 
-class CTypesVDPADTestCase(unittest.TestCase):
-    """ Test loading jmi cppad model dll directly with ctypes """
-    def setUp(self):
-        self.lib = ctypes.CDLL(os.path.join(os.path.split(__file__)[0],
-                                            u'examples/vdp_cppad.dll'))
-    def tearDown(self):
-        pass
-    def testLoaded(self):
-        assert isinstance(self.lib,ctypes.CDLL), "lib is not a CDLL instance"
-        assert isinstance(self.lib._Z7jmi_newPP5jmi_t,ctypes._CFuncPtr), \
-            "lib.jmi_new is not a ctypes._CFuncPtr instance"
 
+class CTypesVDPADTestCase(JMIModelTestCase):
+    """Test loading jmi cppad model dll directly with ctypes."""
     
+    def setUp(self):
+        self.lib = self.loadDLL('Vdp_cppad/vdp_cppad')
+        
+    def testLoaded(self):
+        assert(isinstance(self.lib,ctypes.CDLL), \
+               "lib is not a CDLL instance")
+        assert(isinstance(self.lib._Z7jmi_newPP5jmi_t, \
+                          ctypes._CFuncPtr), \
+               "lib.jmi_new is not a ctypes._CFuncPtr instance")
+
+
 def suite():
     suite = unittest.TestSuite()
     suit.addTest(CTypesVDPTestCase("testLoaded"))
@@ -63,4 +89,3 @@ def suite():
 # run all tests when module is executed from command line
 if __name__ == "__main__":
     unittest.main()
-    
