8bb9b2505e55ee57989b71b39d4e135280981f4a
tove <tove@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2009-06-12 07:52:38 +0000

    Changed Python docstrings and created Doxygen docstrings, #134
    git-svn-id: https://svn.jmodelica.org@526 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Python/src/jmodelica/compiler.py
+++ trunk/Python/src/jmodelica/compiler.py
@@ -1,8 +1,11 @@
-""" Module containing functions for compiling models. Options which are user specific can be set
-    either before importing this module by editing the file options.py or interactively by accessing
-    the default options via the common module. If options are not changed the default option settings
-    will be used.
-"""
+## @package compiler 
+# Module containing functions for compiling models. Options which are user specific can be set
+# either before importing this module by editing the file options.py or interactively by accessing
+# the default options via the common module. If options are not changed the default option settings
+# will be used.
+#
+
+
 #    Copyright (C) 2009 Modelon AB
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -41,39 +44,59 @@ if not jpype.isJVMStarted():
 org = jpype.JPackage('org')
 JCompiler = org.jmodelica.applications.ModelicaCompiler
 
+## Compiles a model. Performs all steps in the compilation of a model: parsing, instantiating, 
+# flattening, code generation and dll generation. Set target to specify the 
+# contents of the object file used to build the .dll. Default is "model". 
+# Other two options are "algorithms" and "ipopt". See makefile in install folder
+# for details.
+#        
+#
+# @param model_file_name Path to file in which the model is contained.
+# @param model_class_name Name of model class in the model file to compile.
+# @param target The build target.
+# @exception CompilerError If one or more error is found during compilation.
+# @exception ModelicaClassNotFoundError If the model class is not found.
+# @exception IOError If the model file is not found, can not be read or
+# any other IO related error.
+# @exception Exception If there are general errors related to the parsing of the model.       
+# @exception JError If there was a runtime exception thrown by the underlying Java classes.
+#
 
 def compile_model(model_file_name, model_class_name, target = "model"):
-    """ Performs all steps in the compilation of a model: parsing, 
-    instantiating, flattening, code generation and dll generation.
-    Outputs are object file, c-code file, xml file and dll which 
-    are all written to the folder in which the compilation is performed.
-    All files will get the default name <model_class_name>.<ext>.
+    """ 
+    Compiles a model.
     
-    @param model_file_name: 
-        Path to file in which the model is contained.
-    @param model_class_name:
-        Name of model class in the model file to compile.
-    @param target:
-        Set target to specify the contents of the object file used 
-        to build the .dll. Default is "model". Other two options
-        are "algorithms" and "ipopt". See makefile in install folder
-        for details.
+    Performs all steps in the compilation of a model: parsing, instantiating, 
+    flattening, code generation and dll generation. Outputs are object file, 
+    c-code file, xml file and dll which are all written to the folder in which 
+    the compilation is performed. All files will get the default name 
+    <model_class_name>.<ext>. Set target to specify the contents of the object 
+    file used to build the .dll. Default is "model". Other two options are 
+    "algorithms" and "ipopt". See makefile in install folder for details.
+    
+    Parameters:
+    
+        model_file_name -- 
+            Path to file in which the model is contained.
+        model_class_name -- 
+            Name of model class in the model file to compile.
+        target -- 
+            The build target.
         
-    @raise CompilerError:
-        Raised if one or more error is found during compilation.
-    @raise ModelicaClassNotFoundError:
-        Raised if the model class is not found.
-    @raise IOError:
-        Raised if the model file is not found, can not be read or
-        any other IO related error.
-    @raise Exception:
-        Raised by the parser for general errors related to the parsing 
-        of the model.       
-    @raise JError:
-        Raised if there was a runtime exception thrown by the underlying Java
-        classes, for example, NullPointerException.
+    Exceptions:
         
-"""
+        CompilerError -- 
+            Raised if one or more error is found during compilation.
+        ModelicaClassNotFoundError -- 
+            If the model class is not found.
+        IOError -- 
+            If the model file is not found, can not be read or any other IO related error.
+        Exception -- 
+            If there are general errors related to the parsing of the model.       
+        JError -- 
+            If there was a runtime exception thrown by the underlying Java classes.
+    """
+        
     xml_variables_path = common._jm_home+os.sep+'CodeGenTemplates'+os.sep+'jmi_modelica_variables_template.xml'
     xml_values_path = common._jm_home+os.sep+'CodeGenTemplates'+os.sep+'jmi_modelica_values_template.xml'
     cppath = common._jm_home+os.sep+'CodeGenTemplates'+os.sep+'jmi_modelica_template.c'
@@ -87,27 +110,39 @@ def compile_model(model_file_name, model_class_name, target = "model"):
     except jpype.JavaException, ex:
         _handle_exception(ex)
 
-
+## Parses a model. Parses a model and returns a reference to the source tree representation.
+# @param model_file_name Path to file in which the model is contained.
+# @return Reference to the root of the source tree representation of the parsed model.
+# @exception CompilerError If one or more error is found during compilation.
+# @exception IOError If the model file is not found, can not be read or any other IO related error.
+# @exception Exception If there are general errors related to the parsing of the model.
+# @exception JError If there was a runtime exception thrown by the underlying Java classes.
+#
 def parse_model(model_file_name):
-    """ Parses a model and returns a reference to the source tree representation.
+    """ 
+    Parses a model.
     
-        @param model_file_name:
+    Parses a model and returns a reference to the source tree representation.
+    
+    Parameters:    
+    
+        model_file_name -- 
             Path to file in which the model is contained.
-        @return: 
+            
+    Return:
+ 
             Reference to the root of the source tree representation of the parsed model.
+    
+    Exceptions:
         
-        @raise CompilerError:
-            Raised if one or more error is found during compilation.
-        @raise IOError:
-            Raised if the model file is not found, can not be read or
-            any other IO related error.
-        @raise Exception:
-            Raised by the parser for general errors related to the parsing 
-            of the model.       
-        @raise JError:
-            Raised if there was a runtime exception thrown by the underlying Java
-            classes, for example, NullPointerException.
-
+        CompilerError --
+            If one or more error is found during compilation.
+        IOError --
+            If the model file is not found, can not be read or any other IO related error.
+        Exception --
+            If there are general errors related to the parsing of the model.       
+        JError -- 
+            If there was a runtime exception thrown by the underlying Java classes.
     """ 
     try:
         sr = JCompiler.parseModel(model_file_name)
@@ -116,29 +151,42 @@ def parse_model(model_file_name):
     except jpype.JavaException, ex:
         _handle_exception(ex)
                
-
+## Generates an instance tree representation. Generates an instance tree representation for a model 
+#using the source tree belonging to the model which must first be created with parse_model.
+# @see: parse_model
+# @param source_root Reference to the root of the source tree representation.
+# @param model_class_name Name of model class in the model file to compile.
+# @return Reference to the root of the instance tree representation.
+# @exception CompilerError If one or more error is found during compilation.
+# @exception ModelicaClassNotFoundError If the model class is not found.
+# @exception JError If there was a runtime exception thrown by the underlying Java classes.
 def instantiate_model(source_root, model_class_name):
-    """ Generates an instance tree representation for a model 
-        using the source tree belonging to the model which must 
-        first be created with parse_model.
-        
-        @see: parse_model
-        
-        @param source_root:
-            Reference to the root of the source tree representation.
-        @param model_class_name:
-            Name of model class in the model file to compile.
-            
-        @return: Reference to the root of the instance tree representation. 
+    """ 
+    Generates an instance tree representation for a model.
     
-        @raise CompilerError:
-            Raised if one or more error is found during compilation.
-        @raise ModelicaClassNotFoundError:
-            Raised if the model class is not found.
-        @raise JError:
-            Raised if there was a runtime exception thrown by the underlying Java
-            classes, for example, NullPointerException.
-
+    
+    Generates an instance tree representation for a model using the source 
+    tree belonging to the model which must first be created with parse_model.
+        
+    Parameters:   
+        
+        source_root -- 
+            Reference to the root of the source tree representation.
+        model_class_name -- 
+            Name of model class in the model file to compile.
+    
+    Returns:
+    
+        Reference to the root of the instance tree representation. 
+    
+    Exceptions:
+    
+        CompilerError -- 
+            If one or more error is found during compilation.
+        ModelicaClassNotFoundError -- 
+            If the model class is not found.
+        JError --
+            If there was a runtime exception thrown by the underlying Java classes.
     """
     
     try:
@@ -148,31 +196,49 @@ def instantiate_model(source_root, model_class_name):
     except jpype.JavaException, ex:
         _handle_exception(ex)
 
-
+## Computes a flattened representation of a model. Computes a flattened representation 
+#of a model using the instance tree belonging to the model which must first be created with 
+#instantiate_model.
+# @see instantiate_model
+# @param model_file_name Path to file in which the model is contained.
+# @param model_class_name Name of model class in the model file to compile.
+# @param inst_prg_root Reference to the instance tree representation.
+# @return Object (FClass) representing the flattened model.
+# @exception CompilerError If one or more error is found during compilation.
+# @exception ModelicaClassNotFoundError If the model class is not found.
+# @exception IOError If the model file is not found, can not be read or any other IO related error.
+# @exception JError If there was a runtime exception thrown by the underlying Java classes.
+#
 def flatten_model(model_file_name, model_class_name, inst_prg_root):
-    """ Computes a flattened representation of a model using the instance tree 
-        belonging to the model which must first be created with instantiate_model.
-        
-        @see: instantiate_model
-        
-        @param model_file_name:
-            Path to file in which the model is contained.
-        @param model_class_name:
-            Name of model class in the model file to compile.
-        @param inst_prg_root: Reference to the instance tree representation. 
-        
-        @return: Object (FClass) representing the flattened model. 
+    """ 
+    Computes a flattened representation of a model. 
     
-        @raise CompilerError:
-            Raised if one or more error is found during compilation.
-        @raise ModelicaClassNotFoundError:
-            Raised if the model class is not found.
-        @raise IOError:
-            Raised if the model file is not found, can not be read or
-            any other IO related error.
-        @raise JError:
-            Raised if there was a runtime exception thrown by the underlying Java
-            classes, for example, NullPointerException.
+    Computes a flattened representation of a model using the instance tree 
+    belonging to the model which must first be created with instantiate_model.
+        
+    Parameters:  
+        
+        model_file_name --
+            Path to file in which the model is contained.
+        model_class_name --
+            Name of model class in the model file to compile.
+        inst_prg_root -- 
+            Reference to the instance tree representation. 
+    
+    Returns:
+    
+        Object (FClass) representing the flattened model. 
+    
+    Exceptions:
+    
+        CompilerError --
+            If one or more error is found during compilation.
+        ModelicaClassNotFoundError --
+            If the model class is not found.
+        IOError --
+            If the model file is not found, can not be read or any other IO related error.
+        JError -- 
+            If there was a runtime exception thrown by the underlying Java classes.
 
     """
 
@@ -183,23 +249,35 @@ def flatten_model(model_file_name, model_class_name, inst_prg_root):
     except jpype.JavaException, ex:
         _handle_exception(ex)
 
-
+## Generates code for a model. Generates c and xml code for a model using the FClass 
+#represenation created with flatten_model and template files located in the JModelica 
+#installation folder. Default output folder is the current folder from which this 
+#module is run.
+# @see flatten_model
+# @param fclass Reference to the flattened model object representation.
+# @exception IOError If the model file is not found, can not be read or any other IO related error.
+# @exception JError If there was a runtime exception thrown by the underlying Java classes.
+#
 def generate_code(fclass):
-    """ Generates c and xml code for a model using the FClass represenation created
-        with flatten_model and template files located in the JModelica installation folder.
-        Default output folder is the current folder from which this module is run.
+    """ 
+    Generates code for a model.
+    
+    Generates code for a model c and xml code for a model using the FClass 
+    represenation created with flatten_model and template files located in 
+    the JModelica installation folder. Default output folder is the current 
+    folder from which this module is run.
         
-        @see: flatten_model
+    Parameters:
         
-        @param fclass: Reference to the flattened model object representation.  
+        fclass -- 
+            Reference to the flattened model object representation.  
         
-        @raise IOError:
-            Raised if the model file is not found, can not be read or
-            any other IO related error.
-        @raise JError:
-            Raised if there was a runtime exception thrown by the underlying Java
-            classes, for example, NullPointerException.
-
+    Exceptions:
+    
+        IOError -- 
+            If the model file is not found, can not be read or any other IO related error.
+        JError --
+            If there was a runtime exception thrown by the underlying Java classes.
     """
     xml_variables_path = common._jm_home+os.sep+'CodeGenTemplates'+os.sep+'jmi_modelica_variables_template.xml'
     xml_values_path = common._jm_home+os.sep+'CodeGenTemplates'+os.sep+'jmi_modelica_values_template.xml'
@@ -210,24 +288,33 @@ def generate_code(fclass):
     except jpype.JavaException, ex:
         _handle_exception(ex)
 
-
+## Compiles a dll. Compiles a c code representation of a model and outputs a .dll file.
+#Default output folder is the current folder from which this module is run. Needs a c-file
+#which is generated with generate_code. Set target to specify the contents of the object 
+# file used to build the .dll. Default is "model". Other two options are "algorithms" and 
+# "ipopt". See makefile in install folder for details.
+# @see: generate_code
+# @param c_file_name
+# @param target
+# @return System return value.
 def compile_dll(c_file_name, target="model"):
-    """ Compiles a c code representation of a model and outputs a .dll file.
-        Default output folder is the current folder from which this module is run.
-        Needs a c-file which is generated with generate_code.
+    """ 
+    Compiles a c code representation of a model and outputs a .dll file.
+    
+    Compiles a c code representation of a model and outputs a .dll file.
+    Default output folder is the current folder from which this module is run.
+    Needs a c-file which is generated with generate_code.
         
-        @see: generate_code
+    Parameters:
         
-        @param c_file_name:
+        c_file_name --
             Name of c-file for which the .dll should be compiled without file extention.
-        @param target:
-            Set target to specify the contents of the object file used 
-            to build the .dll. Default is "model". Other two options
-            are "algorithms" and "ipopt". See makefile in install folder
-            for details.
+        target --
+            Build target.
+
+    Returns:
             
-        @return: System return value.
-      
+        System return value.     
     """
     #make settings
     make_file = common._jm_home+os.sep+'Makefiles'+os.sep+'MakeFile'
