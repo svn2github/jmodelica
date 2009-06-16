1b468e3cf2f99660f86a30a4cf5fe5cda10f0cd7
jmattsson <jmattsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2009-06-16 15:14:00 +0000

    Extracted code under EPL to separate plugin. (#164)
    git-svn-id: https://svn.jmodelica.org@550 7ae51c65-13d5-4921-9d80-6511ddd1419c

---
+++ trunk/EclipseIDE/org.jmodelica.folding/src/org/jmodelica/folding/CharacterPosition.java
@@ -0,0 +1,11 @@
+package org.jmodelica.folding;
+
+import org.eclipse.jface.text.Position;
+
+public class CharacterPosition extends Position {
+
+	public CharacterPosition(int offset, int length) {
+		super(offset, length);
+	}
+
+}
---
+++ trunk/EclipseIDE/org.jmodelica.folding/src/org/jmodelica/folding/CharacterProjectionAnnotation.java
@@ -0,0 +1,14 @@
+package org.jmodelica.folding;
+
+import org.eclipse.jface.text.source.projection.ProjectionAnnotation;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Canvas;
+
+public class CharacterProjectionAnnotation extends ProjectionAnnotation {
+
+	@Override
+	public void paint(GC gc, Canvas canvas, Rectangle rectangle) {
+	}
+
+}
