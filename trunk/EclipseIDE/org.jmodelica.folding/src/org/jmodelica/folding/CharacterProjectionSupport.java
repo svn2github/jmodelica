ad9d69973f24f66ac99065819ba9bbccf65616f4
jmattsson <jmattsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2009-06-20 15:17:11 +0000

    Added licence blurb to altered/new files in org.jmodelica.folding (#164)
    git-svn-id: https://svn.jmodelica.org@567 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/EclipseIDE/org.jmodelica.folding/src/org/jmodelica/folding/CharacterProjectionSupport.java
+++ trunk/EclipseIDE/org.jmodelica.folding/src/org/jmodelica/folding/CharacterProjectionSupport.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2008, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,8 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Modeleon AB     - modification to support folds that aren't limited 
+ *                       to line breaks
  *******************************************************************************/
 package org.jmodelica.folding;
 
