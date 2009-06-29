8896c60867c2297eefd1e5b9e511937577755c57
jakesson <jakesson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2009-06-29 17:29:44 +0000

    Added import of result files and added a nose test, see #166.
    git-svn-id: https://svn.jmodelica.org@608 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Python/src/jmodelica/io.py
+++ trunk/Python/src/jmodelica/io.py
@@ -19,6 +19,10 @@ Module for writing optimization and simulation results to file.
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+import numpy
+import array
+import scipy.io
+
 def export_result_dymola(jmi_model, data, file_name='', format='txt'):
     """
     Export an optimization or simulation result to file in Dymolas
@@ -46,17 +50,17 @@ def export_result_dymola(jmi_model, data, file_name='', format='txt'):
 
     Limitations:
         Currently only textual format is supported.
-            
+
     """
 
     if (format=='txt'):
 
         if file_name=='':
             file_name=jmi_model.get_name() + '_result.txt'
-        
+
         # Open file
         f = open(file_name,'w')
-        
+
         # Write header
         f.write('#1\n')
         f.write('char Aclass(3,11)\n')
@@ -74,7 +78,7 @@ def export_result_dymola(jmi_model, data, file_name='', format='txt'):
         for ref in name_value_refs:
             if (len(names.get(ref))>max_name_length):
                 max_name_length = len(names.get(ref))
-                
+
         f.write('char name(%d,%d)\n' % (len(name_value_refs)+1, max_name_length))
         f.write('time\n')
 
@@ -82,7 +86,7 @@ def export_result_dymola(jmi_model, data, file_name='', format='txt'):
             f.write(names.get(ref)+'\n')
 
         f.write('\n')
-        
+
         # Write descriptions
         descriptions = jmi_model.get_variable_descriptions()
         desc_value_refs = descriptions.keys()
@@ -93,10 +97,10 @@ def export_result_dymola(jmi_model, data, file_name='', format='txt'):
         for ref in desc_value_refs:
             if (len(descriptions.get(ref))>max_desc_length):
                 max_desc_length = len(descriptions.get(ref))
-               
+
         f.write('char description(%d,%d)\n' % (len(name_value_refs)+1, max_desc_length))
         f.write('Time in [s]')
-        
+
         # Loop over all variables, not only those with a description
         for ref in name_value_refs:
             if (desc_value_refs.count(ref)==0):
@@ -144,11 +148,215 @@ def export_result_dymola(jmi_model, data, file_name='', format='txt'):
             for ref in range(n_vars):
                 f.write(" %12.12f" % data[i,ref])
             f.write('\n')
-        
+
         f.write('\n')
 
         f.close()
 
     else:
         raise Error('Export on binary Dymola result files not yet supported.')
+
+
+class Trajectory:
+    """
+    Class for storing a time series.
+    """
+    
+    def __init__(self,t,x):
+        """
+        Constructor for the Trajectory class.
+
+        Parameters:
+            t --
+                Abscissa of the trajectory.
+            x --
+                The ordinate of the trajectory.
+
+        """
+        self.t = t
+        self.x = x
+
+        
+class ResultDymolaTextual:
+    """ Class representing a simulation or optimization result loaded from a
+    Dymola binary file.
+    """
+
+    def __init__(self,fname):
+        """
+        Load a result file written on Dymola textual format.
+
+        Parameters:
+            fname --
+                Name of file.
+        """
+        fid = open(fname,'r')
+        
+        result  = [];
+     
+        # Read Aclass section
+        l = fid.readline()
+        tmp = l.partition('(')
+        while tmp[0]!='char Aclass':
+            l = fid.readline()
+            tmp = l. partition('(')
+        nLines = tmp[2].partition(',')
+        nLines = int(nLines[0])
+        Aclass = []
+        for i in range(0,nLines):
+            Aclass.append(fid.readline().strip())
+        self.Aclass = Aclass
+
+        # Read name section
+        l = fid.readline()
+        tmp = l.partition('(')
+        while tmp[0]!='char name':
+            l = fid.readline()
+            tmp = l. partition('(')
+        nLines = tmp[2].partition(',')
+        nLines = int(nLines[0])
+        name = []
+        for i in range(0,nLines):
+            name.append(fid.readline().strip())
+        self.name = name
+     
+        # Read description section   
+        l = fid.readline()
+        tmp = l.partition('(')
+        while tmp[0]!='char description':
+            l = fid.readline()
+            tmp = l. partition('(')
+        nLines = tmp[2].partition(',')
+        nLines = int(nLines[0])
+        description = []
+        for i in range(0,nLines):
+            description.append(fid.readline().strip())
+        self.description = description
+
+        # Read dataInfo section
+        l = fid.readline()
+        tmp = l.partition('(')
+        while tmp[0]!='int dataInfo':
+            l = fid.readline()
+            tmp = l. partition('(')
+        nLines = tmp[2].partition(',')
+        nCols = nLines[2].partition(')')
+        nLines = int(nLines[0])
+        nCols = int(nCols[0])
+        dataInfo = []
+        for i in range(0,nLines):
+            info = fid.readline().split()
+            dataInfo.append(map(int,info[0:nCols]))
+        self.dataInfo = numpy.array(dataInfo)
+
+        # Find out how many data matrices there are
+        nData = 0
+        for i in range(0,nLines):
+            if dataInfo[i][0] > nData:
+                nData = dataInfo[i][0]
+                
+        self.data = []
+        for i in range(0,nData): 
+            l = fid.readline()
+            tmp = l.partition(' ')
+            while tmp[0]!='float' and tmp[0]!='double':
+                l = fid.readline()
+                tmp = l. partition(' ')
+            tmp = tmp[2].partition('(')
+            nLines = tmp[2].partition(',')
+            nCols = nLines[2].partition(')')
+            nLines = int(nLines[0])
+            nCols = int(nCols[0])
+            data = []
+            for i in range(0,nLines):
+                info = fid.readline().split()
+                data.append(map(float,info[0:nCols]))
+            self.data.append(numpy.array(data))
+
+    def get_variable_index(self,name): 
+        """
+        Retrieve the index in the name vector of a given variable.
+        
+        Parameters:
+            name --
+                Name of variable.
+        
+        Returns:
+            In integer index.
+        """
+        return self.name.index(name)
+
+    def get_variable_data(self,name):
+        """
+        Retrieve the data sequence for a variable with a given name.
+        
+        Parameters:
+            name --
+                Name of the variable.
+
+        Returns:
+            A Trajectory object containing the time vector and the data vector
+            of the variable.
+        """
+        varInd  = self.get_variable_index(name)
+        dataInd = self.dataInfo[varInd][1]-1
+        dataMat = self.dataInfo[varInd][0]-1
+        # Take into account that the 'Time' variable has data matrix index 0,
+        # which means that it is
+        if dataMat<0:
+            dataMat = 0
+        return Trajectory(self.data[dataMat][:,0],self.data[dataMat][:,dataInd])
+	
+	
+class ResultDymolaBinary:
+    """ Class representing a simulation or optimization result loaded from a
+    Dymola binary file.
+    """
+
+    def __init__(self,fname):
+        """
+        Load a result file written on Dymola binary format.
+
+        Parameters:
+            fname --
+                Name of file.
+        """
+        self.raw = scipy.io.loadmat(fname,chars_as_strings=False)
+        name = self.raw['name']
+        self.name = [array.array('u',name[:,i].tolist()).tounicode().rstrip() for i in range(0,name[0,:].size)]
+        description = self.raw['description']
+        self.description = [array.array('u',description[:,i].tolist()).tounicode().rstrip() for i in range(0,description[0,:].size)]
+	
+    def get_variable_index(self,name): 
+        """
+        Retrieve the index in the name vector of a given variable.
+        
+        Parameters:
+            name --
+                Name of variable.
+
+        Returns:
+            In integer index.
+        """
+        return self.name.index(name)
+	
+    def get_variable_data(self,name):
+        """
+        Retrieve the data sequence for a variable with a given name.
+        
+        Parameters:
+            name --
+                Name of the variable.
+
+        Returns:
+            A Trajectory object containing the time vector and the data vector
+            of the variable.
+        """
+        varInd  = self.get_variable_index(name)
+        dataInd = self.raw['dataInfo'][1][varInd]-1
+        dataMat = self.raw['dataInfo'][0][varInd]
+        # Take into account that the 'Time' variable has data matrix index 0
             
+        if dataMat<1:
+            dataMat = 1
+        return Trajectory(self.raw['data_%d'%dataMat][0,:],self.raw['data_%d'%dataMat][dataInd,:])
