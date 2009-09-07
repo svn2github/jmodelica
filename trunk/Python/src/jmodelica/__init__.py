870228188b3e8c6430c5e22db7d74aa5968eb15c
jens_rantil <jens_rantil@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2009-09-07 19:39:19 +0000

    The {{{jmodelica}}} package's {{__init__.py}}} now conforms to the coding standard. See ticket:192.
    git-svn-id: https://svn.jmodelica.org@907 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Python/src/jmodelica/__init__.py
+++ trunk/Python/src/jmodelica/__init__.py
@@ -16,7 +16,8 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-__all__ = ['jmi', 'xmlparser', 'compiler', 'optimicacompiler','optimization', 'examples', 'tests','io']
+__all__ = ['jmi', 'xmlparser', 'compiler', 'optimicacompiler','optimization',
+           'examples', 'tests','io']
 
 import os, os.path
 import warnings
@@ -26,12 +27,14 @@ try:
     if not os.path.exists(_p):
         raise IOError
 except KeyError, IOError:
-    raise EnvironmentError('The environment variable JMODELICA_HOME is not set or points to a non-existing location.')
+    raise EnvironmentError('The environment variable JMODELICA_HOME is not '
+                           'set or points to a non-existing location.')
 
 try:
     _f = os.path.join(os.environ['JMODELICA_HOME'],'startup.py')
     execfile(_f)
 except IOError:
-    warnings.warn('Startup script ''%s'' not found. Environment may be corrupt.' % _f)
+    warnings.warn('Startup script ''%s'' not found. Environment may be ' % _f
+                  'corrupt.')
 
 
