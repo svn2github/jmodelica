9594b284beccca55ffc8d3d46fc8750c95aaa73c
jens_rantil <jens_rantil@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2009-09-07 19:35:54 +0000

    Committing a sketch (set of tests) of the new SUNDIALS simulation interface. I will implement it tomorrow. See ticket:343.
    git-svn-id: https://svn.jmodelica.org@906 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Python/src/jmodelica/tests/simulation/test_sundials.py
+++ trunk/Python/src/jmodelica/tests/simulation/test_sundials.py
@@ -11,6 +11,227 @@ from jmodelica.tests import load_example_standard_model
 from jmodelica.simulation.sundials import solve_using_sundials
 
 
+class TestSundialsOdeSimulator:
+    def setUp(self):
+        """Load the test model."""
+        
+        self.m = load_example_standard_model('VDP_pack_VDP_Opt', 'VDP.mo', 
+                                             'VDP_pack.VDP_Opt')
+        self.simulator = SundialsOdeSimulator(self.m)
+        
+    def test_is_simulator(self):
+        assert isinstance(self.simulator, jmodelica.tests.simulation.Simulator)
+                                             
+    def test_simulation(self):
+        """Run a very basic simulation."""
+        
+        simulator = self.simulator
+        x_before_simulation = simulation.get_model().x.copy()
+        simulator.run()
+        Ts, ys = simulator.get_solution()
+        
+        assert len(Ts) == len(ys), "Time points and solution points must be " \
+                                   "equal lengths."
+        assert len(Ts) >= 5, "A solution was expected got less than 5 points."
+        assert simulator.get_sensitivities() == None, "No sensitivity " \
+                                                      "calculation should " \
+                                                      "have been done."
+        assert not (x_before_simulation==simulation.get_model().x).all(), \
+               "Simulation does seem to have been performed."
+               
+        # Plotting
+        fig = p.figure()
+        p.plot(Ts, ys)
+        p.title('testFixedSimulation(...) output')
+        fig.savefig('TestSundialsOdeSimulator_test_simulation.png')
+               
+    def test_simulation_return_last(self):
+        """Testing the 'return_last'.
+        
+        The 'return_last' enables the user to ignore the whole solution
+        horizon (ie. not accumulate the result).
+        
+        This saves memory and somewhat speed.
+        """
+        simulator = self.simulator
+        simulator.return_last(True)
+        simulator.run()
+        T, Y = simulator.get_solution()
+        nose.tools.assert_raises(TypeError, len, T) # Assert scalar
+        nose.tools.assert_equal(len(Y), 3)
+        
+        
+    def test_simulation_with_sensivity(self):
+        """Run simulation with sensitivity analysis."""
+        
+        simulator = self.simulator
+        simulator.sensitivity_analysis(False)
+        simulator.run()
+        Ts1, ys1 = simulator.get_solution()
+        
+        assert len(Ts) == len(ys), "Time points and solution points must be " \
+                                   "equal lengths."
+        assert len(Ts) >= 5, "A solution was expected got less than 5 points."
+        assert simulator.get_sensitivities() == None, "No sensitivity " \
+                                                      "calculation should " \
+                                                      "have been returned."
+                                                      
+        simulator.sensitivity_analysis(True)
+        simulator.get_model().reset()
+        simulator.run()
+        Ts2, ys2 = simulator.get_solution()
+        N.tools.assert_arrays_almost_equal(Ts1, Ts2)
+        N.tools.assert_arrays_almost_equal(ys1, ys2)
+        assert simulator.get_sensitivities() != None, "Sensitivitie should " \
+                                                      "have been returned."
+        # TODO: Assert exact size of the sensitivity matrix
+                                                      
+        simulator.sensitivity_analysis(False)
+        simulator.get_model().reset()
+        simulator.run()
+        Ts3, ys3 = simulator.get_solution()
+        N.tools.assert_arrays_almost_equal(Ts1, Ts3)
+        N.tools.assert_arrays_almost_equal(ys1, ys3)
+        assert simulator.get_sensitivities() == None, "No sensitivity " \
+                                                      "calculation should " \
+                                                      "have been returned."
+                                                      
+    def test_set_get_verbosity(self):
+        """Test the verbosity setter/getter.
+        
+        Setting verbosity is highly useful for debugging, but can at the same
+        time be very annoying if too much output is cluttering the console.
+        
+        This test test not only tests getters and setters but als documents the
+        available verbosities.
+        
+        """
+        simulator = self.simulator
+        simulator.set_verbosity(SundialsOdeSimulator.QUIET)
+        assert SundialsOdeSimulator.QUIET == 0, "QUIET constant should be zero"
+        simulator.set_verbosity(SundialsOdeSimulator.WHISPER)
+        assert simulator.get_verbosity() == SundialsOdeSimulator.WHISPER
+        simulator.set_verbosity(SundialsOdeSimulator.NORMAL)
+        assert simulator.get_verbosity() == SundialsOdeSimulator.NORMAL
+        simulator.set_verbosity(SundialsOdeSimulator.LOUD)
+        assert simulator.get_verbosity() == SundialsOdeSimulator.LOUD
+        simulator.set_verbosity(SundialsOdeSimulator.SCREAM)
+        assert simulator.get_verbosity() == SundialsOdeSimulator.SCREAM
+        
+    def test_set_get_model(self):
+        """Test the model setter/getter."""
+        simulator = self.simulator
+        assert simulator.get_model() == self.m
+        
+        another_model = load_example_standard_model('VDP_pack_VDP_Opt',
+                                                    'VDP.mo', 
+                                                    'VDP_pack.VDP_Opt')
+        simulator.set_model(another_model)
+        assert simulator.get_model() == another_model
+        
+    def test_simulation_intervals():
+        simulator = self.simulator
+        
+        # First test setters and getters
+        default_start = simulator.get_start_time()
+        default_final = simulator.get_final_time()
+        assert default_start < default_final
+        
+        my_start = 2
+        my_final = 7
+        
+        simulator.set_start_time(my_start)
+        simulator.set_final_time(my_final)
+        assert default_start != simulator.get_start_time(), "A setter failed."
+        assert default_final != simulator.get_final_time(), "A setter failed."
+        
+        # Testincorrect interval
+        nose.tools.assert_raises(sundials.SundialsSimulationException,
+                                 simulator.set_final_time(1)) # 2 !< 1
+        nose.tools.assert_raises(sundials.SundialsSimulationException,
+                                 simulator.set_start_time(99)) # 99 !< 7
+                                 
+        # Finally make a test simulation and verify times
+        simulator.run()
+        T, Y = simulator.get_solution()
+        nose.tools.assert_almost_equal(T[0], my_start)
+        nose.tools.assert_almost_equal(T[-1], my_final)
+                                 
+    def test_absolute_tolerance(self):
+        """Basic testing of setting absolute tolerance.
+        
+        The abstol can be set through a property or a setter.
+        The abstol can be gotten through a property or a getter.
+        """
+        simulator = self.simulator
+        
+        default_tolerance = simulator.get_absolute_tolerance()
+        MY_TOLERANCE = 4e-5
+        nose.tools.assert_not_almost_equal(default_tolerance, MY_TOLERANCE,
+                                           "This test is useless.")
+        simulator.set_absolute_tolerance(MY_TOLERANCE)
+        nose.tools.assert_almost_equal(MY_TOLERANCE,
+                                       simulator.get_absolute_tolerance())
+        
+        # Testing the property abstol
+        MY_TOLERANCE2 = 3e-5
+        simulator.abstol = MY_TOLERANCE2
+        nose.tools.assert_almost_equal(MY_TOLERANCE2,
+                                       simulator.get_absolute_tolerance())
+        
+        # Testing error checking
+        nose.tools.assert_raises(sundials.SundialsSimulationException,
+                                 simulator.set_absolute_tolerance(-1e-4))
+        nose.tools.assert_raises(sundials.SundialsSimulationException,
+                                 simulator.set_absolute_tolerance(0))
+                                 
+    def test_relative_tolerance(self):
+        """Basic testing of setting relative tolerance.
+        
+        The reltol can be set through a property or a setter.
+        The reltol can be gotten through a property or a getter.
+        """
+        simulator = self.simulator
+        
+        default_tolerance = simulator.get_relative_tolerance()
+        MY_TOLERANCE = 4e-5
+        nose.tools.assert_not_almost_equal(default_tolerance, MY_TOLERANCE,
+                                           "This test is useless.")
+        simulator.set_relative_tolerance(MY_TOLERANCE)
+        nose.tools.assert_almost_equal(MY_TOLERANCE,
+                                       simulator.get_relative_tolerance())
+        
+        # Testing the property reltol
+        MY_TOLERANCE2 = 3e-5
+        simulator.reltol = MY_TOLERANCE2
+        nose.tools.assert_almost_equal(MY_TOLERANCE2,
+                                       simulator.get_relative_tolerance())
+        
+        # Testing error checking
+        nose.tools.assert_raises(sundials.SundialsSimulationException,
+                                 simulator.set_relative_tolerance(-1e-4))
+        nose.tools.assert_raises(sundials.SundialsSimulationException,
+                                 simulator.set_relative_tolerance(0))
+                                 
+    def test_time_steps(self):
+        simulator = self.simulator
+        
+        default_time_step = simulator.get_time_step()
+        assert default_time_step == simulator.time_step # Test property
+        
+        MY_TIME_STEP = 2 * default_time_step
+        simulator.set_time_step(MY_TIME_STEP)
+        assert MY_TIME_STEP == simulator.time_step # Test property
+        assert MY_TIME_STEP == simulator.get_time_step()
+        
+        simulator.run()
+        T, Y = simulator.get_solution()
+        assert len(T) > 7, "The asserts below might not hold."
+        nose.tools.assert_almost_equal(T[1]-T[0], MY_TIME_STEP)
+        nose.tools.assert_almost_equal(T[2]-T[1], MY_TIME_STEP)
+        nose.tools.assert_almost_equal(T[5]-T[4], MY_TIME_STEP)
+
+
 class TestSolveUsingSundials:
     """Test simulation of the VDP model using the SUNDIALS interface."""
     
