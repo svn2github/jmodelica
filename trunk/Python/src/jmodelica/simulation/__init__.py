d176beafa81e63b4f23460a5af0202b77bbe2767
chria <chria@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2009-09-23 11:49:43 +0000

    This commit factor out common simulation methods from the class jmodelica.simulation.sundials.SundialsODESimulator into parent class jmodelica.simulation.Simulator. This fixes ticket:380 and are also done in preparation for ticket:344.
    git-svn-id: https://svn.jmodelica.org@975 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Python/src/jmodelica/simulation/__init__.py
+++ trunk/Python/src/jmodelica/simulation/__init__.py
@@ -12,4 +12,149 @@ class SimulationException(Exception):
     pass
 
 class Simulator(object):
-    pass
+    """An object oriented interface for simulating JModelica.org models."""
+    
+    def __init__(self, model=None, start_time=None, final_time=None,
+                 time_step=0.2, return_last=False, verbosity=0):
+        
+        # Setting members
+        self._model = model
+        
+        # Setting defaults
+        self.set_return_last(return_last)
+        self.set_time_step(time_step)
+        self.set_verbosity(verbosity)
+        if start_time is not None:
+            self._start_time = start_time
+        elif model.opt_interval_starttime_fixed():
+            self._start_time = model.opt_interval_get_start_time()
+        else:
+            self._start_time = None
+        if final_time is not None:
+            self._final_time = final_time
+        elif model.opt_interval_finaltime_fixed() and final_time is None:
+            self._final_time = model.opt_interval_get_final_time()
+        else:
+            self._final_time = None
+            
+        if self._final_time is not None and self._start_time is not None and \
+            self._start_time >= self._final_time:
+                raise SimulationException('Start time must be before '
+                                                  'end time.')
+        
+                
+                     
+    def set_time_step(self, time_step):
+        """Sets the time step returned by self.get_solution()."""
+        if time_step <= 0:
+            raise SimulationException("Time step size must be "
+                                              "positive.")
+        self._time_step = time_step
+        
+    def get_time_step(self):
+        """Returns the time step returned by self.get_solution()."""
+        return self._time_step
+        
+    time_step = property(get_time_step, set_time_step, 
+                         doc="The time step size when SUNDIALS should return.")
+    
+    
+    def set_model(self, model):
+        """Set the model on which the simulation should be done on.
+        
+        The model needs to be of type jmodelica.jmi.Model.
+        """
+        if model is None:
+            raise SimulationException("model must not be none")
+            
+        if self.get_sensitivity_analysis():
+            # A new model will require a new sensivity indices class will have
+            # to be reinitialized.
+            self._set_sensitivity_indices(_SensivityIndices(model))
+            
+        self._model = model
+        
+    def get_model(self):
+        """Returns the model on which the simulation is being done."""
+        return self._model
+        
+    model = property(get_model, set_model, doc="The model to simulate.")
+    
+    def set_return_last(self, return_last):
+        """Set this to True if only the last time point should be returned
+           after simulation by self.get_solution(). False otherwise.
+        """
+        if return_last==1 or return_last==True:
+            self._return_last = True
+        elif return_last==0 or return_last==False:
+            self._return_last = False
+        else:
+            raise SimulationException("return_last must be either "
+                                              "True, False, 1 or 0.")
+                                              
+    def get_return_last(self):
+        """Returns True if only the last time point should be returned
+           after simulation by self.get_solution(), False otherwise.
+        """
+        return self._return_last
+        
+    return_last = property(get_return_last, set_return_last,
+                           doc="True if only the last time point should be "
+                               "returned after simulation by "
+                               "self.get_solution(). False otherwise.")
+     
+    def get_start_time(self):
+        """Returns the simulation start time"""
+        return self._start_time
+        
+    def get_final_time(self):
+        """Returns the simulation end time"""
+        return self._final_time
+     
+    def set_simulation_interval(self, start_time, final_time):
+        """Set the interval through the simulation will be made."""
+        if start_time >= final_time:
+            raise SimulationException("start time must be earlier "
+                                              "than the final time.")
+        self._start_time = start_time
+        self._final_time = final_time
+        
+    def get_simulation_interval(self):
+        """Return the simulation interval.
+        
+        The simulation interval consists of a tuple: (start_time, final_time).
+        """
+        return self._start_time, self._final_time
+     
+    # Verbosity levels
+    QUIET = 0
+    WHISPER = 1
+    NORMAL = 2
+    LOUD = 3
+    SCREAM = 4
+    VERBOSE_VALUES = [QUIET, WHISPER, NORMAL, LOUD, SCREAM]
+    def get_verbosity(self):
+        """Return the verbosity of the simulator."""
+        return self._verbosity
+        
+    def set_verbosity(self, verbosity):
+        """Specify how much output should be given: 0 <= verbosity <= 4.
+        
+        The verbosity levels can also be specified using the constants:
+         * SundialsOdeSimulator.QUIET
+         * SundialsOdeSimulator.WHISPER
+         * SundialsOdeSimulator.NORMAL
+         * SundialsOdeSimulator.LOUD
+         * SundialsOdeSimulator.SCREAM
+         
+        If the verbosity level is set to something not within the interval, an
+        error is raised.
+        """
+        if verbosity not in self.VERBOSE_VALUES:
+            raise SimulationException("invalid verbosity value")
+        self._verbosity = verbosity
+        
+    verbosity = property(get_verbosity, set_verbosity,
+                         doc="How explicit the output should be")
+    
+    
--- trunk/Python/src/jmodelica/simulation/sundials.py
+++ trunk/Python/src/jmodelica/simulation/sundials.py
@@ -101,36 +101,17 @@ class SundialsOdeSimulator(Simulator):
         This function also sets some decent default values that can be changed
         by calling the setter methods.
         """
-        # Setting members
-        self._model = model
+        Simulator.__init__(self, model, start_time, final_time,
+                           time_step, return_last, verbosity)
         
         # Setting defaults
         self.set_absolute_tolerance(abstol)
         self.set_relative_tolerance(reltol)
-        self.set_return_last(return_last)
+
         self._set_solution(None, None)
         self._set_sensitivity_indices(None)
         self.set_sensitivity_analysis(sensitivity_analysis)
         self._set_sensitivities(None)
-        self.set_time_step(time_step)
-        self.set_verbosity(verbosity)
-        if start_time is not None:
-            self._start_time = start_time
-        elif model.opt_interval_starttime_fixed():
-            self._start_time = model.opt_interval_get_start_time()
-        else:
-            self._start_time = None
-        if final_time is not None:
-            self._final_time = final_time
-        elif model.opt_interval_finaltime_fixed() and final_time is None:
-            self._final_time = model.opt_interval_get_final_time()
-        else:
-            self._final_time = None
-            
-        if self._final_time is not None and self._start_time is not None and \
-            self._start_time >= self._final_time:
-                raise SundialsSimulationException('Start time must be before '
-                                                  'end time.')
         
     def set_absolute_tolerance(self, abstol):
         """Set the positive absolute tolerance for simulation.
@@ -180,57 +161,7 @@ class SundialsOdeSimulator(Simulator):
     reltol = property(get_relative_tolerance, set_relative_tolerance,
                       doc="The relative tolerance.")
                       
-    def set_model(self, model):
-        """Set the model on which the simulation should be done on.
-        
-        The model needs to be of type jmodelica.jmi.Model.
-        """
-        if model is None:
-            raise SundialsSimulationException("model must not be none")
-            
-        if self.get_sensitivity_analysis():
-            # A new model will require a new sensivity indices class will have
-            # to be reinitialized.
-            self._set_sensitivity_indices(_SensivityIndices(model))
-            
-        self._model = model
-        
-    def get_model(self):
-        """Returns the model on which the simulation is being done."""
-        return self._model
-        
-    model = property(get_model, set_model, doc="The model to simulate.")
     
-    # verbosity levels
-    QUIET = 0
-    WHISPER = 1
-    NORMAL = 2
-    LOUD = 3
-    SCREAM = 4
-    VERBOSE_VALUES = [QUIET, WHISPER, NORMAL, LOUD, SCREAM]
-    def get_verbosity(self):
-        """Return the verbosity of the simulator."""
-        return self._verbosity
-        
-    def set_verbosity(self, verbosity):
-        """Specify how much output should be given: 0 <= verbosity <= 4.
-        
-        The verbosity levels can also be specified using the constants:
-         * SundialsOdeSimulator.QUIET
-         * SundialsOdeSimulator.WHISPER
-         * SundialsOdeSimulator.NORMAL
-         * SundialsOdeSimulator.LOUD
-         * SundialsOdeSimulator.SCREAM
-         
-        If the verbosity level is set to something not within the interval, an
-        error is raised.
-        """
-        if verbosity not in self.VERBOSE_VALUES:
-            raise SundialsSimulationException("invalid verbosity value")
-        self._verbosity = verbosity
-        
-    verbosity = property(get_verbosity, set_verbosity,
-                         doc="How explicit the output should be")
         
     def get_solution(self):
         """Return the solution calculated by SundialsOdeSimulator.run().
@@ -248,27 +179,8 @@ class SundialsOdeSimulator(Simulator):
         self._T = T
         self._Y = Y
         
-    def set_simulation_interval(self, start_time, final_time):
-        """Set the interval through the simulation will be made."""
-        if start_time >= final_time:
-            raise SundialsSimulationException("start time must be earlier "
-                                              "than the final time.")
-        self._start_time = start_time
-        self._final_time = final_time
-        
-    def get_simulation_interval(self):
-        """Return the simulation interval.
-        
-        The simulation interval consists of a tuple: (start_time, final_time).
-        """
-        return self._start_time, self._final_time
-        
-    def get_start_time(self):
-        return self._start_time
-        
-    def get_final_time(self):
-        return self._final_time
-        
+       
+       
     def get_sensitivities(self):
         """Return the sensitivities calculated at final time by self.run().
         
@@ -289,29 +201,7 @@ class SundialsOdeSimulator(Simulator):
                              doc="The calculated sensitivities at final "
                                  "time. This is set by self.run().")
         
-    def set_return_last(self, return_last):
-        """Set this to True if only the last time point should be returned
-           after simulation by self.get_solution(). False otherwise.
-        """
-        if return_last==1 or return_last==True:
-            self._return_last = True
-        elif return_last==0 or return_last==False:
-            self._return_last = False
-        else:
-            raise SundialsSimulationException("return_last must be either "
-                                              "True, False, 1 or 0.")
-                                              
-    def get_return_last(self):
-        """Returns True if only the last time point should be returned
-           after simulation by self.get_solution(), False otherwise.
-        """
-        return self._return_last
-        
-    return_last = property(get_return_last, set_return_last,
-                           doc="True if only the last time point should be "
-                               "returned after simulation by "
-                               "self.get_solution(). False otherwise.")
-        
+       
     def set_sensitivity_analysis(self, sens_analysis):
         """Set to True if sensitivity analysis should be done while simulating.
         
@@ -339,19 +229,6 @@ class SundialsOdeSimulator(Simulator):
                                         "be performed in simulating. False "
                                         "otherwise.")
         
-    def set_time_step(self, time_step):
-        """Sets the time step returned by self.get_solution()."""
-        if time_step <= 0:
-            raise SundialsSimulationException("Time step size must be "
-                                              "positive.")
-        self._time_step = time_step
-        
-    def get_time_step(self):
-        """Returns the time step returned by self.get_solution()."""
-        return self._time_step
-        
-    time_step = property(get_time_step, set_time_step, 
-                         doc="The time step size when SUNDIALS should return.")
     
     def get_sensitivity_indices(self):
         """Returns an object that holds information about the indices of the
