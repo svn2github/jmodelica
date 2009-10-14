1f7c46da1c7abf55de3e0e96558799efde3d829a
chria <chria@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2009-10-14 14:11:43 +0000

    Added an example on simulation the RLC_Circuit with the SundialsDAESimulator. This example also shows howto extract certain variables from the solution and howto write the data to a file. This is related to ticket:344 .
    git-svn-id: https://svn.jmodelica.org@1017 7ae51c65-13d5-4921-9d80-6511ddd1419c

---
+++ trunk/Python/src/jmodelica/examples/RLC.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as N
+import pylab as p
+import matplotlib
+
+import jmodelica
+import jmodelica.jmi as jmi
+from jmodelica.tests import get_example_path
+from jmodelica.simulation.sundials import SundialsDAESimulator
+
+def run_demo(with_plots=True):
+    """
+    An example on how to simulate a model using the DAE simulator. The
+    result can be compared with that of sim_rlc.py which has solved the
+    same problem using dymola. Also writes information to a file.
+    """
+    
+    path = get_example_path()
+    os.chdir(path)
+    
+    # Comile the Modelica model first to C code and
+    # then to a dynamic library
+    libname = 'RLC_Circuit'
+    mofile = 'RLC_Circuit.mo'
+    optpackage = 'RLC_Circuit'
+    
+
+    model = jmi.load_model(libname,path,mofile,
+                     optpackage,compiler='modelica')
+    
+    
+    simulator = SundialsDAESimulator(model, verbosity=3, start_time=0.0, final_time=30.0, time_step=0.01)
+    simulator.run()
+    
+    Ts, ys = simulator.get_solution('sine.y','resistor.v','inductor1.i')
+    fig = p.figure()
+    p.plot(Ts, ys)
+    p.legend(('sine.y','resistor.v','inductor1.i'))
+    p.show()
+    
+    simulator.write_data()
+
+if __name__=="__main__":
+    
+    run_demo()
