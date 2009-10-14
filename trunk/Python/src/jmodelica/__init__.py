ab3f1257af1a50c1c35dadebfba7137fd81e6ecc
tove <tove@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2009-10-14 12:36:56 +0000

    Method for detection of required Python packages added in jmodelica.__init__, #375.
    git-svn-id: https://svn.jmodelica.org@1015 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Python/src/jmodelica/__init__.py
+++ trunk/Python/src/jmodelica/__init__.py
@@ -37,4 +37,87 @@ except IOError:
     warnings.warn('Startup script ''%s'' not found. Environment may be corrupt'
                   % _f)
 
+def check_environment():
+    import sys, time
+    le=30
+    startstr = "Performing JModelica environment check"
+    sys.stdout.write("\n")
+    sys.stdout.write(startstr+" \n")
+    sys.stdout.write("="*len(startstr))
+    sys.stdout.write("\n\n")
+    sys.stdout.flush()
+    time.sleep(0.25)
 
+    # check os
+    platform = sys.platform
+    sys.stdout.write("%s %s" %("Platform".ljust(le,'.'),(str(platform)).ljust(le)+"\n\n"))
+    sys.stdout.flush()
+    time.sleep(0.25)
+    
+    #check python version
+    pyversion = sys.version.partition(" ")[0]
+    
+    if sys.hexversion >= 0x2050000:
+        sys.stdout.write("%s %s" % ("Python version:".ljust(le,'.'),pyversion.ljust(le)))
+    else:  
+        sys.stdout.write("%s %s %s" % ("Python version:".ljust(le,'.'),pyversion.ljust(le),"Error: Required is 2.5.x".ljust(le)))
+    sys.stdout.write("\n")
+    sys.stdout.flush()
+    time.sleep(0.25)    
+    import imp
+    # Test dependencies
+    sys.stdout.write("\n\n")
+    sys.stdout.write("Dependencies: \n\n".rjust(0))
+    modstr="Module"
+    verstr="Version"
+    sys.stdout.write("%s %s" % (modstr.ljust(le), verstr.ljust(le)))
+    sys.stdout.write("\n")
+    sys.stdout.write("%s %s" % (("-"*len(modstr)).ljust(le), ("-"*len(verstr)).ljust(le)))
+    sys.stdout.write("\n")
+    modules=["IPython", "pyreadline", "numpy", "scipy", "matplotlib", "jpype", "lxml", "nose"]
+    for module in modules:
+        try:
+            fp, path, desc = imp.find_module(module)
+            mod = imp.load_module(module, fp, path, desc)
+            vers="undef"
+            vers_required=""
+            try:
+                if module == "IPython":
+                    vers = mod.__version__
+                    vers_required="0.1.0"
+                elif module == "pyreadline":
+                    vers = mod.release.version
+                    vers_required="1.5"
+                elif module == "numpy":
+                    vers = mod.__version__
+                    vers_required="1.2.0"
+                elif module == "scipy":
+                    vers = mod.__version__
+                elif module == "matplotlib":
+                    vers = mod.__version__
+                elif module == "jpype":
+                    vers = mod.__version__                    
+                elif module == "lxml":
+                    vers = imp.load_module('lxml.etree', fp, path, desc).__version__
+                elif module == "nose":
+                    vers = mod.__version__
+            except AttributeError, e:
+                pass
+            if vers_required and vers >= vers_required:
+                sys.stdout.write("%s %s %s" %(module.ljust(le,'.'), vers.ljust(le), ("Ok (version required is at least %s)"%str(vers_required)).ljust(le)))
+            elif vers_required and vers < vers_required:
+                sys.stdout.write("%s %s %s" %(module.ljust(le,'.'), vers.ljust(le), ("Error (version required is at least %s)"%str(vers_required)).ljust(le)))
+            else:
+                sys.stdout.write("%s %s %s" %(module.ljust(le,'.'), vers.ljust(le), "Ok".ljust(le)))
+        except ImportError, e:
+            if module != "nose":
+                sys.stdout.write("%s %s %s " % (module.ljust(le,'.'), vers.ljust(le), "Error (Module required but not found)".ljust(le)))
+            else:
+                sys.stdout.write("%s %s %s" % (module.ljust(le,'.'), vers.ljust(le), "Warning (Module required to run tests)".ljust(le)))
+            pass
+        finally:
+            if fp:
+                fp.close()
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+        time.sleep(0.25)
