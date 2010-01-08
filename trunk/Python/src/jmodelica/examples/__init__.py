f8dca9b811d9077a452bcf60d9e0703195f0505d
jakesson <jakesson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2010-01-08 14:02:05 +0000

    Added example models generated by PyMBS, see #575. The models do not yet run since the vector support in the front-end needs to be improved a bit more.
    git-svn-id: https://svn.jmodelica.org@1242 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Python/src/jmodelica/examples/__init__.py
+++ trunk/Python/src/jmodelica/examples/__init__.py
@@ -1,2 +1,2 @@
 """The JModelica Model example package."""
-__all__ = ['vdp','pendulum','vdp_minimum_time','parameter_estimation_1','cstr','quadtank','ast_example','RLC','cstr2','distillation','cstr_mpc','simulation_with_input','if_example_1','if_example_2']
+__all__ = ['vdp','pendulum','vdp_minimum_time','parameter_estimation_1','cstr','quadtank','ast_example','RLC','cstr2','distillation','cstr_mpc','simulation_with_input','if_example_1','if_example_2','crane']
---
+++ trunk/Python/src/jmodelica/examples/crane.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as N
+import pylab as p
+import matplotlib
+
+import jmodelica
+import jmodelica.jmi as jmi
+from jmodelica.tests import get_example_path
+from jmodelica.simulation.sundials import SundialsDAESimulator
+from jmodelica.compiler import ModelicaCompiler
+from jmodelica.initialization.ipopt import NLPInitialization
+from jmodelica.initialization.ipopt import InitializationOptimizer
+
+def run_demo(with_plots=True):
+    """
+    An example on how to simulate a model using the DAE
+    simulator. Also writes information to a file.
+
+    NOTICE: The script does not run since the compiler does
+    not support all constructs needed.
+    
+    """
+    
+    curr_dir = os.path.dirname(os.path.abspath(__file__));
+
+    model_name = 'PyMBSModels.CraneCrab_recursive_der_state_Test'
+    mofile = curr_dir+'/files/PyMBSModels.mo'
+    
+    mc = ModelicaCompiler()
+    
+    # Comile the Modelica model first to C code and
+    # then to a dynamic library
+    mc.compile_model(mofile,model_name,target='ipopt')
+
+    # Load the dynamic library and XML data
+    model=jmi.Model(model_name)
+
+    # Create DAE initialization object.
+    init_nlp = NLPInitialization(model)
+    
+    # Create an Ipopt solver object for the DAE initialization system
+    init_nlp_ipopt = InitializationOptimizer(init_nlp)
+        
+    # Solve the DAE initialization system with Ipopt
+    init_nlp_ipopt.init_opt_ipopt_solve()
+
+    simulator = SundialsDAESimulator(model, verbosity=3, start_time=0.0, final_time=30.0, time_step=0.01)
+    simulator.run()
+        
+    simulator.write_data()
+
+    # Load the file we just wrote to file
+    res = jmodelica.io.ResultDymolaTextual('RLC_Circuit_result.txt')
+
+    # remains to add plotting
+
+if __name__=="__main__":
+    run_demo()
