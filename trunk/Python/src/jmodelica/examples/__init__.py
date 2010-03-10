f7be61549820475a4fa11aae1c8b3f60ad2889f2
jakesson <jakesson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2010-03-10 20:56:16 +0000

    Updated jmodelica.examples.__init__.py to include the assimulo examples, see #526.
    git-svn-id: https://svn.jmodelica.org@1363 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Python/src/jmodelica/examples/__init__.py
+++ trunk/Python/src/jmodelica/examples/__init__.py
@@ -1,2 +1,2 @@
 """The JModelica Model example package."""
-__all__ = ['vdp','pendulum','vdp_minimum_time','parameter_estimation_1','cstr','quadtank','ast_example','RLC','cstr2','distillation','cstr_mpc','simulation_with_input','if_example_1','if_example_2','crane','sim_rlc']
+__all__ = ['vdp','VDP_assimulo','pendulum','vdp_minimum_time','parameter_estimation_1','cstr','quadtank','ast_example','RLC','RLC_assimulo','cstr2','distillation','cstr_mpc','simulation_with_input','if_example_1','if_example_2','if_example_2_assimulo','crane','sim_rlc']
