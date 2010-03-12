ffd7c00dbfed00c403b44ef5ccbfc1cea78b625e
chria <chria@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2010-03-12 09:27:42 +0000

    Updated the tests for assimulo. Related to ticket:526
    git-svn-id: https://svn.jmodelica.org@1369 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Python/src/jmodelica/tests/simulation/test_assimulo.py
+++ trunk/Python/src/jmodelica/tests/simulation/test_assimulo.py
@@ -21,12 +21,86 @@ mc = ModelicaCompiler()
 oc = OptimicaCompiler()
 oc.set_boolean_option('state_start_values_fixed',True)
 
-class Test_Assimulo:
+
+class Test_JMI_Explicit:
+    """
+    This class tests jmodelica.simulation.assimulo.JMIExplicit
+    """
     
     @classmethod
     def setUpClass(cls):
         """
-        Compile the test models. (This is only run once during the test)
+        Compile the test model.
+        """
+        modelf_ODE = 'files' + sep + 'VDP.mo'
+        fpath_ODE = os.path.join(path_to_examples, modelf_ODE)
+        cpath_ODE = 'VDP_pack.VDP_Opt'
+        fname_ODE = cpath_ODE.replace('.','_',1)
+        
+        oc.compile_model(fpath_ODE, cpath_ODE)
+        
+    def setUp(self):
+        """Load the test model."""
+        package_ODE = 'VDP_pack_VDP_Opt'
+
+        # Load the dynamic library and XML data
+        self.m_ODE = jmi.Model(package_ODE)
+        
+        # Creates the solvers
+        self.ODE = JMIExplicit(self.m_ODE)
+        
+    def test_init(self):
+        """
+        Tests jmodelica.simulation.assimulo.JMIExplicit.__init__
+        """
+        assert self.m_ODE == self.ODE._model
+        
+        for i in range(len(self.ODE.y0)):
+            assert self.m_ODE.real_x[i] == self.ODE.y0[i]
+    
+    def test_f(self):
+        """
+        Tests jmodelica.simulation.assimulo.JMIExplicit.f
+        """
+        test_x = N.array([1.,1.,1.])
+        test_t = 2
+        
+        temp_rhs = self.ODE.f(test_t,test_x)
+        
+        assert temp_rhs[0] == -1.0
+        assert temp_rhs[1] == 1.0
+        nose.tools.assert_almost_equal(temp_rhs[2], 14.77811, 5)
+        
+    def test_reset(self):
+        """
+        Tests jmodelica.simulation.assimulo.JMIExplicit.reset
+        """
+        self.ODE.t0 = 10.0
+        self.ODE._model.real_x = N.array([2.,2.,2.])
+        
+        self.ODE.reset()
+        
+        assert self.ODE._model.t == 10.0
+        assert self.ODE._model.real_x[0] != 2.0
+        assert self.ODE._model.real_x[1] != 2.0
+        assert self.ODE._model.real_x[2] != 2.0
+        
+    def test_g(self):
+        """
+        Tests jmodelica.simulation.assimulo.JMIExplicit.g
+        """
+        #This is not implemented in JMIExplicit yet.
+        pass
+        
+class Test_JMI_Implicit:
+    """
+    This class tests jmodelica.simulation.assimulo.JMIImplicit
+    """
+    
+    @classmethod
+    def setUpClass(cls):
+        """
+        Compile the test model.
         """
         #DAE test model
         modelf_DAE = 'files' + sep + 'Pendulum_pack_no_opt.mo'
@@ -36,30 +110,29 @@ class Test_Assimulo:
 
         mc.compile_model(fpath_DAE, cpath_DAE)
         
-        #ODE test model
-        modelf_ODE = 'files' + sep + 'VDP.mo'
-        fpath_ODE = os.path.join(path_to_examples, modelf_ODE)
-        cpath_ODE = 'VDP_pack.VDP_Opt'
-        fname_ODE = cpath_ODE.replace('.','_',1)
+        modelf_DISC = 'files' + sep + 'IfExpExamples.mo'
+        fpath_DISC = os.path.join(path_to_examples, modelf_DISC)
+        cpath_DISC = 'IfExpExamples.IfExpExample2'
+        fname_DISC = cpath_DISC.replace('.','_',1)
+
+        mc.compile_model(fpath_DISC, cpath_DISC)
         
-        oc.compile_model(fpath_ODE, cpath_ODE)
-   
     def setUp(self):
-        """Load the test models."""
+        """Load the test model."""
         package_DAE = 'Pendulum_pack_Pendulum'
-        package_ODE = 'VDP_pack_VDP_Opt'
+        package_DISC = 'IfExpExamples_IfExpExample2'
 
         # Load the dynamic library and XML data
         self.m_DAE = jmi.Model(package_DAE)
-        self.m_ODE = jmi.Model(package_ODE)
+        self.m_DISC = jmi.Model(package_DISC)
         
         # Creates the solvers
         self.DAE = JMIImplicit(self.m_DAE)
-        self.ODE = JMIExplicit(self.m_ODE)
+        self.DISC = JMIImplicit(self.m_DISC)
         
-    def test_Imp_eps(self):
+    def test_eps(self):
         """
-        This tests the functionality of assimulo.JMIImplicit.eps
+        Tests jmodelica.simulation.assimulo.JMIImplicit.get/set_eps
         """
         nose.tools.assert_raises(JMIModel_Exception, self.DAE._set_eps, 'Test')
         nose.tools.assert_raises(JMIModel_Exception, self.DAE._set_eps, -1)
@@ -71,9 +144,9 @@ class Test_Assimulo:
         self.DAE.eps = 10.0
         assert self.DAE.eps == 10.0
         
-    def test_Imp_max_eIter(self):
+    def test_max_eIteration(self):
         """
-        This tests the functionality of assimulo.JMIImplicit.max_eIter
+        Tests jmodelica.simulation.assimulo.JMIImplicit.get/set_max_eIteration
         """
         nose.tools.assert_raises(JMIModel_Exception, self.DAE._set_max_eIteration, 'Test')
         nose.tools.assert_raises(JMIModel_Exception, self.DAE._set_max_eIteration, -1)
@@ -84,32 +157,12 @@ class Test_Assimulo:
         assert self.DAE.max_eIter == 1
         self.DAE.max_eIter = 10
         assert self.DAE.max_eIter == 10
-    
-    def test_Imp_reset(self):
+        
+    def test_check_eIter(self):
         """
-        This tests the functionality of assimulo.JMIImplicit.reset
+        Tests jmodelica.simulation.assimulo.JMIImplicit.check_eIter
         """
-        #NOT COMPLETE
-        
-        t0_prev = self.DAE._model.t.copy()
-        y0_prev = self.DAE.y0.copy()
-        y0d_prev = self.DAE.yd0.copy()
-        
-        self.DAE._model.t = 10.0
-        self.DAE.t0 = 10.0
-        
-        assert t0_prev != self.DAE._model.t
-        
-        self.DAE.reset()
-        
-        assert 10.0 == self.DAE._model.t
-        
-        #NEEDS MORE EXTENSIVE TESTING
-        
-    def test_Imp_check_eIter(self):
-        """
-        This tests the functionality of assimulo.JMIImplicit.check_eIter
-        """
+        self.DAE.eps = 1e-4
         
         b_mode = [1, -1, 0]
         a_mode = [-1, 1, 1]
@@ -119,41 +172,37 @@ class Test_Assimulo:
         assert iter == True
         assert eIter[0] == -1
         assert eIter[1] == 1
+        assert eIter[2] == 1
+        
+        b_mode = [2, 5, 1]
+        a_mode = [0, 2, 2]
+        
+        [eIter, iter] = self.DAE.check_eIter(b_mode, a_mode)
+        
+        assert iter == False
+        assert eIter[0] == 0
+        assert eIter[1] == 0
         assert eIter[2] == 0
-    
-    def test_Imp_init_mode(self):
-        """
-        This tests the functionality of assimulo.JMIImplicit.init_mode
-        """
-        raise NotImplementedError
-    
-    def test_Imp_event_switch(self):
-        """
-        This tests the functionality of assimulo.JMIImplicit.event_switch
-        """
-        raise NotImplementedError
         
-    def test_Imp_handle_event(self):
+    def test_event_switch(self):
         """
-        This tests the functionality of assimulo.JMIImplicit.handle_event
+        Tests jmodelica.simulation.assimulo.JMIImplicit.event_switch
         """
-        raise NotImplementedError
+        solver = lambda x:1
+        solver.verbosity = 1
+        solver.LOUD = 2
+        solver.switches = [False, False, True]
+        event_info = [1, 0, -1]
         
-    def test_Imp_g_adjust(self):
-        """
-        This tests the functionality of assimulo.JMIImplicit.g_adjust
-        """
-        raise NotImplementedError
+        self.DAE.event_switch(solver,event_info)
         
-    def test_Imp_g(self):
-        """
-        This tests the functionality of assimulo.JMIImplicit.g
-        """
-        raise NotImplementedError
+        assert solver.switches[0] == True
+        assert solver.switches[1] == False
+        assert solver.switches[2] == False
         
-    def test_Imp_f(self):
+    def test_f(self):
         """
-        This tests the functionality of assimulo.JMIImplicit.f
+        Tests jmodelica.simulation.assimulo.JMIImplicit.f
         """
         test_x = N.array([1.,1.,1.,1.])
         test_dx = N.array([2.,2.,2.,2.])
@@ -161,77 +210,144 @@ class Test_Assimulo:
         
         temp_f = self.DAE.f(test_t,test_x,test_dx)
         
-        print temp_f
         assert temp_f[0] == -1.0
         assert temp_f[2] == -1.0
         assert temp_f[3] == -2.0
         nose.tools.assert_almost_equal(temp_f[1], -1.158529, 5)
         
-    def test_Imp_init(self):
+    def test_g(self):
         """
-        This tests the functionality of assimulo.JMIImplicit.__init__
+        Tests jmodelica.simulation.assimulo.JMIImplicit.g
+        """
+        temp_g = self.DISC.g(2.,[1.,2.],[2.,0],[0,0])
+        
+        nose.tools.assert_almost_equal(temp_g[0], -0.429203, 5)
+        nose.tools.assert_almost_equal(temp_g[1], 1.141592, 5)
+        
+    def test_g_adjust(self):
+        """
+        Tests jmodelica.simulation.assimulo.JMIImplicit.g
+        """
+        self.DISC.eps = 2.0
+
+        temp_g_adjust = self.DISC.g_adjust(2.,[1.,2.],[2.,0],[0,0])
+
+        nose.tools.assert_almost_equal(temp_g_adjust[0], -2.429203, 5)
+        nose.tools.assert_almost_equal(temp_g_adjust[1], -0.858407, 5)
+        
+        temp_g_adjust = self.DISC.g_adjust(2.,[1.,2.],[2.,0],[0,1])
+        
+        nose.tools.assert_almost_equal(temp_g_adjust[0], -2.429203, 5)
+        nose.tools.assert_almost_equal(temp_g_adjust[1], 3.141592, 5)
+        
+
+    def test_init(self):
+        """
+        Tests jmodelica.simulation.assimulo.JMIImplicit.__init__
         """
         assert self.m_DAE == self.DAE._model
         assert self.DAE.max_eIter == 50
         assert self.DAE.eps == 1e-9
+        assert self.DAE.jac == self.DAE.j
         
-        temp_y0 = N.append(self.m_DAE.x.copy(), self.m_DAE.w.copy())
-        temp_yd0 = N.append(self.m_DAE.dx.copy(),[0]*len(self.m_DAE.w))
-        temp_algvar = [1.0]*len(self.m_DAE.x) + [0.0]*len(self.m_DAE.w)
+        temp_y0 = N.append(self.m_DAE.real_x.copy(), self.m_DAE.real_w.copy())
+        temp_yd0 = N.append(self.m_DAE.real_dx.copy(),[0]*len(self.m_DAE.real_w))
+        temp_algvar = [1.0]*len(self.m_DAE.real_x) + [0.0]*len(self.m_DAE.real_w)
         
         for i in range(len(temp_y0)):
             assert temp_y0[i] == self.DAE.y0[i]
             assert temp_yd0[i] == self.DAE.yd0[i]
             assert temp_algvar[i] == self.DAE.algvar[i]
+            
+        #Test discontiniuous system
+        assert self.DISC._g_nbr == 2
+        assert self.DISC.event_fcn == self.DISC.g_adjust
         
-        
-        #NOT TESTING EVENT HANDLING
-        
-    def test_Exp_reset(self):
+    def test_reset(self):
         """
-        This tests the functionality of assimulo.JMIExplicit.reset
+        Tests jmodelica.simulation.assimulo.JMIImplicit.reset
+        """   
+        self.DAE.t0 = 10.0
+        self.DAE._model.real_x = N.array([2.,2.,2.,2.])
+        self.DAE._model.real_dx = N.array([2.,2.,2.,2.])
+
+        self.DAE.reset()
+        
+        assert self.DAE._model.t == 10.0
+        assert self.DAE._model.real_x[0] != 2.0
+        assert self.DAE._model.real_x[1] != 2.0
+        assert self.DAE._model.real_x[2] != 2.0
+        assert self.DAE._model.real_dx[0] != 2.0
+        assert self.DAE._model.real_dx[1] != 2.0
+        assert self.DAE._model.real_dx[2] != 2.0        
+       
+        assert self.DAE.y0[0] == 0.1
+
+
+    def test_j(self):
         """
-        temp_t0 = self.ODE._model.t
-        temp_y0 = self.ODE._model.x.copy()
-        
-        self.ODE.t0 = 10.0
-        self.ODE._model.x = N.array([5.,5.,5.])
-        
-        assert temp_t0 != 10.0
-        assert temp_y0[0] != 5.0
-        assert temp_y0[1] != 5.0
-        assert temp_y0[2] != 5.0
-        
-        self.ODE.reset()
-        
-        assert self.ODE._model.t == 10.0
-        N.testing.assert_array_almost_equal(temp_y0, self.ODE.y0)
-        
-        
-    def test_Exp_g(self):
+        Tests jmodelica.simulation.assimulo.JMIImplicit.j
         """
-        This tests the functionality of assimulo.JMIExplicit.g
-        """
-        raise NotImplementedError
         
-    def test_Exp_f(self):
-        """
-        This tests the functionality of assimulo.JMIExplicit.f
-        """
-        test_x = N.array([1.,1.,1.])
+        test_x = N.array([1.,1.,1.,1.])
+        test_dx = N.array([2.,2.,2.,2.])
         test_t = 2
         
-        temp_rhs = self.ODE.f(test_t,test_x)
+        temp_j = self.DAE.j(0.1,test_t,test_x,test_dx)
+        print temp_j
+        assert temp_j[0,0] == -0.1
+        assert temp_j[0,1] == 1.0
+        assert temp_j[1,1] == -0.1
+        nose.tools.assert_almost_equal(temp_j[1,0], 0.5403023, 5)
         
-        assert temp_rhs[0] == -1.0
-        assert temp_rhs[1] == 1.0
-        nose.tools.assert_almost_equal(temp_rhs[2], 14.77811, 5)
-        
-    def test_Exp_init(self):
+    def test_handle_event(self):
         """
-        This tests the functionality of assimulo.JMIExplicit.__init__
+        Tests jmodelica.simulation.assimulo.JMIImplicit.handle_event
         """
-        assert self.m_ODE == self.ODE._model
+        solver = lambda x:1
+        solver.verbosity = 1
+        solver.NORMAL = solver.LOUD = 2
+        solver.t = [[1.0]]
+        solver.y = [[1.,1.]]
+        solver.yd = [[1.,1.]]
+        solver.switches = [False,True]
+        self.DISC.event_switch = lambda x,y:1
+        self.DISC.init_mode = lambda x:1
+        self.DISC.check_eIter = lambda x,y: [True,False]
         
-        for i in range(len(self.ODE.y0)):
-            assert self.m_ODE.x[i] == self.ODE.y0[i]
+        self.DISC.handle_event(solver, [1,1])
+        
+        self.DISC.check_eIter = lambda x,y: [True,True]
+        
+        self.DISC.handle_event(solver, [1,1])
+
+        
+        
+    def test_init_mode(self):
+        """
+        Tests jmodelica.simulation.assimulo.init_mode
+        """
+        solver = lambda x:1
+        solver.switches = [True, True]
+        solver.make_consistency = lambda x:1
+        
+        self.DISC.init_mode(solver)
+        
+        assert self.DISC._model.sw[0] == 1
+        assert self.DISC._model.sw[1] == 1
+    
+    def test_initiate(self):
+        """
+        Tests jmodelica.simulation.assimulo.initiate
+        """
+        self.DAE.init_mode = lambda x:1
+        self.DAE.initiate('Test')
+        
+        self.DISC.handle_event = lambda x,y:1
+        solver = lambda x:1
+        solver.switches = [False, False]
+        
+        self.DISC.initiate(solver)
+
+        assert solver.switches[0] == True
+        assert solver.switches[1] == True
