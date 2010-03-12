0631ec095bdce4d613d4f96a65e7559c26125f8f
chria <chria@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2010-03-12 10:30:39 +0000

    This commit updates the copyright information in the jmodelica.example files. Related to ticket:708
    git-svn-id: https://svn.jmodelica.org@1371 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Python/src/jmodelica/examples/__init__.py
+++ trunk/Python/src/jmodelica/examples/__init__.py
@@ -1,2 +1,19 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2010 Modelon AB
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 """The JModelica Model example package."""
 __all__ = ['vdp','VDP_assimulo','pendulum','vdp_minimum_time','parameter_estimation_1','cstr','quadtank','ast_example','RLC','RLC_assimulo','cstr2','distillation','cstr_mpc','simulation_with_input','if_example_1','if_example_2','if_example_2_assimulo','crane','sim_rlc']
