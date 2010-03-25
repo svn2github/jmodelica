svn2github/jmodelica

commit e1f49bc7418cbb67d5a13ce87154f1d34da0d7d0

- Added an option 'enable_variable_scaling' to switch variable scaling on/off.  - Added C code generation to support scaling of variabels.  - Updated jmodelica.io module to support writing of scaled optimization/simulation results.  - Updated jmodelica.optimization.ipopt to support scaling.  - Removed the JMI function set_scaling_method() since scaling method cannot be changed after code generation. All these changes are with reference to #13.  - Fixed a bug in the jmodelica.tests.test_linearization module, see #722.
git-svn-id: https://svn.jmodelica.org@1406 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Python/src/jmodelica/optimization/ipopt.py
@@ -21,7 +21,7 @@ from ctypes import byref
 import numpy as N
 import numpy.ctypeslib as Nct
 
-from jmodelica import jmi
+from jmodelica import jmi 
 from jmodelica import io
 
 int = N.int32
@@ -594,6 +594,8 @@ class NLPCollocation(object):
 
         p_opt_data = N.zeros(len(p_opt_names))
 
+        sc = self._model.jmimodel.get_variable_scaling_factors()
+
         # Get the parameters
         n_p_opt = self._model.jmimodel.opt_get_n_p_opt()
         if n_p_opt > 0:
@@ -609,7 +611,10 @@ class NLPCollocation(object):
                     i_pi = z_i - self._model._offs_real_pi.value
                     i_pi_opt = p_opt_indices.index(i_pi)
                     traj = res.get_variable_data(name)
-                    p_opt_data[i_pi_opt] = traj.x[0]
+                    if self._model.get_scaling_method() & jmi.JMI_SCALING_VARIABLES > 0:
+                        p_opt_data[i_pi_opt] = traj.x[0]/sc[z_i]
+                    else:
+                        p_opt_data[i_pi_opt] = traj.x[0]
                 except:
                     print "Warning: Could not find value for parameter " + name
                     
@@ -617,15 +622,30 @@ class NLPCollocation(object):
 
         # Initialize variable names
         # Loop over all the names
+
+        sc_dx = self._model.jmimodel.get_variable_scaling_factors()[self._model._offs_real_dx.value:self._model._offs_real_x.value]
+        sc_x = self._model.jmimodel.get_variable_scaling_factors()[self._model._offs_real_x.value:self._model._offs_real_u.value]
+        sc_u = self._model.jmimodel.get_variable_scaling_factors()[self._model._offs_real_u.value:self._model._offs_real_w.value]
+        sc_w = self._model.jmimodel.get_variable_scaling_factors()[self._model._offs_real_w.value:self._model._offs_t.value]
+
         col_index = 1;
+        dx_index = 0;
+        x_index = 0;
+        u_index = 0;
+        w_index = 0;
         for name in dx_names:
             try:
                 #print(dx_names.get(ref))
                 #print(col_index)
                 traj = res.get_variable_data(name)
-                var_data[:,col_index] = traj.x
+                if self._model.get_scaling_method() & jmi.JMI_SCALING_VARIABLES > 0:
+                    var_data[:,col_index] = traj.x/sc_dx[dx_index]
+                else:
+                    var_data[:,col_index] = traj.x
+                dx_index = dx_index + 1
                 col_index = col_index + 1
             except:
+                dx_index = dx_index + 1
                 col_index = col_index + 1
                 print "Warning: Could not find trajectory for variable " + name
         for name in x_names:
@@ -633,9 +653,14 @@ class NLPCollocation(object):
                 #print(x_names.get(ref))
                 #print(col_index)
                 traj = res.get_variable_data(name)
-                var_data[:,col_index] = traj.x
+                if self._model.get_scaling_method() & jmi.JMI_SCALING_VARIABLES > 0:
+                    var_data[:,col_index] = traj.x/sc_x[x_index]
+                else:
+                    var_data[:,col_index] = traj.x
+                x_index = x_index + 1
                 col_index = col_index + 1
             except:
+                x_index = x_index + 1
                 col_index = col_index + 1
                 print "Warning: Could not find trajectory for variable " + name
 
@@ -644,9 +669,14 @@ class NLPCollocation(object):
                 #print(u_names.get(ref))
                 #print(col_index)
                 traj = res.get_variable_data(name)
-                var_data[:,col_index] = traj.x
+                if self._model.get_scaling_method() & jmi.JMI_SCALING_VARIABLES > 0:
+                    var_data[:,col_index] = traj.x/sc_u[u_index]
+                else:
+                    var_data[:,col_index] = traj.x
+                u_index = u_index + 1
                 col_index = col_index + 1
             except:
+                u_index = u_index + 1
                 col_index = col_index + 1
                 print "Warning: Could not find trajectory for variable " + name
 
@@ -656,11 +686,19 @@ class NLPCollocation(object):
                 #print(col_index)
                 traj = res.get_variable_data(name)
                 if N.size(traj.x)==2:
-                    var_data[:,col_index] = N.ones(n_points)*traj.x[0]
+                    if self._model.get_scaling_method() & jmi.JMI_SCALING_VARIABLES > 0:
+                        var_data[:,col_index] = N.ones(n_points)*traj.x[0]/sc_u[w_index]
+                    else:
+                        var_data[:,col_index] = N.ones(n_points)*traj.x[0]
                 else:
-                    var_data[:,col_index] = traj.x
+                    if self._modelget_scaling_method() & jmi.JMI_SCALING_VARIABLES > 0:
+                        var_data[:,col_index] = traj.x/sc_u[w_index]
+                    else:
+                        var_data[:,col_index] = traj.x
+                w_index = w_index + 1
                 col_index = col_index + 1
             except:
+                w_index = w_index + 1
                 col_index = col_index + 1
                 print "Warning: Could not find trajectory for variable " + name
 

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen.jrag
@@ -81,14 +81,30 @@ aspect CCodeGen {
 	}
 		
 	public void FDerExp.prettyPrint_C(Printer p, PrintStream str, String indent, Object o) {
-		str.print(myFV().name_C());
+		if (root().options.getBooleanOption("enable_variable_scaling")) {
+			str.print("(");
+			str.print(myFV().name_C());
+			str.print("*sf(");
+			str.print(myFV().valueReference());
+			str.print("))");
+		} else {
+			str.print(myFV().name_C());
+		}
 	}
 	
 	public void FIdUseExp.prettyPrint_C(Printer p, PrintStream str, String indent, Object o) {
 		if (isArray() && keepAsArray())
 			str.print(getFIdUse().name_C());
 		else
-			p.toString(getFIdUse(), str, indent, o);
+			if (myFV().isReal() && root().options.getBooleanOption("enable_variable_scaling")) {
+				str.print("(");
+				p.toString(getFIdUse(), str, indent, o);
+				str.print("*sf(");
+				str.print(myFV().valueReference());
+				str.print("))");
+			} else {
+				p.toString(getFIdUse(), str, indent, o);
+			}
 	}
 	
 	syn String FIdUse.name_C() = myFV().name_C();
@@ -846,15 +862,26 @@ aspect CCodeGen {
 	}	
 	
 	public void FRealVariable.genStartAttributeResidual_C(int i, String indent, PrintStream str) {
-		str.print(indent + "(*res)[" + i + "] = ");
-		if (startAttributeSet()) {
-			startAttributeExp().prettyPrint_C(str,"");
-		} else {
-			str.print("0.0");
+		if (!(this instanceof FDerivativeVariable)) {
+			str.print(indent + "(*res)[" + i + "] = ");
+			if (startAttributeSet()) {
+				if (root().options.getBooleanOption("enable_variable_scaling")) {
+					str.print("(");
+					startAttributeExp().prettyPrint_C(str,"");
+					str.print(")");
+					str.print("*sf(");
+					str.print(valueReference());
+					str.print(")");
+				} else {
+					startAttributeExp().prettyPrint_C(str,"");
+				}
+			} else {
+				str.print("0.0");
+			}
+			str.print(" - ");
+			str.print(name_C());
+			str.print(";\n");
 		}
-		str.print(" - ");
-		str.print(name_C());
-		str.print(";\n");
 	}
 
 	

trunk/Compiler/ModelicaCBackEnd/src/jastadd/codegen/CGenerator.jrag
@@ -23,6 +23,24 @@ import java.io.*;
 
 public class CGenerator extends GenericGenerator {
 
+	class DAETag_C_scalingMethod extends DAETag {
+		
+		public DAETag_C_scalingMethod(AbstractGenerator myGenerator, 
+		  FClass fclass) {
+			super("C_DAE_scaling_method","C: scaling_method",
+			  myGenerator,fclass);
+		}
+	
+		public void generate(PrintStream genPrinter) {
+			if (fclass.root().options.getBooleanOption("enable_variable_scaling")) {
+				genPrinter.print("JMI_SCALING_VARIABLES");
+			} else {
+				genPrinter.print("JMI_SCALING_NONE");
+			}
+		}
+	
+	}
+	
 	class DAETag_C_equationResiduals extends DAETag {
 		
 		public DAETag_C_equationResiduals(AbstractGenerator myGenerator, 

trunk/Compiler/ModelicaCBackEnd/src/test/modelica/CCodeGenTests.mo
@@ -69,8 +69,6 @@ $C_DAE_initial_guess_equation_residuals$
    (*res)[0] = 0.0 - _z_1;
    (*res)[1] = 1 - _w_2;
    (*res)[2] = 0.0 - _v_3;
-   (*res)[3] = 0.0 - _der_z_4;
-   (*res)[4] = 0.0 - _der_v_5;
 ")})));
 
 		Real x(start=1);

trunk/Compiler/ModelicaCBackEnd/templates/jmi_modelica_template.c
@@ -72,6 +72,10 @@ static const int N_eq_Heq = 0;
 static const int N_eq_Hineq = 0;
 static const int N_t_p = 0;
 
+static const int Scaling_method = $C_DAE_scaling_method$;
+
+#define sf(i) (jmi->variable_scaling_factors[i])
+
 $C_variable_aliases$
 
 #define _real_ci(i) ((*(jmi->z))[jmi->offs_real_ci+i])
@@ -168,7 +172,7 @@ int jmi_new(jmi_t** jmi) {
 	   N_string_ci, N_string_cd, N_string_pi, N_string_pd,
 	   N_real_dx,N_real_x, N_real_u, N_real_w,N_t_p, 
 	   N_real_d,N_integer_d,N_integer_u,N_boolean_d,N_boolean_u,
-	   N_string_d,N_string_u,N_sw,N_sw_init);
+	   N_string_d,N_string_u,N_sw,N_sw_init,Scaling_method);
 
 	// Initialize the DAE interface
 	jmi_dae_init(*jmi, *model_dae_F, N_eq_F, NULL, 0, NULL, NULL,

trunk/Compiler/ModelicaFrontEnd/src/jastadd/FlatAPI.jrag
@@ -31,7 +31,7 @@ aspect FlatAPI {
 	 * Bit 32 is left unused.
 	 *
 	 */
-	inh lazy int FVariable.valueReference();
+	inh lazy int AbstractFVariable.valueReference();
 	eq FClass.getFVariable(int i).valueReference() {
 		int index = 0;
 		index = 0x00000000; 

trunk/Compiler/OptimicaCBackEnd/templates/jmi_optimica_template.c
@@ -14,12 +14,10 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <jmi.h>
-
 
 static const int N_real_ci = $n_real_ci$;
 static const int N_real_cd = $n_real_cd$;
@@ -72,6 +70,10 @@ static const int N_eq_Cineq = $n_cineq$;
 static const int N_eq_Heq = $n_heq$;
 static const int N_eq_Hineq = $n_hineq$;
 static const int N_t_p = $n_tp$;
+
+static const int Scaling_method = $C_DAE_scaling_method$;
+
+#define sf(i) (jmi->variable_scaling_factors[i])
 
 $C_variable_aliases$
 
@@ -173,7 +175,7 @@ int jmi_new(jmi_t** jmi) {
 	   N_string_ci, N_string_cd, N_string_pi, N_string_pd,
 	   N_real_dx,N_real_x, N_real_u, N_real_w,N_t_p, 
 	   N_real_d,N_integer_d,N_integer_u,N_boolean_d,N_boolean_u,
-	   N_string_d,N_string_u,N_sw,N_sw_init);
+	   N_string_d,N_string_u,N_sw,N_sw_init,Scaling_method);
 
 	// Initialize the DAE interface
 	jmi_dae_init(*jmi, *model_dae_F, N_eq_F, NULL, 0, NULL, NULL,

trunk/JMI/src/jmi.c
@@ -34,7 +34,8 @@ int jmi_init(jmi_t** jmi, int n_real_ci, int n_real_cd, int n_real_pi,
 		int n_tp,int n_real_d,
 		int n_integer_d, int n_integer_u,
 		int n_boolean_d, int n_boolean_u,
-		int n_string_d, int n_string_u, int n_sw, int n_sw_init) {
+		int n_string_d, int n_string_u, int n_sw, int n_sw_init,
+		int scaling_method) {
 
 	// Create jmi struct
 	*jmi = (jmi_t*)calloc(1,sizeof(jmi_t));
@@ -156,6 +157,8 @@ int jmi_init(jmi_t** jmi, int n_real_ci, int n_real_cd, int n_real_pi,
 	}
 
 	jmi_->tp = (jmi_real_t*)calloc(jmi_->n_tp,sizeof(jmi_real_t));
+
+	jmi_->scaling_method = scaling_method;
 
 	return 0;
 

commit 17ee815a4055d9d5cdc9a8437ecbbd7d5ea0ecbc

Fixed a minor bug that was introduced in r1406, see #13.
git-svn-id: https://svn.jmodelica.org@1412 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Python/src/jmodelica/optimization/ipopt.py
@@ -691,7 +691,7 @@ class NLPCollocation(object):
                     else:
                         var_data[:,col_index] = N.ones(n_points)*traj.x[0]
                 else:
-                    if self._modelget_scaling_method() & jmi.JMI_SCALING_VARIABLES > 0:
+                    if self._model.get_scaling_method() & jmi.JMI_SCALING_VARIABLES > 0:
                         var_data[:,col_index] = traj.x/sc_u[w_index]
                     else:
                         var_data[:,col_index] = traj.x