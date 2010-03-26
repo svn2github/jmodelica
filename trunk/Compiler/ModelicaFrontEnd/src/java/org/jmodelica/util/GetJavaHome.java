c1269c483ba0fcbed4b8014706f09226e4ae7b0a
jmattsson <jmattsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2010-03-26 14:26:46 +0000

    Added simple heuristic to Java class for determining JAVA_HOME automatically. (#700)
    git-svn-id: https://svn.jmodelica.org@1421 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/GetJavaHome.java
+++ trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/GetJavaHome.java
@@ -5,7 +5,10 @@ public class GetJavaHome {
 	public static void main(String[] args) {
 		String home = System.getProperty("java.home");
 		String sep = System.getProperty("file.separator");
-		System.out.println(home.substring(0, home.lastIndexOf(sep) + 1));
+		int pos = home.lastIndexOf(sep) + 1;
+		if (home.substring(pos).equals("jre"))
+			home = home.substring(0, pos);
+		System.out.println(home);
 	}
 
 }
