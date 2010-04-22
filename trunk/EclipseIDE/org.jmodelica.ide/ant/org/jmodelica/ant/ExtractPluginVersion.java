320449d8aec6e8ecc1c017182c9a79ecf326e834
jmattsson <jmattsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2010-04-22 12:44:27 +0000

    Updated build.xml for Eclipse plugins to patch plugin and feature versions with current svn revision. (#174)
    git-svn-id: https://svn.jmodelica.org@1620 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/EclipseIDE/org.jmodelica.ide/ant/org/jmodelica/ant/ExtractPluginVersion.java
+++ trunk/EclipseIDE/org.jmodelica.ide/ant/org/jmodelica/ant/ExtractPluginVersion.java
@@ -1,12 +1,17 @@
 package org.jmodelica.ant;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ExtractPluginVersion {
 	public static final String VERSION_FILE = "plugin.versions";
@@ -14,6 +19,7 @@ public class ExtractPluginVersion {
 	private String plugin;
 	private String dataDir;
 	private String path;
+	private String rev = ".";
 
 	public void setPlugin(String plug) {
 		plugin = plug;
@@ -25,6 +31,11 @@ public class ExtractPluginVersion {
 
 	public void setPath(String path) {
 		this.path = path;
+	}
+	
+	public void setRevision(String revision) {
+		int i = revision.lastIndexOf(':') + 1;
+		rev = ".r" + revision.substring(i).replaceAll("[^0-9]*", "");
 	}
 
 	public void execute() {
@@ -38,17 +49,20 @@ public class ExtractPluginVersion {
 				String str = scan.next();
 				if (str.equals("Bundle-Version") && scan.hasNext()) {
 					version = scan.next();
-					// TODO: Add some kind of qualifier to the version string
-					// and patch manifest file
+					if (rev.length() > 2)
+						version += rev;
 					out.println(plugin + ".version = " + version);
 				} else if (str.equals("Bundle-SymbolicName") && scan.hasNext()) {
 					name = scan.next().split(";")[0];
 					out.println(plugin + ".id = " + name);
 				}
 			}
+			scan.close();
 			if (name != null && version != null) {
 				out.println(plugin + ".jar = " + String.format("%s_%s.jar", name, version));
 				addVersion(name, version);
+				if (rev.length() > 2) 
+					patchVersion(version);
 			}
 			out.close();
 		} catch (Exception e) {
@@ -56,6 +70,33 @@ public class ExtractPluginVersion {
 		}
 	}
 	
+	private void patchVersion(String version) {
+		try {
+			ArrayList<String> lines = new ArrayList<String>();
+			
+			BufferedReader in = new BufferedReader(new FileReader(path));
+			Pattern pat = Pattern.compile("(Bundle-Version: )([.0-9]*)");
+			String repl = "$1" + version;
+			String line = in.readLine();
+			while (line != null) {
+				Matcher m = pat.matcher(line);
+				line = m.replaceAll(repl);
+				lines.add(line);
+				line = in.readLine();
+			}
+			in.close();
+			
+			PrintStream out = new PrintStream(path);
+			for (String l : lines)
+				out.println(l);
+			out.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
 	private void addVersion(String name, String version) {
 		try {
 			File f = new File(dataDir + File.separator + VERSION_FILE);
