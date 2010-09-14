b39e3b567de6354f016e0c03d1f288d203c8c714
jylikiiskila <jylikiiskila@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2010-09-14 13:46:48 +0000

    ticket:986 Added testfile to the jfsolver module
    git-svn-id: https://svn.jmodelica.org@1979 7ae51c65-13d5-4921-9d80-6511ddd1419c

---
+++ trunk/Python/src/jmodelica/tests/initialization/test_jfsolve.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2010 Modelon AB
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+""" Tests the jmi wrappers for the IPOPT solver module. """
+
+import os.path
+
+import numpy as N
+import nose.tools
+
+import jmodelica
+import jmodelica.jmi as jmi
+from jmodelica import initialize
+from jmodelica.tests import testattr
+from jmodelica.tests import get_files_path
+from jmodelica.compiler import ModelicaCompiler
+from jmodelica.algorithm_drivers import JFSInitAlg
+from jmodelica.initialization.jfsolver import JFSolver
+
+int = N.int32
+N.int = N.int32
+
+mc = ModelicaCompiler()
+
+class TestJFSolve:
+    """ Test evaluation of function in NLPInitialization and solution
+    of initialization problems.
+    
+    """
+    @classmethod
+    def setUpClass(cls):
+        """Sets up the test class."""
+        # Compile the stationary initialization model into a DLL
+        fpath = os.path.join(get_files_path(), 'Modelica', 'CSTRLib.mo')
+        mc.compile_model("CSTRLib.Components.Two_CSTRs_stat_init", 
+                         fpath, target='ipopt')
+        
+    
+    def setUp(self):
+        """Test setUp. Load the test model."""                    
+        # Load model
+        self.model = jmi.JMUModel("CSTRLib_Components_Two_CSTRs_stat_init")
+        self.solver = JFSolver(self.model)
+        
+        # Set inputs for Stationary point A
+        u1_0_A = 1
+        u2_0_A = 1
+        self.model.set_value('u1',u1_0_A)
+        self.model.set_value('u2',u2_0_A)
+        
+        self.dx0 = N.zeros(6)
+        self.x0 = N.array([200., 3.57359316e-02, 446.471014, 100., 1.79867213e-03,453.258466])
+        self.w0 = N.array([100., 100., -48.1909])
+    
+    @testattr(stddist = True)
+    def test_inits(self):
+        """
+        test if solver is correctly initialized
+        """
+        nose.tools.assert_true(self.solver._use_jac)
+        nose.tools.assert_equals(self.solver._neqF0,15)
+        nose.tools.assert_equals(self.solver._dx_size,6)
+        nose.tools.assert_equals(self.solver._x_size,6)
+        nose.tools.assert_equals(self.solver._w_size,3)
+        nose.tools.assert_equals(self.solver._mark,12)
+    
+    @testattr(stddist = True)  
+    def test_jac_settings(self):
+        """
+        test if user can set usage of jacobian
+        """
+        self.solver.set_jac_usage(True)
+        nose.tools.assert_true(self.solver._use_jac)
+        self.solver.set_jac_usage(False)
+        nose.tools.assert_false(self.solver._use_jac)
+        self.solver.set_jac_usage(True)
+        
+    @testattr(stddist = True)
+    def test_initialize(self):
+        """
+        test if the initialize function works
+        """
+        
+        self.solver.set_jac_usage(True)
+        self.solver.initialize()
+        
+        dx = self.model.get_real_dx()
+        x = self.model.get_real_x()
+        w = self.model.get_real_w()
+        
+        # Test equalities
+        for pre,calced in zip(self.dx0,dx):
+            nose.tools.assert_almost_equal(pre,calced,6)
+            
+        for pre,calced in zip(self.x0,x):
+            nose.tools.assert_almost_equal(pre,calced,6)
+            
+        for pre,calced in zip(self.w0,w):
+            nose.tools.assert_almost_equal(pre,calced,6)
