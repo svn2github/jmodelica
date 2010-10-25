747de7aef4bcb018051d9aec4b9814b54712c663
tove <tove@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2010-10-25 14:06:48 +0000

    #1137: Adding new file fmi.h and modified fmi.c. #1119: Modified build system so that fmi.h is copied to install\include.
    git-svn-id: https://svn.jmodelica.org@2141 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/JMI/src/fmi.c
+++ trunk/JMI/src/fmi.c
@@ -16,104 +16,57 @@
     see <http://www.gnu.org/licenses/> or
     <http://www.ibm.com/developerworks/library/os-cpl.html/> respectively.
 */
-
-#include "fmiModelFunctions.h"
-#include "fmiModelTypes.h"
+#include <stdio.h>
+#include "fmi.h"
 #include "jmi.h"
 
 /* Creation and destruction of model instances and setting debug status */
-DllExport fmiComponent fmiInstantiateModel (fmiString            instanceName,
-					    fmiString            GUID,
-					    fmiCallbackFunctions functions,
-					    fmiBoolean           loggingOn) {
- 
+fmiComponent fmi_instantiate_model(fmiString instanceName, fmiString GUID, fmiCallbackFunctions functions, fmiBoolean loggingOn) {
 }
-
-DllExport void      fmiFreeModelInstance(fmiComponent c) {
-
+void fmi_free_model_instance(fmiComponent c) {
 }
-
-DllExport fmiStatus fmiSetDebugLogging  (fmiComponent c, fmiBoolean loggingOn) {
-
+fmiStatus fmi_set_debug_logging(fmiComponent c, fmiBoolean loggingOn) {
 }
-
 
 /* Providing independent variables and re-initialization of caching */
-DllExport fmiStatus fmiSetTime                (fmiComponent c, fmiReal time) {
-
+fmiStatus fmi_set_time(fmiComponent c, fmiReal time) {
 }
-
-DllExport fmiStatus fmiSetContinuousStates    (fmiComponent c, const fmiReal x[], size_t nx) {
-
+fmiStatus fmi_set_continuous_states(fmiComponent c, const fmiReal x[], size_t nx) {
 }
-
-DllExport fmiStatus fmiCompletedIntegratorStep(fmiComponent c, fmiBoolean* callEventUpdate) {
-
+fmiStatus fmi_completed_integrator_step(fmiComponent c, fmiBoolean* callEventUpdate) {
 }
-
-DllExport fmiStatus fmiSetReal                (fmiComponent c, const fmiValueReference vr[], size_t nvr, const fmiReal    value[]) {
-
+fmiStatus fmi_set_real(fmiComponent c, const fmiValueReference vr[], size_t nvr, const fmiReal value[]) {
 }
-
-DllExport fmiStatus fmiSetInteger             (fmiComponent c, const fmiValueReference vr[], size_t nvr, const fmiInteger value[]) {
-
+fmiStatus fmi_set_integer (fmiComponent c, const fmiValueReference vr[], size_t nvr, const fmiInteger value[]) {
 }
-
-DllExport fmiStatus fmiSetBoolean             (fmiComponent c, const fmiValueReference vr[], size_t nvr, const fmiBoolean value[]) {
-
+fmiStatus fmi_set_boolean (fmiComponent c, const fmiValueReference vr[], size_t nvr, const fmiBoolean value[]) {
 }
-
-DllExport fmiStatus fmiSetString              (fmiComponent c, const fmiValueReference vr[], size_t nvr, const fmiString  value[]) {
-
+fmiStatus fmi_set_string(fmiComponent c, const fmiValueReference vr[], size_t nvr, const fmiString value[]) {
 }
-
 
 /* Evaluation of the model equations */
-DllExport fmiStatus fmiInitialize(fmiComponent c, fmiBoolean toleranceControlled,
-				  fmiReal relativeTolerance, fmiEventInfo* eventInfo) {
-
+fmiStatus fmi_initialize(fmiComponent c, fmiBoolean toleranceControlled, fmiReal relativeTolerance, fmiEventInfo* eventInfo) {
+}
+fmiStatus fmi_get_derivatives(fmiComponent c, fmiReal derivatives[] , size_t nx) {
+}
+fmiStatus fmi_get_event_indicators(fmiComponent c, fmiReal eventIndicators[], size_t ni) {
+}
+fmiStatus fmi_get_real(fmiComponent c, const fmiValueReference vr[], size_t nvr, fmiReal value[]) {
+}
+fmiStatus fmi_get_integer(fmiComponent c, const fmiValueReference vr[], size_t nvr, fmiInteger value[]) {
+}
+fmiStatus fmi_get_boolean(fmiComponent c, const fmiValueReference vr[], size_t nvr, fmiBoolean value[]) {
+}
+fmiStatus fmi_get_string(fmiComponent c, const fmiValueReference vr[], size_t nvr, fmiString  value[]) {
+}
+fmiStatus fmi_event_update(fmiComponent c, fmiBoolean intermediateResults, fmiEventInfo* eventInfo) {
+}
+fmiStatus fmi_get_continuous_states(fmiComponent c, fmiReal states[], size_t nx) {
+}
+fmiStatus fmi_get_nominal_continuous_states(fmiComponent c, fmiReal x_nominal[], size_t nx) {
+}
+fmiStatus fmi_get_state_value_references(fmiComponent c, fmiValueReference vrx[], size_t nx) {
+}
+fmiStatus fmi_terminate(fmiComponent c) {
 }
 
-DllExport fmiStatus fmiGetDerivatives    (fmiComponent c, fmiReal derivatives[]    , size_t nx) {
-
-}
-
-DllExport fmiStatus fmiGetEventIndicators(fmiComponent c, fmiReal eventIndicators[], size_t ni) {
-
-}
-
-DllExport fmiStatus fmiGetReal   (fmiComponent c, const fmiValueReference vr[], size_t nvr, fmiReal    value[]) {
-
-}
-
-DllExport fmiStatus fmiGetInteger(fmiComponent c, const fmiValueReference vr[], size_t nvr, fmiInteger value[]) {
-
-}
-
-DllExport fmiStatus fmiGetBoolean(fmiComponent c, const fmiValueReference vr[], size_t nvr, fmiBoolean value[]) {
-
-}
-
-DllExport fmiStatus fmiGetString (fmiComponent c, const fmiValueReference vr[], size_t nvr, fmiString  value[]) {
-
-}
-
-DllExport fmiStatus fmiEventUpdate               (fmiComponent c, fmiBoolean intermediateResults, fmiEventInfo* eventInfo) {
-
-}
-
-DllExport fmiStatus fmiGetContinuousStates       (fmiComponent c, fmiReal states[], size_t nx) {
-
-}
-
-DllExport fmiStatus fmiGetNominalContinuousStates(fmiComponent c, fmiReal x_nominal[], size_t nx) {
-
-}
-
-DllExport fmiStatus fmiGetStateValueReferences   (fmiComponent c, fmiValueReference vrx[], size_t nx) {
-
-}
-
-DllExport fmiStatus fmiTerminate                 (fmiComponent c) {
-
-}
---
+++ trunk/JMI/src/fmi.h
@@ -0,0 +1,52 @@
+/*
+    Copyright (C) 2009 Modelon AB
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3 as published
+    by the Free Software Foundation, or optionally, under the terms of the
+    Common Public License version 1.0 as published by IBM.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License, or the Common Public License, for more details.
+
+    You should have received copies of the GNU General Public License
+    and the Common Public License along with this program.  If not,
+    see <http://www.gnu.org/licenses/> or
+    <http://www.ibm.com/developerworks/library/os-cpl.html/> respectively.
+*/
+#ifndef fmi_h
+#define fmi_h
+
+#include "fmiModelTypes.h"
+
+/* Creation and destruction of model instances and setting debug status */
+fmiComponent fmi_instantiate_model(fmiString instanceName, fmiString GUID, fmiCallbackFunctions functions, fmiBoolean loggingOn);
+void fmi_free_model_instance(fmiComponent c);
+fmiStatus fmi_set_debug_logging(fmiComponent c, fmiBoolean loggingOn);
+
+/* Providing independent variables and re-initialization of caching */
+fmiStatus fmi_set_time(fmiComponent c, fmiReal time);
+fmiStatus fmi_set_continuous_states(fmiComponent c, const fmiReal x[], size_t nx);
+fmiStatus fmi_completed_integrator_step(fmiComponent c, fmiBoolean* callEventUpdate);
+fmiStatus fmi_set_real(fmiComponent c, const fmiValueReference vr[], size_t nvr, const fmiReal value[]);
+fmiStatus fmi_set_integer (fmiComponent c, const fmiValueReference vr[], size_t nvr, const fmiInteger value[]);
+fmiStatus fmi_set_boolean (fmiComponent c, const fmiValueReference vr[], size_t nvr, const fmiBoolean value[]);
+fmiStatus fmi_set_string(fmiComponent c, const fmiValueReference vr[], size_t nvr, const fmiString value[]);
+
+/* Evaluation of the model equations */
+fmiStatus fmi_initialize(fmiComponent c, fmiBoolean toleranceControlled, fmiReal relativeTolerance, fmiEventInfo* eventInfo);
+fmiStatus fmi_get_derivatives(fmiComponent c, fmiReal derivatives[] , size_t nx);
+fmiStatus fmi_get_event_indicators(fmiComponent c, fmiReal eventIndicators[], size_t ni);
+fmiStatus fmi_get_real(fmiComponent c, const fmiValueReference vr[], size_t nvr, fmiReal value[]);
+fmiStatus fmi_get_integer(fmiComponent c, const fmiValueReference vr[], size_t nvr, fmiInteger value[]);
+fmiStatus fmi_get_boolean(fmiComponent c, const fmiValueReference vr[], size_t nvr, fmiBoolean value[]);
+fmiStatus fmi_get_string(fmiComponent c, const fmiValueReference vr[], size_t nvr, fmiString  value[]);
+fmiStatus fmi_event_update(fmiComponent c, fmiBoolean intermediateResults, fmiEventInfo* eventInfo);
+fmiStatus fmi_get_continuous_states(fmiComponent c, fmiReal states[], size_t nx);
+fmiStatus fmi_get_nominal_continuous_states(fmiComponent c, fmiReal x_nominal[], size_t nx);
+fmiStatus fmi_get_state_value_references(fmiComponent c, fmiValueReference vrx[], size_t nx);
+fmiStatus fmi_terminate(fmiComponent c);
+
+#endif
