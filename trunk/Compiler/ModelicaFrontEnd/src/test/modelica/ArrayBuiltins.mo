svn2github/jmodelica

commit 92c243235ddd3601bbdf2dd28b9a8ec069e5a1f4

Fixed a bug in the computation of dependent parameters, see #1153.
git-svn-id: https://svn.jmodelica.org@2161 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/jastadd/FlatAPI.jrag
@@ -608,7 +608,8 @@ aspect FlatAPI {
 	*/
 	
 	inh Set<FVariable> FExp.boundParameters();
-	eq FVariable.getBindingExp().boundParameters()       = Collections.singleton(this);
+	eq FVariable.getBindingExp().boundParameters()   =	Collections.singleton(this);
+	
 	eq Root.getChild().boundParameters()                 = Collections.emptySet();
 	
 	eq FEquation.getRight().boundParameters() {
@@ -649,9 +650,8 @@ aspect FlatAPI {
 	
 	syn lazy boolean FVariable.isDependentParameter() = 
 		isParameter() && !isIndependentParameter();
-	syn lazy boolean FVariable.isIndependentParameter() = 
-		isParameter() && !hasParameterEquation() && 
-		(!hasParamValueExp() || paramValueExp().isIndependentParameterExp());
+	syn lazy boolean FVariable.isIndependentParameter() = isParameter() && 
+		!hasParameterEquation() && (!hasBindingExp() || getBindingExp().isIndependentParameterExp());
 
 	syn boolean FExp.isIndependentParameterExp() = isConstantExp() && !hasCompositeFunc();
 	

commit 07ba20ac0d9381e82af6431f8874852325e90464

* Added support for other types that Real scalars to noEvent(), and added tests. (#1155)  * Updated test that was broken by r2161 to match new behavior. (#1153)
git-svn-id: https://svn.jmodelica.org@2165 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/jastadd/Arrays.jrag
@@ -210,6 +210,8 @@ aspect Arrays {
 	eq FCross.ndims() = 1;
 	eq FIdentity.ndims() = 2;
 	
+	eq FNoEventExp.ndims() = getFExp().ndims();
+	
 	eq FSizeExp.ndims() = hasDim() ? 0 : 1;
 	
 	eq FInfArgsFunctionCall.ndims() = getNumFExp();
@@ -713,6 +715,8 @@ aspect Arrays {
 	}
 	
 	eq FSizeExp.size() = hasDim() ? Size.SCALAR : new Size(getFExp().ndims());
+	
+	eq FNoEventExp.size() = getFExp().size();
 	
 	// TODO: Expand to cover vectorization of all scalar built-in functions (see #528)
 	eq FStreamBuiltIn.size() = getFExp().size();
@@ -1299,6 +1303,17 @@ aspect Arrays {
 	
 	// TODO: Expand to cover vectorization of all scalar built-in functions (see #528)
 	eq FStreamBuiltIn.getArray() {
+		if (!isArray())
+			return super.getArray();
+		
+		Array arr = new Array(indices());
+		Array inner = getFExp().getArray();
+		for (Index i : arr.indices())
+			arr.set(i, createNode(inner.get(i)));
+		return arr;
+	}
+	
+	eq FNoEventExp.getArray() {
 		if (!isArray())
 			return super.getArray();
 		

trunk/Compiler/ModelicaFrontEnd/src/jastadd/ConstantEvaluation.jrag
@@ -1148,6 +1148,8 @@ aspect ConstantEvaluation {
 	  return sum;
   }
   
+  eq FNoEventExp.cevalCalc() = getFExp().ceval();
+  
   syn boolean FMinMaxExp.selectLesser();
   eq FMinExp.selectLesser() = true;
   eq FMaxExp.selectLesser() = false;

trunk/Compiler/ModelicaFrontEnd/src/jastadd/FlatAPI.jrag
@@ -608,9 +608,8 @@ aspect FlatAPI {
 	*/
 	
 	inh Set<FVariable> FExp.boundParameters();
-	eq FVariable.getBindingExp().boundParameters()   =	Collections.singleton(this);
-	
-	eq Root.getChild().boundParameters()                 = Collections.emptySet();
+	eq FVariable.getBindingExp().boundParameters() = Collections.singleton(this);
+	eq Root.getChild().boundParameters()           = Collections.emptySet();
 	
 	eq FEquation.getRight().boundParameters() {
 		if (isParameterEquation()) {

trunk/Compiler/ModelicaFrontEnd/src/jastadd/FlatTypeCheck.jrag
@@ -536,6 +536,7 @@ aspect FlatTypeCheck {
   eq FInfArgsFunctionCall.getChild().argumentDefinedTypeValid()  = false;
   eq FStreamBuiltIn.getChild().argumentDefinedTypeValid()        = false;
   eq FEnumIntegerExp.getChild().argumentDefinedTypeValid()       = false;
+  eq FNoEventExp.getChild().argumentDefinedTypeValid()           = false;
 
   /**
    * \brief Check if the type supplied is valid for this argument.
@@ -547,6 +548,7 @@ aspect FlatTypeCheck {
   eq FTranspose.getChild().argumentTypeValid(FType type)            = type.ndims() >= 2 && type.isPrimitive();
   eq FSumExp.getChild().argumentTypeValid(FType type)               = type.ndims() > 0 && type.hasAdd();
   eq FEnumIntegerExp.getChild().argumentTypeValid(FType type)       = type.isEnumScalar();
+  eq FNoEventExp.getChild().argumentTypeValid(FType type)           = true;
   eq FMinMaxExp.getOriginalArg(int i).argumentTypeValid(FType type) = 
 	  hasY() != getOriginalArg(i).getFExp().isArray() && type.isPrimitive();
   

trunk/Compiler/ModelicaFrontEnd/src/jastadd/PredefinedTypes.jrag
@@ -256,7 +256,7 @@ aspect PredefinedTypes {
 +"function terminal end terminal;\n"
 +"function smooth end smooth;\n"
 +"function noEvent \n"
-+"  input Real x;\n"
++"  input Real x;\n"      // Type not used
 +"  output Real y;\n" 
 +"end noEvent;\n"
 +"function sample end sample;\n"

trunk/Compiler/ModelicaFrontEnd/src/jastadd/Scalarization.jrag
@@ -890,6 +890,9 @@ aspect Scalarization {
 			for (Index i : indices()) 
 				createScalarFEquations(eqns, indexMap, 
 						getLeft().getArray().get(i), getRight().getArray().get(i));
+		} else {
+			throw new UnsupportedOperationException("Bad equation: ndims() for equation '" + 
+					this + "' returned " + ndims()); 
 		}
 	}
 	
@@ -991,10 +994,14 @@ aspect Scalarization {
 		return scalarizeExp(indexMap).extractScalarRecordFExp(suffix);
 	}
 	
+	public FExp FNoEventExp.scalarRecordFExp(Map<String,FExp> indexMap, FQName suffix) {
+		return createNode(getFExp().scalarRecordFExp(indexMap, suffix));
+	}
+	
 	/**
 	 * \brief Create an FExp that represents a specific scalar component of this record.
 	 * 
-	 * Assumed that this expression is already scalarized.
+	 * It is assumed that this expression is already scalarized.
 	 * 
 	 * @param suffix    the suffix to add to a use of the record to access the specific component
 	 */

trunk/Compiler/ModelicaFrontEnd/src/jastadd/TransformCanonical.jrag
@@ -587,6 +587,10 @@ aspect TransformCanonical {
 		return res;
 	}
 	
+	public FExp FNoEventExp.extractRecordFExp(String suffix) {
+		return createNode(getFExp().extractRecordFExp(suffix));
+	}
+	
 	/**
 	 * \brief Generate equations from an attribute of a record variable.
 	 */

trunk/Compiler/ModelicaFrontEnd/src/test/modelica/ArrayBuiltins.mo
@@ -1927,4 +1927,85 @@ end ScalarSize2;
 
 
 
+model NoEventArray1
+ annotation(JModelica(unitTesting = JModelica.UnitTesting(testCase={
+	 JModelica.UnitTesting.TransformCanonicalTestCase(
+		 name="NoEventArray1",
+		 description="noEvent() for Real array",
+		 flatModel="
+fclass ArrayBuiltins.NoEventArray1
+ Real x[1];
+ Real x[2];
+ Real y[1];
+ Real y[2];
+equation
+ x[1] = 1;
+ x[2] = 2;
+ y[1] = noEvent(x[1]);
+ y[2] = noEvent(x[2]);
+end ArrayBuiltins.NoEventArray1;
+")})));
+
+	Real x[2] = {1, 2};
+	Real y[2] = noEvent(x);
+end NoEventArray1;
+
+
+model NoEventArray2
+ annotation(JModelica(unitTesting = JModelica.UnitTesting(testCase={
+     JModelica.UnitTesting.TransformCanonicalTestCase(
+         name="NoEventArray2",
+         description="noEvent() for Boolean array",
+         flatModel="
+fclass ArrayBuiltins.NoEventArray2
+ parameter Boolean x[1] = true /* true */;
+ parameter Boolean x[2] = false /* false */;
+ parameter Boolean y[1];
+ parameter Boolean y[2];
+parameter equation
+ y[1] = noEvent(x[1]);
+ y[2] = noEvent(x[2]);
+end ArrayBuiltins.NoEventArray2;
+")})));
+
+	parameter Boolean x[2] = {true, false};
+	parameter Boolean y[2] = noEvent(x);  // Not very logical, but we need to test this
+end NoEventArray2;
+
+
+model NoEventRecord1
+ annotation(JModelica(unitTesting = JModelica.UnitTesting(testCase={
+     JModelica.UnitTesting.TransformCanonicalTestCase(
+         name="NoEventRecord1",
+         description="",
+         flatModel="
+fclass ArrayBuiltins.NoEventRecord1
+ Real x.a;
+ Real x.b;
+ Real y.a;
+ Real y.b;
+equation
+ x.a = 1;
+ x.b = 2;
+ y.a = noEvent(x.a);
+ y.b = noEvent(x.b);
+
+ record ArrayBuiltins.NoEventRecord1.A
+  Real a;
+  Real b;
+ end ArrayBuiltins.NoEventRecord1.A;
+end ArrayBuiltins.NoEventRecord1;
+")})));
+
+	record A
+		Real a;
+		Real b;
+	end A;
+	
+	A x = A(1, 2);
+	A y = noEvent(x);
+end NoEventRecord1;
+
+
+
 end ArrayBuiltins;

trunk/Compiler/ModelicaFrontEnd/src/test/modelica/CodeGenTests.mo
@@ -68,16 +68,16 @@ n_real_cd: 0
 n_integer_cd: 0
 n_boolean_cd: 0
 n_string_cd: 0
-n_pi: 12
-n_real_pi: 3
-n_integer_pi: 3
-n_boolean_pi: 3
-n_string_pi: 3
-n_pd: 8
-n_real_pd: 2
-n_integer_pd: 2
-n_boolean_pd: 2
-n_string_pd: 2
+n_pi: 16
+n_real_pi: 4
+n_integer_pi: 4
+n_boolean_pi: 4
+n_string_pi: 4
+n_pd: 4
+n_real_pd: 1
+n_integer_pd: 1
+n_boolean_pd: 1
+n_string_pd: 1
 n_real_w: 2
 n_d: 9
 n_real_d: 0