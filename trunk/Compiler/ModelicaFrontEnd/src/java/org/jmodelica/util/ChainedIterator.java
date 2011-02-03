96f12be722269d7d987bfd640ae516e3c31fb05f
jmattsson <jmattsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2011-02-03 16:24:04 +0000

    Updated annotation API to cover all cases needed by graphics annotations. (#832)
    git-svn-id: https://svn.jmodelica.org@2377 7ae51c65-13d5-4921-9d80-6511ddd1419c

---
+++ trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/ChainedIterator.java
@@ -0,0 +1,38 @@
+package org.jmodelica.util;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ChainedIterator<E> implements Iterator<E> {
+	
+	private Iterator<? extends E>[] its;
+	private int i;
+	
+	public ChainedIterator(Iterator<? extends E>[] its) {
+		this.its = its;
+	}
+	
+	public ChainedIterator(Iterator<? extends E> it1, Iterator<? extends E> it2) {
+		this(new Iterator[] { it1, it2 });
+	}
+
+	@Override
+	public boolean hasNext() {
+		while (i < its.length && !its[i].hasNext())
+			i++;
+		return i < its.length;
+	}
+
+	@Override
+	public E next() {
+		if (!hasNext())
+			throw new NoSuchElementException();
+		return its[i].next();
+	}
+
+	@Override
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
+
+}
