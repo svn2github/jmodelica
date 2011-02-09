2ebcae602a19eb40c8ac9956a9339a842b53a746
tove <tove@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2011-02-09 15:47:40 +0000

    Bugfix. (Enables paths with spaces.)
    git-svn-id: https://svn.jmodelica.org@2379 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/run_java.sh
+++ trunk/run_java.sh
@@ -5,12 +5,12 @@
 # Relative path from this script to source dir.
 REL=.
 # Path to source dir (relative to cwd or absolute depending on $0)
-SRC=$(dirname $0)/${REL}
+SRC="$(dirname "$0")"/${REL}
 CLS=$1
 
 shift
-FILE=${SRC}/Compiler/ModelicaFrontEnd/src/java/${CLS//.//}.java
+FILE="${SRC}/Compiler/ModelicaFrontEnd/src/java/${CLS//.//}.java"
 TEMP=$(mktemp -dq /tmp/runjava.XXXXXX)
-javac -d ${TEMP} ${FILE}
-java -cp ${TEMP} ${CLS} $@
+javac -d ${TEMP} "${FILE}"
+java -cp ${TEMP} ${CLS} "$@"
 rm -rf ${TEMP}
