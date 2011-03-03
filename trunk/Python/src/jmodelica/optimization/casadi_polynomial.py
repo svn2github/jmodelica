1bbe656bda3f3df4010f27d467ac784d607d86b8
chria <chria@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2011-03-03 21:35:40 +0000

    Added python methods for stable calculations of Legendre polynomials and its derivatives. Also added root-finding and calculations of gauss weights. Related to ticket:1306
    git-svn-id: https://svn.jmodelica.org@2419 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Python/src/jmodelica/optimization/casadi_polynomial.py
+++ trunk/Python/src/jmodelica/optimization/casadi_polynomial.py
@@ -19,6 +19,7 @@ Module containing the polynomials for the Casadi collocation algorithms.
 """
 
 import numpy as N
+import numpy.linalg
 import scipy.special as SP
 
 try:
@@ -94,6 +95,12 @@ class RadauPol3(RadauPol):
                         [1.0048809399827414e+01, 3.2247448713915885e+00, -3.5678400846904061e+00, 5.5319726474218047e+00],
                         [-1.3821427331607485e+00, 1.1678400846904053e+00, 7.7525512860840973e-01, -7.5319726474218065e+00],
                         [3.3333333333333337e-01, -2.5319726474218085e-01, 1.0531972647421810e+00, 5.0000000000000000e+00]])
+
+class LegendreGaussRadau:
+    """
+    Class containing the LGR points which lie on the interval t=(-1,1].
+    """
+    pass
 
 class LegendreGauss:
     """
@@ -286,3 +293,152 @@ class LagrangePol:
     
     def get_lagrange_polynomials(self):
         return self.L
+        
+def LegendrePn(K, x):
+    """
+    Calculates the Legendre polynomial of degree K at point x, P_n(x) using
+    the recurrence relation:
+    
+        P_l(x) = (2l-1)/l*x*P_(l-1)(x)-(l-1)/l*P_(l-2)(x)
+        
+    Reference: http://mathworld.wolfram.com/LegendrePolynomial.html (eq:43)
+    """
+    p0 = 1.0
+    p1 = x
+    
+    if N==0:
+        return p0
+    elif N==1:
+        return p1
+    else:
+        for n in range(2,K+1):
+            pn = (2*n-1)*x*p1/n-(n-1)*p0/n
+            p0 = p1
+            p1 = pn
+        return pn
+
+def LegendredPn(K, x):
+    """
+    Calculates the derivative of the Legendre polynomial of degree K
+    at point x, P_n'(x) using the relation:
+    
+        P_n'(x) = (n*P_(n-1)(x)-n*x*P_n)/(1-x^2)
+    
+    the end points P_n'(-1.0) and P_n'(1.0) are given by:
+    
+        P_n'(x) = x^(n+1)*n*(n+1)/2.0
+
+    where P_(n-1) and P_n are the Legendre polynomials of degree K-1 and
+    N.
+    
+    Reference: http://mathworld.wolfram.com/LegendrePolynomial.html (eq:44)
+    """
+    p0 = LegendrePn(K-1, x)
+    p1 = LegendrePn(K, x)
+    
+    if N.abs(x)==1.0:
+        pn = x**(K+1)*K*(K+1)/2.0
+    else:
+        pn = (K*p0-K*x*p1)/(1.0-x**2)
+    
+    return pn
+    
+def LegendreddPn(K, x):
+    """
+    Calculates the second derivative of the Legendre polynomial of degree K
+    at point x, P_n''(x) using the relation:
+
+        P_n''(x) = 1/4*(n+1)*(n+2)*P_(n-2)^(2,2)(x)
+        
+    where P_(n-2)^(2,2)(x) is the K-2 degree Jacobi polynomial with a=2 and
+    b=2. The Jacobi polynomial is solved using the recurrence relation:
+    
+        2l(l+4)(2l+2)*P_l^(2,2)(x) = (2l+2)_3*x*P_(l-1)^(2,2)(x)-2(l+1)^2*(2l+4)P_(l-2)^(2,2)(x)
+    
+    Reference: http://mathworld.wolfram.com/JacobiPolynomial.html (eq:12, eq:14)
+    """
+    p0 = 0.0
+    p1 = 0.0
+    
+    if K==0:
+        return p0
+    elif K==1:
+        return p1
+    else:
+        p0 = 1.0
+        p1 = 3.0*x
+        if K==2:
+            pn=p0
+        elif K==3:
+            pn=p1
+        else:
+            for n in range(2, K-1):
+                pn = 1.0/(2.0*n*(n+4)*(2*n+2))*((2.0*n+2)*(2.0*n+3.0)*(2.0*n+4.0)*x*p1-2.0*(n+1.0)**2.0*(2.0*n+4.0)*p0)
+                p0 = p1
+                p1 = pn
+    return pn*1.0/4.0*(K+1.0)*(K+2.0)
+    
+def LegendrePnRoots(K):
+    """
+    Calculates the K roots of the K degree Legendre polynomial by first
+    generating the Jacobi matrix.
+    
+    For Legendre polynomials the Jacobi matrix is:  
+    
+            [    0     1/sqrt(3)      0           0    ]
+            [1/sqrt(3)     0      2/sqrt(15)      0    ]
+            [    0     2/sqrt(15)     0      3/sqrt(35)]
+            [    0         0      3/sqrt(35)      0    ]
+    
+           A(n+1,n) = n/sqrt(4*n^2-1)
+           A(n,n+1) = n/sqrt(4*n^2-1)
+    """
+    supdiag = [i/N.sqrt(4.0*i*i-1) for i in range(1,K)]
+
+    A = N.diag(supdiag, 1)+N.diag(supdiag, -1)
+    r = N.linalg.eig(A)[0]
+    r.sort()
+    return r
+    
+def LegendredPnRoots(K):
+    """
+    Calculates K-1 roots of the derivative of the K degree Legendre Polynomial.
+    The calculations are performed via generation of a Jacobi Matrix.
+    
+    
+            A(n+1,n) = sqrt( n*(n+2) / ( (2n+1)*(2n+3) )
+            A(n,n+1) = sqrt( n*(n+2) / ( (2n+1)*(2n+3) )
+            
+    Reference: http://mathworld.wolfram.com/JacobiPolynomial.html (eq:11, 12)
+    """
+    K = K-1
+    supdiag = [N.sqrt(i*(i+2.0)/((2.0*i+1.0)*(2.0*i+3.0))) for i in range(1,K)]
+    
+    A = N.diag(supdiag, 1)+N.diag(supdiag, -1)
+    r = N.linalg.eig(A)[0]
+    r.sort()
+    return r
+    
+def GaussQuadratureWeights(type, K):
+    """
+    Calculates the K Gauss quadrature weights for a given type of points. The
+    type can either be:
+    
+        type = "LG" , corresponding to Legendre-Gauss points
+        type = "LGL", corresponding to Legendre-Gauss-Lobatto points
+        type = "LGR", corresponding to Legendre-Gauss-Radau points
+    """
+    w = N.zeros(K)
+    
+    if type == "LG":
+        ti = LegendreRoots(K)
+        dPn_ti = [LegendredPn(K,x) for x in ti]
+        w = [2.0/((1.0-ti[i]**2)*x**2) for i,x in enumerate(dPn_ti)]
+    elif type == "LGL":
+        pass 
+    elif type == "LGR":
+        pass
+    else:
+        raise Exception("Unknown option to Gauss Quadrature.")
+                        
+    return N.array(w)
