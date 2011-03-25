3f7894fccba7f69b205087878e7be6caecb2ace3
sofia <sofia@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2011-03-25 15:12:28 +0000

    Made a lot of modifications in the help function quad_err in the file quadratic_error.py. Modified it so that it can handle one-dimensional numpy arrays as inputs. Added some Exception raisings. And, most importantly, made sure that it does not return nan if more than one simulation value at the same simulation time point.
    git-svn-id: https://svn.jmodelica.org@2452 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Python/src/jmodelica/optimization/quadratic_error.py
+++ trunk/Python/src/jmodelica/optimization/quadratic_error.py
@@ -13,19 +13,24 @@ def quad_err(t_meas,y_meas,t_sim,y_sim):
 			The measurement time points.
 			
 		y_meas --
-			ndarray
-			The measurement values. The number of dimensions of the 
+			ndarray (of 1 or 2 dimensions)
+			The measurement values. The number of rows in the 
 			array corresponds to the number of physical quantities
 			measured.
+			NB: Must have same length as t_meas and same number of 
+				rows as y_sim.
 		
 		t_sim --
 			ndarray (of 1 dimension)
-			The simulation time points.
+			The simulation time points. 
+			NB: Must be increasing.
 			
 		y_sim --
-			ndarray
-			The simulation values. The number of dimensions of the array 
+			ndarray (of 1 or 2 dimensions)
+			The simulation values. The number of rows in the array 
 			corresponds to the number of physical quantities simulated.
+			NB: Must have same length as t_sim and same number of 
+				rows as y_meas.
 	
 	Returns::
 	
@@ -34,15 +39,74 @@ def quad_err(t_meas,y_meas,t_sim,y_sim):
 			The quadratic error.
 	"""
 	
-	# The size of the array y_meas
-	m, n = N.shape(y_meas)
+	# The number of dimensions of y_meas
+	dim1 = N.ndim(y_meas)
+	
+	# The number of rows and columns in y_meas
+	if dim1 == 1:
+		m1 = 1
+		n1 = len(y_meas)
+	else:
+		m1 = N.size(y_meas,0)
+		n1 = N.size(y_meas,1)
+	
+	# The number of dimensions of y_sim
+	dim2 = N.ndim(y_sim)
+	
+	# The number of rows and columns in y_sim
+	if dim2 == 1:
+		m2 = 1
+		n2 = len(y_sim)
+	else:
+		m2 = N.size(y_sim,0)
+		n2 = N.size(y_sim,1)
+	
+	if len(t_meas) is not n1:
+		raise ValueError, 't_meas and y_meas must have the same length.'
+	
+	if len(t_sim) is not n2:
+		raise ValueError, 't_sim and y_sim must have the same length.'
+	
+	if m1 is not m2:
+		raise ValueError, 'y_meas and y_sim must have the same number of rows.'
+	
+	if not N.all(N.diff(t_sim) >= 0):
+		raise ValueError, 't_sim must be increasing.'
+	
+	# The number of measurement points
+	n = n1
+	
+	# The number of rows in y_meas and y_sim
+	m = m1
 	
 	# Interpolate to get the simulated values in the measurement points
-	Y_sim = N.zeros((m,n))
-	for i in range(m):
-		Y_sim[i] = N.interp(t_meas,t_sim,y_sim[i])
+	Y_sim = N.zeros([m,n])
+	if m == 1:
+		Y_sim = N.interp(t_meas,t_sim,y_sim)
+	else:
+		for i in range(m):
+			Y_sim[i] = N.interp(t_meas,t_sim,y_sim[i])
+	
+	# Check if the same time point occurs more than once in t_sim and 
+	# then fix the problem
+	for i in range(len(t_sim)):
+		val = t_sim[i]
+		rest = t_sim[i+1:]
+		if val in rest and val in t_meas:
+			ind1 = t_sim == val
+			y_sim_val = y_sim[:,ind1]
+			ind2 = t_meas == val
+			if m == 1:
+				Y_sim[:,ind2] = sum(y_sim_val)*1.0/len(y_sim_val)
+			else:
+				vec = N.sum(y_sim_val,1)*1.0/N.size(y_sim_val,1)
+				for j in range(m):
+					Y_sim[j,ind2] = vec[j]
 		
 	# Evaluate the error
-	err = sum(sum((Y_sim - y_meas)**2,0))
+	err = sum((Y_sim - y_meas)**2,0)
 	
-	return err
+	if m == 1:
+		return err
+	else:
+		return sum(err)
