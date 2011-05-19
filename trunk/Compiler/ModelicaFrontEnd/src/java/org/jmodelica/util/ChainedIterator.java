ef4633e7cfdacaddf36008ad3e65683d0dfe6139
jmattsson <jmattsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2011-05-19 15:26:47 +0000

    Added support for expressions as unnamed arguments of function calls in annotation API. (#832)
    git-svn-id: https://svn.jmodelica.org@2562 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/ChainedIterator.java
+++ trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/ChainedIterator.java
@@ -8,12 +8,8 @@ public class ChainedIterator<E> implements Iterator<E> {
 	private Iterator<? extends E>[] its;
 	private int i;
 	
-	public ChainedIterator(Iterator<? extends E>[] its) {
+	public ChainedIterator(Iterator<? extends E>... its) {
 		this.its = its;
-	}
-	
-	public ChainedIterator(Iterator<? extends E> it1, Iterator<? extends E> it2) {
-		this(new Iterator[] { it1, it2 });
 	}
 
 	public boolean hasNext() {
