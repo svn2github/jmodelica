77653a9940e7f54f728da454643c50e849198272
jmattsson <jmattsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2011-05-19 15:23:43 +0000

    Minor fix in util class. It was missing access specifier.
    git-svn-id: https://svn.jmodelica.org@2561 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/DummyOutputStream.java
+++ trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/DummyOutputStream.java
@@ -2,7 +2,7 @@ package org.jmodelica.util;
 
 import java.io.OutputStream;
 
-class DummyOutputStream extends OutputStream {
+public class DummyOutputStream extends OutputStream {
 	public void write(int b) {}
 	public void write(byte[] b, int off, int len) {}
 }
