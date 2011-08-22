svn2github/jmodelica

commit 84c7cc2140eb316c34f18bf3810d3790be7839ac

* Added front-end support for smooth(), including symbolic differentiation and simple test. (#1481)  * Added code generation for exporting Modelica functions used in a model as C functions. (#1492)  * Added helper methods for the XML code generation and refactored about half of it. (#1519)
git-svn-id: https://svn.jmodelica.org@2715 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/jastadd/PrettyPrint.jrag
@@ -16,6 +16,7 @@
 
 import java.io.PrintStream;
 import java.io.ByteArrayOutputStream;
+import java.util.Collection;
 
 
 	// superclass to all classes that perform the dispatch
@@ -32,6 +33,11 @@ import java.io.ByteArrayOutputStream;
 		}
 
 		public void print(ASTNode node, PrintStream str, String indent) { 
+  		}
+
+		public void print(Collection<? extends ASTNode> nodes, PrintStream str, String indent) {
+			for (ASTNode node : nodes)
+				print(node, str, indent);
   		}
   		
   		public String op(FBinExp e) { return e.op(); }
@@ -1502,6 +1508,7 @@ aspect FlattPrettyPrint {
 	eq FNdimsExp.builtInName()       = "ndims";
 	eq FSizeExp.builtInName()        = "size";
 	eq FNoEventExp.builtInName()     = "noEvent";
+	eq FSmoothExp.builtInName()      = "smooth";
 	eq FPreExp.builtInName()         = "pre";
 	eq FSampleExp.builtInName()      = "sample";
 	eq FInitialExp.builtInName()     = "initial";

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen.jrag
@@ -780,7 +780,11 @@ aspect CCodeGen {
 	}
 
 	public void FNoEventExp.prettyPrint_C(Printer p, PrintStream str, String indent) {
-		p.print(getFExp(),str,indent);
+		p.print(getFExp(), str, indent);
+	}
+
+	public void FSmoothExp.prettyPrint_C(Printer p, PrintStream str, String indent) {
+		p.print(getFExp(), str, indent);
 	}
 
 	public void FInitialExp.prettyPrint_C(Printer p, PrintStream str, String indent) {

trunk/Compiler/ModelicaCBackEnd/src/jastadd/ExportFunctions.jadd
@@ -1,0 +1,216 @@
+/*
+Copyright (C) 2011 Modelon AB
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.Collections;
+import java.util.Comparator;
+
+aspect ExportFunctions {
+	
+	// TODO: handle general case for outputs (currently only single scalar real/integer)
+	
+	/**
+	 * Check if function should generate an export wrapper. 
+	 */
+	syn boolean FFunctionDecl.hasExportWrapper_C() = 
+		myOutputs().size() == 1 && myOutputs().get(0).type().isNumericScalar();
+	
+	/**
+	 * The return type of the export wrapper of this function.
+	 */
+	syn String FFunctionDecl.exportWrapperType_C() = 
+		exportWrapperReturnsValue_C() ? myOutputs().get(0).exportWrapperType_C() : "void";
+	
+	/**
+	 * Check if the export wrapper for the function should have a return type. 
+	 */
+	syn boolean FFunctionDecl.exportWrapperReturnsValue_C() = 
+		myOutputs().size() == 1 && myOutputs().get(0).type().isScalar();
+	
+	/**
+	 * The type of this variable in an export wrapper function.
+	 */
+	syn String FFunctionVariable.exportWrapperType_C() = type().exportWrapperType_C();
+	
+	/**
+	 * The name of the export wrapper of this function.
+	 */
+	syn String FFunctionDecl.funcNameExportWrapper() = 
+		getFQName().funcNameUnderscore(C_SUFFIX_EXPORT);
+	
+	// TODO: handle more types
+	syn String FType.exportWrapperType_C() = "void";
+	eq FRealType.exportWrapperType_C()     = "double";
+	eq FIntegerType.exportWrapperType_C()  = "int";
+
+	public static final String ASTNode.C_SUFFIX_EXPORT = "export";
+
+	public void FFunctionDecl.exportWrapper_C(PrintStream str, String indent) {
+		// TODO: handle multiple outputs/array output
+
+		// Function header
+		str.format("%sDllExport %s %s(", indent, exportWrapperType_C(), funcNameExportWrapper());
+		exportWrapperArgumentDecl_C(str);
+		str.print(") {\n");
+		
+		// Function body
+		// TODO: add conversions from/to our internal representations, where needed
+		String bodyInd = printer_C.indent(indent);
+		FFunctionVariable out = myOutputs().get(0);
+		// - Declare return var
+		str.print(bodyInd);
+		out.exportWrapperDecl_C(str);
+		str.print(";\n");
+		// - Declare temporary vars
+		for (FFunctionVariable var : myInputs()) 
+			var.exportWrapperDeclareTemp_C(str, bodyInd);
+		// - Initialize temporary vars
+		for (FFunctionVariable var : myInputs()) 
+			var.exportWrapperInitTemp_C(str, bodyInd);
+		// - Call function
+		str.format("%s%s(", bodyInd, getFQName().funcNameUnderscore(C_SUFFIX_DEF));
+		for (FFunctionVariable var : myInputs()) 
+			str.format("%s, ", var.name_C());
+		str.format("&%s);\n", out.name_C());
+		// - Return value
+		str.format("%sreturn %s;\n", bodyInd, out.name_C());
+		
+		str.format("%s}\n\n", indent);
+	}
+
+	public void FFunctionDecl.exportWrapperArgumentDecl_C(PrintStream str) {
+		// TODO: handle multiple outputs/array output
+		String sep = "";
+		for (FFunctionVariable var : myInputs()) {
+			str.print(sep);
+			var.exportWrapperDecl_C(str);
+			sep = ", ";
+		}
+	}
+
+	public void FFunctionDecl.exportWrapperArgumentTypeDecl_C(PrintStream str) {
+		// TODO: handle multiple outputs/array output
+		String sep = "";
+		for (FFunctionVariable var : myInputs()) {
+			str.print(sep);
+			var.exportWrapperTypeDecl_C(str);
+			sep = ", ";
+		}
+	}
+
+	public void FFunctionDecl.exportWrapperArgumentCall_C(PrintStream str) {
+		// TODO: handle multiple outputs/array output
+		String sep = "";
+		for (FFunctionVariable var : myInputs()) {
+			str.print(sep);
+			var.exportWrapperCall_C(str);
+			sep = ", ";
+		}
+	}
+	
+	public void FFunctionVariable.exportWrapperDecl_C(PrintStream str) {
+		str.format("%s %s", exportWrapperType_C(), name_C());
+	}
+	
+	public void FFunctionVariable.exportWrapperTypeDecl_C(PrintStream str) {
+		str.print(exportWrapperType_C());
+	}
+	
+	public void FFunctionVariable.exportWrapperCall_C(PrintStream str) {
+		str.print(name_C());
+	}
+	
+	public void FFunctionArray.exportWrapperDecl_C(PrintStream str) {
+		String name = name_C();
+		str.format("%s* %sp", exportWrapperType_C(), name);
+		for (int i = 0; i < ndims(); i++) 
+			str.format(", int %s%d", name, i);
+	}
+	
+	public void FFunctionArray.exportWrapperTypeDecl_C(PrintStream str) {
+		str.format("%s*", exportWrapperType_C());
+		for (int i = 0; i < ndims(); i++) 
+			str.print(", int");
+	}
+	
+	public void FFunctionArray.exportWrapperCall_C(PrintStream str) {
+		String name = name_C();
+		str.format("%sp", name);
+		for (int i = 0; i < ndims(); i++) 
+			str.format(", %s%d", name, i);
+	}
+	
+	public void FFunctionVariable.exportWrapperDeclareTemp_C(PrintStream str, String ind) {
+	}
+	
+	public void FFunctionVariable.exportWrapperInitTemp_C(PrintStream str, String ind) {
+	}
+	
+	public void FFunctionArray.exportWrapperDeclareTemp_C(PrintStream str, String ind) {
+		// NB: makes assumptions about definitions in jmi_array_none.h
+		String name = name_C();
+		str.format("%sint %s_size[%d];\n", ind, name, ndims());
+		str.format("%sjmi_array_t %s_obj;\n", ind, name);
+		str.format("%sjmi_array_t* %s = &%s_obj;\n", ind, name, name);
+	}
+	
+	public void FFunctionArray.exportWrapperInitTemp_C(PrintStream str, String ind) {
+		// NB: makes assumptions about definitions in jmi_array_none.h
+		String name = name_C();
+		str.format("%s%s_obj.var = %sp;\n", ind, name, name);
+		str.format("%s%s_obj.size = %s_size;\n", ind, name, name);
+		str.format("%s%s_obj.num_elems = 1", ind, name);
+		for (int i = 0; i < ndims(); i++)
+			str.format(" * %s%d", name, i);
+		str.print(";\n");
+		for (int i = 0; i < ndims(); i++)
+			str.format("%s%s_size[%d] = %s%d;\n", ind, name, i, name, i);
+	}
+	
+	
+	syn lazy ArrayList<ArrayList<FFunctionDecl>> FClass.exportWrapperGroups() {
+		Map<String,ArrayList<FFunctionDecl>> map = new LinkedHashMap<String,ArrayList<FFunctionDecl>>();
+		for (FFunctionDecl f : getFFunctionDecls()) {
+			if (f.hasExportWrapper_C()) {
+				String key = f.signatureString();
+				ArrayList<FFunctionDecl> list = map.get(key);
+				if (list == null) 
+					map.put(key, list = new ArrayList<FFunctionDecl>());
+				list.add(f);
+			}
+		}
+		
+		ArrayList<ArrayList<FFunctionDecl>> res = new ArrayList<ArrayList<FFunctionDecl>>();
+		for (ArrayList<FFunctionDecl> list : map.values()) {
+			Collections.sort(list, FFunctionDecl.UNAME_COMPARATOR);
+			res.add(list);
+		}
+		return res;
+	}
+	
+	public static final Comparator<FFunctionDecl> FFunctionDecl.UNAME_COMPARATOR = 
+		new Comparator<FFunctionDecl>() {
+		 public int compare(FFunctionDecl f1, FFunctionDecl f2) {
+			 return f1.getFQName().nameUnderscore().compareTo(f2.getFQName().nameUnderscore());
+		 }
+		 
+		 public boolean equals(Object obj) {
+			 return obj == this;
+		 }
+	};
+	
+}

trunk/Compiler/ModelicaCBackEnd/src/jastadd/codegen/CGenerator.jrag
@@ -699,6 +699,85 @@ public class CGenerator extends GenericGenerator {
 				rec.prettyPrint_C(genPrinter, "");
 		}
 	}
+	
+	/**
+	 * Generates wrappers for Modelica functions for exporting in a shared library.
+	 */
+	class DAETag_C_export_functions extends DAETag {
+		
+		public DAETag_C_export_functions(AbstractGenerator myGenerator, FClass fclass) {
+			super("C_export_functions","C: C functions wrapping internal representation of Modelica functions",
+			  myGenerator,fclass);
+		}
+
+		public void generate(PrintStream out) {
+			for (FFunctionDecl func : fclass.getFFunctionDecls())
+				if (func.hasExportWrapper_C())
+					func.exportWrapper_C(out, "");
+		}
+	}
+	
+	/**
+	 * Generates export wrappers for groups of Modelica functions with the same signature.
+	 * 
+	 * Allows less functions to be imported from resulting shared library. 
+	 * Requires $C_export_functions$, and must be after it.
+	 */
+	class DAETag_C_export_wrappers extends DAETag {
+		
+		public DAETag_C_export_wrappers(AbstractGenerator myGenerator, FClass fclass) {
+			super("C_export_wrappers","C: C functions wrapping groups of export function with same signature",
+			  myGenerator,fclass);
+		}
+
+		public void generate(PrintStream out) {
+			int i = 0;
+			String ind = ASTNode.printer_C.indent("");
+			for (ArrayList<FFunctionDecl> grp : fclass.exportWrapperGroups()) {
+				FFunctionDecl first = grp.get(0);
+				String type = first.exportWrapperType_C();
+				String name = "select_" + (++i);
+				
+				out.format("char* %s_names[] = { ", name);
+				String fmt = "\"%s\"";
+				for (FFunctionDecl f : grp) {
+					out.format(fmt, f.getFQName().nameUnderscore());
+					fmt = ", \"%s\"";
+				}
+				out.print(" };\n");
+				
+				out.format("int %s_lengths[] = { ", name);
+				fmt = "%d";
+				for (FFunctionDecl f : grp) {
+					out.format(fmt, f.getFQName().nameUnderscore().length());
+					fmt = ", %d";
+				}
+				out.print(" };\n");
+				
+				out.format("%s (*%s_funcs[])(", type, name);
+				first.exportWrapperArgumentTypeDecl_C(out);
+				out.print(") = { ");
+				fmt = "*%s";
+				for (FFunctionDecl f : grp) {
+					out.format(fmt, f.funcNameExportWrapper());
+					fmt = ", *%s";
+				}
+				out.print(" };\n");
+				
+				out.format("DllExport %s __stdcall %s(char* name, ", type, name);
+				first.exportWrapperArgumentDecl_C(out);
+				out.print(") {\n");
+				out.format("%sint i, j;\n", ind);
+				out.format("%sfor (i = 0, j = 0; name[i] != 0; i++) \n", ind);
+				out.format("%s%swhile (j < %d && i <= %s_lengths[j] && name[i] > %s_names[j][i]) j++;\n", ind, ind, grp.size(), name, name);
+				out.format("%sif (j >= %d || strcmp(%s_names[j], name)) return 0;\n", ind, grp.size(), name);
+				out.format("%sreturn %s_funcs[j](", ind, name);
+				first.exportWrapperArgumentCall_C(out);
+				out.print(");\n");
+				out.print("}\n\n");
+			}
+		}
+	}
 
 	/**
 	 * Generates MODEL_IDENTIFIER.

trunk/Compiler/ModelicaCBackEnd/src/test/modelica/CCodeGenTests.mo
@@ -5102,4 +5102,20 @@ void func_CCodeGenTests_externalArrayFortran6_f_def(jmi_array_t* a_a, jmi_array_
 		
 end externalArrayFortran6;
 
+
+model Smooth1
+ annotation(JModelica(unitTesting = JModelica.UnitTesting(testCase={
+     JModelica.UnitTesting.CCodeGenTestCase(
+         name="Smooth1",
+         description="",
+         template="$C_DAE_equation_residuals$",
+         generatedCode="
+    (*res)[0] = _time - ( 2 ) - (_y_0);
+    (*res)[1] = (COND_EXP_EQ(_sw(0),JMI_TRUE,AD_WRAP_LITERAL(0),(1.0 * (_y_0) * (_y_0) * (_y_0)))) - (_x_1);
+")})));
+
+  Real y = time - 2;
+  Real x = smooth(2, if y < 0 then 0 else y ^ 3);
+end Smooth1;
+
 end CCodeGenTests;

commit 696aa19d6b501be919f846631f2d8bdbd8301891

* Fixed bug introduced by r2715, where der() was ignored while generation XML code. (#1519)  * Fixed tests that had not been run due to erroneous compiler options. (#1538)
git-svn-id: https://svn.jmodelica.org@2748 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/jastadd/PrettyPrint.jrag
@@ -325,6 +325,12 @@ aspect PrettyPrint {
 		str.print(indent + "end " + getName().getID());
 	} 
 
+	// For debugging
+	public void LibNode.prettyPrint(Printer p, PrintStream str, String indent) {
+		str.format("%s%structured library '%s' at '%s'\n", indent, 
+				(getStructured() ? "S" : "Uns"), getName(), getFileName());
+	}
+
 	public void ComponentDecl.prettyPrint(Printer p, PrintStream str, String indent) {
 		str.print(indent);//+getVisibilityType().toString()+" ";
 		

trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/OptionRegistry.java
@@ -42,15 +42,15 @@ public class OptionRegistry {
 	
 		private enum DefOpt {
 			DIVIDE_BY_PARS_IN_TEARING  ("divide_by_vars_in_tearing", false, 
-			"If this option is set to true (default is false), a less restrictive strategy is used for solving equations " +
-			"in the tearing algorithm. Specifically, division by parameters and variables is permitted, by default no" +
-			"such divisions are made during tearing."),
+					"If this option is set to true (default is false), a less restrictive strategy is used for solving equations " +
+					"in the tearing algorithm. Specifically, division by parameters and variables is permitted, by default no" +
+					"such divisions are made during tearing."),
 			ENABLE_TEARING   ("enable_tearing", false, 
 					"If this option is set to true (default is false), tearing of equation systems is enabled."),
 			ADVANCED_TEARING   ("advanced_tearing", false, 
-							"If this option is set to true (default is false), an more advanced tearing algorithm" +
-							"is used. This algorithm may give smaller torn systems, but may also take longer time" +
-							"to execute. This option only has an effect if the option ENABLE_TEARING is set to true."),
+					"If this option is set to true (default is false), an more advanced tearing algorithm" +
+					"is used. This algorithm may give smaller torn systems, but may also take longer time" +
+					"to execute. This option only has an effect if the option ENABLE_TEARING is set to true."),
 			CONV_FREE_DEP_PAR_TO_ALGS   ("convert_free_dependent_parameters_to_algebraics", true, 
 					"If this option is set to true (default is true), free dependent parameters are" +
 					"converted to algebraic variables."),

trunk/Compiler/ModelicaXMLBackEnd/src/jastadd/XMLCodeGen.jrag
@@ -192,6 +192,12 @@ aspect XMLCodeGen {
 		p.print(getFIdUse(), str, p.indent(indent));
 	}
 	
+	public void FDerExp.prettyPrint_XML(Printer p, PrintStream str, String indent) {	
+		open_XML(str, indent);
+		p.print(getFIdUse(), str, p.indent(indent));
+		close_XML(str, indent);
+	}
+	
 	
 	public void FIdUse.prettyPrint_XML(Printer p, PrintStream str, String indent) {
 		open_XML(str, indent);

trunk/Compiler/OptimicaCompiler/src/jastadd/OptimicaCompiler.jrag
@@ -152,6 +152,15 @@ public class OptimicaCompiler extends ModelicaCompiler {
 			return new CADGenerator(new PrettyPrinter(), '$', fc);
 		}
 	}
+	
+	/**
+	 * Set the Optimica c template file path attribute.
+	 * 
+	 * @param template The new c template file path.
+	 */
+	public void setOptimicaCTemplate(String template) {
+		optimicaCTemplatefile = template;
+	}
 
 	/**
 	 * \brief get the C template. 
@@ -162,9 +171,9 @@ public class OptimicaCompiler extends ModelicaCompiler {
 	 */
 	protected String getCTemplatefile(FClass fc) {
 		if (fc instanceof FOptClass) {
-			return this.optimicaCTemplatefile;
+			return optimicaCTemplatefile;
 		} else {
-			return this.cTemplatefile;
+			return cTemplatefile;
 		}
 	}
 
@@ -174,7 +183,7 @@ public class OptimicaCompiler extends ModelicaCompiler {
 		super.setArguments(compilerName, args, programarguments);
 		int arg = programarguments.size();
 		if (args.length >= arg+6) 
-			this.optimicaCTemplatefile = args[arg+5];
+			optimicaCTemplatefile = args[arg+5];
 	}
 	
 	public static void main(String args[]) {		

trunk/Compiler/ModelicaXMLBackEnd/src/test/modelica/XMLCodeGenTests.mo
@@ -714,65 +714,103 @@ end XMLCodeGenTest2;
          name="DirectDependencyTest1",
          description="",
          equation_sorting=true,
-         elimiate_alias_variables=false,
+         eliminate_alias_variables=false,
          template="$XML_variables$",
          generatedCode="
 		<ScalarVariable name=\"u[1]\" valueReference=\"6\" variability=\"continuous\" causality=\"input\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"u[2]\" valueReference=\"7\" variability=\"continuous\" causality=\"input\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"u[3]\" valueReference=\"8\" variability=\"continuous\" causality=\"input\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"u[4]\" valueReference=\"9\" variability=\"continuous\" causality=\"input\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"x[1]\" valueReference=\"10\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"x[2]\" valueReference=\"11\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"x[3]\" valueReference=\"12\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"x[4]\" valueReference=\"13\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"x[5]\" valueReference=\"14\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"x[6]\" valueReference=\"15\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"x[7]\" valueReference=\"16\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"x[8]\" valueReference=\"17\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"x[9]\" valueReference=\"18\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"x[10]\" valueReference=\"19\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"x[11]\" valueReference=\"20\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"x[12]\" valueReference=\"21\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"x[13]\" valueReference=\"22\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"x[14]\" valueReference=\"23\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"x[15]\" valueReference=\"24\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"y1\" valueReference=\"25\" variability=\"continuous\" causality=\"output\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
@@ -781,32 +819,49 @@ end XMLCodeGenTest2;
 				<Name>u[2]</Name>
 				<Name>u[3]</Name>
 			</DirectDependency>
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"y2\" valueReference=\"26\" variability=\"continuous\" causality=\"output\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
 			<DirectDependency>
 				<Name>u[4]</Name>
 			</DirectDependency>
+			<isLinear>true</isLinear>
+			<VariableCategory>algebraic</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"z[1]\" valueReference=\"3\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>state</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"der(z[1])\" valueReference=\"0\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>derivative</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"z[2]\" valueReference=\"4\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>state</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"der(z[2])\" valueReference=\"1\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>derivative</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"z[3]\" valueReference=\"5\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>state</VariableCategory>
 		</ScalarVariable>
 		<ScalarVariable name=\"der(z[3])\" valueReference=\"2\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
 			<Real relativeQuantity=\"false\" />
+			<isLinear>true</isLinear>
+			<VariableCategory>derivative</VariableCategory>
 		</ScalarVariable>
 ")})));
+
   Real x[15];
   Real z[3];
   input Real u[4];

trunk/Compiler/OptimicaFrontEnd/src/test/modelica/OptimicaTests.mo
@@ -242,9 +242,10 @@ end OptimicaTests.ClassAttrTest8;
 
       annotation(JModelica(unitTesting = JModelica.UnitTesting(testCase={
       JModelica.UnitTesting.FClassMethodTestCase(name="FreeDependentParametersTest1",
-      methodName="freeParametersDiagnostics",
-      convert_free_dependent_variables_to_algebraics = false,
-        description="Test that free dependent parameters are computed correctly.", methodResult=
+        methodName="freeParametersDiagnostics",
+        convert_free_dependent_parameters_to_algebraics = false,
+        description="Test that free dependent parameters are computed correctly.", 
+        methodResult=
         "  
 Free independent parameters:
 p1
@@ -266,9 +267,10 @@ p3
 
   	     annotation(JModelica(unitTesting = JModelica.UnitTesting(testCase={
       JModelica.UnitTesting.FClassMethodTestCase(name="FreeDependentParametersTest2",
-      methodName="freeParametersDiagnostics",
-      convert_free_dependent_variables_to_algebraics = false,
-        description="Test that free dependent parameters are computed correctly.", methodResult=
+        methodName="freeParametersDiagnostics",
+        convert_free_dependent_parameters_to_algebraics = false,
+        description="Test that free dependent parameters are computed correctly.", 
+        methodResult=
         "  
 Free independent parameters:
 p1
@@ -293,9 +295,10 @@ p3
 
   	     annotation(JModelica(unitTesting = JModelica.UnitTesting(testCase={
       JModelica.UnitTesting.FClassMethodTestCase(name="FreeDependentParametersTest3",
-      methodName="freeParametersDiagnostics",
-      convert_free_dependent_variables_to_algebraics = false,
-        description="Test that free dependent parameters are computed correctly.", methodResult=
+        methodName="freeParametersDiagnostics",
+        convert_free_dependent_parameters_to_algebraics = false,
+        description="Test that free dependent parameters are computed correctly.", 
+        methodResult=
         "  
 Free independent parameters:
 p1
@@ -318,9 +321,10 @@ p3
 
   	     annotation(JModelica(unitTesting = JModelica.UnitTesting(testCase={
       JModelica.UnitTesting.FClassMethodTestCase(name="FreeDependentParametersTest4",
-      methodName="freeParametersDiagnostics",
-      convert_free_dependent_variables_to_algebraics = false,
-        description="Test that free dependent parameters are computed correctly.", methodResult=
+        methodName="freeParametersDiagnostics",
+        convert_free_dependent_parameters_to_algebraics = false,
+        description="Test that free dependent parameters are computed correctly.", 
+        methodResult=
         "  
 Free independent parameters:
 p1
@@ -344,7 +348,7 @@ p3
   	     annotation(JModelica(unitTesting = JModelica.UnitTesting(testCase={
       JModelica.UnitTesting.FClassMethodTestCase(name="FreeDependentParametersTest5",
         methodName="variableDiagnostics",
-      convert_free_dependent_variables_to_algebraics = false,
+        convert_free_dependent_parameters_to_algebraics = false,
         description="Test linearity of free dependent parameters.", 
 		methodResult="  
 Independent constants: 

trunk/Compiler/OptimicaFrontEnd/src/test/modelica/OptimicaTransformCanonicalTests.mo
@@ -695,9 +695,10 @@ optimization DepParTest4 (objective=1,startTime=0,finalTime=1)
 
   	     annotation(JModelica(unitTesting = JModelica.UnitTesting(testCase={
       JModelica.UnitTesting.FClassMethodTestCase(name="DepParTest4",
-      convert_free_dependent_variables_to_algebraics = false,
-      methodName="freeParametersDiagnostics",
-        description="Test that free dependent parameters are handled correctly.", methodResult=
+        convert_free_dependent_parameters_to_algebraics = false,
+        methodName="freeParametersDiagnostics",
+        description="Test that free dependent parameters are handled correctly.", 
+        methodResult=
         "
 Free independent parameters:
 p2