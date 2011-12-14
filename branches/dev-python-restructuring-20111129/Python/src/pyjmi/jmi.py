bab5518d2419e1e972c508c11e4645bc5e233156
tove <tove@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2011-12-14 08:35:47 +0000

    #1674: Moving fmi.compile_fmu, fmi.compile_fmux and jmi.compile_jmu to compiler.py.
    git-svn-id: https://svn.jmodelica.org@2943 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev-python-restructuring-20111129/Python/src/pyjmi/jmi.py
+++ branches/dev-python-restructuring-20111129/Python/src/pyjmi/jmi.py
@@ -38,7 +38,6 @@ from lxml import etree
 
 from pyjmi.common import xmlparser
 from pyjmi.common.core import BaseModel, unzip_unit, get_unit_name, get_platform_suffix, get_files_in_archive, rename_to_tmp
-from jmodelica.compiler import _get_compiler
 
 from pyjmi.common.io import VariableNotFoundError
 from pyjmi.common.core import TrajectoryLinearInterpolation
@@ -6218,119 +6217,6 @@ class JMIModel(object):
             raise JMIException("Computing the number of columns and \
             non-zero elements failed.")
         return int(dHineq_n_cols.value), int(dHineq_n_nz.value)
-
-def get_jmu_name(class_name):
-    """
-    Computes the JMU name from a class name.
-    
-    Parameters::
-        
-        class_name -- 
-            The name of the model.
-        
-    Returns::
-    
-        The JMU name (replaced dots with underscores).
-    """
-    return get_unit_name(class_name, unit_type='JMU')
-        
-def compile_jmu(class_name, file_name=[], compiler='auto', target='ipopt', 
-    compiler_options={}, compile_to='.', compiler_log_level='warning'):
-    """ 
-    Compile a Modelica or Optimica model to a JMU.
-    
-    A model class name must be passed, all other arguments have default values. 
-    The different scenarios are:
-    
-    * Only class_name is passed: 
-        - Default compiler is ModelicaCompiler.
-        - Class is assumed to be in MODELICAPATH.
-    
-    * class_name and file_name is passed:
-        - file_name can be a single file as a string or a list of file_names 
-          (strings).
-        - Default compiler setting is 'auto' which means that the appropriate 
-          compiler will be selected based on model file ending, i.e. 
-          ModelicaCompiler if .mo file and OptimicaCompiler if a .mop file is 
-          found in file_name list.
-    
-    Library directories can be added to MODELICAPATH by listing them in a 
-    special compiler option 'extra_lib_dirs', for example:
-    
-        compiler_options = 
-            {'extra_lib_dirs':['c:\MyLibs\MyLib1','c:\MyLibs\MyLib2']}
-        
-    Other options for the compiler should also be listed in the compiler_options 
-    dict.
-    
-    The compiler target is 'ipopt' by default which means that libraries for AD 
-    and optimization/initialization algortihms will be available as well as the 
-    JMI. The other targets are:
-    
-        'model' -- 
-            AD and JMI is included.
-        'algorithm' -- 
-            AD and algorithm but no Ipopt linking.
-        'model_noad' -- 
-            Only JMI, that is no AD interface. (Must currently be used when 
-            model includes external functions.)
-    
-    Parameters::
-    
-        class_name -- 
-            The name of the model class.
-            
-        file_name -- 
-            Model file (string) or files (list of strings), can be both .mo or 
-            .mop files.
-            Default: Empty list.
-            
-        compiler -- 
-            'auto' if a compiler should be selected automatically depending on 
-            file ending, 'modelica' if a ModelicaCompiler should be used or 
-            'optimica' if a OptimicaCompiler should be used.
-            Default: 'auto' (i.e. depends on argument file_name)
-            
-        target --
-            Compiler target. 'model', 'algorithm', 'ipopt' or 'model_noad'.
-            Default: 'ipopt'
-            
-        compiler_options --
-            Options for the compiler.
-            Default: Empty dict.
-            
-        compile_to --
-            Specify location of the compiled JMU. Directory will be created if 
-            it does not exist.
-            Default: Current directory.
-            
-        compiler_log_level --
-            Set the log level for the compiler. Valid options are 'warning'/'w', 
-            'error'/'e' or 'info'/'i'.
-            Default: 'warning'
-                
-            
-    Returns::
-    
-        Name of the JMU which has been created.
-    """
-    if isinstance(file_name, basestring):
-        file_name = [file_name]
-        
-    # get a compiler based on 'compiler' argument or files listed in file_name
-    comp = _get_compiler(files=file_name, selected_compiler=compiler)
-    
-    # set compiler options
-    comp.set_options(compiler_options)
-    
-    # set log level
-    comp.set_compiler_log_level(compiler_log_level)
-            
-    # compile jmu in Java
-    comp.compile_JMU(class_name, file_name, target, compile_to)
-    
-    return os.path.join(compile_to, get_jmu_name(class_name))
-
 
 def unzip_jmu(archive, path='.'):
     """
