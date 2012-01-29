aa54b1121d07ffe059144c15b51115bacbab1f2f
jakesson <jakesson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2012-01-29 20:47:45 +0000

    Creating 1.7.x branch.
    git-svn-id: https://svn.jmodelica.org@3063 7ae51c65-13d5-4921-9d80-6511ddd1419c

---
+++ branches/1.7.x/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/AbstractFlatModelicaScanner.java
@@ -0,0 +1,10 @@
+package org.jmodelica.util;
+
+import java.io.IOException;
+import java.util.Map;
+
+public abstract class AbstractFlatModelicaScanner {
+
+	public abstract String nextToken() throws IOException, RuntimeException;
+
+}
