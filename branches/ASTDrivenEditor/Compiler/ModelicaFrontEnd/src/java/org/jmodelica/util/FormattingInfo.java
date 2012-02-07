64a0025e4383a36bfcc018116233095db5ed1278
tmattsson <tmattsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2012-02-07 14:24:44 +0000

    Added a very early, tiny version of a pretty printer that uses the formatting information put into the AST (#1778). Also fixed some documentation on what was committed through r3105 and fixed an error in the Modelica.ast, where the within node has an optional ParseAccess. This should be an Access, which it now is.
    git-svn-id: https://svn.jmodelica.org@3109 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/ASTDrivenEditor/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/FormattingInfo.java
+++ branches/ASTDrivenEditor/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/FormattingInfo.java
@@ -23,7 +23,7 @@ public class FormattingInfo {
 	 * Adds formatting item with information about what type of formatting this is and where it is positioned in
 	 * the source code.
 	 * @param type the type of formatting item this is, should be one of <code>FormattingItem.TYPE_*.</code>
-	 * @param data the string data of this item, eg. actual whitespaces or comment.
+	 * @param data the string data of this item, for example actual whitespaces or comment.
 	 * @param startLine the line at which this formatting item starts.
 	 * @param startColumn the column at which this formatting item starts.
 	 * @param endLine the line at which this formatting item ends.
