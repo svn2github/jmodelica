a3a223631169bf0f1aa9a1b9d7538108f760a25d
tmattsson <tmattsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2012-05-30 07:35:29 +0000

    External Modelica files (files that are outside of all projects in the workspace) are now viewable in the AST-driven editor. They are, however, text-driven. (#1987)
    git-svn-id: https://svn.jmodelica.org@3447 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/ASTDrivenEditorContinuation/EclipseIDE/org.jmodelica.ide/java/org/jmodelica/ide/editor/ASTDocumentProvider.java
+++ branches/ASTDrivenEditorContinuation/EclipseIDE/org.jmodelica.ide/java/org/jmodelica/ide/editor/ASTDocumentProvider.java
@@ -1,8 +1,13 @@
 package org.jmodelica.ide.editor;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IURIEditorInput;
 import org.eclipse.ui.editors.text.FileDocumentProvider;
 
 public class ASTDocumentProvider extends FileDocumentProvider {
@@ -10,6 +15,16 @@ public class ASTDocumentProvider extends FileDocumentProvider {
 	protected IDocument createEmptyDocument() {
 		return new ASTDocument();
 	}
+	
+	protected IDocument createTextFileDocument() {
+		return new Document(); 
+	}
+	
+//	@Override
+//	public boolean isModifiable(Object element) {
+//		boolean modifiable = super.isModifiable(element);
+//		return (modifiable ? true : (element instanceof IURIEditorInput));
+//	}
 
 	@Override
 	protected IDocument createDocument(Object element) throws CoreException {
@@ -20,6 +35,26 @@ public class ASTDocumentProvider extends FileDocumentProvider {
 				setupDocument(element, document);
 
 				return document;
+			} else if (element instanceof IURIEditorInput) {
+				document = createTextFileDocument();
+				IURIEditorInput uriEditorInput = (IURIEditorInput) element;
+				InputStream inputStream = null;
+				try {
+					inputStream = uriEditorInput.getURI().toURL().openStream();
+					setDocumentContent(document, inputStream, getEncoding(element));
+					setupDocument(element, document);
+					return document;
+				} catch (IOException ioException) {
+					System.err.println("Unable to read file.");
+				} finally {
+					try {
+						if (inputStream != null) {
+							inputStream.close();
+						}
+					} catch (IOException ioException) {
+						System.err.println("Unable to close file.");
+					}
+				}
 			}
 		}
 
