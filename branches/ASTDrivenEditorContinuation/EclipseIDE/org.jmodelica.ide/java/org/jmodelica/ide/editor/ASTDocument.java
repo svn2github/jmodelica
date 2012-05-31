87873d5e37fab18d235fcf350bd59d4822b90e68
tmattsson <tmattsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2012-05-31 11:56:23 +0000

    A naive and early implementation that sometimes makes it possible to add white space into a document in the AST-driven editor is provided with this commit. (#2000)
    git-svn-id: https://svn.jmodelica.org@3467 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/ASTDrivenEditorContinuation/EclipseIDE/org.jmodelica.ide/java/org/jmodelica/ide/editor/ASTDocument.java
+++ branches/ASTDrivenEditorContinuation/EclipseIDE/org.jmodelica.ide/java/org/jmodelica/ide/editor/ASTDocument.java
@@ -3,8 +3,12 @@ package org.jmodelica.ide.editor;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.BadPositionCategoryException;
 import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.DocumentEvent;
 import org.eclipse.jface.text.Position;
 import org.jmodelica.modelica.compiler.ASTNode;
+import org.jmodelica.modelica.compiler.BaseNode;
+import org.jmodelica.util.FormattingItem;
+import org.jmodelica.util.ScannedFormattingItem;
 
 
 public class ASTDocument extends Document {
@@ -25,14 +29,14 @@ public class ASTDocument extends Document {
 		printLog("Constructor for ASTDocument called with argument [initialContent = \"" + initialContent + "\"].");
 	}
 	
-	void printLog(String string, boolean force) {
-		if (verbose || force) {
+	void printLog(String string, boolean doPrint) {
+		if (doPrint) {
 			System.out.println(string);
 		}
 	}
 	
 	void printLog(String string) {
-		printLog(string, false);
+		printLog(string, verbose);
 	}
 
 	@Override
@@ -46,6 +50,18 @@ public class ASTDocument extends Document {
 	public void addPosition(Position position) throws BadLocationException {
 		super.addPosition(position);
 		printLog("addPosition([position = " + position + "])");
+	}
+
+	@Override
+	public void replace(int pos, int length, String text, long modificationStamp) throws BadLocationException {
+		super.replace(pos, length, text, modificationStamp);
+		printLog("replace([pos = " + pos + "], [length = " + length + "], [text = " + text + "], [modificationStamp = " + modificationStamp + "])");
+	}
+
+	@Override
+	public void replace(int pos, int length, String text) throws BadLocationException {
+		super.replace(pos, length, text);
+		printLog("replace([pos = " + pos + "], [length = " + length + "], [text = " + text + "])");
 	}
 
 	@Override
@@ -75,6 +91,59 @@ public class ASTDocument extends Document {
 	}
 
 	@Override
+	protected void fireDocumentChanged(DocumentEvent event) {
+		super.fireDocumentChanged(event);
+		printLog("fireDocumentChanged([event = " + event + "])");
+	}
+
+	@Override
+	protected void fireDocumentAboutToBeChanged(DocumentEvent event) {
+		super.fireDocumentAboutToBeChanged(event);		
+		if (event.getText().matches("[ \t\f]*")) {
+			printLog("Adding formatting...", true);
+			try {
+				int startLine = getLineOfOffset(event.getOffset()) + 1;
+				int startColumn = event.getOffset() - getLineInformationOfOffset(event.getOffset()).getOffset() + 1;
+				int endLine = getLineOfOffset(event.getOffset() + event.getLength()) + 1;
+				int endColumn = (event.getOffset() + event.getLength()) - getLineInformationOfOffset(event.getOffset() + event.getLength()).getOffset();
+				BaseNode match = ast.matchSourcePosition(null, startLine, startColumn, endLine, endColumn);
+				boolean success = match.insertMoreFormatting(new ScannedFormattingItem(FormattingItem.Type.NON_BREAKING_WHITESPACE, event.getText(),
+						startLine, startColumn, endLine, endColumn));
+				if (success) {
+					printLog("Done!", true);
+				} else {
+					printLog("Nowhere to place it!", true);
+				}
+			} catch (BadLocationException badLocationException) {
+				System.err.println(badLocationException.getMessage());
+			}
+		}
+		
+		
+//		ParserHandler parserHandler = new ParserHandler();
+//		try {
+//			int startLine = getLineOfOffset(event.getOffset()) + 1;
+//			int startColumn = event.getOffset() - getLineInformationOfOffset(event.getOffset()).getOffset() + 1;
+//			int endLine = getLineOfOffset(event.getOffset() + event.getLength()) + 1;
+//			int endColumn = (event.getOffset() + event.getLength()) - getLineInformationOfOffset(event.getOffset() + event.getLength()).getOffset();
+//			Element element = parserHandler.parseElementString("    Real newReal;\n");
+//			element.propagateFormatting();
+//			element.changeOffset();
+//			ClassDecl classDecl = ast.getClassDeclAt(this, event.getOffset()).value();
+//			((FullClassDecl) classDecl).addNewComponentDecl((ComponentDecl) element);
+//		} catch (ParserException e) {
+//			e.printStackTrace();
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+		printLog("fireDocumentAboutToBeChanged([event = " + event + "])");
+	}
+
+	@Override
 	public String get(int pos, int length) throws BadLocationException {
 		if (ast == null || length == 0) {
 			return "";
@@ -87,25 +156,31 @@ public class ASTDocument extends Document {
 		String result = null;
 		if (length == getLength()) {
 			result = ast.prettyPrintFormatted();
-			printLog("[" + result + "] :: " + result.length() + " characters.");
-			printLog("Number of DefaultFormattingItems in the AST: " + ast.numberOfDefaultFormattingItems() + ".");
 		} else {
 			result = ast.printPart(startLine, startColumn, endLine, endColumn, length);
 		}
+//		return getFallback(result, pos, length, startLine, startColumn, endLine, endColumn);
+
+		return result;
+	}
+
+	@SuppressWarnings("unused")
+	private String getFallback(String result, int pos, int length, int startLine, int startColumn, int endLine, int endColumn) {
 		if (result == null) {
 			StringBuilder sb = new StringBuilder();
 			for (int i = 0; i < length; i++) {
 				sb.append((char) ('A' + (startColumn + i) % ('Z' - 'A' + 1)));
 			}
 			result = sb.toString();
-			printLog("get([pos = " + pos + ", length = " + length + "]): AST lookup failed.", true);
-			printLog("Middle result: [startLine = " + startLine + ", startColumn = " + startColumn + ", endLine = " + endLine + ", endColumn = " + endColumn + "]", true);
+			printLog("get([pos = " + pos + ", length = " + length + "]): AST lookup failed.");
+			printLog("Middle result: [startLine = " + startLine + ", startColumn = " + startColumn + ", endLine = " + endLine + ", endColumn = " + endColumn + "]");
 		} else if (result.length() != length) {
 			printLog("get([pos = " + pos + "], [length = " + length + "]) returns a string with illegal length. The length should be " + length + ", but is " + result.length() + ". -> [" + result + "]");
 			printLog("Middle result: [startLine = " + startLine + ", startColumn = " + startColumn + ", endLine = " + endLine + ", endColumn = " + endColumn + "]");
 		} else {
 			printLog("get([pos = " + pos + ", length = " + length + "]) -> [" + result + "]");
 		}
+
 		return result;
 	}
 
