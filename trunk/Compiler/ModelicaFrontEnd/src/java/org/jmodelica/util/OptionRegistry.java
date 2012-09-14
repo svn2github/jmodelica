f574820ba00a63981303f86ce43b8b2a02fcd08f
jsten <jsten@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2012-09-14 14:16:38 +0000

    Changed the default value for generate_mof_files to false #2240.
    git-svn-id: https://svn.jmodelica.org@3841 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/OptionRegistry.java
+++ trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/OptionRegistry.java
@@ -68,7 +68,7 @@ public class OptionRegistry {
 				"If blocks are needed to compute ODE jacobians they will be generated anyway"),
 		GEN_ODE   ("generate_ode",            false, 
 				"If this option is set to true (default is false), code for solving ODEs are generated. "),
-		GEN_MOF_FILES   ("generate_mof_files", true,
+		GEN_MOF_FILES   ("generate_mof_files", false,
 				"If this option is set to true (default is true), flat model before and after" +
 				" transformations will be generated."),
 		EXTRA_LIB   ("extra_lib_dirs",            "", 
