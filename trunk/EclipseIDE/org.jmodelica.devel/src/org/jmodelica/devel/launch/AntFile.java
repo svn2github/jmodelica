f8a9d4b8c7606678e4ecd643f711cd8f3c119b5c
jmattsson <jmattsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2013-03-18 11:21:35 +0000

    Imported alpha version of developer's plugin. (#2716)
    git-svn-id: https://svn.jmodelica.org@4571 7ae51c65-13d5-4921-9d80-6511ddd1419c

---
+++ trunk/EclipseIDE/org.jmodelica.devel/src/org/jmodelica/devel/launch/AntFile.java
@@ -0,0 +1,13 @@
+package org.jmodelica.devel.launch;
+
+public class AntFile {
+
+	public String path;
+	public String name;
+
+	public AntFile(String path, String name) {
+		this.path = path;
+		this.name = name;
+	}
+
+}
