bd30cc084f700a7e0e5dc81eea2d467678645453
jlindskog <jlindskog@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2013-05-13 07:06:09 +0000

    Fixed small bug introduced in commit 4775. File content changes didn't show correctly in Project Explorer.
    git-svn-id: https://svn.jmodelica.org@4784 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/ASTSyncPlayground/EclipseIDE/org.jmodelica.ide.outline/src/org/jmodelica/ide/outline/cache/tasks/ExplorerOutlineCacheFileChildrenTask.java
+++ branches/ASTSyncPlayground/EclipseIDE/org.jmodelica.ide.outline/src/org/jmodelica/ide/outline/cache/tasks/ExplorerOutlineCacheFileChildrenTask.java
@@ -10,8 +10,10 @@ import org.jmodelica.ide.helpers.OutlineCacheJob;
 import org.jmodelica.ide.outline.cache.AbstractOutlineCache;
 import org.jmodelica.ide.outline.cache.EventCachedFileChildren;
 import org.jmodelica.ide.sync.ASTNodeCacheFactory;
+import org.jmodelica.ide.sync.GlobalRootNode;
 import org.jmodelica.ide.sync.ModelicaASTRegistry;
 import org.jmodelica.modelica.compiler.FullClassDecl;
+import org.jmodelica.modelica.compiler.SourceRoot;
 import org.jmodelica.modelica.compiler.StoredDefinition;
 
 public class ExplorerOutlineCacheFileChildrenTask extends OutlineCacheJob {
@@ -26,15 +28,20 @@ public class ExplorerOutlineCacheFileChildrenTask extends OutlineCacheJob {
 		long time = System.currentTimeMillis();
 		ICachedOutlineNode toReturn = null;
 		ArrayList<ICachedOutlineNode> children = new ArrayList<ICachedOutlineNode>();
-		StoredDefinition def = ModelicaASTRegistry.getInstance().getLatestDef(
-				file);
-		synchronized (def.state()) {
-			toReturn = ASTNodeCacheFactory.cacheNode(def, file, cache);
-			for (Object obj : def.outlineChildren())
-				if (obj instanceof FullClassDecl) {
-					children.add(ASTNodeCacheFactory.cacheNode(
-							(FullClassDecl) obj, toReturn, cache));
+		SourceRoot root = ((GlobalRootNode) ModelicaASTRegistry.getInstance()
+				.doLookup(file.getProject())).getSourceRoot();
+		synchronized (root.state()) {
+			toReturn = ASTNodeCacheFactory.cacheNode(root, file, cache);
+			for (StoredDefinition def : root.getProgram()
+					.getUnstructuredEntitys()) {
+				if (def.getFile().equals(file)) {
+					for (Object obj : def.outlineChildren())
+						if (obj instanceof FullClassDecl)
+							children.add(ASTNodeCacheFactory.cacheNode(
+									(FullClassDecl) obj, toReturn, cache));
+					break;
 				}
+			}
 		}
 		toReturn.setOutlineChildren(children);
 		System.out.println("CacheFileChildren from ExplorerOutline took: "
