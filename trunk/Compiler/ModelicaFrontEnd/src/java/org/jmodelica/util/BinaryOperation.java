svn2github/jmodelica

commit 28c449b82533390d6140e9c5ce8ad5efb8febd89

* Fixed constant evaluation before scalarization of records without binding expression. (#2985)  * Fixed scalarization of parameter records without binding expression. (#2986 & #2987)
git-svn-id: https://svn.jmodelica.org@5056 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/jastadd/ConstantEvaluation.jrag
@@ -84,7 +84,7 @@ aspect ConstantEvaluation {
 	 * \brief CValue represents a constant value and serves as the super class 
 	 * for constant value classes of the primitive types.
 	 */
-	public abstract class CValue implements Iterable<CValue> {
+	public abstract class CValue implements Iterable<CValue>, Cloneable {
 		
 		/**
 		 * \brief An unknown value.
@@ -101,6 +101,17 @@ aspect ConstantEvaluation {
 		 * \brief Default constructor.
 		 */
 	    protected CValue() {
+	    }
+	    
+	    /**
+	     * Copy this constant value.
+	     */
+	    public CValue clone() {
+	    	try {
+				return (CValue) super.clone();
+			} catch (CloneNotSupportedException e) {
+				return null;
+			}
 	    }
 	    
 	    /**
@@ -235,7 +246,20 @@ aspect ConstantEvaluation {
          */
         public CValueArray array() {
         	throw new ConstantEvaluationException(this, "use this as array: ");
-        }      
+        }
+        
+        /**
+         * Expand this CValue so that it represents an array of the given size, 
+         * duplicating values as needed.
+         */
+        public CValue expandArray(Size s) {
+        	if (s == Size.SCALAR)
+        		return this;
+        	CValueArray res = new CValueArray(s);
+        	while (!res.isFilled())
+        		res.addCell(this);
+        	return res;
+        }
  	   
         /**
          * \brief Check if this is a record.
@@ -602,6 +626,14 @@ aspect ConstantEvaluation {
 			for (int i = 0; i < values.length; i++)
 				values[i] = UNKNOWN;
     	}
+	    
+	    public CValue clone() {
+	    	CValueArray res = (CValueArray) super.clone();
+	    	res.values = new CValue[values.length];
+	    	for (int i = 0; i < values.length; i++) 
+				res.values[i] = values[i].clone();
+	    	return res;
+	    }
     	
     	public boolean isArray() {
     		return true;
@@ -616,6 +648,23 @@ aspect ConstantEvaluation {
         	return this;
         }      
         
+        /**
+         * Expand this CValue so that it represents an array of the given size, 
+         * duplicating values as needed.
+         */
+        public CValue expandArray(Size s) {
+        	if (s.ndims() == indices.ndims())
+        		return this;
+        	if (s.ndims() < indices.ndims())
+        		throw new ConstantEvaluationException(this, "contract array to size " + s + ": "); 
+        	
+        	CValueArray res = new CValueArray(s);
+        	while (!res.isFilled())
+        		for (CValue v : values)
+        			res.addCell(v);
+        	return res;
+        }
+ 	   
         /**
          * \brief Set the value of a specific cell of an array constant value.
          */
@@ -648,6 +697,20 @@ aspect ConstantEvaluation {
         	if (!indices.isValid(i))
         		return UNKNOWN;
         	return values[i.internal(indices)];
+        }
+        
+        /**
+         * Get the part of this array corresponding to the given index.
+         */
+        public CValue getPart(Index i) {
+        	if (i == Index.NULL)
+        		return this;
+        	if (indices.ndims() == i.ndims())
+        		return getCell(i);
+        	
+        	CValueArray res = new CValueArray(size().contract(i.ndims(), 0));
+        	System.arraycopy(values, indices.internal(i), res.values, 0, res.values.length);
+        	return res;
         }
         
         /**
@@ -785,7 +848,7 @@ aspect ConstantEvaluation {
     public class CValueRecord extends CValue {
     	
     	protected String name;
-    	protected String[] members;
+    	protected Map<String,Integer> members;
     	protected CValue[] values;
     	
     	/**
@@ -796,12 +859,20 @@ aspect ConstantEvaluation {
     	public CValueRecord(FRecordType type) {
     		name = type.getName();
     		int n = type.getNumComponent();
-    		members = new String[n];
+    		members = new HashMap<String,Integer>(n * 4 / 3 + 1);
     		values = new CValue[n];
     		int i = 0;
     		for (FRecordComponentType mtype : type.getComponents())
-    			members[i++] = mtype.getName();
+    			members.put(mtype.getName(), i++);
     	}
+	    
+	    public CValueRecord clone() {
+	    	CValueRecord res = (CValueRecord) super.clone();
+	    	res.values = new CValue[values.length];
+	    	for (int i = 0; i < values.length; i++) 
+				res.values[i] = values[i].clone();
+	    	return res;
+	    }
   	   
         /**
          * \brief Check if this is a record.
@@ -823,11 +894,18 @@ aspect ConstantEvaluation {
          * \brief Get the internal index for a given member.
          */
         protected int index(String name) {
-        	int i = 0;
-        	for (; i < members.length && !members[i].equals(name); i++);
-        	if (i >= members.length)
+        	Integer i = members.get(name);
+         	if (i == null)
         		throw new ConstantEvaluationException(this, "find record member '" + name + "'");
-        	return i;
+        	return i.intValue();
+        }
+        
+        /**
+         * Get a map from member names to an index that can be used in the index 
+         * versions of methods.
+         */
+        public Map<String,Integer> members() {
+        	return members;
         }
         
         /**
@@ -851,8 +929,7 @@ aspect ConstantEvaluation {
          * \brief Get the value of a specific member of a record constant value.
          */
         public CValue getMember(String name) {
-        	CValue res = values[index(name)];
-        	return res == null ? UNKNOWN : res;
+        	return getMember(index(name));
         }
         
         /**
@@ -1579,9 +1656,9 @@ aspect ConstantEvaluation {
    *  @return The constant value.
    */
   syn CValue FAbstractVariable.ceval() = CValue.UNKNOWN;
-  eq FFunctionVariable.ceval()   = evaluationValue();
+  eq FFunctionVariable.ceval()         = evaluationValue();
   eq FVariable.ceval() {
-	  if (!isConstant() && !isParameter()) {
+	  if (!isConstant() && !isParameter() && !inRecord()) {
 		  return CValue.UNKNOWN;
 	  } else {
 		  CValue val;
@@ -1592,7 +1669,7 @@ aspect ConstantEvaluation {
 		  else if (hasBindingExp())
 			  val = getBindingExp().ceval();
 		  else
-			  val = startAttributeCValue();
+			  val = defaultCValue();
 		  if (isReal())
 			  val = val.convertReal();
 		  return val;
@@ -1608,23 +1685,27 @@ aspect ConstantEvaluation {
 	 * FVariable is not a constant or a parameter, or if it has no binding
 	 * expressions, then a CValueUnknown object is returned.
 	 * 
+	 * This version only evaluates a specific cell in an array, and returns 
+	 * the value for that specific cell.
+	 * 
 	 *  @return The constant value.
 	 */
 	syn CValue FAbstractVariable.ceval(Index i) = CValue.UNKNOWN;
-	eq FFunctionVariable.ceval(Index i)   = evaluationValue().array().getCell(i);
+	eq FFunctionVariable.ceval(Index i)         = evaluationValue().array().getCell(i);
 	eq FVariable.ceval(Index i) {
-		if (!isConstant() && !isParameter()) {
+		if (!isConstant() && !isParameter() && !inRecord()) {
 			return CValue.UNKNOWN;
 		} else {
 			CValue val;
 			if (isForIndex())
-				val = evaluationValue().array().getCell(i);
+				val = evaluationValue();
 			else if (hasParameterEquation())
 				val = parameterEquation().cevalParameter(this, i);
 			else if (hasBindingExp())
 				val = getBindingExp().ceval(i);
 			else
-				val = startAttributeCValue().array().getCell(i);
+				val = startAttributeCValue();
+			val = val.array().getCell(i);
 			if (isReal())
 				val = val.convertReal();
 			return val;
@@ -1685,6 +1766,70 @@ aspect ConstantEvaluation {
 			  return res;
 	  }
 	  return null;
+  }
+  
+  /**
+   * Constant eval for variables without any binding expression, or binding equation.
+   * Uses the start value for non-records.
+   */
+  syn CValue FVariable.defaultCValue() = startAttributeCValue();
+  eq FRecordVariable.defaultCValue() {
+	  if (isArray()) {
+		  CValueArray res = new CValueArray(size());
+		  for (Index i : indices())
+			  res.setCell(i, defaultCValueCell(i));
+		  return res;
+	  } else {
+		  return defaultCValueCell(Index.NULL);
+	  }
+  }
+  
+  /**
+   * Constant eval for variables without any binding expression, or binding equation.
+   * Uses the start value for non-records.
+   */
+  syn CValue FVariable.defaultCValueCell(Index i) = startAttributeCValue();
+  eq FRecordVariable.defaultCValueCell(Index i) {
+	  CValueRecord res = new CValueRecord((FRecordType) type().scalarType());
+	  FRecordDecl rec = myFRecordDecl();
+	  for (int j = 0, n = rec.getNumFVariable(); j < n; j++) 
+		  res.setMember(j, rec.getFVariable(j).ceval());
+	  for (FAttribute a : getFAttributes())
+		  a.applyToCValue(res, i);
+	  return res;
+  }
+  
+  /**
+   * Apply any binding expressions for record members to a record CValue.
+   */
+  public void FAttribute.applyToCValue(CValueRecord rec, Index i) {
+	  if (rec.members().containsKey(name())) {
+		  int j = rec.members().get(name());
+		  if (hasValue()) {
+			  CValue val = getValue().ceval();
+			  if (i != Index.NULL && rec.getMember(j).size().ndims() + i.ndims() == val.size().ndims())
+				  val = val.array().getPart(i);
+			  rec.setMember(j, val);
+		  } else {
+			  rec.getMember(j).applyAttributes(getFAttributes(), i);
+		  }
+	  }
+  }
+  
+  /**
+   * Apply attributes in attrs to any records in this value.
+   */
+  public void CValue.applyAttributes(List<FAttribute> attrs, Index i) {}
+  
+  public void CValueArray.applyAttributes(List<FAttribute> attrs, Index i) {
+	  if (values.length > 0 && values[0].isRecord()) 
+		  for (Index j : indices) 
+			  getCell(j).applyAttributes(attrs, i.expand(j));
+  }
+  
+  public void CValueRecord.applyAttributes(List<FAttribute> attrs, Index i) {
+	  for (FAttribute a : attrs)
+		  a.applyToCValue(this, i);
   }
   
   

trunk/Compiler/ModelicaCBackEnd/src/test/modelica/CCodeGenTests.mo
@@ -4833,9 +4833,9 @@ model DependentParametersWithScalingTest1
     JMI_RECORD_STATIC(R_0_r, tmp_1)
     JMI_RECORD_STATIC(R_0_r, tmp_2)
     _p2_1 = (3 * (_p1_0*sf(0)))/sf(2);
-    _r2_x_2 = ((_r_x_3*sf(1)))/sf(3);
-    _p3_4 = (func_CCodeGenTests_DependentParametersWithScalingTest1_F_exp((_p2_1*sf(2))))/sf(4);
-    tmp_2->x = (_r2_x_2*sf(3));
+    _r2_x_4 = ((_r_x_3*sf(1)))/sf(4);
+    _p3_2 = (func_CCodeGenTests_DependentParametersWithScalingTest1_F_exp((_p2_1*sf(2))))/sf(3);
+    tmp_2->x = (_r2_x_4*sf(4));
     func_CCodeGenTests_DependentParametersWithScalingTest1_FR_def(tmp_2, tmp_1);
     _temp_1_x_5 = (tmp_1->x)/sf(5);
     _r3_x_6 = ((_temp_1_x_5*sf(5)))/sf(6);

trunk/Compiler/ModelicaFrontEnd/src/jastadd/Arrays.jrag
@@ -2385,7 +2385,7 @@ aspect ArrayHelpers {
 		 */
 		public boolean isEmpty() {
 			for (int i = 0; i < size.length; i++)
-				if (size[i] == 0)
+				if (read(i) == 0)
 					return true;
 			return false;
 		}
@@ -3581,12 +3581,16 @@ aspect ArrayHelpers {
 		}
 		
 		/**
-		 * \brief Creates a new Index that describes the last <code>ndims() - level</code>
-		 *        dimensions of this Index.
+		 * Returns an Index that describes the last <code>ndims() - level</code> dimensions 
+		 * of this Index.
 		 * 
 		 * @param level  the first index to copy. Must be between 0 and ndims(), inclusive.
 		 */
 		public Index subIndex(int level) {
+			if (level == 0)
+				return this;
+			if (level == index.length)
+				return NULL;
 			Index copy = new Index(index.length - level);
 			System.arraycopy(index, level, copy.index, 0, copy.index.length);
 			return copy;
@@ -3638,12 +3642,15 @@ aspect ArrayHelpers {
 		}
 		
 		/**
-		 * \brief Return an Index that is the result of appending <code>i</code>
-		 *        to this Index.
+		 * Returns an Index that is the result of appending <code>i</code> to this Index.
 		 * 
 		 * @param i  the Index to append.
 		 */
 		public Index expand(Index i) {
+			if (index.length == 0)
+				return i;
+			if (i.index.length == 0)
+				return this;
 			Index expanded = new Index(index.length + i.index.length);
 			System.arraycopy(index, 0, expanded.index, 0, index.length);
 			System.arraycopy(i.index, 0, expanded.index, index.length, i.index.length);
@@ -3871,12 +3878,12 @@ aspect ArrayHelpers {
 		 * \brief Calculate the internal index in an Array that spans this Indices.
 		 */
 		protected int internal(Index i) {
-			if (i.ndims() != ndims)
-				throw new UnsupportedOperationException("Index dimensions does not match.");
+			if (i.ndims() > ndims)
+				throw new UnsupportedOperationException("Too many dimensions in Index.");
 			int[] index = i.index();
 			int res = 0;
 			for (int j = 0; j < ndims; j++)
-				res = res * size.get(j) + index[j] - 1;
+				res = res * size.get(j) + (j < index.length ? index[j] - 1 : 0);
 			return res;
 		}
 		

trunk/Compiler/ModelicaFrontEnd/src/jastadd/FlatAPI.jrag
@@ -1390,23 +1390,23 @@ aspect Attributes {
 	
 	syn CValue FVariable.attributeCValueString(String name) {
 		FAttribute a = findAttribute(name);
-		return attributeSet(a) ? attributeExp(a).ceval() : new CValueString("");
+		return (attributeSet(a) ? attributeExp(a).ceval() : new CValueString("")).expandArray(size());
 	}
 	syn CValue FVariable.attributeCValueBoolean(String name, boolean def) {
 		FAttribute a = findAttribute(name);
-		return attributeSet(a) ? attributeExp(a).ceval() : new CValueBoolean(def);
+		return (attributeSet(a) ? attributeExp(a).ceval() : new CValueBoolean(def)).expandArray(size());
 	}
 	syn CValue FVariable.attributeCValueReal(String name, double def) {
 		FAttribute a = findAttribute(name);
-		return attributeSet(a) ? attributeExp(a).ceval() : new CValueReal(def);
+		return (attributeSet(a) ? attributeExp(a).ceval() : new CValueReal(def)).expandArray(size());
 	}
 	syn CValue FVariable.attributeCValueInteger(String name, int def) {
 		FAttribute a = findAttribute(name);
-		return attributeSet(a) ? attributeExp(a).ceval() : new CValueInteger(def);
+		return (attributeSet(a) ? attributeExp(a).ceval() : new CValueInteger(def)).expandArray(size());
 	}
 	syn CValue FVariable.attributeCValueEnum(String name, FType type, int def) {
 		FAttribute a = findAttribute(name);
-		return attributeSet(a) ? attributeExp(a).ceval() : new CValueEnum(type, def);
+		return (attributeSet(a) ? attributeExp(a).ceval() : new CValueEnum(type, def)).expandArray(size());
 	}
 
 	// Quantity attribute

trunk/Compiler/ModelicaFrontEnd/src/jastadd/FlatVariability.jrag
@@ -325,7 +325,7 @@ aspect FlatVariability {
 	eq FFloorExp.expVariability()   = getFExp().variability().combineDown(fDiscrete());
 	
 	eq FIterExp.expVariability() {
-		FTypePrefixVariability total = getArray().iteratorFExp().next().variability();
+		FTypePrefixVariability total = size().isEmpty() ? fConstant() : getArray().iteratorFExp().next().variability();
 		for (CommonForIndex ind : getForIndexList())
 			if (ind.hasFExp())
 				total = total.combine(ind.getFExp().variability());

trunk/Compiler/ModelicaFrontEnd/src/jastadd/Scalarization.jrag
@@ -235,7 +235,7 @@ aspect Scalarization {
 		// (except in reduction-expressions, and they are handled elsewhere)
 		if (isRecord()) {
 			FExp bexp = hasBindingExp() ? getBindingExp() : null;
-			scalarRecordFVariables(vars, new FQNameEmpty(), null, bexp);
+			scalarRecordFVariables(vars, new FQNameEmpty(), null, bexp, Index.NULL, false, Collections.<BindingExpSource>emptyList());
 		} else if (isArray()) {
 			for (Index i : indices()) 
 				vars.addChild(createScalarFVariable(i));
@@ -355,41 +355,56 @@ aspect Scalarization {
 	/**
 	 * \brief Generate scalar FVariables for this record or record component.
 	 * 
-	 * @param vars    the list of FVariables to add the variables to
-	 * @param prefix  add this as a prefix to the name
-	 * @param last    an FVariable containing the attributes, variability, 
-	 *                etc inherited from the surrounding record
-	 * @param bexp    binding expression inherited from surrounding record
+	 * @param vars     the list of FVariables to add the variables to
+	 * @param prefix   add this as a prefix to the name
+	 * @param last     an FVariable containing the attributes, variability, 
+	 *                 etc inherited from the surrounding record
+	 * @param bexp     binding expression inherited from surrounding record
+	 * @param bi       index of part of binding expression to use
+	 * @param inParam  this variable is within a constant or parameter record
+	 * @param attrs    attributes to search for modifications, before searching in this variable
 	 */
 	public void FVariable.scalarRecordFVariables(
-			List<FVariable> vars, FQName prefix, FVariable last, FExp bexp) {
-		if (bexp == null && hasBindingExp() && (isConstant() || isIndependentParameter()))
+			List<FVariable> vars, FQName prefix, FVariable last, FExp bexp, Index bi, 
+			boolean inParam, java.util.List<BindingExpSource> attrs) {
+		inParam = (inParam || isConstant() || isParameter()) && !isDependentParameter();
+		if (bexp == null && hasBindingExp() && inParam && getBindingExp().isConstantExp()) {
 			bexp = getBindingExp();
+			bi = Index.NULL;
+		}
 		if (isArray()) {
 			FExp nbexp = null;
 			for (Index i : scalarizationIndices(last)) {
-				if (bexp != null)
-					nbexp = bexp.getArray().get(i);
+				if (bexp != null) {
+					Index nbi = bi.expand(i);
+					nbi = nbi.subIndex(nbi.ndims() - bexp.ndims());
+					nbexp = bexp.getArray().get(nbi);
+				}
 				FQName name = prefix.copyAndAppend(getFQName(), i.createFArraySubscripts());
-				scalarRecordCellFVariables(vars, name, last, nbexp, i);
+				scalarRecordCellFVariables(vars, name, last, nbexp, i, inParam, attrs);
 			}
 		} else {
 			FQName name = prefix.copyAndAppend(getFQName());
-			scalarRecordCellFVariables(vars, name, last, bexp, Index.NULL);
+			if (bexp != null && bexp.isArray())
+				bexp = bexp.getArray().get(bi);
+			scalarRecordCellFVariables(vars, name, last, bexp, Index.NULL, inParam, attrs);
 		}
 	}
 	
 	/**
 	 * \brief Generate scalar FVariables for a single array cell of this record or record component.
 	 * 
-	 * @param vars  the list of FVariables to add the variables to
-	 * @param name  the name of the new variable
-	 * @param last  an FVariable containing the attributes, variability, etc inherited from surrounding record
-	 * @param bexp  binding expression inherited from surrounding record or array
-	 * @param i     the index of this scalar variable in an array, or Index.NULL for scalar components
+	 * @param vars     the list of FVariables to add the variables to
+	 * @param name     the name of the new variable
+	 * @param last     an FVariable containing the attributes, variability, etc inherited from surrounding record
+	 * @param bexp     binding expression inherited from surrounding record or array
+	 * @param i        the index of this scalar variable in an array, or Index.NULL for scalar components
+	 * @param inParam  this variable is within a constant or parameter record
+	 * @param attrs    attributes to search for modifications, before searching in this variable
 	 */
 	public void FVariable.scalarRecordCellFVariables(
-			List<FVariable> vars, FQName name, FVariable last, FExp bexp, Index i) {
+			List<FVariable> vars, FQName name, FVariable last, FExp bexp, Index i, 
+			boolean inParam, java.util.List<BindingExpSource> attrs) {
 		FVariable fv = copyForName(name);
 		Map<String,FExp> im = Collections.emptyMap();
 		if (bexp != null)
@@ -402,19 +417,74 @@ aspect Scalarization {
 	}
 	
 	public void FRecordVariable.scalarRecordCellFVariables(
-			List<FVariable> vars, FQName name, FVariable last, FExp bexp, Index i) {
+			List<FVariable> vars, FQName name, FVariable last, FExp bexp, Index i, 
+			boolean inParam, java.util.List<BindingExpSource> attrs) {
 		FVariable tmp = (FVariable) fullCopy();
 		myFClass().addDummyVariable(tmp);
 		tmp.inheritFrom(last);
-		FExp nbexp = null;
 		Map<String, FExp> emptyIndexMap = Collections.emptyMap();
 		for (FVariable comp : myFRecordDecl().getFVariables()) {
-			if (bexp != null)
+			java.util.List<BindingExpSource> nAttrs = Collections.emptyList();
+			FExp nbexp = null;
+			Index nbi = Index.NULL;
+			if (bexp != null) {
 				nbexp = bexp.scalarRecordFExp(emptyIndexMap, new FQNameString(comp.name()));
-			else
-				nbexp = attributeExp(comp.name());
-			comp.scalarRecordFVariables(vars, name, tmp, nbexp);
+			} else {
+				ArrayList<BindingExpSource> foundAttr = new ArrayList<BindingExpSource>();
+				String cName = comp.name();
+				for (BindingExpSource bes : attrs)
+					bes.addIfMatching(foundAttr, cName, i);
+				BindingExpSource.addIfMatching(foundAttr, this, cName, i);
+				for (int j = 0; nbexp == null && j < foundAttr.size(); j++) {
+					nbexp = foundAttr.get(j).getFExp();
+					if (nbexp != null)
+						nbi = foundAttr.get(j).getI();
+				}
+				if (nbexp == null)
+					nAttrs = foundAttr;
+			}
+			comp.scalarRecordFVariables(vars, name, tmp, nbexp, nbi, inParam, nAttrs);
 		}
+	}
+	
+	/**
+	 * Used to hold information on available candidates for binding expression when scalarizing 
+	 * records.
+	 */
+	public class BindingExpSource {
+		
+		private FAttribute attr;
+		private Index index;
+		
+		private BindingExpSource(FAttribute a, Index i) {
+			attr = a;
+			index = i;
+		}
+		
+		private static void addIfExists(
+				java.util.List<BindingExpSource> attrs, FAttribute a, Index i) {
+			if (a != null)
+				attrs.add(new BindingExpSource(a, i));
+		}
+		
+		public static void addIfMatching(
+				java.util.List<BindingExpSource> attrs, FVariable fv, String name, Index i) {
+			addIfExists(attrs, fv.findAttribute(name), i);
+		}
+		
+		public void addIfMatching(
+				java.util.List<BindingExpSource> attrs, String name, Index i) {
+			addIfExists(attrs, attr.findAttribute(name), index.expand(i));
+		}
+		
+		public FExp getFExp() {
+			return attr.hasValue() ? attr.getValue() : null;
+		}
+		
+		public Index getI() {
+			return index;
+		}
+		
 	}
 	
 	/**
@@ -549,13 +619,26 @@ aspect Scalarization {
 			la.add(a.scalarize(i));            // TODO: really send i here?
 		FAttribute res = new FAttribute(getType().fullCopy(), getName().fullCopy(), 
 				new Opt(), getAttributeSet(), getLevel(), new Opt(), getFFinalOpt().fullCopy(), la);
-		if (hasValue()) {
-			FExp v = getValue();
-			if (v.isArray())
-				v = v.getArray().get(i);
-			res.setValue(v.scalarize(Collections.<String,FExp>emptyMap()));
-		}
+		if (hasValue()) 
+			res.setValue(scalarizeValue(i));
 		return res;
+	}
+	
+	/**
+	 * Scalarize the value of this attribute, picking out the specific element from an array 
+	 * expression.
+	 * 
+	 * Returns null if attribute has no value.
+	 */
+	public FExp FAttribute.scalarizeValue(Index i) {
+		if (!hasValue()) 
+			return null;
+		FExp v = getValue();
+		if (v.isArray()) {
+			i = i.subIndex(i.ndims() - v.ndims());
+			v = v.getArray().get(i);
+		}
+		return v.scalarize(Collections.<String,FExp>emptyMap());
 	}
 	
 	/**

commit 0aa2d2f6b3733f1f8a4451d27eae8fe346f46b70

Fixed bug concerning parameter arrays in OptimicaCompiler introduced by r5056. (#2985)
git-svn-id: https://svn.jmodelica.org@5057 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/jastadd/ConstantEvaluation.jrag
@@ -17,6 +17,7 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Iterator;
+import org.jmodelica.util.BinaryOperation;
 
 /**
  * \brief Provides methods to evaluate flat constant 
@@ -278,14 +279,33 @@ aspect ConstantEvaluation {
         }
         
         /**
-         * \brief Reduce an array of constant boolean values to a single boolean.
+         * Reduce an array to a single value.
+         * 
+         * For a scalar, returns itself. For an empty array, returns zero.
+         * 
+         * @param op    the operation to use to combine two values
+         * @param zero  the start value
+         */
+        public CValue reduce(BinaryOperation<CValue> op, CValue zero) {
+        	return this;
+        }
+        
+        /**
+         * Reduce an array of constant boolean values to a single boolean that is the logical 
+         * or of the array.
          * 
          * Used for the test expression of while statements. For scalar constant values, 
          * just returns the boolean value.
          */
-        public boolean reduceBoolean() {
-        	return booleanValue();
+        public boolean reduceBooleanOr() {
+        	return reduce(OR_OPERATION, CValueBoolean.FALSE).booleanValue();
         }
+        
+        private static final BinaryOperation<CValue> OR_OPERATION = new BinaryOperation<CValue>() {
+        	public CValue op(CValue x, CValue y) {
+        		return (x.booleanValue() || y.booleanValue()) ? CValueBoolean.TRUE : CValueBoolean.FALSE;
+        	}
+        };
 		
 		/**
 		 * \brief Returns the array size of this constant value.
@@ -714,15 +734,17 @@ aspect ConstantEvaluation {
         }
         
         /**
-         * \brief Reduce an array of constant boolean values to a single boolean.
+         * Reduce an array to a single value.
          * 
-         * Used for the test expression of while statements.
+         * For a scalar, returns itself. For an empty array, returns zero.
+         * 
+         * @param op    the operation to use to combine two values
+         * @param zero  the start value
          */
-        public boolean reduceBoolean() {
-        	boolean res = false;
+        public CValue reduce(BinaryOperation<CValue> op, CValue zero) {
         	for (CValue val : values)
-        		res = res || val.booleanValue();
-        	return res;
+        		zero = op.op(zero, val);
+        	return zero;
         }
 		
 		/**
@@ -2516,7 +2538,7 @@ aspect ConstantEvaluation {
 
   public int FIfWhenClause.evaluate(Map<CommonVariableDecl, CValue> values) {
 	  CValue test = getTest().ceval();
-	  if (test.reduceBoolean()) {
+	  if (test.reduceBooleanOr()) {
 		  int res = getFStatements().evaluateList(values);
 		  return res | FStatement.EVAL_IFHIT;
 	  }

trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/BinaryOperation.java
@@ -1,0 +1,5 @@
+package org.jmodelica.util;
+
+public interface BinaryOperation<T> {
+	public T op(T x, T y);
+}

trunk/Compiler/OptimicaFrontEnd/src/jastadd/OptimicaFlatAPI.jrag
@@ -41,9 +41,11 @@ aspect OptimicaFlatAPI {
 	syn CValue  FVariable.freeAttributeCValue() = attributeCValueBoolean(FAttribute.FREE, false);
 		
 	/**
-	 * \brief Get the free attribute value.
+	 * Get the free attribute value.
+	 * 
+	 * For an array, this is true if it is true for any cell in the array.
 	 */
-	syn boolean FVariable.freeAttribute()       = freeAttributeCValue().booleanValue();	
+	syn boolean FVariable.freeAttribute()       = freeAttributeCValue().reduceBooleanOr();	
 
 	// === initialGuess ===
 	/**