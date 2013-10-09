svn2github/jmodelica

commit cad0cf954dafa0818de7ce54802da82409bb668c

Refactored FPreExp, it is now a subclass for FIdUseExp and it will be easier to handle in BiPGraph and other places, #3118.
git-svn-id: https://svn.jmodelica.org@5322 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/jastadd/FlatNameBinding.jrag
@@ -566,15 +566,7 @@ aspect FlatPreBinding {
 	inh FAbstractVariable FIdUse.lookupPreFV(FQName fqn);	
 	inh FAbstractVariable FExp.lookupPreFV(FQName fqn);	
 	inh FAbstractVariable FAbstractVariable.lookupPreFV(FQName fqn);	
-	syn FAbstractVariable FPreExp.myFV() {
-		if (getFExp() instanceof FIdUseExp) {
-		    //System.out.println("FPreExp.myFV(): " + name());
-			return getFExp().lookupPreFV(((FIdUseExp) getFExp()).getFIdUse().getFQName());
-    	} else {
-    		return unknownFVariable();
-    	}
-    }
-    
+	syn FAbstractVariable FPreExp.myFV() = lookupPreFV(getFIdUse().getFQName());
 	eq Root.getChild().lookupPreFV(FQName fqn)   = null;
 	eq FClass.getChild().lookupPreFV(FQName fqn) = lookupFV(fqn.preName());
 

trunk/Compiler/ModelicaFrontEnd/src/jastadd/TransformCanonical.jrag
@@ -499,17 +499,13 @@ aspect TransformCanonical {
 		if (root().options.getBooleanOption("automatic_add_initial_equations")) {
 			for (Var var : g.getUnmatchedVariables()) {	
 				FVariable fv = var.getVariable();
-				FExp lhs = new FIdUseExp(new FIdUse(fv.getFQName().fullCopy()));
+				FIdUseExp lhs = fv.isDiscrete() ? fv.createFPreExp() : fv.createUseExp();
 				FExp rhs = fv.startAttributeSet() ? fv.startAttributeExp() : fv.type().zeroLiteral();
-				if (fv.isDiscrete()) {
-					fv = (FVariable) fv.myPreVariable();
-					lhs = new FPreExp(lhs);
-				}
 				FEquation fe = new FEquation(lhs, rhs);
 				addFInitialEquation(fe);
 				
 				eqn = g.addEquation("eq_" + (g.getEquations().size() + 1), fe, 0);
-				v = g.getVariable(fv.name());
+				v = g.getVariable(lhs.name());
 				g.addEdge(eqn,v);
 			}
 			root().flushAllRecursive();
@@ -1503,9 +1499,7 @@ aspect TransformCanonical {
                         generate = true;
                     
                     if (generate) {
-                        FExp lhs = new FIdUseExp(new FIdUse(fv.getFQName().fullCopy()));
-                        if (fv.isDiscrete())
-                            lhs = new FPreExp(lhs);
+                        FExp lhs = fv.isDiscrete() ? fv.createFPreExp() : fv.createUseExp();
                         FExp rhs = fv.startAttributeSet() ? fv.startAttributeExp() : fv.type().zeroLiteral();
                         addFInitialEquation(new FEquation(lhs, rhs));
                     }
@@ -4700,20 +4694,20 @@ aspect EventGeneratingExps {
 	syn FExp FEventGenExp.createGuardExp(FVariable var);
 	eq FFloorFuncExp.createGuardExp(FVariable var) {
 		FArray guards = new FArray();
-		guards.addFExp(new FLtExp(getX().fullCopy(), new FPreExp(var.createUseExp())));
-		guards.addFExp(new FGeqExp(getX().fullCopy(), new FAddExp(new FPreExp(var.createUseExp()), new FIntegerLitExp(1))));
+		guards.addFExp(new FLtExp(getX().fullCopy(), new FPreExp(var.createFIdUse())));
+		guards.addFExp(new FGeqExp(getX().fullCopy(), new FAddExp(new FPreExp(var.createFIdUse()), new FIntegerLitExp(1))));
 		return guards;
 	}
 	eq FCeilFuncExp.createGuardExp(FVariable var) {
 		FArray guards = new FArray();
-		guards.addFExp(new FLeqExp(getX().fullCopy(), new FSubExp(new FPreExp(var.createUseExp()), new FIntegerLitExp(1))));
-		guards.addFExp(new FGtExp(getX().fullCopy(), new FPreExp(var.createUseExp())));
+		guards.addFExp(new FLeqExp(getX().fullCopy(), new FSubExp(new FPreExp(var.createFIdUse()), new FIntegerLitExp(1))));
+		guards.addFExp(new FGtExp(getX().fullCopy(), new FPreExp(var.createFIdUse())));
 		return guards;
 	}
 	eq FDivFuncExp.createGuardExp(FVariable var) {
 		FArray guards = new FArray();
-		guards.addFExp(new FLtExp(new FDivFuncExp(getX().fullCopy(), getY().fullCopy()), new FPreExp(var.createUseExp())));
-		guards.addFExp(new FGeqExp(new FDivFuncExp(getX().fullCopy(), getY().fullCopy()), new FAddExp(new FPreExp(var.createUseExp()), new FIntegerLitExp(1))));
+		guards.addFExp(new FLtExp(new FDivFuncExp(getX().fullCopy(), getY().fullCopy()), new FPreExp(var.createFIdUse())));
+		guards.addFExp(new FGeqExp(new FDivFuncExp(getX().fullCopy(), getY().fullCopy()), new FAddExp(new FPreExp(var.createFIdUse()), new FIntegerLitExp(1))));
 		return guards;
 	} 
 	

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen.jrag
@@ -114,12 +114,12 @@ aspect CCodeGen {
 		if (myFV().isReal() && !inFunction() && 
 				root().options.getBooleanOption("enable_variable_scaling")) {
 			str.print("(");
-			str.print("pre" + ((FIdUseExp)getFExp()).myFV().name_C());
+			str.print(myFV().name_C());
 			str.print("*sf(");
-			str.print(((FIdUseExp)getFExp()).myFV().valueReference());
+			str.print(myFV().valueReference());
 			str.print("))");
 		} else {
-			str.print("pre" + ((FIdUseExp)getFExp()).myFV().name_C());
+			str.print(myFV().name_C());
 		}
 	}
 	

trunk/Compiler/ModelicaFrontEnd/src/jastadd/ComplianceCheck.jadd
@@ -92,7 +92,7 @@ aspect ComplianceCheck {
 	    complianceOnlyFMU("The sample() function-like operator is");
 	}
 
-	public void FPreExp.complianceCheck() {
+	public void InstPreExp.complianceCheck() {
 	    complianceOnlyFMU("The pre() function-like operator is");
 	}
 	

trunk/Compiler/ModelicaFrontEnd/src/jastadd/ConstantEvaluation.jrag
@@ -1367,7 +1367,6 @@ aspect ConstantEvaluation {
   eq FNoEventExp.cevalCalc() = getFExp().ceval();
   eq FSmoothExp.cevalCalc()  = getFExp().ceval();
   
-  eq FPreExp.cevalCalc()     = variability().constantVariability() ? getFExp().ceval()   : CValue.UNKNOWN;
   eq FEdgeExp.cevalCalc()    = variability().constantVariability() ? CValueBoolean.FALSE : CValue.UNKNOWN;
   eq FChangeExp.cevalCalc()  = variability().constantVariability() ? CValueBoolean.FALSE : CValue.UNKNOWN;
   eq FSampleExp.cevalCalc()  = CValue.UNKNOWN;
@@ -1491,6 +1490,7 @@ aspect ConstantEvaluation {
   eq FDerExp.cevalCalc() = 
 			getFIdUse().variability().discreteOrLess() ? new CValueInteger(0) : CValue.UNKNOWN;
   eq FInstAccessExp.cevalCalc() = getInstAccess().ceval();
+  eq FPreExp.cevalCalc()  = variability().constantVariability() ? super.cevalCalc()   : CValue.UNKNOWN;
   
   syn CValue InstAccess.ceval()        = ceval(Index.NULL);
   syn CValue InstAccess.ceval(Index i) = CValue.UNKNOWN;

trunk/Compiler/ModelicaFrontEnd/src/jastadd/Constructors.jrag
@@ -261,6 +261,13 @@ aspect Constructors {
 	}
 	
 	/**
+	 * Surounds the FExp <code>exp</code> with pre if it is an non literal exp, else return exp.
+	 */
+	public static FExp FPreExp.create(FExp exp) {
+		return exp.isLiteralExp() ? exp : new FPreExp(exp.asFIdUse());
+	}
+	
+	/**
 	 * \brief Constructs an if equation.
 	 */
 	public FIfEquation.FIfEquation(FEquationType type, List<FAbstractEquation> eqns, FExp guard, Opt<FIfWhenElseEquation> elseClause) {
@@ -862,7 +869,6 @@ aspect InheritedFactoryMethods {
 	public FTimeExp        FTimeExp.createEmptyNode()        { return new FTimeExp();        }
 	public FNoEventExp     FNoEventExp.createEmptyNode()     { return new FNoEventExp();     }
 	public FSmoothExp      FSmoothExp.createEmptyNode()      { return new FSmoothExp();      }
-	public FPreExp         FPreExp.createEmptyNode()         { return new FPreExp();         }
 	public FEdgeExp        FEdgeExp.createEmptyNode()        { return new FEdgeExp();        }
 	public FChangeExp      FChangeExp.createEmptyNode()      { return new FChangeExp();      }
 	public FSampleExp      FSampleExp.createEmptyNode()      { return new FSampleExp();      }
@@ -880,6 +886,8 @@ aspect InheritedFactoryMethods {
 	public InstHDerExp    InstHDerExp.createEmptyNode()    { return new InstHDerExp();    }
 	public FHDerExp       FHDerExp.createEmptyNode()       { return new FHDerExp();       }
 	public FInstAccessExp FInstAccessExp.createEmptyNode() { return new FInstAccessExp(); }
+	public FPreExp        FPreExp.createEmptyNode()        { return new FPreExp();      }
+	public InstPreExp      InstPreExp.createEmptyNode()    { return new InstPreExp();      }
 	
 	// Array expressions
 	public FArray          FArray.createEmptyNode()          { return new FArray();          }

trunk/Compiler/ModelicaFrontEnd/src/jastadd/ContentsCheck.jadd
@@ -75,7 +75,7 @@ aspect ContentCheck {
 			error("The end operator may only be used in array subscripts");
 	}
 	
-	public void FPreExp.contentCheck() {
+	public void InstPreExp.contentCheck() {
 		if (inFunction()) { 
 			error("The pre() operator may not be used inside functions");
 		}

trunk/Compiler/ModelicaFrontEnd/src/jastadd/FlatAPI.jrag
@@ -1078,8 +1078,8 @@ aspect FlatAPI {
 		if (!activeAtInitialization()) {
 			for (FAbstractEquation e : getFAbstractEquations()) {
 				for (FIdUseExp id : e.simpleLefts()) {
-					l.add(new FEquation((FIdUseExp)id.fullCopy(),
-					   new FPreExp((FIdUseExp)id.fullCopy())));
+					l.add(new FEquation(id.fullCopy(),
+					   new FPreExp(id.getFIdUse().fullCopy())));
 				}
 			}
 		} else {
@@ -1667,8 +1667,7 @@ aspect FVariableUses {
 	coll HashSet<FIdUseExp> FAbstractVariable.uses() [new LinkedHashSet<FIdUseExp>()] 
 		with add root FlatRoot;
 	
-	FIdUseExp contributes this when !isDerUse() to 
-		FAbstractVariable.uses() for myFV();
+	FIdUseExp contributes this to FAbstractVariable.uses() for myFV();
 	
 	/**
 	 * All uses of this variable that are not the left-hand side of an equation. 
@@ -1779,7 +1778,7 @@ aspect FVariableUses {
 		with addAll root FlatRoot;
 	
 	FIdUseExp contributes boundParameters() 
-		when !isDerUse() && !myFV().isUnknown() && myFV().isParameter()
+		when !myFV().isUnknown() && myFV().isParameter()
 		to FAbstractVariable.dependentParameters() for myFV();
 	
 	syn lazy ArrayList<FRealVariable> FAbstractVariable.dependentRealParameters() =
@@ -1803,7 +1802,7 @@ aspect FVariableUses {
 		[new LinkedHashSet<FVariable>()] with add root FAbstractEquation;
 
     FIdUseExp contributes (FVariable) myFV() when 
-        !myFV().isUnknown() && isNormalUse() && myFV().isInput() to 
+        !myFV().isUnknown() && myFV().isInput() to 
         FAbstractEquation.retrieveInputs() for myFEquation();
 
 	syn lazy LinkedHashSet<FVariable> FAbstractEquation.inputs() = 
@@ -1838,8 +1837,6 @@ aspect FVariableUses {
 
 	inh boolean FIdUseExp.inAlgebraicAccessLocation();
 	eq BaseNode.getChild().inAlgebraicAccessLocation()   = true;
-	eq FDerExp.getChild().inAlgebraicAccessLocation()    = false;
-	eq FPreExp.getChild().inAlgebraicAccessLocation()    = false;
 	eq FResidual.getIterationVariable().inAlgebraicAccessLocation() = false;
 	
 	syn LinkedHashSet<FVariable> FAbstractEquation.algebraicVariables() = 
@@ -1873,29 +1870,6 @@ aspect FVariableUses {
 		new LinkedHashSet<FVariable>(FClass.intersectFVColl(myFClass().discreteStringVariables(),algebraicVariables()));
 
 
-    /**
-     * Check if this use is a normal use, i.e. not a der() or pre() use.
-     */
-    inh boolean FIdUseExp.isNormalUse();
-    eq FPreExp.getChild().isNormalUse()  = false;
-    eq FDerExp.getChild().isNormalUse()  = false;
-    eq BaseNode.getChild().isNormalUse() = true;
-
-    /**
-     * Check if this is the use of a der() expression.
-     */
-    inh boolean FIdUseExp.isDerUse();
-    eq FDerExp.getChild().isDerUse()  = true;
-    eq BaseNode.getChild().isDerUse() = false;
-
-    /**
-     * Check if this is the use of a pre() expression.
-     */
-    inh boolean FIdUseExp.isPreUse();
-    eq FPreExp.getChild().isPreUse()  = true;
-    eq BaseNode.getChild().isPreUse() = false;
-
-
 	/**
 	 * \brief Collection attribute returning all derivative variables (FDerivativeVariables)
 	 * referenced in an equation.
@@ -1915,7 +1889,7 @@ aspect FVariableUses {
 		[new LinkedHashSet<FVariable>()] with add root FAbstractEquation;
 
     FIdUseExp contributes (FVariable) myFV() when 
-        !myFV().isUnknown() && !isDerUse() && myFV().isDifferentiatedVariable() to 
+        !myFV().isUnknown() && myFV().isDifferentiatedVariable() to 
         FAbstractEquation.differentiatedRealVariables() for myFEquation();
 
 	/**
@@ -1926,7 +1900,7 @@ aspect FVariableUses {
 		[new LinkedHashSet<FVariable>()] with add root FAbstractEquation;
 
     FIdUseExp contributes (FVariable) myFV() when 
-        !myFV().isUnknown() && isNormalUse() && myFV().isDiscreteVariable() to 
+        !myFV().isUnknown() && myFV().isDiscreteVariable() to 
         FAbstractEquation.discreteVariables() for myFEquation();
 
     /**
@@ -1937,7 +1911,7 @@ aspect FVariableUses {
         [new LinkedHashSet<FVariable>()] with add root FAbstractEquation;
 
     FIdUseExp contributes (FVariable) myFV() when
-        !myFV().isUnknown() && isNormalUse() && myFV().isNonFixedParameter() to
+        !myFV().isUnknown() && myFV().isNonFixedParameter() to
         FAbstractEquation.nonFixedParameters() for myFEquation();
 
 	/**
@@ -2051,7 +2025,7 @@ aspect LinearFVariables {
 		[new LinkedHashSet<FIdUseExp>()] with add root FAbstractEquation;
 
     FIdUseExp contributes this when 
-        !myFV().isUnknown() && isNormalUse() to 
+        !myFV().isUnknown() to 
         FAbstractEquation.collectUses() for myFEquation();
     
     syn Set<FIdUseExp> FAbstractEquation.collectUses(Set<FVariable> vars) {

commit 65ee4434bd0f88fde8d1d5040f4887eb7b22a5bf

Fixed pre() alias elimination bug that was introduced in r5322, ticket #3118.
git-svn-id: https://svn.jmodelica.org@5326 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/jastadd/FlatNameBinding.jrag
@@ -567,6 +567,7 @@ aspect FlatPreBinding {
 	inh FAbstractVariable FExp.lookupPreFV(FQName fqn);	
 	inh FAbstractVariable FAbstractVariable.lookupPreFV(FQName fqn);	
 	syn FAbstractVariable FPreExp.myFV() = lookupPreFV(getFIdUse().getFQName());
+	syn FAbstractVariable FPreExp.myNonPreFV() = lookupFV(getFIdUse().getFQName());
 	eq Root.getChild().lookupPreFV(FQName fqn)   = null;
 	eq FClass.getChild().lookupPreFV(FQName fqn) = lookupFV(fqn.preName());
 

trunk/Compiler/ModelicaFrontEnd/src/jastadd/TransformCanonical.jrag
@@ -1775,6 +1775,16 @@ aspect TransformCanonical {
 		}
 	}
 	
+	rewrite FPreExp {
+		when (rewriteAlias && myNonPreFV().isAlias()) to FExp {
+			FVariable fv = (FVariable) myNonPreFV();
+			FExp new_exp = fv.alias().createFPreExp();
+			if (fv.isNegated()) 
+				new_exp = new FNegExp(new_exp);
+			return new_exp;
+		}
+	}
+	
 	rewrite FAssignStmt {
 		when (rewriteAlias && getLeft().myFV().isAlias()) to FAssignStmt {
 			FVariable fv = (FVariable) getLeft().myFV();

trunk/Compiler/ModelicaFrontEnd/src/test/modelica/NameTests.mo
@@ -3414,6 +3414,30 @@ Semantic error at line 3271, column 9:
 ")})));
 end PreErrorTest;
 
+model PreAliasTest1
+	discrete Real x;
+	discrete Real y;
+initial equation
+	pre(x) = 42;
+equation
+	x = y;
+	x = pre(x) * pre(y) + 3.14;
+
+	annotation(__JModelica(UnitTesting(tests={
+		TransformCanonicalTestCase(
+			name="PreAliasTest1",
+			description="Test so that pre() variables are rewritten correctly",
+			flatModel="
+fclass NameTests.PreAliasTest1
+ discrete Real x;
+initial equation 
+ pre(x) = 42;
+equation
+ x = pre(x) * pre(x) + 3.14;
+end NameTests.PreAliasTest1;
+")})));
+end PreAliasTest1;
+
 
 model AssignedInWhenRecursion
     model A