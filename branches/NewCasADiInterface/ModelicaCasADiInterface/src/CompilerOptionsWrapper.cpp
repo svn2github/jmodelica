591a967ebb32866b70ce8f5d9ae161d79514badf
blennernas <blennernas@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2013-10-22 12:17:06 +0000

    #3174 Java exceptions are now caught in CompilerOptionsWrapper, consequently CasADiInterface does now crash if given incorrect options.
    git-svn-id: https://svn.jmodelica.org@5385 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/NewCasADiInterface/ModelicaCasADiInterface/src/CompilerOptionsWrapper.cpp
+++ branches/NewCasADiInterface/ModelicaCasADiInterface/src/CompilerOptionsWrapper.cpp
@@ -6,29 +6,101 @@
 namespace ModelicaCasADi 
 {
 void CompilerOptionsWrapper::setStringOption(std::string opt, std::string val) {
-    optr.setStringOption(StringFromUTF(opt.c_str()), StringFromUTF(val.c_str()));
+    try 
+    {
+        optr.setStringOption(StringFromUTF(opt.c_str()), StringFromUTF(val.c_str()));
+    }
+    catch (JavaError e) 
+    {
+        std::cout << "Java error occurred: " << std::endl;
+        describeJavaException();
+        clearJavaException();
+    }
 }
 void CompilerOptionsWrapper::setBooleanOption(std::string opt, bool val) {
+    try 
+    {
     optr.setBooleanOption(StringFromUTF(opt.c_str()), val);
+    }
+    catch (JavaError e) 
+    {
+        std::cout << "Java error occurred: " << std::endl;
+        describeJavaException();
+        clearJavaException();
+    }
 }
 void CompilerOptionsWrapper::setIntegerOption(std::string opt, int val) {
+    try 
+    {
     optr.setIntegerOption(StringFromUTF(opt.c_str()), val);
+    }
+    catch (JavaError e) 
+    {
+        std::cout << "Java error occurred: " << std::endl;
+        describeJavaException();
+        clearJavaException();
+    }
 }
 void CompilerOptionsWrapper::setRealOption(std::string opt, double val) {
+    try 
+    {
     optr.setRealOption(StringFromUTF(opt.c_str()), val);
+    }
+    catch (JavaError e) 
+    {
+        std::cout << "Java error occurred: " << std::endl;
+        describeJavaException();
+        clearJavaException();
+    }
 }
 
 void CompilerOptionsWrapper::addStringOption(std::string opt, std::string val) {
+    try 
+    {
     optr.addStringOption(StringFromUTF(opt.c_str()), StringFromUTF(val.c_str()));
+    }
+    catch (JavaError e) 
+    {
+        std::cout << "Java error occurred: " << std::endl;
+        describeJavaException();
+        clearJavaException();
+    }    
 }
 void CompilerOptionsWrapper::addBooleanOption(std::string opt, bool val) {
+    try 
+    {
     optr.addBooleanOption(StringFromUTF(opt.c_str()), val);
+    }
+    catch (JavaError e) 
+    {
+        std::cout << "Java error occurred: " << std::endl;
+        describeJavaException();
+        clearJavaException();
+    }
 }
 void CompilerOptionsWrapper::addIntegerOption(std::string opt, int val) {
+    try 
+    {
     optr.addIntegerOption(StringFromUTF(opt.c_str()), val);
+    }
+    catch (JavaError e) 
+    {
+        std::cout << "Java error occurred: " << std::endl;
+        describeJavaException();
+        clearJavaException();
+    }
 }
 void CompilerOptionsWrapper::addRealOption(std::string opt, double val) {
+    try 
+    {
     optr.addRealOption(StringFromUTF(opt.c_str()), val);
+    }
+    catch (JavaError e) 
+    {
+        std::cout << "Java error occurred: " << std::endl;
+        describeJavaException();
+        clearJavaException();
+    }
 }
 
 
