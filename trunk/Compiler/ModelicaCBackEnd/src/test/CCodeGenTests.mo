svn2github/jmodelica

commit 8cb83b36ab8a2244282d01d1814fc1b93cd24526

Made some cleanup in the BLT structure, removed some unnecessary code. Ticket #3320.
git-svn-id: https://svn.jmodelica.org@5734 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen.jrag
@@ -1906,7 +1906,7 @@ aspect CCodeGen {
 		
 		Map<FAbstractVariable,String> solverArrayMap = new HashMap<FAbstractVariable,String>();
 		int i = 0;
-		for (FVariable fv : blockResidualVariables_C()) 
+		for (FVariable fv : iterationVariables()) 
 			solverArrayMap.put(fv, "x[" + (i++) + "]");
 		
 		String indent = ASTNode.printer_C.indent(outerIndent);
@@ -1919,22 +1919,22 @@ aspect CCodeGen {
 
 		// Nominal values
 		str.print(indent + "if (evaluation_mode == JMI_BLOCK_NOMINAL) {\n");
-		for (FVariable fv : blockResidualVariables_C()) 
+		for (FVariable fv : iterationVariables()) 
 			fv.genBlockResidualAttribute_C(nextIndent, str, FAttribute.NOMINAL, solverArrayMap);
 		
 		// Min values
 		str.print(indent + "} else if (evaluation_mode == JMI_BLOCK_MIN) {\n");
-		for (FVariable fv : blockResidualVariables_C()) 
+		for (FVariable fv : iterationVariables()) 
 			fv.genBlockResidualAttribute_C(nextIndent, str, FAttribute.MIN, solverArrayMap);
 		
 		// Max values
 		str.print(indent + "} else if (evaluation_mode == JMI_BLOCK_MAX) {\n");
-		for (FVariable fv : blockResidualVariables_C()) 
+		for (FVariable fv : iterationVariables()) 
 			fv.genBlockResidualAttribute_C(nextIndent, str, FAttribute.MAX, solverArrayMap);
 		
 		// Variable value references
 		str.print(indent + "} else if (evaluation_mode == JMI_BLOCK_VALUE_REFERENCE) {\n");
-		for (FVariable fv : blockResidualVariables_C()) 
+		for (FVariable fv : iterationVariables()) 
 			fv.genBlockResidualValuref_C(nextIndent, str, solverArrayMap);
 		
 		// Equation nominal values
@@ -1943,7 +1943,7 @@ aspect CCodeGen {
 		
 		str.print(indent + "} else if (evaluation_mode == JMI_BLOCK_INITIALIZE) {\n");
 		// Loop over all active variables and write their value to x	
-		for (FVariable fv : blockResidualVariables_C()) 
+		for (FVariable fv : iterationVariables()) 
 			fv.genBlockResidualInit_C(nextIndent, str, solverArrayMap);
 		
 		// Generate code for Jacobian
@@ -1951,7 +1951,7 @@ aspect CCodeGen {
 		
 		str.print(indent + "} else if (evaluation_mode == JMI_BLOCK_EVALUATE) {\n");
 		// Loop over all active variables and write the values in x to the z vector
-		for (FVariable fv : blockResidualVariables_C()) 
+		for (FVariable fv : iterationVariables()) 
 			fv.genBlockResidualEvalInit_C(nextIndent, str, solverArrayMap);
 		Set<FAbstractEquation> visitedList = new HashSet<FAbstractEquation>();
 		genBlockResidualFunctionBeforeEqns_C(nextIndent, str, inInitial, visitedList);
@@ -1962,7 +1962,7 @@ aspect CCodeGen {
 		// Write back
 		str.print(indent + "} else if (evaluation_mode == JMI_BLOCK_WRITE_BACK) {\n");
 		// Loop over all active variables and write their value to x	
-		for (FVariable fv : blockResidualVariables_C()) 
+		for (FVariable fv : iterationVariables()) 
 			fv.genBlockResidualWriteback_C(nextIndent, str, solverArrayMap);
 
 		str.print(indent + "}\n");
@@ -2048,7 +2048,7 @@ aspect CCodeGen {
 
 	public Set<ASTNode> AbstractEquationBlock.blockResidualNodes_C() {
 		Set<ASTNode> res = new LinkedHashSet<ASTNode>();
-		res.addAll(blockResidualEquations_C());
+		res.addAll(realEquations());
 		return res;
 	}
     
@@ -2066,22 +2066,6 @@ aspect CCodeGen {
 	    Set<ASTNode> res = super.blockResidualNodes_C();
 	    res.addAll(solvedEquations());
 		return res;
-	}
-	
-	public Collection<FVariable> AbstractEquationBlock.blockResidualVariables_C() {
-		return iterationVariables();
-	}
-	
-	public Collection<FAbstractEquation> AbstractEquationBlock.blockResidualEquations_C() {
-		return allEquations();
-	}
-	
-	public Collection<FAbstractEquation> EquationBlock.blockResidualEquations_C() {
-		return realEquations();
-	}
-	
-	public Collection<SimpleEquationBlock> EquationBlock.blockResidualBlocks_C() {
-		return unsolvedBlocks;
 	}
 	
 	public void AbstractEquationBlock.genBlockResidualFunctionAfterEqns_C(String indent, PrintStream str, Set<FAbstractEquation> visited) {
@@ -2106,7 +2090,7 @@ aspect CCodeGen {
 	@Override
 	public void EquationBlock.genBlockResidualFunctionEqns_C(Enumerator enumerator, String indent, PrintStream str, 
 			Set<FAbstractEquation> visited, Map<FAbstractVariable,String> solverArrayMap) {
-		for (SimpleEquationBlock block : blockResidualBlocks_C())
+		for (SimpleEquationBlock block : unsolvedBlocks())
 			block.genBlockResidualFunctionEqns_C(enumerator, indent, str, visited, solverArrayMap);
 	}
 	
@@ -2141,7 +2125,7 @@ aspect CCodeGen {
 	}
 	
 	public void AbstractEquationBlock.genBlockResidualFunctionEqnNominals_C(Enumerator enumerator, String indent, PrintStream str) {
-		for (FAbstractEquation e : blockResidualEquations_C()) {
+		for (FAbstractEquation e : realEquations()) {
 			e.genNominal_C(enumerator, indent, str, null);
 		}
 	}

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CADCodeGen.jrag
@@ -1029,7 +1029,7 @@ aspect CADCodeGen {
 		str.println("if (evaluation_mode == JMI_BLOCK_INITIALIZE) {");
 		int var_counter = 0;
 		// Loop over all unsolved variables and write their value to x	
-		for (FVariable fv : blockResidualVariables_C()) {
+		for (FVariable fv : iterationVariables()) {
 			str.print(innerIndent);
 			str.println("x["+var_counter+"] = " + fv.name_C() + ";");
 			var_counter++;
@@ -1045,7 +1045,7 @@ aspect CADCodeGen {
 		// Loop over all active variables and write the values in x to the z vector
 		var_counter = 0;
 		// Iteration is over the unsolved variables. 
-		for (FVariable fv : blockResidualVariables_C()) {
+		for (FVariable fv : iterationVariables()) {
 			str.print(innerIndent);
 			str.println("(*dz)[ jmi_get_index_from_value_ref(" + fv.valueReference() + ")-jmi->offs_real_dx] = dx[" + var_counter + "];");
 			str.print(innerIndent);
@@ -1067,7 +1067,7 @@ aspect CADCodeGen {
 		// Code that stores the evaluated dz
 		// dz entries for the solved variables are updated below as well
 		var_counter = 0;
-		for (FVariable fv : blockResidualVariables_C()) {
+		for (FVariable fv : iterationVariables()) {
 			str.print(innerIndent);
 			str.println("(*dz)[jmi_get_index_from_value_ref(" + fv.valueReference() + ")-jmi->offs_real_dx] = -(*dF)[" + var_counter + "];");
 			var_counter++;
@@ -1091,7 +1091,7 @@ aspect CADCodeGen {
 		
 		// Reset all variables. global dz is updated elsewhere and dz_active should
 		// always be reset so as not to cause incorrect seeds
-		for (FVariable fv : blockResidualVariables_C()) {
+		for (FVariable fv : iterationVariables()) {
 			str.print(innerIndent);
 			str.println("(*dz)[jmi_get_index_from_value_ref(" + fv.valueReference() + ")-jmi->offs_real_dx] = 0;");
 		}
@@ -1157,7 +1157,7 @@ aspect CADCodeGen {
      * @see AbstractEquationBlock#genBlockResidualFunctionBeforeEqns_CAD(String, PrintStream, boolean, Set<FAbstractEquation>).
      */
     public void EquationBlock.genBlockResidualFunctionBeforeEqns_CAD(String indent, PrintStream str, boolean inInitial, Set<FAbstractEquation> visited) {
-        for (SimpleEquationBlock block : blockResidualBlocks_C())
+        for (SimpleEquationBlock block : unsolvedBlocks())
             block.genBlockResidualFunctionBeforeEqns_CAD(indent, str, inInitial, visited);
     }
     
@@ -1215,7 +1215,7 @@ aspect CADCodeGen {
      * @see AbstractEquationBlock.genBlockResidualFunctionEqns_CAD(Enumerator, String, PrintStream, Set<FAbstractEquation>).
      */
     public void EquationBlock.genBlockResidualFunctionEqns_CAD(Enumerator enumerator, String indent, PrintStream str, Set<FAbstractEquation> visited) {
-        for (SimpleEquationBlock block : blockResidualBlocks_C())
+        for (SimpleEquationBlock block : unsolvedBlocks())
             block.genBlockResidualFunctionEqns_CAD(enumerator, indent, str, visited);
     }
 	
@@ -1224,7 +1224,7 @@ aspect CADCodeGen {
 	 */
 	@Override
 	public void TornEquationBlock.genBlockResidualFunctionEqns_CAD(Enumerator enumerator, String indent, PrintStream str, Set<FAbstractEquation> visited) {
-		for (SimpleEquationBlock block : blockResidualBlocks_C())
+		for (SimpleEquationBlock block : unsolvedBlocks())
 			block.genBlockResidualFunctionEqns_CAD(enumerator, indent, str, visited);
 	}
 	

trunk/Compiler/ModelicaFrontEnd/src/jastadd/BLT.jrag
@@ -20,6 +20,7 @@ import java.util.Collections;
 
 import org.jmodelica.util.exceptions.BLTException;
 import org.jmodelica.util.ErrorCheckType;
+import org.jmodelica.util.ChainedIterable;
 
 aspect BLT{
 	
@@ -1088,11 +1089,9 @@ aspect BLT{
             for (FAbstractEquation fe : eqns) {
                 Set<FVariable> RLHS = fe.referencedFVariablesInLHS();
                 Set<FVariable> RRHS = fe.referencedFVariablesInRHS();
-                Iterator<FVariable> it = new ChainedIterator<FVariable>(RLHS.iterator(), RRHS.iterator());
                 FExp l = ((FEquation)fe).getLeft();
                 FExp r = ((FEquation)fe).getRight();
-                while (it.hasNext()) {
-                    FVariable fv = it.next();
+                for (FVariable fv : new ChainedIterable<FVariable>(RLHS, RRHS)) {
                     Integer colObj = columnMap.get(fv);
                     if (colObj == null)
                         continue;

trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/ChainedIterable.java
@@ -1,0 +1,18 @@
+package org.jmodelica.util;
+
+import java.util.Iterator;
+
+public class ChainedIterable<T> implements Iterable<T> {
+    private Iterable<? extends T>[] its;
+    public ChainedIterable(Iterable<? extends T> ... its) {
+        this.its = its;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        Iterator<? extends T>[] iterators = new Iterator[its.length];
+        for (int i = 0; i < its.length; i++)
+            iterators[i] = its[i].iterator();
+        return new ChainedIterator(iterators);
+    }
+}

commit 08015eb698833dfc5a6ba7fb5fe5424b4656aa2d

Fixed error that was introduced in r5734. Ticket #3320.
git-svn-id: https://svn.jmodelica.org@5739 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen.jrag
@@ -2048,7 +2048,7 @@ aspect CCodeGen {
 
 	public Set<ASTNode> AbstractEquationBlock.blockResidualNodes_C() {
 		Set<ASTNode> res = new LinkedHashSet<ASTNode>();
-		res.addAll(realEquations());
+		res.addAll(allEquations());
 		return res;
 	}
     

trunk/Compiler/ModelicaCBackEnd/src/test/CCodeGenTests.mo
@@ -567,6 +567,50 @@ $C_ode_initialization$
 ")})));
 end CCodeGenTest17;
 
+model CCodeGenTest18
+    parameter Boolean[3] table = {false, true, false};
+    Boolean x;
+    Integer index = integer(time);
+algorithm
+    if index < 4 then
+        x := table[index];
+    else
+        x := true;
+    end if;
+    
+    annotation(__JModelica(UnitTesting(tests={
+        CCodeGenTestCase(
+            name="CCodeGenTest18",
+            description="Test generation of temporary variables",
+            template="
+$C_ode_derivatives$
+",
+			generatedCode="
+    JMI_ARRAY_STATIC(tmp_1, 3, 1)
+    model_ode_guards(jmi);
+/************* ODE section *********/
+/************ Real outputs *********/
+/****Integer and boolean outputs ***/
+/**** Other variables ***/
+    _index_4 = COND_EXP_EQ(LOG_EXP_OR(LOG_EXP_OR(_sw(0), _sw(1)), _atInitial), JMI_TRUE, floor(_time), pre_index_4);
+    JMI_ARRAY_STATIC_INIT_1(tmp_1, 3)
+    jmi_array_ref_1(tmp_1, 1) = _table_1_0;
+    jmi_array_ref_1(tmp_1, 2) = _table_2_1;
+    jmi_array_ref_1(tmp_1, 3) = _table_3_2;
+    _x_3 = pre_x_3;
+    if (COND_EXP_LT(_index_4, 4, JMI_TRUE, JMI_FALSE)) {
+        JMI_ARRAY_STATIC_INIT_1(tmp_1, 3)
+        jmi_array_ref_1(tmp_1, 1) = _table_1_0;
+        jmi_array_ref_1(tmp_1, 2) = _table_2_1;
+        jmi_array_ref_1(tmp_1, 3) = _table_3_2;
+        _x_3 = func_temp_1_exp(_index_4, tmp_1);
+    } else {
+        _x_3 = JMI_TRUE;
+    }
+/********* Write back reinits *******/
+
+")})));
+end CCodeGenTest18;
 
 model CLogExp1
  Boolean x = true;