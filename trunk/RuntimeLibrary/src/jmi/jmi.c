5019d078ba56572ada2717327d793da70155d718
chria <chria@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2014-01-24 08:57:54 +0000

    Added a log message for the states when performing a model evaluation. Related to ticket:3418
    git-svn-id: https://svn.jmodelica.org@5864 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/RuntimeLibrary/src/jmi/jmi.c
+++ trunk/RuntimeLibrary/src/jmi/jmi.c
@@ -434,6 +434,7 @@ int jmi_ode_derivatives(jmi_t* jmi) {
     if((jmi->jmi_callbacks.log_options.log_level >= 5)) {
         node = jmi_log_enter_fmt(jmi->log, logInfo, "EquationSolve", 
                                  "Model equations evaluation invoked at <t:%E>", t[0]);
+        jmi_log_reals(jmi->log, node, logInfo, "States", jmi_get_real_x(jmi), jmi->n_real_x);
     }
 
     jmi->block_level = 0; /* to recover from errors */
