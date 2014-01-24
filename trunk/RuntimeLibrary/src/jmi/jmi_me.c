f88c59a2631489b94703828ec34b156cee1aedf9
chria <chria@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2014-01-24 14:31:07 +0000

    Added an extra try if evaluation of the derivatives failed in get_derivatives and get_event_indicators. Related to ticket:3425
    git-svn-id: https://svn.jmodelica.org@5867 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/RuntimeLibrary/src/jmi/jmi_me.c
+++ trunk/RuntimeLibrary/src/jmi/jmi_me.c
@@ -638,16 +638,37 @@ int jmi_get_derivatives(jmi_t* jmi, jmi_real_t derivatives[] , size_t nx) {
     if (jmi->recomputeVariables == 1) {
         retval = jmi_ode_derivatives(jmi);
         if(retval != 0) {
-            if (jmi->jmi_callbacks.log_options.log_level >= 5){
-                jmi_log_leave(jmi->log, node);
-            }
-            jmi_log_node(jmi->log, logError, "Error",
-                "Evaluating the derivatives failed at <t:%g>", jmi_get_t(jmi)[0]);
+            
+            /* Store the current time and states */
+            jmi_real_t time = *(jmi_get_t(jmi));
+            jmi_real_t *x = jmi->jmi_callbacks.allocate_memory(jmi->n_real_x, sizeof(jmi_real_t));
+            memcpy(x, jmi_get_real_x(jmi), jmi->n_real_x*sizeof(jmi_real_t));
+            
+            jmi_log_node(jmi->log, logWarning, "Warning",
+                "Evaluating the derivatives failed at <t:%g>, retrying with restored values.", jmi_get_t(jmi)[0]);
             /* If it failed, reset to the previous succesful values */
             jmi_reset_last_successful_values(jmi);
-            return -1;
-        }
+            
+            /* Restore the current time and states */
+            memcpy (jmi_get_real_x(jmi), x, jmi->n_real_x*sizeof(jmi_real_t));
+            *(jmi_get_t(jmi)) = time;
+            jmi->jmi_callbacks.free_memory(x);
+            
+            /* Try again */
+            retval = jmi_ode_derivatives(jmi);
+            if(retval != 0) {
+                if (jmi->jmi_callbacks.log_options.log_level >= 5){
+                    jmi_log_leave(jmi->log, node);
+                }
+                jmi_log_node(jmi->log, logError, "Error",
+                    "Evaluating the derivatives failed at <t:%g>", jmi_get_t(jmi)[0]);
+                /* If it failed, reset to the previous succesful values */
+                jmi_reset_last_successful_values(jmi);
+            
+                return -1;
+            }
         jmi->recomputeVariables = 0;
+        }
     }
     memcpy (derivatives, jmi_get_real_dx(jmi), nx*sizeof(jmi_real_t));
     
@@ -689,19 +710,40 @@ int jmi_get_event_indicators(jmi_t* jmi, jmi_real_t eventIndicators[], size_t ni
                                 "Call to get event indicators at <t:%g>.", jmi_get_t(jmi)[0]);
     }
     
-
     if (jmi->recomputeVariables == 1) {
         retval = jmi_ode_derivatives(jmi);
         if(retval != 0) {
-            if (jmi->jmi_callbacks.log_options.log_level >= 5){
-                jmi_log_leave(jmi->log, node);
-            }
-            jmi_log_node(jmi->log, logError, "Error",
-                "Evaluating the derivatives failed while evaluating the event indicators at <t:%g>", jmi_get_t(jmi)[0]);
+            
+            /* Store the current time and states */
+            jmi_real_t time = *(jmi_get_t(jmi));
+            jmi_real_t *x = jmi->jmi_callbacks.allocate_memory(jmi->n_real_x, sizeof(jmi_real_t));
+            memcpy(x, jmi_get_real_x(jmi), jmi->n_real_x*sizeof(jmi_real_t));
+            
+            jmi_log_node(jmi->log, logWarning, "Warning",
+                "Evaluating the derivatives failed while evaluating the event indicators at <t:%g>, retrying with restored values.", jmi_get_t(jmi)[0]);
+            /* If it failed, reset to the previous succesful values */
             jmi_reset_last_successful_values(jmi);
-            return -1;
-        }
+            
+            /* Restore the current time and states */
+            memcpy (jmi_get_real_x(jmi), x, jmi->n_real_x*sizeof(jmi_real_t));
+            *(jmi_get_t(jmi)) = time;
+            jmi->jmi_callbacks.free_memory(x);
+            
+            /* Try again */
+            retval = jmi_ode_derivatives(jmi);
+            if(retval != 0) {
+                if (jmi->jmi_callbacks.log_options.log_level >= 5){
+                    jmi_log_leave(jmi->log, node);
+                }
+                jmi_log_node(jmi->log, logError, "Error",
+                    "Evaluating the derivatives failed while evaluating the event indicators at <t:%g>", jmi_get_t(jmi)[0]);
+                /* If it failed, reset to the previous succesful values */
+                jmi_reset_last_successful_values(jmi);
+            
+                return -1;
+            }
         jmi->recomputeVariables = 0;
+        }
     }
     retval = jmi_dae_R_perturbed(jmi,eventIndicators);
     
