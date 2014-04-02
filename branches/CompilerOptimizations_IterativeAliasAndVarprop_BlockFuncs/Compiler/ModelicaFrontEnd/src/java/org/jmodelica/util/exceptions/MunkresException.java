3c6eb27c40d632d155981c93ffbede0c35cdf3a1
jmattsson <jmattsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2014-04-02 07:32:30 +0000

    Deleting branch to prepare to recreate (attempt without deleting failed, see r6161). (#3412)
    git-svn-id: https://svn.jmodelica.org@6162 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/CompilerOptimizations_IterativeAliasAndVarprop_BlockFuncs/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/exceptions/MunkresException.java
+++
@@ -1,9 +0,0 @@
-package org.jmodelica.util.exceptions;
-
-public class MunkresException extends IndexReductionException {
-    private static final long serialVersionUID = 1;
-
-    public MunkresException(String message) {
-        super(message);
-    }
-}
