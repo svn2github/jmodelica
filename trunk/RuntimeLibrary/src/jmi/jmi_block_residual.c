7c24daa2a360fcf553cae31054cb2abaa3a2cd74
toivo <toivo@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2014-06-24 07:35:00 +0000

    Rename log node `<IntegratedSolverSolve>` to `<SolverInvocation>` for #3708.
    git-svn-id: https://svn.jmodelica.org@6516 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/RuntimeLibrary/src/jmi/jmi_block_residual.c
+++ trunk/RuntimeLibrary/src/jmi/jmi_block_residual.c
@@ -305,8 +305,8 @@ int jmi_solve_block_residual(jmi_block_residual_t * block) {
     }
 
     {
-        jmi_log_node_t node = jmi_log_enter_fmt(jmi->log, logInfo, "IntegratedSolverSolve",
-                                  "Starting integrated solver at <t:%E> in <block:%s> with <nvars:%d> variables",
+        jmi_log_node_t node = jmi_log_enter_fmt(jmi->log, logInfo, "SolverInvocation",
+                                  "Starting solver at <t:%E> in <block:%s> with <nvars:%d> variables",
                                   jmi_get_t(jmi)[0], block->block_solver->label, block->block_solver->n);
         ef = jmi_block_solver_solve(block->block_solver,jmi_get_t(jmi)[0],
                  (jmi->atInitial == JMI_TRUE || jmi->atEvent == JMI_TRUE) &&
