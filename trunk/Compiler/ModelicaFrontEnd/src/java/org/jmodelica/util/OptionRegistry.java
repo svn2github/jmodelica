e9cb847bb1ec277d402aa8a6bf1167f69e125fe8
jylikiiskila <jylikiiskila@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2014-10-06 14:33:17 +0000

    #4004 Resetting default value of Brent usage.
    git-svn-id: https://svn.jmodelica.org@6857 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/OptionRegistry.java
+++ trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/OptionRegistry.java
@@ -472,7 +472,7 @@ abstract public class OptionRegistry {
         USE_BRENT_IN_1D
             ("use_Brent_in_1d",
                 runtime,
-                true,
+                false,
                 "Use Brent search to improve accuracy in solution of 1D non-linear equations."),
         BLOCK_SOLVER_EXPERIMENTAL_MODE
             ("block_solver_experimental_mode",
