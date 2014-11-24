svn2github/jmodelica

commit 5cb3bd7c6eed7add1151ff6b4d1a57b06e5eb968

#4069 Refactored incidence calculations in BiPGraph.
git-svn-id: https://svn.jmodelica.org@7025 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/structural/Graphs.jadd
@@ -33,9 +33,6 @@ import org.jmodelica.util.FilteredIterator;
 
 aspect Graphs {
 
-syn boolean FAbstractEquation.lhsVarsAreSolved() = false;
-eq FAlgorithm.lhsVarsAreSolved()                 = true;
-
 public abstract class AbstractBiPGraph<E extends AbstractEq<E, V>, V extends AbstractVar<E, V>, C extends SCCBlock<E, V>> {
 
     private static final String DEFAULT_EQ_PREFIX = "eq";
@@ -248,33 +245,13 @@ public abstract class AbstractBiPGraph<E extends AbstractEq<E, V>, V extends Abs
             if (n_existing != 0 && n_existing != n_eq)
                 throw new IllegalArgumentException("Equation already exist in BiPGraph but with different number of scalars, previously " + n_existing + ", now " + n_eq + ", equation:\n" + e);
             
-            // For equations that counts as several scalar equations (function calls, algorithms), 
-            // the left hand side variables depends on all the right-hand side ones, 
-            // but not on the other left-hand side ones
-            Set<FVariable> rhsVars = emptySet;
-            FVariable lhsVars[] = new FVariable[n_eq];
-            if (n_eq > 1) {
-                rhsVars = e.referencedFVariablesInRHS();
-                int i = 0;
-                for (FVariable fv : e.referencedFVariablesInLHS())
-                    lhsVars[i++] = fv;
-                if (e.lhsVarsAreSolved())
-                    rhsVars.removeAll(Arrays.asList(lhsVars));
-            }
+            IncidenceMap incMap = createIncidenceMap(e, Collections.<V>emptySet());
+            
             if (n_existing == 0) {
                 groupMembers = new ArrayList<E>();
                 for (int i = n_eq - 1; 0 <= i; i--) { // During tarjan the order will be reversed,
                                                       // need to reverse here to preserve the initial order.
-                    FTypePrefixVariability variability;
-                    if (lhsVars[i] != null) {
-                        variability = lhsVars[i].variability();
-                        if (variability.parameterOrLess())
-                            variability = lhsVars[i].type().funcOutputVariability();
-                        else
-                            variability = lhsVars[i].variability().combineDown(variability);
-                    } else {
-                        variability = e.variability();
-                    }
+                    FTypePrefixVariability variability = incMap.variability(i);
                     boolean add = false;
                     for (VarType varType : variableTypes)
                         if (varType.isOk(variability))
@@ -291,18 +268,95 @@ public abstract class AbstractBiPGraph<E extends AbstractEq<E, V>, V extends Abs
                 }
             }
             for (E eqn : groupMembers) {
-                for (FVariable fVar : ASTNode.lookupFVariablesInSet(e.findFIdUseExpsInTree())) {
-                    V var = getVariable(fVar.name());
-                    if (var == null)
-                        continue;
-                    int i = eqn.groupNumber();
-                    if (lhsVars[i] == null || lhsVars[i] == fVar || rhsVars.contains(fVar))
-                        addEdge(eqn, var);
+                incMap.addEdges(eqn);
+            }
+        }
+    }
+    
+    protected IncidenceMap createIncidenceMap(FAbstractEquation e, Set<V> additionalDependencies) {
+        if (e.numScalarEquations() > 1) {
+            return new MultiIncidenceMap(e, additionalDependencies);
+        } else {
+            return new IncidenceMap(e, additionalDependencies);
+        }
+    }
+    
+    /**
+     * Handles incidences between variables in an FAbstractEquation.
+     * The base case is a scalar equation where all variables are 
+     * dependent on each other.
+     */
+    protected class IncidenceMap {
+        protected Set<V> vars;
+        protected FAbstractEquation equation;
+        
+        public IncidenceMap(FAbstractEquation e, Set<V> additionalDeps) {
+            equation = e;
+            vars = lookupVarsInSet(equation.findFIdUseExpsInTree());
+            vars.addAll(additionalDeps);
+        }
+        
+        public FTypePrefixVariability variability(int i) {
+            return equation.variability();
+        }
+        
+        public void addEdges(E eqn) {
+            for (V var : vars) {
+                addEdge(eqn, var);
+            }
+        }
+    }
+    
+    /**
+     * Handles incidences between variables in a non-scalar FAbstractEquation.
+     * Isolates LHS variables from each other.
+     */
+    protected class MultiIncidenceMap extends IncidenceMap {
+        protected Map<Integer, V> lhs;
+        protected Set<V> rhs;
+        
+        public MultiIncidenceMap(FAbstractEquation e, Set<V> additionalDeps) {
+            super(e, additionalDeps);
+            Set<V> lhsSet = lookupVarsInSet(equation.FIdUseExpsInLHS());
+            rhs = lookupVarsInSet(equation.FIdUseExpsInRHS());
+            if (equation instanceof FAlgorithm) {
+                rhs.removeAll(lhsSet);
+            }
+            rhs.addAll(additionalDeps);
+            
+            lhs = new HashMap<Integer, V>();
+            int i = 0;
+            for (V var : lhsSet) {
+                lhs.put(i++, var);
+            }
+        }
+        
+        @Override
+        public FTypePrefixVariability variability(int i) {
+            V var = lhs.get(i);
+            if (var == null) {
+                return super.variability(i);
+            }
+            FTypePrefixVariability v = var.getVariable().variability();
+            if (v.parameterOrLess()) {
+                v = var.getVariable().type().funcOutputVariability();
+            }
+            return v;
+        }
+        
+        @Override
+        public void addEdges(E eqn) {
+            int i = eqn.groupNumber();
+            V lhsVar = lhs.get(i);
+            for (V var : vars) {
+                if (var == lhsVar || rhs.contains(var)) {
+                    addEdge(eqn, var);
                 }
             }
         }
     }
-
+    
+    
     public boolean match(E e, V v) {
         if (!canMatch(e, v))
             return false;

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/structural/IndexReduction.jrag
@@ -709,17 +709,9 @@ aspect IndexReduction {
         // Construct a list of all the variables that should be common in all
         // of the equations.
         // Always add variables from extra equations
-        Set<IndexReductionVar> rhsVars = new HashSet<IndexReductionVar>();
+        Set<IndexReductionVar> additionalDependencies = new HashSet<IndexReductionVar>();
         for (FAbstractEquation extraFEqn : extraEquations)
-            rhsVars.addAll(lookupVarsInSet(extraFEqn.referencedFIdUseExps()));
-        boolean multipleEqns = originalDiffedFEqns.size() > 1 || baseEqn.getEquation().numScalarEquations() > 1;
-        if (multipleEqns) {
-            for (FAbstractEquation eqn : originalDiffedFEqns)
-                rhsVars.addAll(lookupVarsInSet(eqn.FIdUseExpsInRHS()));
-        } else {
-            for (FAbstractEquation eqn : originalDiffedFEqns)
-                rhsVars.addAll(lookupVarsInSet(eqn.findFIdUseExpsInTree()));
-        }
+            additionalDependencies.addAll(lookupVarsInSet(extraFEqn.referencedFIdUseExps()));
         
         Collection<IndexReductionEq> diffedOriginalEqs = new ArrayList<IndexReductionEq>();
         
@@ -739,35 +731,18 @@ aspect IndexReduction {
         Iterator<IndexReductionEq> eqnIt = baseEqn.getGroupMembers().iterator();
         for (FAbstractEquation newEqn : originalDiffedFEqns) {
             result.addEquation(newEqn);
-            // Create list of variables that are unique for each BiP equation
-            Map<Integer, IndexReductionVar> lhsVars = Collections.emptyMap();
-            if (multipleEqns) {
-                lhsVars = new HashMap<Integer, IndexReductionVar>();
-                int i = 0;
-                for (IndexReductionVar var : lookupVarsInSet(newEqn.FIdUseExpsInLHS()))
-                    lhsVars.put(i++, var);
-                if (newEqn.lhsVarsAreSolved())
-                    rhsVars.removeAll(lhsVars.values());
-            }
+            
+            IncidenceMap incMap = createIncidenceMap(newEqn, additionalDependencies);
             
             // Build new differentiated BiP equations for each scalar.
             for (int i = 0; i < newEqn.numScalarEquations(); i++) {
                 IndexReductionEq original = eqnIt.next();
                 ASTNode.log.info("About to add equation: %s", newEqn);
                 // Add the differentiated equation
-                IndexReductionEq diffedEqn = addEquation(newEqn, i, newEqn.variability(), original.getName() + "_d");
+                IndexReductionEq diffedEqn = addEquation(newEqn, i, incMap.variability(i), original.getName() + "_d");
                 
-                // Add special edges
-                IndexReductionVar var = lhsVars.get(i);
-                if (var != null) {
-                    ASTNode.log.info("*** %s", var);
-                    addEdge(diffedEqn, var);
-                }
-                // Add common edges
-                for (IndexReductionVar rhsVar : rhsVars) {
-                    ASTNode.log.info("*** %s", rhsVar);
-                    addEdge(diffedEqn, rhsVar);
-                }
+                incMap.addEdges(diffedEqn);
+                
                 diffedOriginalEqs.add(diffedEqn);
                 diffedEqn.setExtraEquations(extraEquations);
                 

commit 78029f160f4c4bd33a77d0f13cb737ad7afe130f

#4069 Fixes bugs introduced in r7025
git-svn-id: https://svn.jmodelica.org@7034 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/structural/Graphs.jadd
@@ -239,8 +239,8 @@ public abstract class AbstractBiPGraph<E extends AbstractEq<E, V>, V extends Abs
         Set<FVariable> emptySet = Collections.emptySet();
         for (FAbstractEquation e : eqns) {
             
-            IncidenceMap incMap = createIncidenceMap(e, Collections.<V>emptySet());
-            int n_eq = incMap.numEqs(variableTypes);
+            IncidenceMap incMap = createIncidenceMap(e, Collections.<V>emptySet(), variableTypes);
+            int n_eq = incMap.numEqs();
             
             Collection<E> groupMembers = getEquations(e);
             int n_existing = groupMembers.size();
@@ -261,14 +261,15 @@ public abstract class AbstractBiPGraph<E extends AbstractEq<E, V>, V extends Abs
                 }
             }
             for (E eqn : groupMembers) {
-                incMap.addEdges(eqn);
+                incMap.addEdges(eqn, false);
             }
         }
     }
     
-    protected IncidenceMap createIncidenceMap(FAbstractEquation e, Set<V> additionalDependencies) {
+    
+    protected IncidenceMap createIncidenceMap(FAbstractEquation e, Set<V> additionalDependencies, EnumSet<VarType> variableTypes) {
         if (e.numScalarEquations() > 1) {
-            return new MultiIncidenceMap(e, additionalDependencies);
+            return new MultiIncidenceMap(e, additionalDependencies, variableTypes);
         } else {
             return new IncidenceMap(e, additionalDependencies);
         }
@@ -282,25 +283,30 @@ public abstract class AbstractBiPGraph<E extends AbstractEq<E, V>, V extends Abs
     protected class IncidenceMap {
         protected Set<V> vars;
         protected FAbstractEquation equation;
+        protected int neq;
         
         public IncidenceMap(FAbstractEquation e, Set<V> additionalDeps) {
             equation = e;
             vars = lookupVarsInSet(equation.findFIdUseExpsInTree());
             vars.addAll(additionalDeps);
+            neq = equation.numScalarEquations();
         }
         
         public FTypePrefixVariability variability(int i) {
             return equation.variability();
         }
         
-        public void addEdges(E eqn) {
+        public void addEdges(E eqn, boolean log) {
             for (V var : vars) {
+                if (log) {
+                    ASTNode.log.info("*** %s", var);
+                }
                 addEdge(eqn, var);
             }
         }
         
-        public int numEqs(EnumSet<VarType> variableTypes) {
-            return equation.numScalarEquations();
+        public int numEqs() {
+            return neq;
         }
     }
     
@@ -312,20 +318,52 @@ public abstract class AbstractBiPGraph<E extends AbstractEq<E, V>, V extends Abs
         protected Map<Integer, V> lhs;
         protected Set<V> rhs;
         
-        public MultiIncidenceMap(FAbstractEquation e, Set<V> additionalDeps) {
+        public MultiIncidenceMap(FAbstractEquation e, Set<V> additionalDeps, EnumSet<VarType> variableTypes) {
             super(e, additionalDeps);
-            Set<V> lhsSet = lookupVarsInSet(equation.FIdUseExpsInLHS());
+            Set<FIdUseExp> lhsUses = equation.FIdUseExpsInLHS();
+            Set<V> lhsSet = lookupVarsInSet(lhsUses);
             rhs = lookupVarsInSet(equation.FIdUseExpsInRHS());
             if (equation instanceof FAlgorithm) {
                 rhs.removeAll(lhsSet);
             }
             rhs.addAll(additionalDeps);
             
+            if (equation instanceof FFunctionCallEquation) {
+                buildLhsFunction(variableTypes, lhsUses);
+            } else {
+                lhs = new HashMap<Integer, V>();
+                int i = 0;
+                for (V var : lhsSet) {
+                    lhs.put(i++, var);
+                }
+                neq = equation.numScalarEquations();
+            }
+        }
+        
+        private void buildLhsFunction(EnumSet<VarType> variableTypes, Set<FIdUseExp> lhsUses) {
             lhs = new HashMap<Integer, V>();
             int i = 0;
-            for (V var : lhsSet) {
+            for (FIdUseExp use : lhsUses) {
+                if (variableTypes != null) {
+                    FAbstractVariable fv = use.myFV();
+                    if (fv != null) {
+                        boolean add = false;
+                        for (VarType t : variableTypes) {
+                            if (t.isOk(variability(fv))) {
+                                add = true;
+                                break;
+                            }
+                        }
+                        if (!add) {
+                            continue;
+                        }
+                    }
+                }
+                
+                V var = getVariable(use.name());
                 lhs.put(i++, var);
             }
+            neq = lhs.size();
         }
         
         @Override
@@ -337,7 +375,7 @@ public abstract class AbstractBiPGraph<E extends AbstractEq<E, V>, V extends Abs
             return variability(var.getVariable());
         }
         
-        private FTypePrefixVariability variability(FVariable fv) {
+        private FTypePrefixVariability variability(FAbstractVariable fv) {
             FTypePrefixVariability v = fv.variability();
             if (v.parameterOrLess()) {
                 v = fv.type().funcOutputVariability();
@@ -346,30 +384,17 @@ public abstract class AbstractBiPGraph<E extends AbstractEq<E, V>, V extends Abs
         }
         
         @Override
-        public void addEdges(E eqn) {
+        public void addEdges(E eqn, boolean log) {
             int i = eqn.groupNumber();
             V lhsVar = lhs.get(i);
             for (V var : vars) {
                 if (var == lhsVar || rhs.contains(var)) {
+                    if (log) {
+                        ASTNode.log.info("*** %s", var);
+                    }
                     addEdge(eqn, var);
                 }
             }
-        }
-        
-        @Override
-        public int numEqs(EnumSet<VarType> variableTypes) {
-            int n = 0;
-            Set<FVariable> fvs = ASTNode.lookupFVariablesInSet(equation.FIdUseExpsInLHS());
-            for (FVariable fv : fvs) {
-                for (VarType t : variableTypes) {
-                    if (t.isOk(variability(fv))) {
-                        n++;
-                        break;
-                    }
-                }
-            }
-            n += equation.numScalarEquations() - fvs.size();
-            return n;
         }
     }
     

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/structural/IndexReduction.jrag
@@ -732,7 +732,7 @@ aspect IndexReduction {
         for (FAbstractEquation newEqn : originalDiffedFEqns) {
             result.addEquation(newEqn);
             
-            IncidenceMap incMap = createIncidenceMap(newEqn, additionalDependencies);
+            IncidenceMap incMap = createIncidenceMap(newEqn, additionalDependencies, null);
             
             // Build new differentiated BiP equations for each scalar.
             for (int i = 0; i < newEqn.numScalarEquations(); i++) {
@@ -741,7 +741,7 @@ aspect IndexReduction {
                 // Add the differentiated equation
                 IndexReductionEq diffedEqn = addEquation(newEqn, i, incMap.variability(i), original.getName() + "_d");
                 
-                incMap.addEdges(diffedEqn);
+                incMap.addEdges(diffedEqn, true);
                 
                 diffedOriginalEqs.add(diffedEqn);
                 diffedEqn.setExtraEquations(extraEquations);

trunk/Compiler/ModelicaCBackEnd/src/test/CCodeGenTests.mo
@@ -9859,6 +9859,63 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
 ")})));
 end BlockTest17;
 
+model BlockTest18
+    function f
+        input Real x;
+        output Real a2;
+        output Integer a1;
+      algorithm
+        a1 := 3;
+        a2 := x;
+        annotation(smoothOrder=1,Inline=false);
+    end f;
+    Integer a;
+    parameter Real b = 1;
+    Real x;
+  equation
+    (b,a) = f(x);
+
+    annotation(__JModelica(UnitTesting(tests={
+        CCodeGenTestCase(
+            name="BlockTest18",
+            description="Mixed function call equation in block",
+            inline_functions="none",
+            template="$C_dae_blocks_residual_functions$",
+            generatedCode="
+static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int evaluation_mode) {
+    /***** Block: 1 *****/
+    jmi_real_t** res = &residual;
+    int ef = 0;
+    jmi_ad_var_t tmp_1;
+    jmi_ad_var_t tmp_2;
+    if (evaluation_mode == JMI_BLOCK_NOMINAL) {
+    } else if (evaluation_mode == JMI_BLOCK_START) {
+    } else if (evaluation_mode == JMI_BLOCK_MIN) {
+    } else if (evaluation_mode == JMI_BLOCK_MAX) {
+    } else if (evaluation_mode == JMI_BLOCK_VALUE_REFERENCE) {
+        x[0] = 1;
+    } else if (evaluation_mode == JMI_BLOCK_NON_REAL_VALUE_REFERENCE) {
+        x[0] = 268435459;
+    } else if (evaluation_mode == JMI_BLOCK_ACTIVE_SWITCH_INDEX) {
+    } else if (evaluation_mode == JMI_BLOCK_EQUATION_NOMINAL) {
+    } else if (evaluation_mode == JMI_BLOCK_INITIALIZE) {
+        x[0] = _x_2;
+    } else if (evaluation_mode & JMI_BLOCK_EVALUATE || evaluation_mode & JMI_BLOCK_WRITE_BACK) {
+        if ((evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) == 0) {
+            _x_2 = x[0];
+        }
+        func_CCodeGenTests_BlockTest18_f_def0(_x_2, &tmp_1, &tmp_2);
+        if (evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) {
+            _a_0 = (tmp_2);
+        }
+        if (evaluation_mode & JMI_BLOCK_EVALUATE) {
+            (*res)[0] = tmp_1 - (_b_1);
+        }
+    }
+    return ef;
+}
+")})));
+end BlockTest18;
 
 
 model Algorithm1