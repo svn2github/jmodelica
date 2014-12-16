3cc16e07f70a754529434a227db2ea448b10989e
jkampe <jkampe@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2014-12-16 07:31:10 +0000

    #4132 Disables execution halt in XERBLA
    git-svn-id: https://svn.jmodelica.org@7149 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/ThirdParty/Blas/xerbla.f
+++ trunk/ThirdParty/Blas/xerbla.f
@@ -32,7 +32,7 @@
 *
       WRITE (*,FMT=9999) SRNAME,INFO
 *
-      STOP
+*      STOP *** TODO: Call error handling #4132
 *
  9999 FORMAT (' ** On entry to ',A6,' parameter number ',I2,' had ',
      +       'an illegal value')
