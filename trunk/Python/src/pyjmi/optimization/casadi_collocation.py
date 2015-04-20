svn2github/jmodelica

commit 970608fe3917eb6bcc9da82a828236c54b949cd8

Record more back tracking info in `LocalDAECollocator` for #4153:
* Record mapping from model variables to nlp variables (`xx`) in `add_named_var`
* Add method `_assemble_back_tracking_info` to create reverse mappings for variables and equations
* Rename field `c_sources` to `c_dests` since it records the destination constraints of different equations


git-svn-id: https://svn.jmodelica.org@7510 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Python/src/pyjmi/optimization/casadi_collocation.py
@@ -992,6 +992,7 @@ class LocalDAECollocator(CasadiCollocator):
         self._create_constraints_and_cost()
         self._create_blocking_factors_constraints_and_cost()
         self._compute_bounds_and_init()
+        self._assemble_back_tracking_info()
         self._create_solver()
 
     def _create_model_variable_structures(self):
@@ -1304,8 +1305,19 @@ class LocalDAECollocator(CasadiCollocator):
         to record the source information.
         """
         if kind == 'xx':
+            # Record back tracking information
+            if var not in self.xx_dests:
+                dest = self.xx_dests[var] = {'i':[], 'k':[], 'inds':[]}
+            else:
+                dest = self.xx_dests[var]
+
+            dest['i'].append(i)
+            dest['k'].append(k)
+            dest['inds'].append(self.n_named_xx)
+            
             self.n_named_xx += 1
         else:
+            # Consider: do we want to record parameters tracking info as well?
             assert kind == 'pp'
             self.n_named_pp += 1
 
@@ -1438,6 +1450,10 @@ class LocalDAECollocator(CasadiCollocator):
         # Count the number of named variables to see that they match up even
         # if named_vars is off - also needed for back tracking of variables
         self.n_named_xx = 0
+        # Create storage to track connection between nlp variables and model variables
+        # NB: internal format of xx_dests is subject to change!
+        self.xx_dests = {} # map from model variables to their xx instances
+
         if self.named_vars:
             self.named_xx = named_xx = []
 
@@ -3192,21 +3208,21 @@ class LocalDAECollocator(CasadiCollocator):
         n_eq = pos[1] - pos[0]
         if n_eq == 0:
             return # don't record empty residuals
-        if eqtype not in self.c_sources:
-            source = self.c_sources[eqtype] = {'kind':kind, 'n_eq':n_eq, 'i':[], 'k':[], 'inds':[]}
+        if eqtype not in self.c_dests:
+            dest = self.c_dests[eqtype] = {'kind':kind, 'n_eq':n_eq, 'i':[], 'k':[], 'inds':[]}
         else:
-            source = self.c_sources[eqtype]
+            dest = self.c_dests[eqtype]
         
-        assert source['kind'] == kind, ("Residual type '" + eqtype +
-            "' previously recorded as kind '" + source['kind'] +
+        assert dest['kind'] == kind, ("Residual type '" + eqtype +
+            "' previously recorded as kind '" + dest['kind'] +
             "' cannot record it as kind '" + kind + "'")
-        assert source['n_eq'] == n_eq, ("Residual type '" + eqtype +
-            "' previously recorded with " + repr(source['n_eq']) +
+        assert dest['n_eq'] == n_eq, ("Residual type '" + eqtype +
+            "' previously recorded with " + repr(dest['n_eq']) +
             "equations per point, cannot record it with " + repr(n_eq))
 
-        source['i'].append(i)
-        source['k'].append(k)
-        source['inds'].append(N.arange(pos[0], pos[1], dtype=N.int))
+        dest['i'].append(i)
+        dest['k'].append(k)
+        dest['inds'].append(N.arange(pos[0], pos[1], dtype=N.int))
 
     def add_c_eq(self, eqtype, constr, i=-1, k=-1):
         """
@@ -3328,8 +3344,9 @@ class LocalDAECollocator(CasadiCollocator):
         # Create constraint storage
         self.c_e = c_e = casadi.MX()
         self.c_i = c_i = casadi.MX()
-        # NB: internal format of c_e_sources/c_i_sources is subject to change!
-        self.c_sources = {}
+        # Create storage to track connection between nlp constraints and equations
+        # NB: internal format of c_dests is subject to change!
+        self.c_dests = {} # map from equation types to nlp constraints
 
         # Initial conditions
         i = 1
@@ -4276,6 +4293,47 @@ class LocalDAECollocator(CasadiCollocator):
         self.xx_ub = xx_ub
         self.xx_init = xx_init
 
+    def _assemble_back_tracking_info(self):
+        # Create mapping from nlp constraints to model equations
+        # We need to know the number of equality and inequality constraints at this point
+        n_e = self.c_e.numel()
+        n_i = self.c_i.numel()
+        n_c = n_e + n_i
+
+        self.c_sources = {}
+        self.c_sources['eqtype'] = N.zeros(n_c, dtype=object)
+        self.c_sources['eqind'] = N.zeros(n_c, dtype=N.int)
+        self.c_sources['i'] = N.zeros(n_c, dtype=N.int)
+        self.c_sources['k'] = N.zeros(n_c, dtype=N.int)
+
+        self.c_sources['eqtype'].fill('other')
+        self.c_sources['eqind'].fill(-1)
+        self.c_sources['i'].fill(-1)
+        self.c_sources['k'].fill(-1)
+
+        for eqtype in self.get_nlp_constraint_types():
+            c_inds, time, i, k = self.get_nlp_constraint_indices(eqtype)
+            self.c_sources['eqtype'][c_inds] = eqtype
+            self.c_sources['eqind'][c_inds] = N.arange(c_inds.shape[1], dtype=N.int)
+            self.c_sources['i'][c_inds] = i[:, N.newaxis]
+            self.c_sources['k'][c_inds] = k[:, N.newaxis]
+
+        # Create mapping from nlp variables xx to model variables
+        self.xx_sources = {}
+        self.xx_sources['var'] = N.zeros(self.n_xx, dtype=object)
+        self.xx_sources['i'] = N.zeros(self.n_xx, dtype=N.int)
+        self.xx_sources['k'] = N.zeros(self.n_xx, dtype=N.int)
+
+        self.xx_sources['var'].fill(None)
+        self.xx_sources['i'].fill(-1)
+        self.xx_sources['k'].fill(-1)
+
+        for (var, dest) in self.xx_dests.iteritems():
+            v_inds = dest['inds']
+            self.xx_sources['var'][v_inds] = var
+            self.xx_sources['i'][v_inds] = dest['i']
+            self.xx_sources['k'][v_inds] = dest['k']
+
     def _create_solver(self):
         # Concatenate constraints
         constraints = casadi.vertcat([self.c_e, self.c_i])
@@ -5188,7 +5246,7 @@ class LocalDAECollocator(CasadiCollocator):
         """
         Get a list of all types of constraints that have mappings to the NLP
         """
-        return self.c_sources.keys()
+        return self.c_dests.keys()
 
     def get_nlp_constraint_indices(self, eqtype):
         """
@@ -5222,11 +5280,11 @@ class LocalDAECollocator(CasadiCollocator):
                 -1 if not applicable.
 
         """
-        if eqtype not in self.c_sources:
+        if eqtype not in self.c_dests:
             return (N.zeros((0,0), dtype=N.int), N.zeros(0),
                 N.zeros(0, dtype=N.int), N.zeros(0, dtype=N.int))
         
-        source = self.c_sources[eqtype]
+        source = self.c_dests[eqtype]
         
         indices = N.vstack(source['inds'])
         if source['kind'] == 'ineq':

commit 284d27fb1e81350c6db10e9514e8f8e73408cc97

Fix errors with some numpy versions introduced in r7510, and tidy up the interface for back tracking a bit, for #4153:
* When constructing int arrays with numpy, always use `dtype=N.int`
* Change argument order of some back tracking methods
* Add optional `eqinds` argument to `OptimizationSolver.get_equations`
* Rename back tracking methods that print to begin with `print_` instead of `list_`


git-svn-id: https://svn.jmodelica.org@7533 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Python/src/pyjmi/optimization/casadi_collocation.py
@@ -4299,8 +4299,8 @@ class LocalDAECollocator(CasadiCollocator):
         for dests in (self.c_dests, self.xx_dests):
             for dest in dests.itervalues():
                 inds = N.vstack(dest['inds'])
-                i = N.array(dest['i'])
-                k = N.array(dest['k'])
+                i = N.array(dest['i'], dtype=N.int)
+                k = N.array(dest['k'], dtype=N.int)
                 tinds = N.lexsort((k, i))
                 dest['inds'] = inds[tinds, :]
                 dest['i']    = i[tinds]
@@ -5311,15 +5311,15 @@ class LocalDAECollocator(CasadiCollocator):
         
         dest = self.c_dests[eqtype]
         
-        indices = N.array(dest['inds'])
+        indices = N.array(dest['inds'], dtype=N.int)
         if dest['kind'] == 'ineq':
             # inequalities come after equalities in the constraints
             indices += self.c_e.numel()
         else:
             assert dest['kind'] == 'eq', "Unrecognized equation kind"
 
-        i = N.array(dest['i'])
-        k = N.array(dest['k'])
+        i = N.array(dest['i'], dtype=N.int)
+        k = N.array(dest['k'], dtype=N.int)
         
         time = self.get_times(i, k)
 
@@ -5355,9 +5355,9 @@ class LocalDAECollocator(CasadiCollocator):
 
         dest = self.xx_dests[var]
 
-        inds = N.array(dest['inds'])
-        i = N.array(dest['i'])
-        k = N.array(dest['k'])
+        inds = N.array(dest['inds'], dtype=N.int)
+        i = N.array(dest['i'], dtype=N.int)
+        k = N.array(dest['k'], dtype=N.int)
         
         time = self.get_times(i, k)
 
@@ -5720,7 +5720,7 @@ class OptimizationSolver(object):
         """
         return self.collocator.get_nlp_variable_indices(var)
 
-    def get_residuals(self, eqtype, point='opt', raw=False, inds=None):
+    def get_residuals(self, eqtype, inds=None, point='opt', raw=False):
         """
         Get the residuals for a given equation type.
 
@@ -5732,6 +5732,12 @@ class OptimizationSolver(object):
 
                     'initial', 'dae', 'path_eq', 'path_ineq',
                     'point_eq', 'point_ineq'
+
+            inds --
+                Iterable of equation indices within eqtype that the residuals
+                should be returned for, or None if residuals should be
+                returned for all equations.
+                Default: None
 
             point --
                 The point where the residuals should be evaluated,
@@ -5745,12 +5751,6 @@ class OptimizationSolver(object):
                 0 if not violating the bounds, the difference from
                 the violated bound otherwise.
                 Default: False
-
-            inds --
-                Iterable of equation indices within eqtype that the residuals
-                should be returned for, or None if residuals should be
-                returned for all equations.
-                Default: None
 
         Returns::
 
@@ -5847,7 +5847,7 @@ class OptimizationSolver(object):
         duals = duals[inds]
         return duals, time, i, k
 
-    def get_residual_norms(self, point='opt', ord=N.inf, eqtype=None):
+    def get_residual_norms(self, eqtype=None, point='opt', ord=N.inf):
         """
         List the norms of different parts of the residual, in descending order
 
@@ -5855,6 +5855,12 @@ class OptimizationSolver(object):
         to the norm.
 
         Parameters::
+            eqtype --
+                If None, list all equation types and their residual norms.
+                Otherwise, list the residual norm of each equation
+                of the given type, along with its index within the type.
+                Default: None
+
             point --
                 The point where the residuals should be evaluated,
                 'opt' for the optimization solution or
@@ -5863,12 +5869,6 @@ class OptimizationSolver(object):
 
             ord --
                 Vector norm order used with numpy.linalg.norm
-
-            eqtype --
-                If None, list all equation types and their residual norms.
-                Otherwise, list the residual norm of each equation
-                of the given type, along with its index within the type.
-                Default: None
         """
         result = []
 
@@ -5886,19 +5886,26 @@ class OptimizationSolver(object):
         result.sort(reverse = True)
         return result
 
-    def get_equations(self, eqtype):
+    def get_equations(self, eqtype, eqinds=None):
         """
-        Get the model equations corresponding to the given equation type
+        Get model equations corresponding to the given equation type
 
-        The equations are in the same order as the corresponding residuals
-        and constraint duals.
-
-        The only equation types supported are
+        Parameters::
+            eqtype --
+                Type of equation. The only values supported are
 
                     'initial', 'dae',
                     'path_eq', 'path_ineq',
                     'point_eq', 'point_ineq'
+
+            eqinds --
+                Indices of the equations to be returned.
+                If None, return all equations of the given type in index order.
+                Default: None
         """
+        if eqinds is not None:
+            return self.get_equations(eqtype)[eqinds]
+
         op = self.collocator.op
         # To be safe, only returning copies
         if eqtype == 'initial':      return op.getInitialEquations()
@@ -6009,7 +6016,7 @@ class OptimizationSolver(object):
         c_sources, xx_sources = self.collocator.c_sources, self.collocator.xx_sources
         combos = set(zip(c_sources['eqtype'][c_inds], c_sources['eqind'][c_inds], xx_sources['var'][xx_inds]))
 
-        return combos        
+        return combos
 
     def find_nonfinite_jacobian_entries(self, point='opt'):
         """
@@ -6031,7 +6038,7 @@ class OptimizationSolver(object):
         c_inds, xx_inds = N.nonzero(J-J)
         return self.get_model_jacobian_entries(c_inds, xx_inds)
 
-    def list_jacobian_entries(self, entries):
+    def print_jacobian_entries(self, entries):
         """
         Print a list of Jacobian entries
 
@@ -6054,7 +6061,7 @@ class OptimizationSolver(object):
                 last_eqtype, last_eqind = eqtype, eqind
             print "wrt", var
 
-    def list_nonfinite_jacobian_entries(self, point='opt'):
+    def print_nonfinite_jacobian_entries(self, point='opt'):
         """
         List combinations of model equations and variables where the Jacobian is nonfinite
 
@@ -6067,4 +6074,4 @@ class OptimizationSolver(object):
         """
         print "Nonfinite Jacobian entries:"
         print "---------------------------"
-        self.list_jacobian_entries(self.find_nonfinite_jacobian_entries(point))
+        self.print_jacobian_entries(self.find_nonfinite_jacobian_entries(point))