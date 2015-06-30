5b042733dc6f03f2b466838aac54c57f49ce1c68
efredriksson <efredriksson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2015-06-30 15:05:11 +0000

    #4452 Changed jmi_log_leave to jmi_log_unwind according to ticket in two places.
    git-svn-id: https://svn.jmodelica.org@7790 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/RuntimeLibrary/src/jmi/jmi.c
+++ trunk/RuntimeLibrary/src/jmi/jmi.c
@@ -534,7 +534,7 @@ int jmi_ode_derivatives(jmi_t* jmi) {
     if ((jmi->jmi_callbacks.log_options.log_level >= 5)) {
         jmi_log_reals(jmi->log, node, logInfo, "Derivatives", jmi_get_real_dx(jmi), jmi->n_real_x);
         jmi_log_fmt(jmi->log, node, logInfo, "Model equations evaluation finished");
-        jmi_log_leave(jmi->log, node);
+        jmi_log_unwind(jmi->log, node);
     }
 
     return return_status;
@@ -574,7 +574,7 @@ int jmi_ode_initialize(jmi_t* jmi) {
 
     if ((jmi->jmi_callbacks.log_options.log_level >= 5)) {
         jmi_log_fmt(jmi->log, node, logInfo, "Model equations evaluation finished");
-        jmi_log_leave(jmi->log, node);
+        jmi_log_unwind(jmi->log, node);
     }
     return return_status;
 }
