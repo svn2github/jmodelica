a88f83e3e740e8bf5759de794526f77e82be0666
jmattsson <jmattsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2015-07-24 12:50:28 +0000

    Fixed build error introduced in r7852. (#4190)
    git-svn-id: https://svn.jmodelica.org@7853 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/test/TestSpecification.java
+++ trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/test/TestSpecification.java
@@ -2,9 +2,6 @@ package org.jmodelica.util.test;
 
 import java.io.File;
 
-import org.junit.runner.RunWith;
-
-@RunWith(org.jmodelica.junit.TreeJModelicaRunner.class)
 public abstract class TestSpecification {
 
     private File[] dirs;
