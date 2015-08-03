svn2github/jmodelica

commit 6fad5899bc4f78ac1190c732301a5a38b78e8e74

Fixed sorting of Problems where some have location and some do not. (#4490)
git-svn-id: https://svn.jmodelica.org@7863 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/Problem.java
@@ -9,9 +9,12 @@ public class Problem implements Comparable<Problem>, Serializable {
     private static final long serialVersionUID = 1;
     
     public int compareTo(Problem other) {
-        if (fileName == null) {
+        if (fileName == null || other.fileName == null) {
+            if (fileName != null)
+                return -1;
             if (other.fileName != null)
                 return 1;
+            return 0;
         } else {
             if (!fileName.equals(other.fileName))
                 return fileName.compareTo(other.fileName);

commit 0dab48c182e66c17f65f19d44f078c39cde47ac6

Fixed bug introduced in r7863 that caused all problems detected on flat model except one to be discarded. (#4490)
git-svn-id: https://svn.jmodelica.org@7864 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/Problem.java
@@ -14,7 +14,6 @@ public class Problem implements Comparable<Problem>, Serializable {
                 return -1;
             if (other.fileName != null)
                 return 1;
-            return 0;
         } else {
             if (!fileName.equals(other.fileName))
                 return fileName.compareTo(other.fileName);