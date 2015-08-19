svn2github/jmodelica

commit 7cd93a6d86ed4d0f8f90e9c48e2efba3c0bf14b2

#4468 Function calls positioned as arguments are no longer extracted to temporary clauses.
git-svn-id: https://svn.jmodelica.org@7904 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/optimizations/InlineFunctions.jrag
@@ -242,10 +242,14 @@ aspect FunctionInlining {
          * @param noEvent  if true, make sure no events are generated for the equation
          */
         public void addVarAndEqn(String oldName, FExp value, boolean noEvent) {
-            FTypePrefixVariability variability = value.variability().fullCopy();
-            if (value.type().addVarInInline()) {
-                boolean nonFixedParam = variability.parameterVariability() && !value.valueIsFixed();
-                FVariable var = addVar(oldName, variability, value.type(), nonFixedParam);
+            addVarAndEqn(oldName, value, noEvent, value.type(), value.variability(), value.valueIsFixed());
+        }
+        
+        public void addVarAndEqn(String oldName, FExp value, boolean noEvent, FType type,
+                FTypePrefixVariability variability, boolean fixed) {
+            if (type.addVarInInline()) {
+                boolean nonFixedParam = variability.parameterVariability() && !fixed;
+                FVariable var = addVar(oldName, variability, type, nonFixedParam);
                 if (variability.constantVariability()) {
                     value = value.ceval().buildLiteral();
                     var.setBindingExp(value);
@@ -318,14 +322,17 @@ aspect FunctionInlining {
             FQName name = type.isArray() ? 
                     new FQNameFull(n, type.size().flattenFArraySubscripts(new FQNameEmpty())) : 
                     new FQNameString(n);
-            
+            FVariable var = addVar(name, variability, type, nonFixedParam);
+            setSizeOf(oldName, type.size());
+            replMap.put(oldName, var.createUseExp());
+            return var;
+        }
+        
+        protected FVariable addVar(FQName name, FTypePrefixVariability variability, FType type, boolean nonFixedParam) {
             FVariable var = type.createTempFVariable(name, variability);
             if (nonFixedParam)
                 var.setAttribute(FAttribute.FIXED, "Boolean", new FBooleanLitExpFalse(), 0);
-            
-            setSizeOf(oldName, type.size());
             lookupMap.addVariable(var);
-            replMap.put(oldName, var.createUseExp());
             addVar(var);
             return var;
         }
@@ -424,6 +431,27 @@ aspect FunctionInlining {
     public void FExp.addInliningVarsAndEqns(AbstractFunctionInliner fi, String oldName, boolean noEvent) {
         fi.addVarAndEqn(oldName, this, noEvent);
     }
+    
+    public void FFunctionCall.addInliningVarsAndEqns(AbstractFunctionInliner fi, String oldName, boolean noEvent) {
+        if (isComposite()) {
+            FTypePrefixVariability v = variability();
+            if (v.constantVariability()) {
+                try {
+                    dynamicFExp(ceval().buildLiteral()).addInliningVarsAndEqns(fi, oldName, noEvent);
+                    return;
+                } catch (ConstantEvaluationException e) {
+                    
+                }
+            }
+            String tempName = fi.nextTempVarName();
+            FExp exp = type().addInliningVarsAndEqns(fi, oldName, noEvent, this, tempName);
+            List<FFunctionCallLeft> l = new List<FFunctionCallLeft>();
+            l.add(new FFunctionCallLeft(new Opt<FExp>(exp)));
+            fi.addEquation(new FFunctionCallEquation(l, fullCopy()), v, v.parameterVariability() && !valueIsFixed());
+        } else {
+            super.addInliningVarsAndEqns(fi, oldName, noEvent);
+        }
+    }
 
     public void FArray.addInliningVarsAndEqns(AbstractFunctionInliner fi, String oldName, boolean noEvent) {
         fi.setSizeOf(oldName, size());
@@ -438,7 +466,39 @@ aspect FunctionInlining {
             myArgFExp(part.getName()).addInliningVarsAndEqns(fi, oldName + part.getName(), noEvent);
     }
 
-
+    public FExp FType.addInliningVarsAndEqns(AbstractFunctionInliner fi, String oldName, boolean noEvent,
+            FExp src, String tempName) {
+        if (isArray()) {
+            FArray a = new FArray();
+            for (Index i : indices()) {
+                a.addFExp(addInliningVarsAndEqnsCell(fi, oldName + i, noEvent, src, tempName + i));
+            }
+            return a;
+        } else {
+            return addInliningVarsAndEqnsCell(fi, oldName, noEvent, src, tempName);
+        }
+    }
+    
+    public FExp FType.addInliningVarsAndEqnsCell(AbstractFunctionInliner fi, String oldName, boolean noEvent,
+            FExp src, String tempName) {
+        FExp e = src.dynamicFExp(new FIdUseExp(tempName));
+        FTypePrefixVariability v = src.variability();
+        boolean f = src.valueIsFixed();
+        FType type = scalarType();
+        fi.addVarAndEqn(oldName, e, noEvent, type, v, f);
+        fi.addVar(new FQNameString(tempName), v, type, v.parameterVariability() && !f);
+        return e.fullCopy();
+    }
+    
+    public FExp FRecordType.addInliningVarsAndEqnsCell(AbstractFunctionInliner fi, String oldName, boolean noEvent,
+            FExp src, String tempName) {
+        FRecordConstructor frc = new FRecordConstructor(getName());
+        for (FRecordComponentType frct : getComponents()) {
+            frc.addArg(frct.getFType().addInliningVarsAndEqns(fi, oldName + "." + frct.getName(), noEvent, src, tempName + "." + frct.getName()));
+        }
+        return frc;
+    }
+    
     /**
      * Should expressions of this type generate new varriables during inlining?
      */
@@ -754,10 +814,34 @@ aspect FunctionInlining {
     public void FFunctionCall.inlineFunctions(AbstractFunctionInliner fi) {
         if (isInlinableFunctionCall(fi)) {
             inline(fi);
-            replaceMe(fi.getReplacementExp(myCallOutputs().get(0).name()).fullCopy());
+            replaceMe(type().replaceInlined(fi, this, myCallOutputs().get(0).name()));
         } else {
             super.inlineFunctions(fi);
         }
+    }
+    
+    public FExp FType.replaceInlined(AbstractFunctionInliner fi, FExp src, String name) {
+        if (isArray()) {
+            FArray a = new FArray();
+            for (Index i : indices()) {
+                a.addFExp(replaceInlinedCell(fi, src, name + i));
+            }
+            return a;
+        } else {
+            return replaceInlinedCell(fi, src, name);
+        }
+    }
+    
+    public FExp FType.replaceInlinedCell(AbstractFunctionInliner fi, FExp src, String name) {
+        return fi.getReplacementExp(name).fullCopy();
+    }
+    
+    public FExp FRecordType.replaceInlinedCell(AbstractFunctionInliner fi, FExp src, String name) {
+        FRecordConstructor frc = new FRecordConstructor(getName());
+        for (FRecordComponentType frct : getComponents()) {
+            frc.addArg(frct.getFType().replaceInlined(fi, src, name + "." + frct.getName()));
+        }
+        return frc;
     }
 
     public void FIdUseExp.inlineFunctions(AbstractFunctionInliner fi) {

trunk/Compiler/ModelicaMiddleEnd/src/test/FunctionInlining.mo
@@ -2016,8 +2016,10 @@ model MultipleOutputsInline4
     function f1
         input Real x;
         output Real y;
+        R r;
       algorithm
-        y := f2(f3(x));
+        r := f3(x);
+        y := f2(r);
         annotation (Inline=true);
     end f1;
     Real x,y;
@@ -2034,18 +2036,18 @@ model MultipleOutputsInline4
 fclass FunctionInlining.MultipleOutputsInline4
  Real x;
  Real y;
- discrete String temp_4;
- discrete Integer temp_5;
- discrete Boolean temp_6;
- discrete FunctionInlining.MultipleOutputsInline4.E temp_7;
+ discrete String temp_3;
+ discrete Integer temp_4;
+ discrete Boolean temp_5;
+ discrete FunctionInlining.MultipleOutputsInline4.E temp_6;
 initial equation 
- pre(temp_4) = \"\";
- pre(temp_5) = 0;
- pre(temp_6) = false;
- pre(temp_7) = FunctionInlining.MultipleOutputsInline4.E.E1;
+ pre(temp_3) = \"\";
+ pre(temp_4) = 0;
+ pre(temp_5) = false;
+ pre(temp_6) = FunctionInlining.MultipleOutputsInline4.E.E1;
 equation
  x = 1;
- (FunctionInlining.MultipleOutputsInline4.R(y, temp_4, temp_5, temp_6, temp_7)) = FunctionInlining.MultipleOutputsInline4.f3(x);
+ (FunctionInlining.MultipleOutputsInline4.R(y, temp_3, temp_4, temp_5, temp_6)) = FunctionInlining.MultipleOutputsInline4.f3(x);
 
 public
  function FunctionInlining.MultipleOutputsInline4.f3
@@ -2346,10 +2348,8 @@ equation
 fclass FunctionInlining.IfEquationInline7
  Real x;
  Real y;
- Real temp_1.p;
 equation
- temp_1.p = if time > 0 then 0.0 else time;
- x = if time > 0 then 4 else temp_1.p;
+ x = if time > 0 then 4 else time;
  0.0 = if time > 0 then y - 2 else time - (y + 1);
 end FunctionInlining.IfEquationInline7;
 ")})));
@@ -3605,5 +3605,305 @@ initial equation
 end FunctionInlining.InitialSystemInlining1;
 ")})));
 end InitialSystemInlining1;
+
+model ChainedCallInlining1
+    record R
+        Real x;
+    end R;
+    
+    function f1
+        input Real x;
+        output R y = R(x);
+        algorithm
+    end f1;
+    
+    function f2
+        input R x;
+        output Real y = x.x;
+        algorithm
+    end f2;
+    
+    Real y = f2(f1(time));
+annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="ChainedCallInlining1",
+            description="Test inlining chained function calls",
+            flatModel="
+fclass FunctionInlining.ChainedCallInlining1
+ Real y;
+equation
+ y = time;
+end FunctionInlining.ChainedCallInlining1;
+")})));
+end ChainedCallInlining1;
+
+model ChainedCallInlining2
+    record R
+        Real x;
+    end R;
+    
+    function f1
+        input Real x;
+        output R y = R(x);
+        algorithm
+    end f1;
+    
+    function f2
+        input R x;
+        output Real y = x.x;
+        algorithm
+        annotation(Inline=false);
+    end f2;
+    
+    Real y = f2(f1(time));
+annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="ChainedCallInlining2",
+            description="Test inlining chained function calls",
+            flatModel="
+fclass FunctionInlining.ChainedCallInlining2
+ Real y;
+equation
+ y = FunctionInlining.ChainedCallInlining2.f2(FunctionInlining.ChainedCallInlining2.R(time));
+
+public
+ function FunctionInlining.ChainedCallInlining2.f2
+  input FunctionInlining.ChainedCallInlining2.R x;
+  output Real y;
+ algorithm
+  y := x.x;
+  return;
+ annotation(Inline = false);
+ end FunctionInlining.ChainedCallInlining2.f2;
+
+ record FunctionInlining.ChainedCallInlining2.R
+  Real x;
+ end FunctionInlining.ChainedCallInlining2.R;
+
+end FunctionInlining.ChainedCallInlining2;
+")})));
+end ChainedCallInlining2;
+
+model ChainedCallInlining3
+    record R
+        Real x;
+    end R;
+    
+    function f1
+        input Real x;
+        output R y = R(x);
+        algorithm
+        annotation(Inline=false);
+    end f1;
+    
+    function f2
+        input R x;
+        output Real y = x.x;
+        algorithm
+    end f2;
+    
+    Real y = f2(f1(time));
+annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="ChainedCallInlining3",
+            description="Test inlining chained function calls",
+            flatModel="
+fclass FunctionInlining.ChainedCallInlining3
+ Real y;
+equation
+ (FunctionInlining.ChainedCallInlining3.R(y)) = FunctionInlining.ChainedCallInlining3.f1(time);
+
+public
+ function FunctionInlining.ChainedCallInlining3.f1
+  input Real x;
+  output FunctionInlining.ChainedCallInlining3.R y;
+ algorithm
+  y.x := x;
+  return;
+ annotation(Inline = false);
+ end FunctionInlining.ChainedCallInlining3.f1;
+
+ record FunctionInlining.ChainedCallInlining3.R
+  Real x;
+ end FunctionInlining.ChainedCallInlining3.R;
+
+end FunctionInlining.ChainedCallInlining3;
+")})));
+end ChainedCallInlining3;
+
+model ChainedCallInlining4
+    record R1
+        Real x1;
+        Real[2] x2;
+    end R1;
+    
+    record R2
+        R1[2] r1;
+        Real[2] z1;
+    end R2;
+    
+    function f1
+        input Real x;
+        output R2[:] y = {R2({R1(x,{x+1,x+2}),R1(x+3,{x+4,x+5})}, {x+6, x+7})};
+        algorithm
+        annotation(Inline=true);
+    end f1;
+    
+    function f2
+        input R2[:] x;
+        output Real y;
+        algorithm
+            y := sum(x[1].r1[2].x2[1:2]);
+    end f2;
+      
+      Real y = f2(f1(time));
+annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="ChainedCallInlining4",
+            description="Test inlining chained function calls",
+            flatModel="
+fclass FunctionInlining.ChainedCallInlining4
+ Real y;
+ Real temp_11;
+equation
+ y = temp_11 + 4 + (temp_11 + 5);
+ temp_11 = time;
+end FunctionInlining.ChainedCallInlining4;
+")})));
+end ChainedCallInlining4;
+
+model ChainedCallInlining5
+    record R1
+        Real x1;
+        Real[2] x2;
+    end R1;
+    
+    record R2
+        R1[2] r1;
+        Real[2] z1;
+    end R2;
+    
+    function f1
+        input Real x;
+        output R2[1] y = {R2({R1(x,{x+1,x+2}),R1(x+3,{x+4,x+5})}, {x+6, x+7})};
+        algorithm
+        annotation(Inline=true);
+    end f1;
+    
+    function f2
+        input R2[1] x;
+        output Real y;
+        algorithm
+            y := x[1].r1[2].x2[1] / x[1].r1[2].x2[2];
+    end f2;
+      
+      Real y = f2(f1(time));
+annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="ChainedCallInlining5",
+            description="Test inlining chained function calls",
+            flatModel="
+fclass FunctionInlining.ChainedCallInlining5
+ Real y;
+ Real temp_11;
+equation
+ y = (temp_11 + 4) / (temp_11 + 5);
+ temp_11 = time;
+end FunctionInlining.ChainedCallInlining5;
+")})));
+end ChainedCallInlining5;
+
+model ChainedCallInlining6
+    record R1
+        Real x1;
+        Real[2] x2;
+    end R1;
+    
+    record R2
+        R1[2] r1;
+        Real[2] z1;
+    end R2;
+    
+    function f1
+        input Real x;
+        output R2[1] y = {R2({R1(x,{x+1,x+2}),R1(x+3,{x+4,x+5})}, {x+6, x+7})};
+        algorithm
+        annotation(Inline=true);
+    end f1;
+    
+    function f2
+        input R2[1] x;
+        output Real y;
+        algorithm
+            y := x[1].r1[2].x2[1] / x[1].r1[2].x2[2];
+    end f2;
+      
+      Real y = f2(f1(f2(f1(time))));
+  annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="ChainedCallInlining6",
+            description="Test inlining chained function calls",
+            flatModel="
+fclass FunctionInlining.ChainedCallInlining6
+ Real y;
+ Real temp_11;
+ Real temp_30;
+equation
+ y = (temp_11 + 4) / (temp_11 + 5);
+ temp_11 = (temp_30 + 4) / (temp_30 + 5);
+ temp_30 = time;
+end FunctionInlining.ChainedCallInlining6;
+")})));
+end ChainedCallInlining6;
+
+model ChainedCallInlining7
+    record R1
+        Real x1;
+        Real[2] x2;
+    end R1;
+    
+    record R2
+        R1[2] r1;
+        Real[2] z1;
+    end R2;
+    
+    function f1
+        input Real x;
+        output R2[1] y = {R2({R1(x,{x+1,x+2}),R1(x+3,{x+4,x+5})}, {x+6, x+7})};
+        algorithm
+        annotation(Inline=true);
+    end f1;
+    
+    function f2
+        input R2[1] x;
+        output Real y;
+        algorithm
+            y := x[1].r1[2].x2[1] / x[1].r1[2].x2[2];
+    end f2;
+    
+    function f3
+        input Real x;
+        output Real y;
+    algorithm
+        y := f2(f1(f2(f1(x))));
+    end f3;
+      
+      Real y = f3(time);
+  annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="ChainedCallInlining7",
+            description="Test inlining chained function calls",
+            flatModel="
+fclass FunctionInlining.ChainedCallInlining7
+ Real y;
+ Real temp_13;
+ Real temp_32;
+equation
+ y = (temp_13 + 4) / (temp_13 + 5);
+ temp_13 = (temp_32 + 4) / (temp_32 + 5);
+ temp_32 = time;
+end FunctionInlining.ChainedCallInlining7;
+")})));
+end ChainedCallInlining7;
 
 end FunctionInlining;

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen.jrag
@@ -530,10 +530,13 @@ aspect CCodeGen {
         int lefts = myLefts().size();
         for (int i = 0; i < outputs; i++) {
             str.print(sep);
-            if (i < lefts)
+            if (i < lefts) {
                 myLefts().get(i).genArgument_C(str);
-            else
+            } else if (i == 0 && lefts == 0 && isFunctionCallArg()) {
+                str.print(tempName_C());
+            } else {
                 str.print("NULL");
+            }
             sep = ", ";
         }
     }
@@ -3283,11 +3286,22 @@ aspect CCodeGen {
     
 	public void FExp.genTempInput_C(CodePrinter p, CodeStream str, String indent) {
 		if (isComposite() && !keepAsArray()) {
-			String name = tempName_C();
-            type().print(p.createInitPrinter(str), indent, name, this, false);
-			type().genTempInputAssigns_C(p, str, indent, name, this);
+            type().print(p.createInitPrinter(str), indent, tempName_C(), this, false);
+            genTempInputAssigns_C(p, str, indent);
 		}
 	}
+    
+    public void FExp.genTempInputAssigns_C(CodePrinter p, CodeStream str, String indent) {
+        type().genTempInputAssigns_C(p, str, indent, tempName_C(), this);
+    }
+    
+    public void FFunctionCall.genTempInputAssigns_C(CodePrinter p, CodeStream str, String indent) {
+        if (isComposite()) {
+            prettyPrintCall_C(p, str, indent);
+        } else {
+            super.genTempInputAssigns_C(p, str, indent);
+        }
+    }
     
     public void FPartialFunctionCall.genTempInput_C(CodePrinter p, CodeStream str, String indent) {
         if (myFCallable().isPartialFunction()) {

trunk/Compiler/ModelicaCBackEnd/src/test/CCodeGenTests.mo
@@ -3925,10 +3925,10 @@ model CUnknownArray3
 void func_CCodeGenTests_CUnknownArray3_f1_def0(jmi_array_t* x1_a, jmi_ad_var_t* y1_o) {
     JMI_DYNAMIC_INIT()
     JMI_DEF(REA, y1_v)
-    JMI_ARR(DYNAREAL, jmi_ad_var_t, jmi_array_t, temp_1_a, -1, 1)
-    JMI_ARRAY_INIT_1(DYNAREAL, jmi_ad_var_t, jmi_array_t, temp_1_a, jmi_array_size(x1_a, 0), 1, jmi_array_size(x1_a, 0))
-    func_CCodeGenTests_CUnknownArray3_f2_def2(x1_a, temp_1_a);
-    y1_v = func_CCodeGenTests_CUnknownArray3_f3_exp1(temp_1_a);
+    JMI_ARR(DYNAREAL, jmi_ad_var_t, jmi_array_t, tmp_1, -1, 1)
+    JMI_ARRAY_INIT_1(DYNAREAL, jmi_ad_var_t, jmi_array_t, tmp_1, jmi_array_size(x1_a, 0), 1, jmi_array_size(x1_a, 0))
+    func_CCodeGenTests_CUnknownArray3_f2_def2(x1_a, tmp_1);
+    y1_v = func_CCodeGenTests_CUnknownArray3_f3_exp1(tmp_1);
     JMI_RET(GEN, y1_o, y1_v)
     JMI_DYNAMIC_FREE()
     return;

trunk/Compiler/ModelicaFlatTree/src/jastadd/Arrays.jrag
@@ -2211,16 +2211,21 @@ aspect Arrays {
 
     eq FFunctionCall.getArray() {
         FClass fc = myFClass();
-        if (!isArray() || fc == null)
+        if (!isComposite() || fc == null) {
             return super.getArray();
+        }
         
-        if (variability().knownParameterOrLess() && inKeptBExp()) {
+        if (isArray() && variability().knownParameterOrLess() && inKeptBExp()) {
             Array arr = new Array(indices());
             CValueArray carr = (CValueArray) cevalArray(Index.NULL);
             for (Index i : arr.indices()) {
                 arr.set(i, carr.getCell(i).buildLiteral());
             }
             return arr;
+        }
+        
+        if (isFunctionCallArg()) {
+            return super.getArray();
         }
         
         FArraySubscripts fas = collectIterExpSubscripts();

trunk/Compiler/ModelicaFlatTree/src/jastadd/FlatAPI.jrag
@@ -2568,8 +2568,9 @@ aspect FVariableUses {
     eq FClass.getChild().inFunctionCall()        = false;
     eq InstRoot.getChild().inFunctionCall()      = false;
     
-    
-    
+    inh boolean FExp.isFunctionCallArg();
+    eq FFunctionCall.getArg().isFunctionCallArg() = true;
+    eq BaseNode.getChild().isFunctionCallArg()    = false;
     
     /**
      * Returnes the function input that this expressions corresponds to.

trunk/Compiler/ModelicaFlatTree/src/jastadd/FlatUtil.jrag
@@ -1659,6 +1659,7 @@ aspect FlatFunctionUtils {
      eq InstNode.getChild().functionCallIsExp()             = true;
      eq FFunctionCallEquation.getCall().functionCallIsExp() = false;
      eq FFunctionCallStmt.getCall().functionCallIsExp()     = false;
+     eq FFunctionCall.getArg(int i).functionCallIsExp()     = !getArg(i).isComposite();
      
      inh lazy ArrayList<FFunctionCallLeft> FAbstractFunctionCall.myLefts();
      eq FExp.getChild().myLefts()                 = new ArrayList<FFunctionCallLeft>();

trunk/Compiler/ModelicaFrontEnd/src/test/ArrayTests.mo
@@ -1628,31 +1628,13 @@ fclass ArrayTests.Subscripts.SubscriptExpression10
  parameter Real c[1] = 1 /* 1 */;
  parameter Real c[2] = 2 /* 2 */;
  parameter Real c[3] = 3 /* 3 */;
- parameter Real temp_1.a;
- parameter Real temp_2.a;
- parameter Real temp_3.a;
- parameter Real temp_4.a;
- parameter Real temp_5.a;
- parameter Real temp_6.a;
- parameter Real temp_7.a;
- parameter Real temp_8.a;
- parameter Real temp_9.a;
  parameter Real m[1].b;
  parameter Real m[2].b;
  parameter Real m[3].b;
 parameter equation
- (ArrayTests.Subscripts.SubscriptExpression10.R(temp_1.a)) = ArrayTests.Subscripts.SubscriptExpression10.f2(c[1]);
- (ArrayTests.Subscripts.SubscriptExpression10.R(temp_2.a)) = ArrayTests.Subscripts.SubscriptExpression10.f2(c[2]);
- (ArrayTests.Subscripts.SubscriptExpression10.R(temp_3.a)) = ArrayTests.Subscripts.SubscriptExpression10.f2(c[3]);
- (ArrayTests.Subscripts.SubscriptExpression10.R(temp_4.a)) = ArrayTests.Subscripts.SubscriptExpression10.f2(c[1]);
- (ArrayTests.Subscripts.SubscriptExpression10.R(temp_5.a)) = ArrayTests.Subscripts.SubscriptExpression10.f2(c[2]);
- (ArrayTests.Subscripts.SubscriptExpression10.R(temp_6.a)) = ArrayTests.Subscripts.SubscriptExpression10.f2(c[3]);
- (ArrayTests.Subscripts.SubscriptExpression10.R(temp_7.a)) = ArrayTests.Subscripts.SubscriptExpression10.f2(c[1]);
- (ArrayTests.Subscripts.SubscriptExpression10.R(temp_8.a)) = ArrayTests.Subscripts.SubscriptExpression10.f2(c[2]);
- (ArrayTests.Subscripts.SubscriptExpression10.R(temp_9.a)) = ArrayTests.Subscripts.SubscriptExpression10.f2(c[3]);
- m[1].b = ArrayTests.Subscripts.SubscriptExpression10.f1(ArrayTests.Subscripts.SubscriptExpression10.R(temp_1.a));
- m[2].b = ArrayTests.Subscripts.SubscriptExpression10.f1(ArrayTests.Subscripts.SubscriptExpression10.R(temp_5.a));
- m[3].b = ArrayTests.Subscripts.SubscriptExpression10.f1(ArrayTests.Subscripts.SubscriptExpression10.R(temp_9.a));
+ m[1].b = ArrayTests.Subscripts.SubscriptExpression10.f1(ArrayTests.Subscripts.SubscriptExpression10.f2(c[1]));
+ m[2].b = ArrayTests.Subscripts.SubscriptExpression10.f1(ArrayTests.Subscripts.SubscriptExpression10.f2(c[2]));
+ m[3].b = ArrayTests.Subscripts.SubscriptExpression10.f1(ArrayTests.Subscripts.SubscriptExpression10.f2(c[3]));
 
 public
  function ArrayTests.Subscripts.SubscriptExpression10.f1
@@ -5942,18 +5924,12 @@ fclass ArrayTests.Constructors.Iterators.ArrayIterTest8
  Real z[2];
  Real w[1];
  Real w[2];
- Real temp_1[1].a;
- Real temp_1[1].b;
- Real temp_1[2].a;
- Real temp_1[2].b;
 equation
  z[1] = time;
  z[2] = time * 2;
- (ArrayTests.Constructors.Iterators.ArrayIterTest8.R(temp_1[1].a, temp_1[1].b)) = ArrayTests.Constructors.Iterators.ArrayIterTest8.f1(z[1]);
- (ArrayTests.Constructors.Iterators.ArrayIterTest8.R(temp_1[2].a, temp_1[2].b)) = ArrayTests.Constructors.Iterators.ArrayIterTest8.f1(z[2]);
- w[1] = ArrayTests.Constructors.Iterators.ArrayIterTest8.f2(ArrayTests.Constructors.Iterators.ArrayIterTest8.R(temp_1[1].a, temp_1[1].b));
- w[2] = ArrayTests.Constructors.Iterators.ArrayIterTest8.f2(ArrayTests.Constructors.Iterators.ArrayIterTest8.R(temp_1[2].a, temp_1[2].b));
-
+ w[1] = ArrayTests.Constructors.Iterators.ArrayIterTest8.f2(ArrayTests.Constructors.Iterators.ArrayIterTest8.f1(z[1]));
+ w[2] = ArrayTests.Constructors.Iterators.ArrayIterTest8.f2(ArrayTests.Constructors.Iterators.ArrayIterTest8.f1(z[2]));
+ 
 public
  function ArrayTests.Constructors.Iterators.ArrayIterTest8.f2
   input ArrayTests.Constructors.Iterators.ArrayIterTest8.R x;

commit 2f20964b670d223740d5cccf16f2674497409102

#4468 Fixes bug introduced in r7904
git-svn-id: https://svn.jmodelica.org@7926 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/optimizations/InlineFunctions.jrag
@@ -445,7 +445,7 @@ aspect FunctionInlining {
             }
             fi.setSizeOf(oldName, size());
             String tempName = fi.nextTempVarName();
-            FExp exp = type().addInliningVarsAndEqns(fi, oldName, noEvent, this, tempName);
+            FExp exp = type().addInliningVarsAndEqns(fi, oldName, noEvent, this, tempName, "");
             List<FFunctionCallLeft> l = new List<FFunctionCallLeft>();
             l.add(new FFunctionCallLeft(new Opt<FExp>(exp)));
             fi.addEquation(new FFunctionCallEquation(l, fullCopy()), v, v.parameterVariability() && !valueIsFixed());
@@ -468,20 +468,32 @@ aspect FunctionInlining {
     }
 
     public FExp FType.addInliningVarsAndEqns(AbstractFunctionInliner fi, String oldName, boolean noEvent,
-            FExp src, String tempName) {
+            FExp src, String tempName, String suffix) {
         if (isArray()) {
+            return addInliningVarsAndEqns(fi, oldName, noEvent, src, tempName, suffix, 0, indices().iterator());
+        } else {
+            return addInliningVarsAndEqnsCell(fi, oldName, noEvent, src, tempName, suffix);
+        }
+    }
+    
+    public FExp FType.addInliningVarsAndEqns(AbstractFunctionInliner fi, String oldName, boolean noEvent,
+            FExp src, String tempName, String suffix, int dim, Iterator<Index> indices) {
+        if (dim < size().ndims()) {
             FArray a = new FArray();
-            for (Index i : indices()) {
-                a.addFExp(addInliningVarsAndEqnsCell(fi, oldName + i, noEvent, src, tempName + i));
+            for (int i = 0; i < size().get(dim); i++) {
+                a.addFExp(addInliningVarsAndEqns(fi, oldName, noEvent, src, tempName, suffix, dim + 1, indices));
             }
             return a;
         } else {
-            return addInliningVarsAndEqnsCell(fi, oldName, noEvent, src, tempName);
+            suffix = suffix + indices.next();
+            return addInliningVarsAndEqnsCell(fi, oldName, noEvent, src, tempName, suffix);
         }
     }
     
     public FExp FType.addInliningVarsAndEqnsCell(AbstractFunctionInliner fi, String oldName, boolean noEvent,
-            FExp src, String tempName) {
+            FExp src, String tempName, String suffix) {
+        tempName = tempName + suffix;
+        oldName = oldName + suffix;
         FExp e = src.dynamicFExp(new FIdUseExp(tempName));
         FTypePrefixVariability v = src.variability();
         boolean f = src.valueIsFixed();
@@ -492,10 +504,10 @@ aspect FunctionInlining {
     }
     
     public FExp FRecordType.addInliningVarsAndEqnsCell(AbstractFunctionInliner fi, String oldName, boolean noEvent,
-            FExp src, String tempName) {
+            FExp src, String tempName, String suffix) {
         FRecordConstructor frc = new FRecordConstructor(getName());
         for (FRecordComponentType frct : getComponents()) {
-            frc.addArg(frct.getFType().addInliningVarsAndEqns(fi, oldName + "." + frct.getName(), noEvent, src, tempName + "." + frct.getName()));
+            frc.addArg(frct.getFType().addInliningVarsAndEqns(fi, oldName, noEvent, src, tempName, suffix + "." + frct.getName()));
         }
         return frc;
     }

trunk/Compiler/ModelicaMiddleEnd/src/test/FunctionInlining.mo
@@ -3986,4 +3986,39 @@ end FunctionInlining.ChainedCallInlining9;
 ")})));
 end ChainedCallInlining9;
 
+model ChainedCallInlining10
+    record R
+        Real[2,1] x;
+    end R;
+
+    function f1
+        input R r;
+        output Real[:,:] y = f2(f2(r.x));
+    algorithm
+    annotation(Inline=true);
+    end f1;
+    
+    function f2
+        input Real[:,:] x;
+        output Real[:,:] y = x;
+    algorithm
+    end f2;
+  
+    Real[:,:] y = f1(R({{time},{time+1}}));
+    
+  annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="ChainedCallInlining10",
+            description="Test inlining chained function calls",
+            flatModel="
+fclass FunctionInlining.ChainedCallInlining10
+ Real y[1,1];
+ Real y[2,1];
+equation
+ y[1,1] = time;
+ y[2,1] = time + 1;
+end FunctionInlining.ChainedCallInlining10;
+")})));
+end ChainedCallInlining10;
+
 end FunctionInlining;