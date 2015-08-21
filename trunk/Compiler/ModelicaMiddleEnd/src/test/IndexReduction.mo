svn2github/jmodelica

commit d0bed43cbbcd784b6b75894d0dc8440371be1559

#4534 Improved temporary variable elimination in function call lefts.
git-svn-id: https://svn.jmodelica.org@7929 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/TransformCanonical.jrag
@@ -2089,10 +2089,9 @@ aspect TransformCanonical {
 		while (temps.size() < oldSize) {
 			ArrayList<FVariable> temps2 = new ArrayList<FVariable>(temps.size());
 			for (FVariable fv : temps) {
-				if (fv.shouldEliminateTemporary())
-					fv.inlineUses();
-				else
+                if (!fv.inlineUses()) {
 					temps2.add(fv);
+                }
 			}
 			oldSize = temps.size();
 			temps = temps2;
@@ -2116,7 +2115,6 @@ aspect TransformCanonical {
         isTemporary() && 
         nonDefiningUses().size() <= 1 && 
         definingUses().size() <= 1 && 
-        !usedAsFunctionCallLeft() && 
         !isPreVariable() && myPreVariable().isUnknown() && 
         (definingUses().isEmpty() || !definingUses().iterator().next().myFEquation().generatesEvents()) && 
         !isDifferentiatedVariable() && !isDerivativeVariable();
@@ -2126,18 +2124,25 @@ aspect TransformCanonical {
 	 * 
 	 * NB: This assumes that the variable and any equations marked for removal will be removed.
 	 */
-	public void FVariable.inlineUses() {
+    public boolean FVariable.inlineUses() {
+        if (!shouldEliminateTemporary()) {
+            return false;
+        }
 		FExp value;
 		if (hasBindingExp()) {
 			value = getBindingExp();
 		} else {
 			if (definingUses().isEmpty())
-				return;
+                return nonDefiningUses().isEmpty();
 			FAbstractEquation eqn = definingUses().iterator().next().myFEquation();
-			if (!(eqn instanceof FEquation)) 
-				return;
-			value = ((FEquation) eqn).getRight();
-			eqn.markedForRemoval = true;
+            if (eqn instanceof FEquation) {
+                value = ((FEquation) eqn).getRight();
+                eqn.markForRemoval();
+            } else if (eqn instanceof FFunctionCallEquation) {
+                return inlineUsesFunctionCallLeft();
+            } else {
+                return false;
+            }
 		}
 		
 		if (nonDefiningUses().isEmpty())
@@ -2146,12 +2151,40 @@ aspect TransformCanonical {
 		
 		for (FIdUseExp use : nonDefiningUses())
 			use.replaceMe(value.unboundCopy());
+        return true;
 	}
 	
+    public boolean FVariable.inlineUsesFunctionCallLeft() {
+        FIdUseExp defineUse = definingUses().iterator().next();
+        FExp value;
+        if (nonDefiningUses().size() > 0) {
+            FIdUseExp nonDefineUse = nonDefiningUses().iterator().next();
+            value = nonDefineUse.inlineUsesFunctionCallLeft();
+            if (value == null) {
+                return false;
+            }
+            value.myFEquation().markForRemoval();
+        } else {
+            value = new FNoExp();
+        }
+        
+        definingUses().remove(defineUse);
+        defineUse.replaceMe(value);
+        return true;
+    }
+    
+    inh FIdUseExp FIdUseExp.inlineUsesFunctionCallLeft();
+    eq BaseNode.getChild().inlineUsesFunctionCallLeft()  = null;
+    eq FEquation.getRight().inlineUsesFunctionCallLeft() = getLeft() instanceof FIdUseExp ? (FIdUseExp)getLeft() : null;
+    
 	/**
 	 * Flag for removing equation.
 	 */
 	public boolean FAbstractEquation.markedForRemoval = false;
+	
+    public void FAbstractEquation.markForRemoval() {
+        markedForRemoval = true;
+    }
 	
 	/**
 	 * Remove all uses in an expression from the uses lists of their respective variables.

trunk/Compiler/ModelicaMiddleEnd/src/test/IndexReduction.mo
@@ -1615,15 +1615,13 @@ fclass IndexReduction.Variability1
  Real a;
  Real _der_x;
  Real _der_y;
- Real temp_3;
  Real temp_4;
 initial equation 
  a = 0.0;
 equation
  (x, p) = IndexReduction.Variability1.F1(y + a);
  _der_x = _der_y * 2;
- (temp_3, temp_4) = IndexReduction.Variability1.F1((y + a) * (_der_y + der(a)));
- _der_x = temp_3;
+ (_der_x, temp_4) = IndexReduction.Variability1.F1((y + a) * (_der_y + der(a)));
  0.0 = temp_4;
 
 public
@@ -3380,8 +3378,6 @@ fclass IndexReduction.FunctionInlining.Test8
  Real _der_der_x;
  Real _der_der_y;
  Real _der_der_b;
- Real temp_5;
- Real temp_10;
 initial equation 
  y = 0.0;
  vy = 0.0;
@@ -3392,13 +3388,11 @@ equation
  der(vy) = a * y;
  (b, ) = IndexReduction.FunctionInlining.Test8.F(x, y);
  b = time;
- (temp_5, ) = IndexReduction.FunctionInlining.Test8.F(_der_x, der(y));
- _der_b = temp_5;
+ (_der_b, ) = IndexReduction.FunctionInlining.Test8.F(_der_x, der(y));
  _der_b = 1.0;
  _der_der_x = _der_vx;
  _der_der_y = der(vy);
- (temp_10, ) = IndexReduction.FunctionInlining.Test8.F(_der_der_x, _der_der_y);
- _der_der_b = temp_10;
+ (_der_der_b, ) = IndexReduction.FunctionInlining.Test8.F(_der_der_x, _der_der_y);
  _der_der_b = 0.0;
 
 public

trunk/Compiler/ModelicaCBackEnd/src/test/CADCodeGenTests.mo
@@ -4043,7 +4043,7 @@ $CAD_functions$
 jmi_real_t** dz = jmi->dz;
     /*********** ODE section ***********/
     jmi_ode_unsolved_block_dir_der(jmi, jmi->dae_block_residuals[0]);
-    _der_x_8 = _x_4 * _y_5;
+    _der_x_6 = _x_4 * _y_5;
     (*dz)[jmi_get_index_from_value_ref(0)-jmi->offs_real_dx] = (*dz)[jmi_get_index_from_value_ref(1)-jmi->offs_real_dx] * _y_5 + _x_4 * (*dz)[jmi_get_index_from_value_ref(6)-jmi->offs_real_dx];
     /*********** Real outputs **********/
     /*** Integer and boolean outputs ***/
@@ -4059,14 +4059,10 @@ jmi_real_t** dz = jmi->dz;
     tmp_der_2->re = AD_WRAP_LITERAL(0);
     tmp_der_2->im = (*dz)[jmi->offs_t];
     func_CADCodeGenTests_CADRecord1_add_der_AD0(tmp_var_1, tmp_var_2, tmp_der_1, tmp_der_2, tmp_var_0, tmp_der_0);
-    _temp_1_re_6 = tmp_var_0->re;
-    _temp_1_im_7 = tmp_var_0->im;
-    (*dz)[jmi_get_index_from_value_ref(7)-jmi->offs_real_dx] = tmp_der_0->re;
-    (*dz)[jmi_get_index_from_value_ref(8)-jmi->offs_real_dx] = tmp_der_0->im;
-    _c2_re_2 = _temp_1_re_6;
-    (*dz)[jmi_get_index_from_value_ref(4)-jmi->offs_real_dx] = (*dz)[jmi_get_index_from_value_ref(7)-jmi->offs_real_dx];
-    _c2_im_3 = _temp_1_im_7;
-    (*dz)[jmi_get_index_from_value_ref(5)-jmi->offs_real_dx] = (*dz)[jmi_get_index_from_value_ref(8)-jmi->offs_real_dx];
+    _c2_re_2 = tmp_var_0->re;
+    _c2_im_3 = tmp_var_0->im;
+    (*dz)[jmi_get_index_from_value_ref(4)-jmi->offs_real_dx] = tmp_der_0->re;
+    (*dz)[jmi_get_index_from_value_ref(5)-jmi->offs_real_dx] = tmp_der_0->im;
 
 static int dae_block_dir_der_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* dx,jmi_real_t* residual, jmi_real_t* dRes, int evaluation_mode) {
     /***** Block: 1 *****/
@@ -4298,7 +4294,6 @@ $CAD_dae_blocks_residual_functions$
 $CAD_functions$
 ",
             generatedCode="
-
 /******** Declarations *******/
     jmi_ad_var_t v_0;
     jmi_ad_var_t d_0;
@@ -4324,13 +4319,13 @@ jmi_real_t** dz = jmi->dz;
     d_0 = (*dz)[jmi_get_index_from_value_ref(2)-jmi->offs_real_dx] * _A_1_1_0 + _dx_1_1_8 * (*dz)[jmi_get_index_from_value_ref(4)-jmi->offs_real_dx];
     v_1 = _dx_1_2_9 * _A_2_1_2;
     d_1 = (*dz)[jmi_get_index_from_value_ref(3)-jmi->offs_real_dx] * _A_2_1_2 + _dx_1_2_9 * (*dz)[jmi_get_index_from_value_ref(6)-jmi->offs_real_dx];
-    _der_dx_1_1_14 = v_0 + v_1;
+    _der_dx_1_1_10 = v_0 + v_1;
     (*dz)[jmi_get_index_from_value_ref(0)-jmi->offs_real_dx] = d_0 + d_1;
     v_2 = _dx_1_1_8 * _A_1_2_1;
     d_2 = (*dz)[jmi_get_index_from_value_ref(2)-jmi->offs_real_dx] * _A_1_2_1 + _dx_1_1_8 * (*dz)[jmi_get_index_from_value_ref(5)-jmi->offs_real_dx];
     v_3 = _dx_1_2_9 * _A_2_2_3;
     d_3 = (*dz)[jmi_get_index_from_value_ref(3)-jmi->offs_real_dx] * _A_2_2_3 + _dx_1_2_9 * (*dz)[jmi_get_index_from_value_ref(7)-jmi->offs_real_dx];
-    _der_dx_1_2_15 = v_2 + v_3;
+    _der_dx_1_2_11 = v_2 + v_3;
     (*dz)[jmi_get_index_from_value_ref(1)-jmi->offs_real_dx] = d_2 + d_3;
     /*********** Real outputs **********/
     /*** Integer and boolean outputs ***/
@@ -4350,50 +4345,32 @@ static int dae_block_dir_der_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* dx,jmi_rea
     jmi_real_t** dz;
     if (evaluation_mode == JMI_BLOCK_INITIALIZE) {
         x[0] = _A_2_2_3;
-        x[1] = _temp_1_2_1_12;
-        x[2] = _temp_1_1_2_11;
-        x[3] = _temp_1_1_1_10;
-        x[4] = _temp_1_2_2_13;
-        x[5] = _A_2_1_2;
-        x[6] = _A_1_2_1;
-        x[7] = _A_1_1_0;
+        x[1] = _A_2_1_2;
+        x[2] = _A_1_2_1;
+        x[3] = _A_1_1_0;
         return 0;
     } else if (evaluation_mode == JMI_BLOCK_EVALUATE) {
         dz = jmi->dz_active_variables;
         (*dz)[ jmi_get_index_from_value_ref(7)-jmi->offs_real_dx] = dx[0];
         _A_2_2_3 = x[0];
-        (*dz)[ jmi_get_index_from_value_ref(14)-jmi->offs_real_dx] = dx[1];
-        _temp_1_2_1_12 = x[1];
-        (*dz)[ jmi_get_index_from_value_ref(13)-jmi->offs_real_dx] = dx[2];
-        _temp_1_1_2_11 = x[2];
-        (*dz)[ jmi_get_index_from_value_ref(12)-jmi->offs_real_dx] = dx[3];
-        _temp_1_1_1_10 = x[3];
-        (*dz)[ jmi_get_index_from_value_ref(15)-jmi->offs_real_dx] = dx[4];
-        _temp_1_2_2_13 = x[4];
-        (*dz)[ jmi_get_index_from_value_ref(6)-jmi->offs_real_dx] = dx[5];
-        _A_2_1_2 = x[5];
-        (*dz)[ jmi_get_index_from_value_ref(5)-jmi->offs_real_dx] = dx[6];
-        _A_1_2_1 = x[6];
-        (*dz)[ jmi_get_index_from_value_ref(4)-jmi->offs_real_dx] = dx[7];
-        _A_1_1_0 = x[7];
+        (*dz)[ jmi_get_index_from_value_ref(6)-jmi->offs_real_dx] = dx[1];
+        _A_2_1_2 = x[1];
+        (*dz)[ jmi_get_index_from_value_ref(5)-jmi->offs_real_dx] = dx[2];
+        _A_1_2_1 = x[2];
+        (*dz)[ jmi_get_index_from_value_ref(4)-jmi->offs_real_dx] = dx[3];
+        _A_1_1_0 = x[3];
     } else if (evaluation_mode == JMI_BLOCK_EVALUATE_INACTIVE) {
         dz = jmi->dz;
     } else if (evaluation_mode == JMI_BLOCK_WRITE_BACK) {
         dz = jmi->dz;
         (*dz)[jmi_get_index_from_value_ref(7)-jmi->offs_real_dx] = -(*dF)[0];
-        (*dz)[jmi_get_index_from_value_ref(14)-jmi->offs_real_dx] = -(*dF)[1];
-        (*dz)[jmi_get_index_from_value_ref(13)-jmi->offs_real_dx] = -(*dF)[2];
-        (*dz)[jmi_get_index_from_value_ref(12)-jmi->offs_real_dx] = -(*dF)[3];
-        (*dz)[jmi_get_index_from_value_ref(15)-jmi->offs_real_dx] = -(*dF)[4];
-        (*dz)[jmi_get_index_from_value_ref(6)-jmi->offs_real_dx] = -(*dF)[5];
-        (*dz)[jmi_get_index_from_value_ref(5)-jmi->offs_real_dx] = -(*dF)[6];
-        (*dz)[jmi_get_index_from_value_ref(4)-jmi->offs_real_dx] = -(*dF)[7];
+        (*dz)[jmi_get_index_from_value_ref(6)-jmi->offs_real_dx] = -(*dF)[1];
+        (*dz)[jmi_get_index_from_value_ref(5)-jmi->offs_real_dx] = -(*dF)[2];
+        (*dz)[jmi_get_index_from_value_ref(4)-jmi->offs_real_dx] = -(*dF)[3];
     } else {
         return -1;
     }
     if (evaluation_mode == JMI_BLOCK_EVALUATE_INACTIVE || evaluation_mode == JMI_BLOCK_EVALUATE) {
-        (*res)[0] = _temp_1_2_2_13 - (_A_2_2_3);
-        (*dF)[0] = (*dz)[jmi_get_index_from_value_ref(15)-jmi->offs_real_dx] - ((*dz)[jmi_get_index_from_value_ref(7)-jmi->offs_real_dx]);
         JMI_ARRAY_INIT_2(STATREAL, jmi_ad_var_t, jmi_array_t, tmp_var_0, 4, 2, 2, 2)
         JMI_ARRAY_INIT_2(STATREAL, jmi_ad_var_t, jmi_array_t, tmp_der_0, 4, 2, 2, 2)
         JMI_ARRAY_INIT_2(STATREAL, jmi_ad_var_t, jmi_array_t, tmp_var_1, 4, 2, 2, 2)
@@ -4417,25 +4394,15 @@ static int dae_block_dir_der_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* dx,jmi_rea
         jmi_array_ref_2(tmp_der_2, 2,1) = (*dz)[jmi_get_index_from_value_ref(10)-jmi->offs_real_dx];
         jmi_array_ref_2(tmp_der_2, 2,2) = (*dz)[jmi_get_index_from_value_ref(11)-jmi->offs_real_dx];
         func_CADCodeGenTests_CADArray1_f_der_AD0(tmp_var_1, tmp_var_2, tmp_der_1, tmp_der_2, tmp_var_0, tmp_der_0);
-        (*res)[1] = jmi_array_val_2(tmp_var_0, 1,1) - (_temp_1_1_1_10);
-        (*res)[2] = jmi_array_val_2(tmp_var_0, 1,2) - (_temp_1_1_2_11);
-        (*res)[3] = jmi_array_val_2(tmp_var_0, 2,1) - (_temp_1_2_1_12);
-        (*res)[4] = jmi_array_val_2(tmp_var_0, 2,2) - (_temp_1_2_2_13);
-        (*dF)[1] = jmi_array_val_2(tmp_der_0, 1,1) - ((*dz)[jmi_get_index_from_value_ref(12)-jmi->offs_real_dx]);
-        (*dF)[2] = jmi_array_val_2(tmp_der_0, 1,2) - ((*dz)[jmi_get_index_from_value_ref(13)-jmi->offs_real_dx]);
-        (*dF)[3] = jmi_array_val_2(tmp_der_0, 2,1) - ((*dz)[jmi_get_index_from_value_ref(14)-jmi->offs_real_dx]);
-        (*dF)[4] = jmi_array_val_2(tmp_der_0, 2,2) - ((*dz)[jmi_get_index_from_value_ref(15)-jmi->offs_real_dx]);
-        (*res)[5] = _temp_1_2_1_12 - (_A_2_1_2);
-        (*dF)[5] = (*dz)[jmi_get_index_from_value_ref(14)-jmi->offs_real_dx] - ((*dz)[jmi_get_index_from_value_ref(6)-jmi->offs_real_dx]);
-        (*res)[6] = _temp_1_1_2_11 - (_A_1_2_1);
-        (*dF)[6] = (*dz)[jmi_get_index_from_value_ref(13)-jmi->offs_real_dx] - ((*dz)[jmi_get_index_from_value_ref(5)-jmi->offs_real_dx]);
-        (*res)[7] = _temp_1_1_1_10 - (_A_1_1_0);
-        (*dF)[7] = (*dz)[jmi_get_index_from_value_ref(12)-jmi->offs_real_dx] - ((*dz)[jmi_get_index_from_value_ref(4)-jmi->offs_real_dx]);
+        (*res)[0] = jmi_array_val_2(tmp_var_0, 1,1) - (_A_1_1_0);
+        (*res)[1] = jmi_array_val_2(tmp_var_0, 1,2) - (_A_1_2_1);
+        (*res)[2] = jmi_array_val_2(tmp_var_0, 2,1) - (_A_2_1_2);
+        (*res)[3] = jmi_array_val_2(tmp_var_0, 2,2) - (_A_2_2_3);
+        (*dF)[0] = jmi_array_val_2(tmp_der_0, 1,1) - ((*dz)[jmi_get_index_from_value_ref(4)-jmi->offs_real_dx]);
+        (*dF)[1] = jmi_array_val_2(tmp_der_0, 1,2) - ((*dz)[jmi_get_index_from_value_ref(5)-jmi->offs_real_dx]);
+        (*dF)[2] = jmi_array_val_2(tmp_der_0, 2,1) - ((*dz)[jmi_get_index_from_value_ref(6)-jmi->offs_real_dx]);
+        (*dF)[3] = jmi_array_val_2(tmp_der_0, 2,2) - ((*dz)[jmi_get_index_from_value_ref(7)-jmi->offs_real_dx]);
         (*dz)[jmi_get_index_from_value_ref(7)-jmi->offs_real_dx] = 0;
-        (*dz)[jmi_get_index_from_value_ref(14)-jmi->offs_real_dx] = 0;
-        (*dz)[jmi_get_index_from_value_ref(13)-jmi->offs_real_dx] = 0;
-        (*dz)[jmi_get_index_from_value_ref(12)-jmi->offs_real_dx] = 0;
-        (*dz)[jmi_get_index_from_value_ref(15)-jmi->offs_real_dx] = 0;
         (*dz)[jmi_get_index_from_value_ref(6)-jmi->offs_real_dx] = 0;
         (*dz)[jmi_get_index_from_value_ref(5)-jmi->offs_real_dx] = 0;
         (*dz)[jmi_get_index_from_value_ref(4)-jmi->offs_real_dx] = 0;

trunk/Compiler/ModelicaCBackEnd/src/test/CCodeGenTests.mo
@@ -6711,8 +6711,7 @@ void model_init_eval_parameters_0(jmi_t* jmi) {
     _p3_4 = (func_CCodeGenTests_DependentParametersWithScalingTest1_F_exp0((_p2_1*sf(2))))/sf(4);
     tmp_2->x = (_r2_x_2*sf(3));
     func_CCodeGenTests_DependentParametersWithScalingTest1_FR_def1(tmp_2, tmp_1);
-    _temp_1_x_5 = (tmp_1->x)/sf(5);
-    _r3_x_6 = ((_temp_1_x_5*sf(5)))/sf(6);
+    _r3_x_5 = (tmp_1->x)/sf(5);
 }
 
 void model_init_eval_parameters_base(jmi_t* jmi) {
@@ -12200,32 +12199,30 @@ void func_CCodeGenTests_Algorithm9_fw_def1(R_0_r* r_v) {
     /**** Other variables ***/
     JMI_ARRAY_INIT_1(STATREAL, jmi_ad_var_t, jmi_array_t, tmp_1, 2, 1, 2)
     func_CCodeGenTests_Algorithm9_f_def0(tmp_1);
-    _temp_2_1_8 = (jmi_array_val_1(tmp_1, 1));
-    _temp_2_2_9 = (jmi_array_val_1(tmp_1, 2));
-    _r_a_1_0 = 2 * _temp_2_1_8;
-    _r_a_2_1 = 2 * _temp_2_2_9;
+    _temp_2_1_6 = (jmi_array_val_1(tmp_1, 1));
+    _temp_2_2_7 = (jmi_array_val_1(tmp_1, 2));
+    _r_a_1_0 = 2 * _temp_2_1_6;
+    _r_a_2_1 = 2 * _temp_2_2_7;
     JMI_ARRAY_INIT_1(STATREAL, jmi_ad_var_t, jmi_array_t, tmp_2, 2, 1, 2)
     func_CCodeGenTests_Algorithm9_f_def0(tmp_2);
-    _temp_3_1_10 = (jmi_array_val_1(tmp_2, 1));
-    _temp_3_2_11 = (jmi_array_val_1(tmp_2, 2));
-    _r_a_2_1 = _temp_3_1_10;
-    _r_a_3_2 = _temp_3_2_11;
+    _temp_3_1_8 = (jmi_array_val_1(tmp_2, 1));
+    _temp_3_2_9 = (jmi_array_val_1(tmp_2, 2));
+    _r_a_2_1 = _temp_3_1_8;
+    _r_a_3_2 = _temp_3_2_9;
     JMI_ARRAY_INIT_1(STATREAL, jmi_ad_var_t, jmi_array_t, tmp_4, 3, 1, 3)
     tmp_3->a = tmp_4;
     func_CCodeGenTests_Algorithm9_fw_def1(tmp_3);
-    _temp_4_a_1_12 = (jmi_array_val_1(tmp_3->a, 1));
-    _temp_4_a_2_13 = (jmi_array_val_1(tmp_3->a, 2));
-    _temp_4_a_3_14 = (jmi_array_val_1(tmp_3->a, 3));
-    _r_a_1_0 = _temp_4_a_1_12;
-    _r_a_2_1 = _temp_4_a_2_13;
-    _r_a_3_2 = _temp_4_a_3_14;
+    _temp_4_a_1_10 = (jmi_array_val_1(tmp_3->a, 1));
+    _temp_4_a_2_11 = (jmi_array_val_1(tmp_3->a, 2));
+    _temp_4_a_3_12 = (jmi_array_val_1(tmp_3->a, 3));
+    _r_a_1_0 = _temp_4_a_1_10;
+    _r_a_2_1 = _temp_4_a_2_11;
+    _r_a_3_2 = _temp_4_a_3_12;
     _re_a_1_3 = 1;
     JMI_ARRAY_INIT_1(STATREAL, jmi_ad_var_t, jmi_array_t, tmp_5, 2, 1, 2)
     func_CCodeGenTests_Algorithm9_f_def0(tmp_5);
-    _temp_1_1_6 = (jmi_array_val_1(tmp_5, 1));
-    _temp_1_2_7 = (jmi_array_val_1(tmp_5, 2));
-    _re_a_2_4 = _temp_1_1_6;
-    _re_a_3_5 = _temp_1_2_7;
+    _re_a_2_4 = (jmi_array_val_1(tmp_5, 1));
+    _re_a_3_5 = (jmi_array_val_1(tmp_5, 2));
     /********* Write back reinits *******/
 ")})));
 end Algorithm9;

trunk/Compiler/ModelicaFlatTree/src/jastadd/FlatAPI.jrag
@@ -2543,7 +2543,7 @@ aspect FVariableUses {
      * Check if this variable is used on the left hand side of a function call equation or statement.
      */
     syn lazy boolean FAbstractVariable.usedAsFunctionCallLeft() {
-        for (FExp e : nonDefiningUses()) 
+        for (FExp e : definingUses()) 
             if (e.inFunctionCallLeft())
                 return true;
         return false;
@@ -2598,9 +2598,12 @@ aspect FVariableUses {
     /**
      * Check if this is the left hand side of an equation.
      */
-    inh boolean FIdUseExp.isLeftHandSide();
+    inh boolean FExp.isLeftHandSide();
     eq BaseNode.getChild().isLeftHandSide()    = false;
     eq FEquation.getLeft().isLeftHandSide()    = true;
+    eq FFunctionCallLeft.getChild().isLeftHandSide()  = true;
+    eq FArray.getChild().isLeftHandSide()             = isLeftHandSide();
+    eq FRecordConstructor.getChild().isLeftHandSide() = isLeftHandSide();
     
     /**
      * Check if this is the left hand side of an assignment or function call statement.

trunk/Compiler/ModelicaFrontEnd/src/test/FunctionTests.mo
@@ -6941,12 +6941,8 @@ equation
 fclass FunctionTests.ArrayOutputScalarization9
  Real x[1];
  Real x[2];
- Real temp_1[1];
- Real temp_1[2];
 equation
- ({temp_1[1],temp_1[2]}) = FunctionTests.ArrayOutputScalarization9.f();
- x[1] = temp_1[1];
- x[2] = temp_1[2];
+ ({x[1], x[2]}) = FunctionTests.ArrayOutputScalarization9.f();
 
 public
  function FunctionTests.ArrayOutputScalarization9.f
@@ -13151,27 +13147,8 @@ fclass FunctionTests.UnknownSize.Misc.Misc1
  Real y[4];
  Real y[5];
  Real y[6];
- discrete Integer temp_1[1];
- discrete Integer temp_1[2];
- discrete Integer temp_1[3];
- discrete Integer temp_1[4];
- discrete Integer temp_1[5];
- discrete Integer temp_1[6];
-initial equation 
- pre(temp_1[1]) = 0;
- pre(temp_1[2]) = 0;
- pre(temp_1[3]) = 0;
- pre(temp_1[4]) = 0;
- pre(temp_1[5]) = 0;
- pre(temp_1[6]) = 0;
 equation
- ({temp_1[1], temp_1[2], temp_1[3], temp_1[4], temp_1[5], temp_1[6]}) = FunctionTests.UnknownSize.Misc.Misc1.f(3);
- y[1] = temp_1[1];
- y[2] = temp_1[2];
- y[3] = temp_1[3];
- y[4] = temp_1[4];
- y[5] = temp_1[5];
- y[6] = temp_1[6];
+ ({y[1], y[2], y[3], y[4], y[5], y[6]}) = FunctionTests.UnknownSize.Misc.Misc1.f(3);
 
 public
  function FunctionTests.UnknownSize.Misc.Misc1.f
@@ -13186,7 +13163,6 @@ public
  end FunctionTests.UnknownSize.Misc.Misc1.f;
 
 end FunctionTests.UnknownSize.Misc.Misc1;
-
 ")})));
 end Misc1;
 
@@ -13213,18 +13189,8 @@ fclass FunctionTests.UnknownSize.Misc.Misc2
  Real y[1];
  Real y[2];
  Real y[3];
- discrete Integer temp_1[1];
- discrete Integer temp_1[2];
- discrete Integer temp_1[3];
-initial equation 
- pre(temp_1[1]) = 0;
- pre(temp_1[2]) = 0;
- pre(temp_1[3]) = 0;
 equation
- ({temp_1[1], temp_1[2], temp_1[3]}) = FunctionTests.UnknownSize.Misc.Misc2.f({1, 2, 3});
- y[1] = temp_1[1];
- y[2] = temp_1[2];
- y[3] = temp_1[3];
+ ({y[1], y[2], y[3]}) = FunctionTests.UnknownSize.Misc.Misc2.f({1, 2, 3});
 
 public
  function FunctionTests.UnknownSize.Misc.Misc2.f
@@ -13239,7 +13205,6 @@ public
  end FunctionTests.UnknownSize.Misc.Misc2.f;
 
 end FunctionTests.UnknownSize.Misc.Misc2;
-
 ")})));
 end Misc2;
 

trunk/Compiler/ModelicaFrontEnd/src/test/RecordTests.mo
@@ -3399,11 +3399,9 @@ model RecordScalarize32
             variability_propagation=false,
             flatModel="
 fclass RecordTests.RecordScalarize32
- parameter Real temp_1.x;
  parameter Real pb.a.x;
 parameter equation
- (RecordTests.RecordScalarize32.A(temp_1.x)) = RecordTests.RecordScalarize32.f(2);
- pb.a.x = temp_1.x;
+ (RecordTests.RecordScalarize32.A(pb.a.x)) = RecordTests.RecordScalarize32.f(2);
 
 public
  function RecordTests.RecordScalarize32.f
@@ -3450,11 +3448,9 @@ model RecordScalarize33
             flatModel="
 fclass RecordTests.RecordScalarize33
  Real b.a1.x;
- parameter Real temp_2.x;
  parameter Real b.a2.x;
 parameter equation
- (RecordTests.RecordScalarize33.A(temp_2.x)) = RecordTests.RecordScalarize33.f(2);
- b.a2.x = temp_2.x;
+ (RecordTests.RecordScalarize33.A(b.a2.x)) = RecordTests.RecordScalarize33.f(2);
 equation
  (RecordTests.RecordScalarize33.A(b.a1.x)) = RecordTests.RecordScalarize33.f(time);
 
@@ -4369,12 +4365,8 @@ model RecordOutput1
 fclass RecordTests.RecordOutput1
  Real z.y;
  Real z.x;
- Real temp_1.y;
- Real temp_1.x;
 equation
- (RecordTests.RecordOutput1.A(temp_1.y, temp_1.x)) = RecordTests.RecordOutput1.f();
- z.y = temp_1.y;
- z.x = temp_1.x;
+ (RecordTests.RecordOutput1.A(z.y, z.x)) = RecordTests.RecordOutput1.f();
 
 public
  function RecordTests.RecordOutput1.f
@@ -5348,15 +5340,11 @@ model RecordParam3
 			inline_functions="none",
 			flatModel="
 fclass RecordTests.RecordParam3
- parameter Real temp_1[1];
- parameter Real temp_1[2];
- Real a1.y;
  parameter Real a1.x[1];
  parameter Real a1.x[2];
+ Real a1.y;
 parameter equation
- ({temp_1[1], temp_1[2]}) = RecordTests.RecordParam3.f(1);
- a1.x[1] = temp_1[1];
- a1.x[2] = temp_1[2];
+ ({a1.x[1], a1.x[2]}) = RecordTests.RecordParam3.f(1);
 equation
  a1.y = 2;
 
@@ -5453,10 +5441,6 @@ model RecordParam6
 			inline_functions="none",
 			flatModel="
 fclass RecordTests.RecordParam6
- parameter Real temp_1[1];
- parameter Real temp_1[2];
- parameter Real temp_2[1];
- parameter Real temp_2[2];
  parameter Real a1.x[1];
  parameter Real a1.x[2];
  parameter Real a2.x[1];
@@ -5466,12 +5450,8 @@ fclass RecordTests.RecordParam6
  parameter Real a2.y[1];
  parameter Real a2.y[2];
 parameter equation
- ({temp_1[1], temp_1[2]}) = RecordTests.RecordParam6.f();
- ({temp_2[1], temp_2[2]}) = RecordTests.RecordParam6.f();
- a1.x[1] = temp_1[1];
- a1.x[2] = temp_1[2];
- a2.x[1] = temp_2[1];
- a2.x[2] = temp_2[2];
+ ({a1.x[1], a1.x[2]}) = RecordTests.RecordParam6.f();
+ ({a2.x[1], a2.x[2]}) = RecordTests.RecordParam6.f();
  a1.y[1] = a1.x[1];
  a1.y[2] = a1.x[2];
  a2.y[1] = a2.x[1];

trunk/Compiler/ModelicaMiddleEnd/src/test/FunctionInlining.mo
@@ -2036,18 +2036,9 @@ model MultipleOutputsInline4
 fclass FunctionInlining.MultipleOutputsInline4
  Real x;
  Real y;
- discrete String temp_3;
- discrete Integer temp_4;
- discrete Boolean temp_5;
- discrete FunctionInlining.MultipleOutputsInline4.E temp_6;
-initial equation 
- pre(temp_3) = \"\";
- pre(temp_4) = 0;
- pre(temp_5) = false;
- pre(temp_6) = FunctionInlining.MultipleOutputsInline4.E.E1;
 equation
  x = 1;
- (FunctionInlining.MultipleOutputsInline4.R(y, temp_3, temp_4, temp_5, temp_6)) = FunctionInlining.MultipleOutputsInline4.f3(x);
+ (FunctionInlining.MultipleOutputsInline4.R(y, , , , )) = FunctionInlining.MultipleOutputsInline4.f3(x);
 
 public
  function FunctionInlining.MultipleOutputsInline4.f3
@@ -3290,19 +3281,15 @@ fclass FunctionInlining.InlineAnnotation10
  Real z;
  discrete Integer t;
  Real _der_x;
- Real temp_6;
- discrete Integer temp_7;
 initial equation 
  pre(t) = 0;
  y = 0.0;
- pre(temp_7) = 0;
 equation
  _der_x = time;
- x = temp_6;
  z = der(y);
  t = if time < pre(t) or time >= pre(t) + 1 or initial() then integer(time) else pre(t);
  _der_x = y * der(y);
- (temp_6, temp_7) = FunctionInlining.InlineAnnotation10.f2(y);
+ (x, ) = FunctionInlining.InlineAnnotation10.f2(y);
 
 public
  function FunctionInlining.InlineAnnotation10.f2

commit 4bc170dfce672d442887d74e0b3f6ea47df9d79f

#4534 Fixes bug introduced in r7929
git-svn-id: https://svn.jmodelica.org@7946 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/TransformCanonical.jrag
@@ -2175,7 +2175,12 @@ aspect TransformCanonical {
     
     inh FIdUseExp FIdUseExp.inlineUsesFunctionCallLeft();
     eq BaseNode.getChild().inlineUsesFunctionCallLeft()  = null;
-    eq FEquation.getRight().inlineUsesFunctionCallLeft() = getLeft() instanceof FIdUseExp ? (FIdUseExp)getLeft() : null;
+    eq FEquation.getRight().inlineUsesFunctionCallLeft() = getLeft().getEquationLeftInlineUse();
+    
+    syn FIdUseExp FExp.getEquationLeftInlineUse() = null;
+    eq FIdUseExp.getEquationLeftInlineUse()       = this;
+    eq FAbstractDerExp.getEquationLeftInlineUse() = null;
+    eq FPreExp.getEquationLeftInlineUse()         = null;
     
 	/**
 	 * Flag for removing equation.

trunk/Compiler/ModelicaMiddleEnd/src/test/IndexReduction.mo
@@ -1615,13 +1615,15 @@ fclass IndexReduction.Variability1
  Real a;
  Real _der_x;
  Real _der_y;
+ Real temp_3;
  Real temp_4;
 initial equation 
  a = 0.0;
 equation
  (x, p) = IndexReduction.Variability1.F1(y + a);
  _der_x = _der_y * 2;
- (_der_x, temp_4) = IndexReduction.Variability1.F1((y + a) * (_der_y + der(a)));
+ (temp_3, temp_4) = IndexReduction.Variability1.F1((y + a) * (_der_y + der(a)));
+ _der_x = temp_3;
  0.0 = temp_4;
 
 public
@@ -3378,6 +3380,8 @@ fclass IndexReduction.FunctionInlining.Test8
  Real _der_der_x;
  Real _der_der_y;
  Real _der_der_b;
+ Real temp_5;
+ Real temp_10;
 initial equation 
  y = 0.0;
  vy = 0.0;
@@ -3388,11 +3392,13 @@ equation
  der(vy) = a * y;
  (b, ) = IndexReduction.FunctionInlining.Test8.F(x, y);
  b = time;
- (_der_b, ) = IndexReduction.FunctionInlining.Test8.F(_der_x, der(y));
+ (temp_5, ) = IndexReduction.FunctionInlining.Test8.F(_der_x, der(y));
+ _der_b = temp_5;
  _der_b = 1.0;
  _der_der_x = _der_vx;
  _der_der_y = der(vy);
- (_der_der_b, ) = IndexReduction.FunctionInlining.Test8.F(_der_der_x, _der_der_y);
+ (temp_10, ) = IndexReduction.FunctionInlining.Test8.F(_der_der_x, _der_der_y);
+ _der_der_b = temp_10;
  _der_der_b = 0.0;
 
 public