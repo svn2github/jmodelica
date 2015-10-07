svn2github/jmodelica

commit d5e0fbc2be387ec048a3a1ce815375bed2e33aeb

#4392 Added nominal calculations and code generation for residual equations in blocks.
git-svn-id: https://svn.jmodelica.org@8067 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen.jrag
@@ -3436,8 +3436,10 @@ aspect CCodeGen {
             relExp.genBlockResidualFunction_printSwitchIndex_C(p, str, nextIndent, directlyActiveSwitchEnumerator);
         
         // Equation nominal values
-        str.print(indent + "} else if (evaluation_mode == JMI_BLOCK_EQUATION_NOMINAL) {\n");
-        p.C_blockResidualAttributePrinter.printEquationNominal(p, str, nextIndent, this);
+        if (hasEquationNominal_C()) {
+            str.print(indent + "} else if (evaluation_mode == JMI_BLOCK_EQUATION_NOMINAL) {\n");
+            p.C_blockResidualAttributePrinter.printEquationNominal(p, str, nextIndent, this);
+        }
         
         str.print(indent + "} else if (evaluation_mode == JMI_BLOCK_INITIALIZE) {\n");
         // Loop over all active variables and write their value to x	
@@ -3626,10 +3628,65 @@ aspect CCodeGen {
             }
 
             public void printEquationNominal(CodePrinter p, CodeStream str, String indent, AbstractEquationBlock block) {
+                block.genEquationNominal_C(p, str, indent);
             }
         }
     }
+    
+    public abstract boolean AbstractEquationBlock.hasEquationNominal_C();
+    public boolean EquationBlock.hasEquationNominal_C() {
+        for (SimpleEquationBlock block : unsolvedBlocks()) {
+            if (block.hasEquationNominal_C()) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean SimpleEquationBlock.hasEquationNominal_C() { return realVariables().size() > 0; }
+    public boolean DynamicStateBlock.hasEquationNominal_C() { return true; } // To trigger error message
+    
+    
+    public abstract void AbstractEquationBlock.genEquationNominal_C(CodePrinter p, CodeStream str, String indent);
+    
+    @Override
+    public void EquationBlock.genEquationNominal_C(CodePrinter p, CodeStream str, String indent) {
+        Enumerator e = new Enumerator();
+        for (SimpleEquationBlock block : unsolvedBlocks()) {
+            block.genEquationNominal_C(p, str, indent, e);
+        }
+    }
 
+    @Override
+    public void DynamicStateBlock.genEquationNominal_C(CodePrinter p, CodeStream str, String indent) {
+        throw new UnsupportedOperationException("DynamicStateBlock.genEquationNominal_C() is not implemented!");
+    }
+
+    @Override
+    public void SimpleEquationBlock.genEquationNominal_C(CodePrinter p, CodeStream str, String indent) {
+        genEquationNominal_C(p, str, indent, new Enumerator());
+    }
+    
+    public void SimpleEquationBlock.genEquationNominal_C(CodePrinter p, CodeStream str, String indent, Enumerator e) {
+        for (int i = 0; i < realVariables().size(); i++) {
+            str.format("%s(*res)[%d] = ", indent, e.next());
+            equation().genEquationNominal_C(p, str, indent);
+            str.print(";\n");
+        }
+    }
+    
+    public void FAbstractEquation.genEquationNominal_C(CodePrinter p, CodeStream str, String indent) { str.print("1"); }
+    public void FEquation.genEquationNominal_C(CodePrinter p, CodeStream str, String indent) {
+        FExp nom = nominal();
+        if (nom.variability().knownParameterOrLess()) {
+            try {
+                nom = getLeft().dynamicFExp(nom.ceval().buildLiteral());
+            } catch (ConstantEvaluationException e) {
+                
+            }
+        }
+        p.print(nom, str, indent);
+    }
+    
 	public void FVariable.genBlockResidualValuref_C(CodePrinter p, CodeStream str, String indent, 
 	        Map<FVariable,String> solverArrayMap) {
 		if (valueReference() != -1) 

trunk/Compiler/ModelicaFlatTree/src/jastadd/FlatAPI.jrag
@@ -2546,6 +2546,66 @@ aspect Attributes {
 
 }
 
+aspect Nominals {
+    syn FExp FEquation.nominal() = getLeft().dynamicFExp(FExp.nominalMax(getLeft(), getRight()));
+    
+    public static FExp FExp.nominalMax(FExp e1, FExp e2) {
+        return new FMaxExp(e1.nominal(), new Opt<FExp>(e2.nominal()));
+    }
+    public static FExp FExp.nominalDefault() {
+        return new FIntegerLitExp(1);
+    }
+    
+    syn FExp FExp.nominal() = FExp.nominalDefault();
+    eq FLitExp.nominal()    = new FAbsExp(fullCopy());
+    
+    eq FDotAddExp.nominal() = FExp.nominalMax(getLeft(), getRight());
+    eq FDotSubExp.nominal() = FExp.nominalMax(getLeft(), getRight());
+    eq FDotMulExp.nominal() = new FMulExp(getLeft().nominal(), getRight().nominal());
+    eq FDotDivExp.nominal() = new FDivExp(getLeft().nominal(), getRight().nominal());
+    eq FDotPowExp.nominal() = new FPowExp(getLeft().nominal(), getRight().nominal());
+    eq FNegExp.nominal()    = getFExp().nominal();
+    eq FAbsExp.nominal()    = getFExp().nominal();
+    eq FMinMaxExp.nominal() = hasY() ? FExp.nominalMax(getX(), getY()) : getX().nominal();
+    
+    eq FExpExp.nominal()    = new FExpExp(getFExp().nominal());
+    eq FLogExp.nominal()    = new FLogExp(getFExp().nominal());
+    eq FLog10Exp.nominal()  = new FLog10Exp(getFExp().nominal());
+    
+    eq FIfExp.nominal()     = FExp.nominalMax(getThenExp(), getElseExp());
+    
+    eq FNoEventExp.nominal() = getFExp().nominal();
+    eq FSmoothExp.nominal()  = getFExp().nominal();
+    
+    eq FHomotopyExp.nominal()    = FExp.nominalMax(getActual(), getSimplified());
+    eq FSpatialDistExp.nominal() = FExp.nominalMax(getIn0(), getIn1());
+    eq FDelayExp.nominal()       = getFExp().nominal();
+    
+    eq FFunctionCall.nominal()   = FExp.nominalDefault();
+    
+    eq FIdUseExp.nominal() = myFV().nominal();
+    
+    syn FExp FAbstractVariable.nominal() {
+        if (variability().parameterOrLess() && valueIsFixed()) {
+            return new FAbsExp(createUseExp());
+        } else {
+            return variableNominal();
+        }
+    }
+    
+    syn FExp FAbstractVariable.variableNominal() {
+        return FExp.nominalDefault();
+    }
+    
+    eq FRealVariable.variableNominal() {
+        if (nominalAttributeSet()) {
+            return new FAbsExp(nominalAttributeExp().unboundCopy());
+        } else {
+            return super.variableNominal();
+        }
+    }
+}
+
 aspect FVariableUses {
     
     /**

trunk/Compiler/ModelicaCBackEnd/src/test/LocalIterationBack.mo
@@ -53,6 +53,7 @@ static int dae_block_1(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
     } else if (evaluation_mode == JMI_BLOCK_ACTIVE_SWITCH_INDEX) {
     } else if (evaluation_mode == JMI_BLOCK_DIRECTLY_ACTIVE_SWITCH_INDEX) {
     } else if (evaluation_mode == JMI_BLOCK_EQUATION_NOMINAL) {
+        (*res)[0] = 23;
     } else if (evaluation_mode == JMI_BLOCK_INITIALIZE) {
         x[0] = _b_1;
     } else if (evaluation_mode & JMI_BLOCK_EVALUATE || evaluation_mode & JMI_BLOCK_WRITE_BACK) {
@@ -84,6 +85,7 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
     } else if (evaluation_mode == JMI_BLOCK_ACTIVE_SWITCH_INDEX) {
     } else if (evaluation_mode == JMI_BLOCK_DIRECTLY_ACTIVE_SWITCH_INDEX) {
     } else if (evaluation_mode == JMI_BLOCK_EQUATION_NOMINAL) {
+        (*res)[0] = 20;
     } else if (evaluation_mode == JMI_BLOCK_INITIALIZE) {
         x[0] = _c_2;
     } else if (evaluation_mode & JMI_BLOCK_EVALUATE || evaluation_mode & JMI_BLOCK_WRITE_BACK) {

commit 09805179f8e74c4bbde94b274a517ce3794c04ad

#4392 Fixes bug introduced in r8067
git-svn-id: https://svn.jmodelica.org@8070 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen.jrag
@@ -3488,7 +3488,9 @@ aspect CCodeGen {
         // Equation nominal values
         if (hasEquationNominal_C()) {
             genOpenEvalMode_C(str, indent, "JMI_BLOCK_EQUATION_NOMINAL");
-            p.C_blockResidualAttributePrinter.printEquationNominal(p, str, nextIndent, this);
+            Map<FAbstractEquation,FExp> nominalMap = new HashMap<FAbstractEquation,FExp>();
+            genEquationNominalVarDecl_C(p, str, p.indent(indent), nominalMap);
+            genEquationNominal_C(p, str, p.indent(indent), nominalMap);
             genCloseEvalMode_C(str, indent);
         }
         
@@ -3701,10 +3703,6 @@ aspect CCodeGen {
             public void printVariability(CodePrinter p, CodeStream str, String indent, Collection<FVariable> iterVars) {
                 str.print(ASTNode.fConstant().genJMIVariabilityConstant_C() + ", ");
             }
-            
-            public void printEquationNominal(CodePrinter p, CodeStream str, String indent, AbstractEquationBlock block) {
-                block.genEquationNominal_C(p, str, indent);
-            }
         }
     }
     
@@ -3721,46 +3719,65 @@ aspect CCodeGen {
     public boolean DynamicStateBlock.hasEquationNominal_C() { return true; } // To trigger error message
     
     
-    public abstract void AbstractEquationBlock.genEquationNominal_C(CodePrinter p, CodeStream str, String indent);
+    public abstract void AbstractEquationBlock.genEquationNominalVarDecl_C(CodePrinter p, CodeStream str, String indent, Map<FAbstractEquation,FExp> nominalMap);
     
     @Override
-    public void EquationBlock.genEquationNominal_C(CodePrinter p, CodeStream str, String indent) {
+    public void EquationBlock.genEquationNominalVarDecl_C(CodePrinter p, CodeStream str, String indent, Map<FAbstractEquation,FExp> nominalMap) {
+        for (SimpleEquationBlock block : unsolvedBlocks())
+            block.genEquationNominalVarDecl_C(p, str, indent, nominalMap);
+    }
+    
+    @Override
+    public void DynamicStateBlock.genEquationNominalVarDecl_C(CodePrinter p, CodeStream str, String indent, Map<FAbstractEquation,FExp> nominalMap) {
+        throw new UnsupportedOperationException("DynamicStateBlock.genEquationNominalVarDecl_C() is not implemented!");
+    }
+    
+    public void SimpleEquationBlock.genEquationNominalVarDecl_C(CodePrinter p, CodeStream str, String indent, Map<FAbstractEquation,FExp> nominalMap) {
+        FExp nom = equation().equationNominal_C();
+        nominalMap.put(equation(), nom);
+        p.printVarDecls(nom, str, indent);
+    }
+    
+    public abstract void AbstractEquationBlock.genEquationNominal_C(CodePrinter p, CodeStream str, String indent, Map<FAbstractEquation,FExp> nominalMap);
+    
+    @Override
+    public void EquationBlock.genEquationNominal_C(CodePrinter p, CodeStream str, String indent, Map<FAbstractEquation,FExp> nominalMap) {
         Enumerator e = new Enumerator();
         for (SimpleEquationBlock block : unsolvedBlocks()) {
-            block.genEquationNominal_C(p, str, indent, e);
+            block.genEquationNominal_C(p, str, indent, nominalMap, e);
         }
     }
 
     @Override
-    public void DynamicStateBlock.genEquationNominal_C(CodePrinter p, CodeStream str, String indent) {
+    public void DynamicStateBlock.genEquationNominal_C(CodePrinter p, CodeStream str, String indent, Map<FAbstractEquation,FExp> nominalMap) {
         throw new UnsupportedOperationException("DynamicStateBlock.genEquationNominal_C() is not implemented!");
     }
 
     @Override
-    public void SimpleEquationBlock.genEquationNominal_C(CodePrinter p, CodeStream str, String indent) {
-        genEquationNominal_C(p, str, indent, new Enumerator());
+    public void SimpleEquationBlock.genEquationNominal_C(CodePrinter p, CodeStream str, String indent, Map<FAbstractEquation,FExp> nominalMap) {
+        genEquationNominal_C(p, str, indent, nominalMap, new Enumerator());
     }
     
-    public void SimpleEquationBlock.genEquationNominal_C(CodePrinter p, CodeStream str, String indent, Enumerator e) {
+    public void SimpleEquationBlock.genEquationNominal_C(CodePrinter p, CodeStream str, String indent, Map<FAbstractEquation,FExp> nominalMap, Enumerator e) {
+        FExp nom = nominalMap.get(equation());
         for (int i = 0; i < realVariables().size(); i++) {
-            str.format("%s(*res)[%d] = ", indent, e.next());
-            equation().genEquationNominal_C(p, str, indent);
-            str.print(";\n");
-        }
-    }
-    
-    public void FAbstractEquation.genEquationNominal_C(CodePrinter p, CodeStream str, String indent) { str.print("1"); }
-    public void FEquation.genEquationNominal_C(CodePrinter p, CodeStream str, String indent) {
-        FExp nom = nominal();
-        if (nom.variability().knownParameterOrLess()) {
-            try {
-                nom = getLeft().dynamicFExp(nom.ceval().buildLiteral());
-            } catch (ConstantEvaluationException e) {
-                
+            int num = e.next();
+            if (i == 0) {
+                p.printPreSteps(nom, str, indent);
+                str.format("%s(*res)[%d] = ", indent, num);
+                p.print(nom, str, indent);
+                p.printPostSteps(nom, str, indent);
+                str.print(";\n");
+            } else {
+                str.format("%s(*res)[%d] = (*res)[%d];\n", indent, num, num - 1);
             }
         }
-        p.print(nom, str, indent);
     }
+    
+    public FExp FAbstractEquation.equationNominal_C() {
+        return nominal();
+    }
+    
     
 	public void FVariable.genBlockResidualValuref_C(CodePrinter p, CodeStream str, String indent, 
 	        Map<FVariable,String> solverArrayMap) {

trunk/Compiler/ModelicaFlatTree/src/jastadd/FlatAPI.jrag
@@ -2547,7 +2547,22 @@ aspect Attributes {
 }
 
 aspect Nominals {
-    syn FExp FEquation.nominal() = getLeft().dynamicFExp(FExp.nominalMax(getLeft(), getRight()));
+    syn FExp FAbstractEquation.nominal() {
+        FExp nom = FExp.nominalDefault();
+        nom.setParent(this);
+        return nom;
+    }
+    eq FEquation.nominal() {
+        FExp nom = getLeft().dynamicFExp(FExp.nominalMax(getLeft(), getRight()));
+        if (nom.variability().knownParameterOrLess()) {
+            try {
+                nom = getLeft().dynamicFExp(nom.ceval().buildLiteral());
+            } catch (ConstantEvaluationException e) {
+                
+            }
+        }
+        return nom;
+    }
     
     public static FExp FExp.nominalMax(FExp e1, FExp e2) {
         return new FMaxExp(e1.nominal(), new Opt<FExp>(e2.nominal()));

trunk/Compiler/ModelicaCBackEnd/src/test/CCodeGenTests.mo
@@ -10955,6 +10955,64 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
 ")})));
 end BlockTest21;
 
+model BlockTest22
+    
+    record R
+        Real y;
+    end R;
+    
+    function f
+        input R x;
+        output Real y = x.y;
+    algorithm
+        annotation(Inline=false);
+    end f;
+    
+    function f2
+        input Real x;
+        output Real y = x;
+    algorithm
+        annotation(Inline=false);
+    end f2;
+    
+    parameter R p = R(3);
+    Real x(nominal=f(p));
+equation
+    x = f2(x);
+
+    annotation(__JModelica(UnitTesting(tests={
+        CCodeGenTestCase(
+            name="BlockTest22",
+            description="Start value in block",
+            template="$C_dae_blocks_residual_functions$",
+            generatedCode="
+static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int evaluation_mode) {
+    /***** Block: 1 *****/
+    jmi_real_t** res = &residual;
+    int ef = 0;
+    if (evaluation_mode == JMI_BLOCK_NOMINAL) {
+        x[0] = 3.0;
+    } else if (evaluation_mode == JMI_BLOCK_VALUE_REFERENCE) {
+        x[0] = 1;
+    } else if (evaluation_mode == JMI_BLOCK_EQUATION_NOMINAL) {
+        JMI_RECORD_STATIC(R_0_r, tmp_1)
+        tmp_1->y = _p_y_0;
+        (*res)[0] = jmi_max(jmi_abs(func_CCodeGenTests_BlockTest22_f_exp0(tmp_1)), AD_WRAP_LITERAL(1));
+    } else if (evaluation_mode == JMI_BLOCK_INITIALIZE) {
+        x[0] = _x_1;
+    } else if (evaluation_mode & JMI_BLOCK_EVALUATE || evaluation_mode & JMI_BLOCK_WRITE_BACK) {
+        if ((evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) == 0) {
+            _x_1 = x[0];
+        }
+        if (evaluation_mode & JMI_BLOCK_EVALUATE) {
+            (*res)[0] = func_CCodeGenTests_BlockTest22_f2_exp1(_x_1) - (_x_1);
+        }
+    }
+    return ef;
+}
+")})));
+end BlockTest22;
+
 model InactiveBlockSwitch1
     function F
         input Real i1[:];
@@ -11333,7 +11391,7 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
         x[2] = 2;
     } else if (evaluation_mode == JMI_BLOCK_EQUATION_NOMINAL) {
         (*res)[0] = 1;
-        (*res)[1] = 1;
+        (*res)[1] = (*res)[0];
         (*res)[2] = 3;
     } else if (evaluation_mode == JMI_BLOCK_INITIALIZE) {
         x[0] = _x_0;
@@ -11378,7 +11436,7 @@ static int dae_init_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int
         x[2] = 2;
     } else if (evaluation_mode == JMI_BLOCK_EQUATION_NOMINAL) {
         (*res)[0] = 1;
-        (*res)[1] = 1;
+        (*res)[1] = (*res)[0];
         (*res)[2] = 3;
     } else if (evaluation_mode == JMI_BLOCK_INITIALIZE) {
         x[0] = _x_0;
@@ -15105,7 +15163,7 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
         x[1] = 1;
     } else if (evaluation_mode == JMI_BLOCK_EQUATION_NOMINAL) {
         (*res)[0] = 1;
-        (*res)[1] = 1;
+        (*res)[1] = (*res)[0];
     } else if (evaluation_mode == JMI_BLOCK_INITIALIZE) {
         x[0] = _y_1;
         x[1] = _x_0;
@@ -20682,7 +20740,7 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
         x[1] = 4;
     } else if (evaluation_mode == JMI_BLOCK_EQUATION_NOMINAL) {
         (*res)[0] = 1;
-        (*res)[1] = 1;
+        (*res)[1] = (*res)[0];
     } else if (evaluation_mode == JMI_BLOCK_INITIALIZE) {
         x[0] = _x2_1;
         x[1] = _x1_0;