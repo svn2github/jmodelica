c1cb26c469d525e04a5af4a7e1bc4d69b2ca4fad
efredriksson <efredriksson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2015-10-18 21:28:18 +0000

    #4392 Reverting the option change for the test made in r8125.
    git-svn-id: https://svn.jmodelica.org@8126 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/OptionRegistry.java
+++ trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/OptionRegistry.java
@@ -624,7 +624,7 @@ abstract public class OptionRegistry {
             ("block_solver_experimental_mode",
              OptionType.runtime, 
              Category.experimental,
-             1024,
+             0,
              "Activates experimental features of equation block solvers",
              0, Integer.MAX_VALUE),
         NLE_SOLVER_DEFAULT_TOL
