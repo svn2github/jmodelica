610dcbaad07d756571017005b79915417139b6d7
maxelsson <maxelsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2015-11-05 10:28:21 +0000

    #4625. Updated greybox identification example. Added DrumBoiler.mo and drumboiler data (DBData.pickle)
    git-svn-id: https://svn.jmodelica.org@8200 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Python/src/pyjmi/examples/greybox_identification.py
+++ trunk/Python/src/pyjmi/examples/greybox_identification.py
@@ -28,90 +28,72 @@ from pyfmi import load_fmu
 from pyjmi import transfer_optimization_problem, get_files_path
 from pyjmi.optimization.greybox import GreyBox
 
-
-## NOT NEEDED ? ##
-#from pyjmi import CasadiModel
-#import cPickle as pickle
-#from pyjmi.common.core import TrajectoryLinearInterpolation
-#from pyjmi.optimization.casadi_collocation import *
+import cPickle as pickle
 
 def run_demo():
-	"""
-	
-	"""
-	# Locate the model and file paths 
-	file_path = os.path.join(get_files_path(), "DrumBoiler.mo")
-	modelPath = "DrumBoiler"
+    """
+    
+    """
+    # Locate the model and file paths 
+    file_path = os.path.join(get_files_path(), "DrumBoiler.mo")
+    modelPath = "DrumBoiler"
 
+    # Load measurement data
+    with open(os.path.join(get_files_path(),"DBdata.pickle")) as fp:
+       RCdata = pickle.load(fp) 
+    measurements = RCdata['measurements'] 
+    time = RCdata['time']
+    # Extract control signal data from measurements
+    inputs={}
+    inputs['uc']= measurements.pop('uc')
+    inputs['fc']= measurements.pop('fc')
 
-	# Create reference data
-	nullModelReferenceCost = 5631.068823 #the cost from the null model
-	reference = {'TD':{'cost':5466.596971, 'costred':164.471852, 'risk':0.000000},'TR':{'cost':5629.636935, 'costred':1.431888, 'risk':0.731872},'A4':{'cost':5630.900339, 'costred':0.168484, 'risk':0.996721}
-				,'distE':{'cost':4362.561987, 'costred':1268.506836, 'risk':0.000000},'distP':{'cost':4399.728897, 'costred':1231.339926, 'risk':0.000000}} #Create reference by running without framework
-	tol = 1e-3
+    # Transfer model to Casadi interface
+    op = transfer_optimization_problem(modelPath, file_path, accept_model=True )
+    op_opts = op.optimize_options()
 
+    # Create greybox object
+    GB = GreyBox(op, op_opts, measurements, inputs, time)
 
-	# Load measurement data
-	with open("DBdata.pickle") as fp:
-	   RCdata = pickle.load(fp) 
-	measurements = RCdata['measurements'] 
-	time = RCdata['time'] 
-		
-	# Extract control signal data from measurements
-	inputs={}
-	inputs['uc']= measurements.pop('uc')
-	inputs['fc']= measurements.pop('fc')
+    # Set some variable attributes
+    GB.set_variable_attribute(GB.get_noise_covariance_variable('E'), 'max', 100)
+    GB.set_variable_attribute(GB.get_noise_covariance_variable('P'), 'max', 100)
+    GB.set_variable_attribute('x10', 'initialGuess', 148)
+    GB.set_variable_attribute('x20', 'initialGuess', 27.5)
+    
+    # Define null model free parameters and other parameters to free
+    nullModelFree = set(['GreyBox_r_E', 'GreyBox_r_P', 'x10', 'x20'])
+    optimizeParameters = set(["TD","TR","A4","distE","distP"])
+    
+    # Optimize null model
+    identification = GB.identify(nullModelFree)
+    
+    # Create dictionaries for results
+    testcases = []
+    
+    for var in optimizeParameters:
+        # Add var to set of free parameters
+        testcases.append(identification.release([var]))
+    
+    print("=======================SUMMARY========================")
 
+    results = identification.compare(testcases)
 
-	MLClasspath = "DrumBoilerpackage.DrumBoiler"
-	modelPath = "DrumBoiler.mop"
-	# Transfer model to Casadi interface
-	op = transfer_optimization_problem(MLClasspath, modelPath, accept_model=True )
-	op_opts = op.optimize_options()
-
-	# Create greybox object
-	GB = GreyBox(op, op_opts, measurements, inputs, time)
-		
-	# Set some variable attributes
-	GB.set_variable_attribute('GreyBox_r_E', 'max', 100)
-	GB.set_variable_attribute('GreyBox_r_P', 'max', 100)
-	GB.set_variable_attribute('x10', 'initialGuess', 148)
-	GB.set_variable_attribute('x20', 'initialGuess', 27.5)
-		
-	# Define null model free parameters and other parameters to free
-	nullModelFree = set(['GreyBox_r_E', 'GreyBox_r_P', 'x10', 'x20'])
-	optimizeParameters = set(["TD","TR","A4","distE","distP"])
-		
-	# Define parameters to free (noise covariances and initial guesses )
-	GB.set_free_parameters(nullModelFree)
-		
-	# Optimize null model
-	identification = GB.identify()
-
-		
-	# Assert result 
-	assert(N.abs(identification.cost - nullModelReferenceCost) < tol)
-		
-	# Create dictionaries for results
-	testcases = []
-
-	for var in optimizeParameters:
-		# Add var to set of free parameters
-		testcases.append(identification.release([var]))
-		
-		
-		
-	print("=======================SUMMARY========================")
-
-	results = identification.compare(testcases)
-
-
-	# assert results
-	for var in optimizeParameters:
-		assert(N.abs(results[var]['cost'] - reference[var]['cost']) < tol)
-		assert(N.abs(results[var]['costred'] - reference[var]['costred']) < tol)
-		assert(N.abs(results[var]['risk'] - reference[var]['risk']) < tol)
+    # reference data
+    nullModelReferenceCost = 5631.068823 #the cost from the null model
+    reference = {'TD':{'cost':5466.596971, 'costred':164.471852, 'risk':0.000000},'TR':{'cost':5629.636935, 'costred':1.431888, 'risk':0.731872},'A4':{'cost':5630.900339, 'costred':0.168484, 'risk':0.996721}
+                ,'distE':{'cost':4362.561987, 'costred':1268.506836, 'risk':0.000000},'distP':{'cost':4399.728897, 'costred':1231.339926, 'risk':0.000000}} #Create reference by running without framework
+    tol = 1e-3
+      
+    # Assert nullmodel 
+    assert(N.abs(identification.cost - nullModelReferenceCost) < tol)
+    
+    # Assert results  
+    for i, var in enumerate(optimizeParameters):
+        assert(N.abs(results[i]['cost'] - reference[var]['cost']) < tol)
+        assert(N.abs(results[i]['costred'] - reference[var]['costred']) < tol)
+        assert(N.abs(results[i]['risk'] - reference[var]['risk']) < tol)
 
 
 if __name__=="__main__":
-run_demo()
+    run_demo()
