svn2github/jmodelica

commit 729c7a7e0fceee349f35c651f415114743fa0cb4

#4703 Removed checking against max/min_residual_scaling_factors when in jmi_residual_scaling_manual scaling mode. Added row scaling of Jacobian for condition number checking when in jmi_residual_scaling_manual mode.
git-svn-id: https://svn.jmodelica.org@8302 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/RuntimeLibrary/src/jmi/jmi_kinsol_solver.c
@@ -1833,7 +1833,8 @@ static void jmi_update_f_scale(jmi_block_solver_t *block) {
 
     /* Form scaled Jacobian as needed for automatic scaling and condition number checking*/
     if (bsop->residual_equation_scaling_mode == jmi_residual_scaling_auto ||
-        bsop->residual_equation_scaling_mode == jmi_residual_scaling_hybrid)
+        bsop->residual_equation_scaling_mode == jmi_residual_scaling_hybrid ||
+        bsop->residual_equation_scaling_mode == jmi_residual_scaling_manual)
     {
         for (i = 0; i < N; i++) {
             int j;
@@ -1868,7 +1869,7 @@ static void jmi_update_f_scale(jmi_block_solver_t *block) {
             }
     }
 
-    if(use_scaling_flag) {
+    if(bsop->residual_equation_scaling_mode == jmi_residual_scaling_auto || bsop->residual_equation_scaling_mode == jmi_residual_scaling_hybrid) {
         solver->using_max_min_scaling_flag = 0; /* NOT using max/min scaling */
         /* check that scaling factors have reasonable magnitude */
         for(i = 0; i < N; i++) {
@@ -1952,7 +1953,6 @@ static void jmi_update_f_scale(jmi_block_solver_t *block) {
         char norm = 'I';
         double Jnorm = 1.0, Jcond = 1.0;
         int info;
-
         if(use_scaling_flag) {
             for(i = 0; i < N; i++){
                 int j;

commit 1aa0d529636c090939407d118f7ebaa40c551d10

#4703 Fixed bug introduced in r8302.
git-svn-id: https://svn.jmodelica.org@8307 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/RuntimeLibrary/src/jmi/jmi_kinsol_solver.c
@@ -1928,6 +1928,21 @@ static void jmi_update_f_scale(jmi_block_solver_t *block) {
             }
             jmi_log_leave(block->log, outer);
         }
+    } else if(bsop->residual_equation_scaling_mode == jmi_residual_scaling_manual) {
+        for(i = 0; i < N; i++) {
+            scale_ptr[i] = 1/scale_ptr[i];
+        }
+        /* Scaling is not updated */
+        if (block->callbacks->log_options.log_level >= 4) {
+            jmi_log_node_t outer = jmi_log_enter_fmt(block->log, logInfo, "ResidualScalingUpdated", "<block:%s>", block->label);
+            if (block->callbacks->log_options.log_level >= 5) {
+                jmi_log_node_t inner = jmi_log_enter_vector_(block->log, outer, logInfo, "scaling");
+                realtype* res = scale_ptr;
+                for (i=0;i<N;i++) jmi_log_real_(block->log, 1/res[i]);
+                jmi_log_leave(block->log, inner);
+            }
+            jmi_log_leave(block->log, outer);
+        }
     }
 
     if (solver->using_max_min_scaling_flag && !solver->J_is_singular_flag) {