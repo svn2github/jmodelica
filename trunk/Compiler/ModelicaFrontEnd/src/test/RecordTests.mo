svn2github/jmodelica

commit f514f83dec02c41c4813e47ce9f95ea9b7d241bc

Recommitting r8441 with fix for infinite loop, and some other test issues. (#4777)
git-svn-id: https://svn.jmodelica.org@8483 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/jastadd/instance/InstFunctionBinding.jrag
@@ -107,12 +107,53 @@ aspect InstFunctionBinding {
 		for (InstFunctionArgument arg : getArgs())
 			arg.setBoundInput(input);
 	}
-	
-	/**
-	 * Get the input component this argument is bound to.
-	 */
-	syn InstComponentDecl InstFunctionArgument.getBoundInput() = boundInput;
-	
+
+    /**
+     * Get the input component this argument is bound to.
+     */
+    syn InstComponentDecl InstFunctionArgument.getBoundInput() = 
+        isInstRecordConstructorArg() ? myRecordConstructorVirtualInstComponentDecl() : boundInput;
+
+    /**
+     * Is this an argument to a record constructor in the instance tree?
+     */
+    inh boolean InstFunctionArgument.isInstRecordConstructorArg();
+    eq InstRecordConstructor.getArg().isInstRecordConstructorArg()     = true;
+    eq InstMultipleBoundArgument.getArg().isInstRecordConstructorArg() = isInstRecordConstructorArg();
+    eq BaseNode.getChild().isInstRecordConstructorArg()                = false;
+
+    /**
+     * Get the virtual component instance that is connected to this argument.
+     * 
+     * Only valid for arguments to record constructors in the instance tree.
+     */
+    inh InstComponentDecl InstFunctionArgument.myRecordConstructorVirtualInstComponentDecl();
+    eq BaseNode.getChild().myRecordConstructorVirtualInstComponentDecl()                 = null;
+    eq InstRecordConstructor.getArg(int i).myRecordConstructorVirtualInstComponentDecl() {
+        Iterator<InstComponentDecl> cmps = allInstComponentDecls().iterator();
+        for (int j = 0; j < i; j++) {
+            cmps.next();
+        }
+        return cmps.next();
+    }
+
+    /**
+     * Get the expression to use for this argument.
+     */
+    syn FExp InstDefaultArgument.getFExp() {
+        InstComponentDecl bi = getBoundInput();
+        if (bi != boundInput) {
+            InstValueModification mod = bi.myInstValueMod();
+            if (mod != null && !mod.isDefaultRecordMod()) {
+                return mod.getFExp();
+            }
+        }
+        return getDeclarationFExp();
+    }
+
+    syn boolean InstModification.isDefaultRecordMod()     = false;
+    eq InstDefaultRecordModification.isDefaultRecordMod() = true;
+
 	/**
 	 * Argument nodes representing arguments that are missing or given multiple times do 
 	 * not get a location set when they are created. Set the location for those nodes, do nothing 
@@ -538,7 +579,7 @@ aspect InstFunctionBinding {
             int i = 0;
             for (InstFunctionArgument arg : bound) {
                 if (arg.isOKArg()) {
-                    FType argType = arg.getFExp().type();
+                    FType argType = arg.getFExpNoTransform().type();
                     FType inType = inputs.get(i).type();
                     if (!inType.dimensionCompatible(argType, true))
                         score++;
@@ -758,7 +799,7 @@ aspect InstFunctionBinding {
         }
         return res;
     }
-    
+
     /**
      * Calculate if this is a vectorized function call.
      */

trunk/Compiler/ModelicaFrontEnd/src/jastadd/instance/InstLookupComponents.jrag
@@ -291,8 +291,9 @@ aspect InstLookupComponents {
     inh InstLookupResult<InstComponentDecl> InstConstraining.lookupInstComponent(String name);
     inh lazy InstLookupResult<InstComponentDecl> InstNode.lookupInstComponent(String name);
     inh lazy InstLookupResult<InstComponentDecl> InstModification.lookupInstComponent(String name);
-    eq InstNode.getChild().lookupInstComponent(String name) = genericLookupInstComponent(name);
-    eq InstRoot.getChild().lookupInstComponent(String name) = InstLookupResult.notFound();
+    eq InstNode.getChild().lookupInstComponent(String name)   = genericLookupInstComponent(name);
+    eq InstRoot.getChild().lookupInstComponent(String name)   = InstLookupResult.notFound();
+    eq InstImport.getChild().lookupInstComponent(String name) = InstLookupResult.notFound();
 
     eq InstComponentDecl.getInstModification().lookupInstComponent(String name)          = lookupInstComponent(name);
     eq InstComponentDecl.getInstConstrainingComponent().lookupInstComponent(String name) = lookupInstComponent(name);
@@ -408,11 +409,11 @@ aspect InstLookupComponents {
 
     syn InstLookupResult<InstComponentDecl> InstValueModification.lookupInstComponentForBindingExp(String name) = lookupInstComponent(name);
     eq InstDefaultRecordModification.lookupInstComponentForBindingExp(String name)                              = 
-        (getSource() != null) ? getSource().lookupInstComponent(name) : lookupInstComponentInSurrounding(name);
+        (sourceInstMod() != null) ? sourceInstMod().lookupInstComponent(name) : lookupInstComponentInSurrounding(name);
 
     inh InstLookupResult<InstComponentDecl> InstDefaultRecordModification.lookupInstComponentInSurrounding(String name);
     eq InstNode.getChild().lookupInstComponentInSurrounding(String name) = genericLookupInstComponent(name);
-
+    eq Root.getChild().lookupInstComponentInSurrounding(String name)     = InstLookupResult.notFound();
     /**
      * Get a specific child access, triggering transformations on that child, but *not* on the entire list.
      */
@@ -510,12 +511,38 @@ aspect InstLookupComponents {
                 return InstLookupResult.<InstComponentDecl>found(((InstForIndex) fi).getInstPrimitive());
         return lookupInstComponent(name);
     }
+    
+    inh InstLookupResult<InstComponentDecl> InstRecordConstructor.lookupInstComponent(String name);
+    eq InstRecordConstructor.getChild().lookupInstComponent(String name)             = lookupInstComponent(name);
+    eq InstRecordConstructor.getInstComponentDecl().lookupInstComponent(String name) = genericLookupInstComponent(name);
+    
+    
+    syn lazy InstLookupResult<InstComponentDecl> InstRecordConstructor.genericLookupInstComponent(String name) {
+        for (InstComponentDecl icd : getInstComponentDecls()) {
+            if (icd.matches(name)) {
+                return InstLookupResult.<InstComponentDecl>found(icd);
+            }
+        }
+        for (InstExtends ie : getInstExtendss()) {
+            InstLookupResult<InstComponentDecl> res = ie.memberInstComponent(name);
+            if (res.successful()) {
+                return res;
+            }
+        }
+        return lookupInstComponent(name);
+    }
+
+    /**
+     * Lookup name in the record declaration.
+     */
+    syn InstComponentDecl InstRecordConstructor.declarationInstComponent(String name) = 
+        myInstClassDecl().memberInstComponent(name).target(INST_UNKNOWN_COMPONENT, this);
 
     syn lazy InstLookupResult<InstComponentDecl> InstNode.genericLookupInstComponent(String name) {
         InstLookupResult<InstComponentDecl> res = memberInstComponent(name);
         if (res.successful())
             return res;
-        for (InstImport ii : instImports()) {
+        for (InstImport ii : getInstImports()) {
             res = ii.lookupInstConstantInImport(name);
             if (res.successful())
                 return res;
@@ -540,11 +567,11 @@ aspect InstLookupComponents {
         if (isArray()) {
             return arrayMemberInstComponent(name, ndims());
         } else {    
-            for (InstComponentDecl ic : instComponentDecls()) 
+            for (InstComponentDecl ic : getInstComponentDecls()) 
                 if (ic.matches(name))
                     return findInnerComponentIfAny(ic);
 
-            for (InstExtends ie : instExtends()) {
+            for (InstExtends ie : getInstExtendss()) {
                 InstLookupResult<InstComponentDecl> res = ie.memberInstComponent(name);
                 if (res.successful())
                     return res;
@@ -572,7 +599,7 @@ aspect InstLookupComponents {
         if (res.successful())
             return res;
         
-        for (InstImport ii : instImports()) {
+        for (InstImport ii : getInstImports()) {
             res = ii.lookupInstConstantInImport(name);
             if (res.successful())
                 return res;
@@ -587,7 +614,7 @@ aspect InstLookupComponents {
         if (res.successful())
             return res;
         
-        for (InstExtends ie : instExtends()) {
+        for (InstExtends ie : getInstExtendss()) {
             res = ie.memberInstConstantWithExtends(name);
             if (res.successful())
                 return res;
@@ -598,7 +625,7 @@ aspect InstLookupComponents {
     eq InstSimpleShortClassDecl.memberInstConstantWithExtends(String name) = actualInstClass().memberInstConstantWithExtends(name);
 
     syn lazy InstLookupResult<InstComponentDecl> InstNode.memberInstConstant(String name) {
-        for (InstComponentDecl ic : instComponentDecls()) 
+        for (InstComponentDecl ic : getInstComponentDecls()) 
             if (ic.getComponentDecl().isConstant() && ic.matches(name))
                 return InstLookupResult.found(ic);
         
@@ -704,6 +731,7 @@ aspect LookupInstComponentsInModifications {
     inh InstLookupResult<InstComponentDecl> InstComponentRedeclare.lookupInstReplacingComponent(String name);
     eq InstNamedModification.getChild().lookupInstReplacingComponent(String name) = getName().qualifiedLookupInstComponent(name);
     eq InstNode.getChild().lookupInstReplacingComponent(String name)              = memberInstComponent(name).unresolved();
+    eq Root.getChild().lookupInstReplacingComponent(String name)                  = InstLookupResult.notFound();
 
 }
 

commit 007479ec08595291735d97eb0ff972186b41d1a9

Fixed bugs introduced by r8483. (#4777)
git-svn-id: https://svn.jmodelica.org@8492 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/jastadd/instance/InstFunctionBinding.jrag
@@ -18,6 +18,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
+import org.jmodelica.util.Criteria;
+import org.jmodelica.util.collections.FilteredIterator;
+
 
 aspect InstFunctionBinding {
 
@@ -127,14 +130,18 @@ aspect InstFunctionBinding {
      * 
      * Only valid for arguments to record constructors in the instance tree.
      */
-    inh InstComponentDecl InstFunctionArgument.myRecordConstructorVirtualInstComponentDecl();
-    eq BaseNode.getChild().myRecordConstructorVirtualInstComponentDecl()                 = null;
-    eq InstRecordConstructor.getArg(int i).myRecordConstructorVirtualInstComponentDecl() {
-        Iterator<InstComponentDecl> cmps = allInstComponentDecls().iterator();
-        for (int j = 0; j < i; j++) {
-            cmps.next();
-        }
-        return cmps.next();
+    syn InstComponentDecl InstFunctionArgument.myRecordConstructorVirtualInstComponentDecl() = 
+        lookupRecordConstructorVirtualInstComponentDecl(boundInput.name());
+
+    /**
+     * Lookup the virtual component instance that should be connected to an argument of a given name.
+     * 
+     * Only valid for arguments to record constructors in the instance tree.
+     */
+    inh InstComponentDecl InstFunctionArgument.lookupRecordConstructorVirtualInstComponentDecl(String name);
+    eq BaseNode.getChild().lookupRecordConstructorVirtualInstComponentDecl(String name) = null;
+    eq InstRecordConstructor.getArg(int i).lookupRecordConstructorVirtualInstComponentDecl(String name) {
+        return localLookupInstComponent(name).targetOrNull();
     }
 
     /**

trunk/Compiler/ModelicaFrontEnd/src/jastadd/instance/InstLookupComponents.jrag
@@ -538,13 +538,18 @@ aspect InstLookupComponents {
                 return InstLookupResult.<InstComponentDecl>found(((InstForIndex) fi).getInstPrimitive());
         return lookupInstComponent(name);
     }
+
     
     inh InstLookupResult<InstComponentDecl> InstRecordConstructor.lookupInstComponent(String name);
     eq InstRecordConstructor.getChild().lookupInstComponent(String name)             = lookupInstComponent(name);
     eq InstRecordConstructor.getInstComponentDecl().lookupInstComponent(String name) = genericLookupInstComponent(name);
-    
-    
+
     syn lazy InstLookupResult<InstComponentDecl> InstRecordConstructor.genericLookupInstComponent(String name) {
+        InstLookupResult<InstComponentDecl> res = localLookupInstComponent(name);
+        return res.successful() ? res : lookupInstComponent(name);
+    }
+
+    syn lazy InstLookupResult<InstComponentDecl> InstRecordConstructor.localLookupInstComponent(String name) {
         for (InstComponentDecl icd : getInstComponentDecls()) {
             if (icd.matches(name)) {
                 return InstLookupResult.<InstComponentDecl>found(icd);
@@ -556,7 +561,7 @@ aspect InstLookupComponents {
                 return res;
             }
         }
-        return lookupInstComponent(name);
+        return InstLookupResult.notFound();
     }
 
     /**

trunk/Compiler/ModelicaFrontEnd/src/jastadd/instance/InstanceTree.jrag
@@ -490,8 +490,16 @@ aspect Environments {
     /**
      * Get the InstValueModification corresponding to the specified cell (1-based index) of the array.
      */
-    syn InstValueModification InstValueModification.arrayInstModification(int i) = 
-        (getNumInstArrayModification() > 0) ? getInstArrayModification(i - 1) : this;
+    syn InstValueModification InstValueModification.arrayInstModification(int i) {
+        int n = getNumInstArrayModification();
+        if (n <= 0) {
+            return this;
+        } else {
+            // Make sure we always return something
+            int j = (i > 0 && i <= n) ? i - 1 : 0;
+            return getInstArrayModification(j);
+        }
+    }
 
     syn InstModification InstModification.matchInstModification(String name) = null;
     eq InstCompleteModification.matchInstModification(String name) {

trunk/Compiler/ModelicaFrontEnd/src/test/RecordTests.mo
@@ -2708,6 +2708,133 @@ end RecordTests.RecordConstructor22;
 end RecordConstructor22;
 
 
+model RecordConstructor23
+    record A
+        parameter Integer n;
+        final parameter Integer m = n + 1;
+        parameter Real x[:, n];
+    end A;
+    
+    record B
+        extends A(n = 1, x = {{1}, {2}});
+    end B;
+    
+    model C
+        parameter A a;
+    end C;
+    
+    extends C(a = B());
+
+    annotation(__JModelica(UnitTesting(tests={
+        FlatteningTestCase(
+            name="RecordConstructor23",
+            description="Record constructor for record with final parameter",
+            flatModel="
+fclass RecordTests.RecordConstructor23
+ parameter RecordTests.RecordConstructor23.A a(x(size() = {2, 1})) = RecordTests.RecordConstructor23.B(1, n + 1, {{1}, {2}});
+
+public
+ record RecordTests.RecordConstructor23.A
+  parameter Integer n;
+  parameter Integer m;
+  parameter Real x[:,n];
+ end RecordTests.RecordConstructor23.A;
+
+ record RecordTests.RecordConstructor23.B
+  parameter Integer n;
+  parameter Integer m;
+  parameter Real x[2,1];
+ end RecordTests.RecordConstructor23.B;
+
+end RecordTests.RecordConstructor23;
+")})));
+end RecordConstructor23;
+
+
+model RecordConstructor24
+    record A
+        parameter Integer n;
+        constant Integer m = 1;
+        parameter Real x[:, n];
+    end A;
+    
+    record B
+        extends A(n = 1, x = {{1}, {2}});
+    end B;
+    
+    model C
+        parameter A a;
+    end C;
+    
+    extends C(a = B());
+
+    annotation(__JModelica(UnitTesting(tests={
+        FlatteningTestCase(
+            name="RecordConstructor24",
+            description="Record constructor for record with constant",
+            flatModel="
+fclass RecordTests.RecordConstructor24
+ parameter RecordTests.RecordConstructor24.A a(x(size() = {2, 1})) = RecordTests.RecordConstructor24.B(1, 1, {{1}, {2}});
+
+public
+ record RecordTests.RecordConstructor24.A
+  parameter Integer n;
+  constant Integer m;
+  parameter Real x[:,n];
+ end RecordTests.RecordConstructor24.A;
+
+ record RecordTests.RecordConstructor24.B
+  parameter Integer n;
+  constant Integer m;
+  parameter Real x[2,1];
+ end RecordTests.RecordConstructor24.B;
+
+end RecordTests.RecordConstructor24;
+")})));
+end RecordConstructor24;
+
+
+model RecordConstructor25
+    record A
+        parameter Real x = 1;
+    end A;
+
+    block B
+        parameter Integer n = 1;
+        parameter A[n] a1 = { A() };
+    end B;
+
+    block C
+        parameter A[:] a2 = { A() };
+    end C;
+
+    block D
+        extends C;
+        replaceable B b(n = size(a2, 1), a1 = a2);
+    end D;
+
+    D d(redeclare B b, a2 = {A(2), A(3)});
+
+    annotation(__JModelica(UnitTesting(tests={
+        FlatteningTestCase(
+            name="RecordConstructor25",
+            description="Array of records with constructors used in modification on redeclare",
+            flatModel="
+fclass RecordTests.RecordConstructor25
+ structural parameter Integer d.b.n = 2 /* 2 */;
+ parameter RecordTests.RecordConstructor25.A d.b.a1[2] = d.a2[1:2];
+ parameter RecordTests.RecordConstructor25.A d.a2[2] = {RecordTests.RecordConstructor25.A(2), RecordTests.RecordConstructor25.A(3)} /* { RecordTests.RecordConstructor25.A(2), RecordTests.RecordConstructor25.A(3) } */;
+
+public
+ record RecordTests.RecordConstructor25.A
+  parameter Real x;
+ end RecordTests.RecordConstructor25.A;
+
+end RecordTests.RecordConstructor25;
+")})));
+end RecordConstructor25;
+
+
 model RecordScalarize1
  record A
   Real a;