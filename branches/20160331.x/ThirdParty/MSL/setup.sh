62b3347ab6d148a15a72aa7c71d18a9de53f873d
jmattsson <jmattsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2016-04-07 17:05:13 +0000

    Switched MSL in 20160331.x branch to the one from 1.17.x branch. (#4899)
    git-svn-id: https://svn.jmodelica.org@8674 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/20160331.x/ThirdParty/MSL/setup.sh
+++ branches/20160331.x/ThirdParty/MSL/setup.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-svn co https://svn.modelica.org/projects/Modelica/tags/v3.2.1+build.3 .
-svn merge --accept base -c 7502,7545,7977,7573,7479 https://svn.modelica.org/projects/Modelica/trunk
+svn co https://svn.modelica.org/projects/Modelica/tags/v3.2.1+build.4 .
+svn merge --accept base -c 7479,7502,7545,7562,7573,7977,8067,8626,8652,8663 https://svn.modelica.org/projects/Modelica/trunk
 
 rm -r .svn
 rm -r ModelicaTest
 rm -r ModelicaReference
 rm ObsoleteModelica3.mo
-#rm Modelica/StateGraph.mo
-#rm Modelica/Electrical/Digital.mo
-#rm Modelica/Electrical/Machines.mo
-#rm -r Modelica/Electrical/QuasiStationary
-#rm -r Modelica/Fluid
