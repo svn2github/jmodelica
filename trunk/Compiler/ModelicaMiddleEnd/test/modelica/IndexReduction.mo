svn2github/jmodelica

commit 49b0c9ca86fe096a142d173edf712411336967f6

Changed variability computation for algorithm. It is now computed as the highest variability of all of the referenced variables. Ticket #4900.
git-svn-id: https://svn.jmodelica.org@8775 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFlatTree/src/jastadd/Variability.jrag
@@ -170,7 +170,7 @@ aspect Variability {
         return var;
     }
     eq FAlgorithm.variability() {
-        FTypePrefixVariability rhsVar = containsFTimeExp() ? fContinuous() : fConstant();
+        FTypePrefixVariability rhsVar = fConstant();
         FTypePrefixVariability lhsVar = fConstant();
         for (FVariable fv : referencedFVariablesInLHS()) {
             lhsVar = lhsVar.combine(fv.variability());
@@ -178,7 +178,7 @@ aspect Variability {
         for (FVariable fv : referencedFVariablesInRHS()) {
             rhsVar = rhsVar.combine(fv.variability());
         }
-        return lhsVar.combineDown(rhsVar);
+        return lhsVar.combine(rhsVar);
     }
 
     syn boolean FAbstractEquation.isConstant() = false;

trunk/Compiler/ModelicaMiddleEnd/test/modelica/IndexReduction.mo
@@ -1601,7 +1601,8 @@ algorithm
         i := -1;
     end if;
 
-    annotation(__JModelica(UnitTesting(tests={
+    // This test seems to be invalid, i is assinged within a test which uses continuous variability. See #4900.
+    annotation(__JModelica_disabled(UnitTesting(tests={
         TransformCanonicalTestCase(
             name="AlgorithmVariability1",
             description="Test so that variability calculations are done propperly for algorithms",
@@ -1643,6 +1644,44 @@ equation
  2 * x * _der_der_x + 2 * _der_x * _der_x + (2 * y * _der_der_y + 2 * der(y) * der(y)) = 0.0;
 end IndexReduction.AlgorithmVariability1;")})));
   end AlgorithmVariability1;
+
+  model AlgorithmVariability2
+    Real x;
+    Real y;
+    Real z;
+    parameter Integer p = 1;
+    parameter Integer[:] it = {1};
+    parameter Real[:] rt = {2.0};
+equation
+    // Trigger index reduction
+    y = time;
+    z = x + der(y);
+algorithm
+    x := if it[1] == p then rt[1] else 0;
+
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="AlgorithmVariability2",
+            description="Test so that variability calculations are done propperly for algorithms",
+            flatModel="
+fclass IndexReduction.AlgorithmVariability2
+ Real x;
+ Real y;
+ Real z;
+ parameter Integer p = 1 /* 1 */;
+ parameter Integer it[1] = 1 /* 1 */;
+ parameter Real rt[1] = 2.0 /* 2.0 */;
+ Real _der_y;
+equation
+ y = time;
+ z = x + _der_y;
+algorithm
+ x := if it[1] == p then rt[1] else 0;
+equation
+ _der_y = 1.0;
+end IndexReduction.AlgorithmVariability2;
+")})));
+  end AlgorithmVariability2;
 
     model Variability1
         function F1

commit 13cfcbc5bc0f94b147de2fd8e9f61cdaa5fe08b1

Fixed variability computation bug for algorithms that was introduced in r8775. Ticket #4900.
git-svn-id: https://svn.jmodelica.org@8779 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFlatTree/src/jastadd/Variability.jrag
@@ -170,15 +170,11 @@ aspect Variability {
         return var;
     }
     eq FAlgorithm.variability() {
-        FTypePrefixVariability rhsVar = fConstant();
-        FTypePrefixVariability lhsVar = fConstant();
-        for (FVariable fv : referencedFVariablesInLHS()) {
-            lhsVar = lhsVar.combine(fv.variability());
+        FTypePrefixVariability var = fConstant();
+        for (FIdUseExp exp : findFIdUseExpsInTree()) {
+            var = var.combine(exp.variability());
         }
-        for (FVariable fv : referencedFVariablesInRHS()) {
-            rhsVar = rhsVar.combine(fv.variability());
-        }
-        return lhsVar.combine(rhsVar);
+        return var;
     }
 
     syn boolean FAbstractEquation.isConstant() = false;

trunk/Compiler/ModelicaMiddleEnd/test/modelica/IndexReduction.mo
@@ -1683,6 +1683,43 @@ end IndexReduction.AlgorithmVariability2;
 ")})));
   end AlgorithmVariability2;
 
+  model AlgorithmVariability2
+    Real y;
+    Real x;
+    Real z;
+equation
+    // Trigger index reduction
+    y = time * 2;
+    z = x + der(y);
+algorithm
+    when time > 0.25 then
+        x := y / 2;
+    end when;
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="AlgorithmVariability3",
+            description="Test so that variability calculations are done propperly for algorithms, it should honor in discrete location",
+            flatModel="
+fclass IndexReduction.AlgorithmVariability2
+ Real x;
+ Real y;
+ Real z;
+ parameter Integer p = 1 /* 1 */;
+ parameter Integer it[1] = 1 /* 1 */;
+ parameter Real rt[1] = 2.0 /* 2.0 */;
+ Real _der_y;
+equation
+ y = time;
+ z = x + _der_y;
+algorithm
+ x := if it[1] == p then rt[1] else 0;
+equation
+ _der_y = 1.0;
+end IndexReduction.AlgorithmVariability2;
+")})));
+  end AlgorithmVariability2;
+
     model Variability1
         function F1
             input Real a;