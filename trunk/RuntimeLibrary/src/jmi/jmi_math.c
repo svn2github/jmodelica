21ad2b79907852b485db53a274416a049385fa1f
aramle <aramle@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2016-05-17 11:54:36 +0000

    #4980 Made jmi_sample return false during initialization.
    git-svn-id: https://svn.jmodelica.org@8860 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/RuntimeLibrary/src/jmi/jmi_math.c
+++ trunk/RuntimeLibrary/src/jmi/jmi_math.c
@@ -286,7 +286,7 @@ jmi_real_t jmi_dremainder(jmi_real_t x, jmi_real_t y) {
 
 jmi_ad_var_t jmi_sample(jmi_t* jmi, jmi_real_t offset, jmi_real_t h) {
     jmi_real_t t = jmi_get_t(jmi)[0];
-    if (!jmi->atEvent || SURELY_LT_ZERO(t-offset)) {
+    if (!jmi->atEvent || SURELY_LT_ZERO(t-offset) || jmi->atInitial) {
       /*printf("jmi_sample1: %f %f %12.12f %12.12f\n",offset,fmod((t-offset),h),(t-offset));*/
         return JMI_FALSE;
     }
