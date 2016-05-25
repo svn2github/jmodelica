svn2github/jmodelica

commit 6363bff303d4bdebab52d6babac0810d997ba693

#4949 Refactored expression expansion in scalarization
git-svn-id: https://svn.jmodelica.org@8891 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/scalarization/ScalarizeFunctions.jrag
@@ -60,7 +60,8 @@ aspect FunctionScalarization {
     /**
      * Should this expression be extracted into its own statement?
      */
-    syn boolean FExp.extractTemp() = isArray() && size().isUnknown() && !canKeepAsArray(true) && canAcceptArray();
+    syn boolean FExp.extractTemp() = isComposite() && size().isUnknown() && isFunctionCallIO();
+    eq FIdUseExp.extractTemp()     = super.extractTemp() && isSlice();
     eq FMulExp.extractTemp()       = !isElementWise() && extractAsUnknown();
     eq FReductionExp.extractTemp() = getFExp().size().isUnknown();
     eq FMinMaxExp.extractTemp()    = getX().size().isUnknown();
@@ -71,6 +72,11 @@ aspect FunctionScalarization {
     eq FVectorExp.extractTemp()    = getFExp().size().isUnknown() || inUnknownSize();
     eq FMatrixExp.extractTemp()    = getFExp().size().isUnknown() || inUnknownSize();
     eq FIdTempExp.extractTemp()    = false;
+    
+    inh boolean FExp.isFunctionCallIO();
+    eq BaseNode.getChild().isFunctionCallIO()         = false;
+    eq FFunctionCall.getArg().isFunctionCallIO()      = true;
+    eq FFunctionCallLeft.getFExp().isFunctionCallIO() = true;
     
     syn boolean FExp.extractAsUnknown() = size().isUnknown();
     eq FReductionExp.extractAsUnknown() = getFExp().size().isUnknown();
@@ -250,6 +256,7 @@ aspect FunctionScalarization {
             temp = type().createRecordConstructor(indexMap, temp);
         }
         createFunctionCallClause(clauses, indexMap, temp);
+        useTempVar = true;
     }
     
     public void FVectorFunctionCall.knownArrayTemporaries(List clauses, List vars, Map<String,FExp> indexMap) {
@@ -595,7 +602,7 @@ aspect FunctionScalarization {
     public void FExp.addArrayUsesToIndexMap(Map<String,FExp> indexMap, FExp[] names) {
         if (useTempVar) {
             if (isArray()) {
-                indexMap.put(tempVarName(), tempExp(names));
+                indexNames = names;
             }
         } else {
             super.addArrayUsesToIndexMap(indexMap, names);

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen.jrag
@@ -1848,7 +1848,7 @@ aspect CCodeGen {
     syn boolean FFunctionCallLeft.replaceWithTemp() = hasFExp() && getFExp().replaceWithTemp(true);
     syn boolean FExp.replaceWithTemp(boolean output) = isComposite() || (output && type().isString());
     eq FLitExp.replaceWithTemp(boolean output)   = output;
-    eq FIdUseExp.replaceWithTemp(boolean output) = (output && (!inFunction() || alsoInRHS() || type().isString()));
+    eq FIdUseExp.replaceWithTemp(boolean output) = isSlice() || (output && (!inFunction() || alsoInRHS() || type().isString()));
     
     syn boolean FIdUseExp.alsoInRHS() {
         FAbstractFunctionCall call = myFunctionCall();

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/optimizations/InlineFunctions.jrag
@@ -1744,15 +1744,9 @@ aspect FunctionInlining {
     /*
      * We want to be able to scalarize all expressions (except size exps) fully during inlining.
      */
-    refine Scalarization eq FFunctionCallLeft.getFExp().canAcceptArray() = 
-        refined() && !duringFunctionInlining();
-    refine Scalarization eq FFunctionCall.getArg(int i).canAcceptArray()      = 
-        refined() && (!duringFunctionInlining() || getArg(i).isFFunctionCall());
-    refine Scalarization eq FFunctionCall.getArg(int i).wantsRecordCon()      = 
-        refined() || (duringFunctionInlining() && !getArg(i).isFFunctionCall());
-    eq FFunctionCallLeft.getFExp().wantsRecordCon()                      = 
-        !inFunction() || duringFunctionInlining();
-
+    refine Scalarization eq FExp.scalarizeExpanded()      = duringFunctionInlining() ? isComposite() : refined();
+    refine Scalarization eq FIdUseExp.scalarizeExpanded() = duringFunctionInlining() ? isComposite() : refined();
+    
     inh boolean FFunctionCallLeft.duringFunctionInlining();
     inh boolean FExp.duringFunctionInlining();
     eq FFunctionDecl.getChild().duringFunctionInlining() = duringInlining;

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/scalarization/Scalarization.jrag
@@ -693,11 +693,11 @@ aspect Scalarization {
     /*
      * Attributes for creating FTempIdExps and function temps.
      */
-    syn FIdTempExp FExp.tempExp() = tempExp(tempVarName());
+    syn FIdTempExp FExp.tempExp() = tempExp(indexNames);
     syn FIdTempExp FExp.tempExp(String name) = new FIdTempExp(new FIdUse(tempFQName(name)));
     syn FIdTempExp FExp.tempExp(FArraySubscripts fas) = new FIdTempExp(new FIdUse(tempVarName(),fas));
     syn FIdTempExp FExp.tempExp(FExp[] subs) {
-        if (subs.length > 0) {
+        if (subs != null && subs.length > 0) {
             List<FSubscript> l = new List<FSubscript>();
             for (FExp e : subs) {
                 l.add(new FExpSubscript(e.fullCopy()));
@@ -705,7 +705,7 @@ aspect Scalarization {
             FArraySubscripts fas = new FArraySubscripts(l);
             return tempExp(fas);
         } else {
-            return tempExp();
+            return tempExp(tempVarName());
         }
     }
     
@@ -887,25 +887,13 @@ aspect Scalarization {
     
     public FExp FExp.scalarRecordFExp(Map<String,FExp> indexMap, FQName suffix) {
         if (useTempVar) {
-            FQName fqn;
-            String name = tempVarName();
-            if (indexMap.containsKey(name)) {
-                fqn = ((FIdUseExp)indexMap.get(name)).getFIdUse().getFQName();
-            } else {
-                fqn = new FQNameFull(tempVarName());
-            }
-            fqn = fqn.copyAndAppend(suffix);
-            return new FIdUseExp(new FIdUse(fqn));
+            return dynamicFExp(tempExp()).scalarRecordFExp(indexMap, suffix);
         }
         return scalarize(indexMap);
     }
     
     public FExp FRecordConstructor.scalarRecordFExp(Map<String,FExp> indexMap, FQName suffix) {
         return suffix.scalarRecordFExpForCon(indexMap, this);
-    }
-    
-    public FExp FFunctionCall.scalarRecordFExp(Map<String,FExp> indexMap, FQName suffix) {
-        return scalarize(indexMap).scalarRecordFExp(indexMap, suffix);
     }
     
     public FExp FNoEventExp.scalarRecordFExp(Map<String,FExp> indexMap, FQName suffix) {
@@ -1004,7 +992,7 @@ aspect Scalarization {
     
     public FExp FExp.extractArrayCell(Map<String,FExp> indexMap, FExp[] subs) {
         addArrayUsesToIndexMap(indexMap, subs);
-        return scalarize(indexMap);
+        return dynamicFExp(scalarize(indexMap));
     }
     
     public FExp FIdUseExp.extractArrayCell(Map<String,FExp> indexMap, Index i) {
@@ -1273,6 +1261,14 @@ aspect Scalarization {
 	 * variable or member with the given name.
 	 */
 	public FExp FType.createRecordConstructorArray(Map<String,FExp> indexMap, FExp exp, Iterator<Index> it, int dim) {
+        Size s = size();
+        if (s.isEmpty()) {
+            List<FExp> dims = new List<FExp>();
+            for (int i = 0, n = s.ndims(); i < n ; i++) {
+                dims.add(s.createFExp(i));
+            }
+            return new FFillExp(dims, zeroLiteral());
+        }
 		FArray arr = new FArray();
 		boolean last = dim == ndims() - 1;
 		for (int i = 0, n = size().get(dim); i < n; i++) {
@@ -1462,40 +1458,25 @@ aspect Scalarization {
 	 * The FArrays are needed because function calls need arrays to be passed as arrays.
 	 */
 	public FExp FExp.scalarize(Map<String,FExp> indexMap) {
-		FExp res;
-        if (useTempVar) 
-               res = indexMap.containsKey(tempVarName()) ? 
-                       indexMap.get(tempVarName()).fullCopy() : 
-                       new FIdTempExp(new FIdUse(tempVarName()));
-		else if (type().isRecord() && wantsRecordCon())
-            return type().createRecordConstructor(indexMap, this);
- 		else if (buildScalarArray() && !inUnknownSize()) 
- 			res = getArray().buildScalarized(indexMap, this);
-		else
-			res = scalarizeExp(indexMap);
-		return res;
-	}
-	
-	syn boolean FExp.buildScalarArray() = !size().isUnknown() && isArray() && !keepAsArray(true);
-	eq FArray.buildScalarArray() = false;
-	
-	syn boolean FExp.keepAsArray() = keepAsArray(false);
-	syn boolean FExp.keepAsArray(boolean scal) = canKeepAsArray(scal) && canAcceptArray();
-
-	syn boolean FExp.canKeepAsArray(boolean scal) = false;
-	eq FIdUseExp.canKeepAsArray(boolean scal)     = !isSlice();
-	eq FFunctionCall.canKeepAsArray(boolean scal) = scal;
-    eq FVectorFunctionCall.canKeepAsArray(boolean scal) = false;
-	
-	inh boolean FExp.canAcceptArray();
-	eq BaseNode.getChild().canAcceptArray()         = false;
-	eq FFunctionCallLeft.getFExp().canAcceptArray() = inFunction();
-	eq FFunctionCall.getArg(int i).canAcceptArray() = getArg(i).isFFunctionCall() || inFunction();
-	eq FSizeExp.getFExp().canAcceptArray()          = inFunction();
-	
-	syn boolean FExp.isFFunctionCall() = false;
-	eq FFunctionCall.isFFunctionCall() = true;
-	
+        FExp exp = useTempVar ? 
+                dynamicFExp(tempExp()) :
+                this;
+        if (scalarizeExpanded()) {
+            return type().createRecordConstructor(indexMap, exp);
+        } else {
+            return exp.scalarizeExp(indexMap);
+        }
+    }
+    
+    syn boolean FExp.scalarizeExpanded()       = isComposite() && indexNames == null && !(size().isUnknown() || inUnknownSize());
+    eq FIdUseExp.scalarizeExpanded()           = !inFunction() && super.scalarizeExpanded();
+    eq FFunctionCall.scalarizeExpanded()       = (useTempVar || isVectorized()) && super.scalarizeExpanded();
+    eq FArray.scalarizeExpanded()              = useTempVar && super.scalarizeExpanded();
+    eq FRecordConstructor.scalarizeExpanded()  = useTempVar && super.scalarizeExpanded();
+    
+    syn boolean FFunctionCall.isVectorized() = false;
+    eq FVectorFunctionCall.isVectorized() = true;
+    
 	/**
 	 * Scalarize expressions.
 	 */
@@ -1583,12 +1564,6 @@ aspect Scalarization {
     eq FFunctionCall.getArg(int i).shouldKeepArrayDuringScalarization() = 
     	getArg(i).isArray() && myCallInputs().get(i).ndims() == getArg(i).ndims();
     
-	inh boolean FExp.wantsRecordCon();
-	eq BaseNode.getChild().wantsRecordCon()         = false;
-	eq FFunctionCall.getArg(int i).wantsRecordCon() = !getArg(i).isFFunctionCall() && !inFunction();
-	eq FRecordConstructor.getArg().wantsRecordCon() = wantsRecordCon();
-	eq FArray.getFExp().wantsRecordCon()            = wantsRecordCon();
-
     public FExp FIfExp.scalarizeExp(Map<String,FExp> indexMap) {
         if (getIfExp().variability().indexParameterOrLess()) {
             try {
@@ -1775,8 +1750,9 @@ aspect Scalarization {
 	
 	public FExp FRecordConstructor.scalarizeExp(Map<String,FExp> indexMap) {
 		FRecordConstructor rc = new FRecordConstructor(getRecord().scalarize(indexMap), new List());
-		for (FExp arg : getArgs())
-			rc.addArg(arg.scalarize(indexMap));
+        for (FExp arg : getArgs()) {
+            rc.addArg(arg.scalarize(indexMap));
+        }
 		return rc;
 	}
 	
@@ -1800,8 +1776,9 @@ aspect Scalarization {
 	
 	public FFunctionCall FFunctionCall.scalarizeFunction(Map<String,FExp> indexMap) {
 		List<FExp> args = new List<FExp>();
-		for (FExp arg : getArgs())
-			args.add(arg.scalarize(indexMap));
+        for (FExp arg : getArgs()) {
+            args.add(arg.scalarize(indexMap));
+        }
         return createScalarized(getName().fullCopy(), args, getSizes());
 	}
     

commit 3285b47fe066ec1f4219be1c2123133b23d2c344

#4949 Fixes bug introduced in r8891
git-svn-id: https://svn.jmodelica.org@8894 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/scalarization/ScalarizeFunctions.jrag
@@ -300,6 +300,8 @@ aspect FunctionScalarization {
             useTempVar = false;
             type().scalarizeAssignment(vars, clauses, indexMap, left, right);
             useTempVar = true;
+            left.indexNames = null;
+            right.indexNames = null;
         }
     }
     

trunk/Compiler/ModelicaFrontEnd/test/modelica/FunctionTests.mo
@@ -6852,6 +6852,69 @@ end FunctionTests.ArrayExpInFunc44;
 ")})));
 end ArrayExpInFunc44;
 
+model ArrayExpInFunc45
+    function f1
+        input Real[:] x;
+        output Real[:] y = x;
+        algorithm 
+    end f1;
+    function f2
+        input Integer n;
+        output Real s = sum(f1(1:n));
+        algorithm
+        annotation(Inline = false);
+    end f2;
+    Real y = f2(3);
+
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="ArrayExpInFunc45",
+            description="Scalarization of functions: unknown size in scalar operator",
+            variability_propagation=false,
+            flatModel="
+fclass FunctionTests.ArrayExpInFunc45
+ Real y;
+equation
+ y = FunctionTests.ArrayExpInFunc45.f2(3);
+
+public
+ function FunctionTests.ArrayExpInFunc45.f2
+  input Integer n;
+  output Real s;
+  Real temp_1;
+  Real[:] temp_2;
+  Integer[:] temp_3;
+ algorithm
+  init temp_2 as Real[max(n, 0)];
+  init temp_3 as Integer[max(n, 0)];
+  for i1 in 1:max(n, 0) loop
+   temp_3[i1] := i1;
+  end for;
+  (temp_2) := FunctionTests.ArrayExpInFunc45.f1(temp_3);
+  temp_1 := 0.0;
+  for i1 in 1:max(n, 0) loop
+   temp_1 := temp_1 + temp_2[i1];
+  end for;
+  s := temp_1;
+  return;
+ annotation(Inline = false);
+ end FunctionTests.ArrayExpInFunc45.f2;
+
+ function FunctionTests.ArrayExpInFunc45.f1
+  input Real[:] x;
+  output Real[:] y;
+ algorithm
+  init y as Real[size(x, 1)];
+  for i1 in 1:size(x, 1) loop
+   y[i1] := x[i1];
+  end for;
+  return;
+ end FunctionTests.ArrayExpInFunc45.f1;
+
+end FunctionTests.ArrayExpInFunc45;
+")})));
+end ArrayExpInFunc45;
+
 model ArrayOutputScalarization1
  function f
   output Real x[2] = {1,2};