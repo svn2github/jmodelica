svn2github/jmodelica

commit 1d220bbfd850610ab58651632c1b94d9a786724b

#4968 Recommiting r9035 r9038 r9039 with bugfixes
git-svn-id: https://svn.jmodelica.org@9057 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/jastadd/instance/InstTypeAnalysis.jrag
@@ -112,18 +112,25 @@ aspect InstTypeAnalysis {
         return type;
     }
 
-    eq InstPartialFunction.type() = myInstClass().functionType().sizedType(size());
+    eq InstPartialFunction.type() = functionType().sizedType(size());
     
-    syn FFunctionType InstClassDecl.functionType() {
+    public interface InstCallable{
+        public FType functionType();
+    }
+    
+    syn FFunctionType InstClassDecl.functionType()       = getFunctionType();
+    syn FFunctionType InstPartialFunction.functionType() = myInstClass().functionType();
+    
+    syn FFunctionType InstClassDecl.getFunctionType() {
         List<FRecordComponentType> inputTypes = new List();
         List<FRecordComponentType> outputTypes = new List();
         for (InstComponentDecl icd : myInputs())
-            inputTypes.add(new FRecordComponentType(icd.qualifiedName(), icd.type()));
+            inputTypes.add(new FRecordComponentType(icd.name(), icd.type().treeCopy()));
         for (InstComponentDecl icd : myOutputs())
-            outputTypes.add(new FRecordComponentType(icd.qualifiedName(), icd.type()));
-        return new FFunctionType(Size.SCALAR, qualifiedName(), inputTypes, outputTypes, null);
+            outputTypes.add(new FRecordComponentType(icd.name(), icd.type().treeCopy()));
+        return new FFunctionType(Size.SCALAR, actualInstClass().qualifiedName(), inputTypes, outputTypes, null);
     }
-
+    
     eq InstRecordConstructor.type() {
         FRecordType res = myInstClassDecl().createEmptyFRecordType(Size.SCALAR);
         for (InstComponentDecl icd : allInstComponentDecls()) {
@@ -149,17 +156,20 @@ aspect InstTypeAnalysis {
     syn lazy FType InstComponentDecl.type() = fUnknownType();
 
     eq InstFunctionCall.type() = hasOutputs() ? typeOfOutput(0) : fUnknownType();
-    eq InstPartialFunctionCall.type() = functionType();
     
-    syn FFunctionType InstPartialFunctionCall.functionType() {
-         FFunctionType type = myInstCallable().actualInstClassDecl().functionType();
-         Set<String> s = new HashSet(namedArgs());
-         List<FRecordComponentType> n = new List<FRecordComponentType>();
-         for (FRecordComponentType frc : type.getInputs())
-             if (!s.contains(frc.getName()))
-                 n.add(frc);
-         type.setInputList(n);
-         return type;
+    syn lazy FType InstFunctionCall.getFType() = myInstCallable().actualInstClassDecl().functionType().removeInputs().createSizeFExp(this);
+    eq InstVectorFunctionCall.getFType()       = myInstCallable().actualInstClassDecl().functionType().createSizeFExp(this).sizedType(vectorizedSize());
+    eq InstPartialFunctionCall.getFType() {
+        FFunctionType type = (FFunctionType)myInstCallable().actualInstClassDecl().functionType().createSizeFExp(this);
+        Set<String> s = new HashSet(namedArgs());
+        List<FRecordComponentType> n = new List<FRecordComponentType>();
+        for (FRecordComponentType frc : type.getInputs()) {
+            if (!s.contains(frc.getName())) {
+                n.add(frc);
+            }
+        }
+        type.setInputList(n);
+        return type;
     }
     
     syn ArrayList<String> InstPartialFunctionCall.namedArgs() {
@@ -167,6 +177,15 @@ aspect InstTypeAnalysis {
         for (InstComponentDecl icd : myInputsToBind(myInstCallable()))
             res.add(icd.name());
         return res;
+    }
+    
+    /**
+     * Remove input types
+     * @return this
+     */
+    public FFunctionType FFunctionType.removeInputs() {
+        setInputList(new List<FRecordComponentType>());
+        return this;
     }
     
     syn boolean InstNode.isReal()            = false;

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen.jrag
@@ -1073,7 +1073,7 @@ aspect CCodeGen {
 	public static final String ASTNode.C_SUFFIX_DEF      = "def";
 	public static final String ASTNode.C_SUFFIX_EXP      = "exp";
 	public static final String ASTNode.C_SUFFIX_REC      = "r";
-	public static final String ASTNode.C_SUFFIX_REC_EXT  = "r_ext";
+    public static final String ASTNode.C_SUFFIX_EXT      = "_ext";
 	public static final String ASTNode.C_SUFFIX_ENUM     = "e";
 	public static final String ASTNode.C_SUFFIX_INDEX    = "i";
 	public static final String ASTNode.C_SUFFIX_VARIABLE = "v";
@@ -1204,14 +1204,12 @@ aspect CCodeGen {
     eq FFunctionType.nameScalar_C() = myFFunctionDecl().funcNameUnderscore(C_SUFFIX_FP) + "*";
     
     syn String FType.nameArray_C()        = "jmi_array_t";
-    eq FRecordType.nameArray_C()          = (getFClass() != null ? myFRecordDecl().name_C() :
-        ASTNode.underScore(getName())) + "_" + C_SUFFIX_REC + C_SUFFIX_ARRAY;
+    eq FRecordType.nameArray_C()          = nameScalar_C() + C_SUFFIX_ARRAY;
     eq FStringType.nameArray_C()          = "jmi_string_array_t";
     eq FExternalObjectType.nameArray_C()  = "jmi_extobj_array_t";
     
     syn String FType.nameScalarExt_C()    = nameScalar_C();
-    eq FRecordType.nameScalarExt_C()      = (getFClass() != null ? myFRecordDecl().name_C() :
-        ASTNode.underScore(getName())) + "_" + C_SUFFIX_REC_EXT;
+    eq FRecordType.nameScalarExt_C()      = nameScalar_C() +  C_SUFFIX_EXT;
     eq FIntegerType.nameScalarExt_C()     = "jmi_int_t";
     eq FBooleanType.nameScalarExt_C()     = "jmi_int_t";
     eq FEnumType.nameScalarExt_C()        = "jmi_int_t";
@@ -4937,11 +4935,11 @@ aspect CCodeGen {
     }
     
     syn FFunctionCallStmt FExternalObjectVariable.getDestructorCall() {
-        FIdUse name = new FIdUse(type().name() + ".destructor");
-        Size[] sizes = new Size[0];
+        String strName = type().name() + ".destructor";
+        FIdUse name = new FIdUse(strName);
         List<FExp> args = new List<FExp>();
         args.add(createUseExp());
-        FFunctionCall call = new FFunctionCall(name, args, sizes);
+        FFunctionCall call = new FFunctionCall(name, args, FFunctionType.create(strName, new ArrayList<FVariable>(), myFClass()));
         return new FFunctionCallStmt(new List<FFunctionCallLeft>(), call);
     }
     

trunk/Compiler/ModelicaFlatTree/src/jastadd/Arrays.jrag
@@ -820,6 +820,7 @@ aspect Arrays {
 	eq InstNode.getChild().expandSizeForIterExp(Size s) = s;
 	
 	eq FSubscriptedExp.size() = getFArraySubscripts().accessSize();
+    eq FComponentExp.size()   = type().size();
 	
 	eq FTranspose.size() {
 		Size size = getFExp().size();
@@ -1715,17 +1716,14 @@ aspect Arrays {
 
     eq FVectorFunctionCall.getArray() {
         Array arr = new Array(indices());
-        Size[] sizes = (Size[]) getSizes().clone();
-        sizes[0] = Size.SCALAR;
         boolean[] vec = getVectorized();
-        
         int j;
         for (Index i : arr.indices()) {
             List<FExp> args = new List<FExp>();
             j = 0;
             for (FExp arg : getArgs()) 
                 args.add(vec[j++] ? arg.getArray().subArrayFExp(i) : arg);
-            arr.set(i, new FFunctionCall(getName().fullCopy(), args, sizes));
+            arr.set(i, new FFunctionCall(getName().fullCopy(), args, getFType().scalarType()));
         }
         return arr;
     }

trunk/Compiler/ModelicaFlatTree/src/jastadd/ConstantEvaluation.jrag
@@ -4177,7 +4177,7 @@ aspect ConstantEvaluation {
         for (FRecordComponentType comp : comps)
             if (comp.getName().equals(name))
                 return comp.getFType();
-        return null;
+        return fUnknownType();
     }
     
     

trunk/Compiler/ModelicaFlatTree/src/jastadd/Constructors.jrag
@@ -1455,4 +1455,13 @@ aspect InheritedFactoryMethods {
 	eq FMinExp.createCompareNode(FExp x, FExp y) = new FLtExp(x, y);
 	eq FMaxExp.createCompareNode(FExp x, FExp y) = new FGtExp(x, y);
 	
+    public static FFunctionType FFunctionType.create(String name, Iterable<FVariable> fvs, FClass fclass) {
+        List<FRecordComponentType> in = new List<FRecordComponentType>();
+        List<FRecordComponentType> out = new List<FRecordComponentType>();
+        for (FVariable fv : fvs) {
+            out.add(new FRecordComponentType(fv.name(), fv.type().treeCopy()));
+        }
+        return new FFunctionType(Size.SCALAR, name, in, out, fclass);
+    }
+    
 }

trunk/Compiler/ModelicaFlatTree/src/jastadd/FlatAPI.jrag
@@ -1564,6 +1564,7 @@ aspect FlatAPI {
     
     inh FClass FAbstractVariable.myFClass();
     inh FClass FExp.myFClass();
+    inh FClass FType.myFClass();
     inh FClass FIdUse.myFClass();
     inh FClass FQName.myFClass();
     inh FClass FFunctionDecl.myFClass();

trunk/Compiler/ModelicaFlatTree/src/jastadd/Sizes.jrag
@@ -247,9 +247,7 @@ aspect AccessSizes {
 aspect FunctionCallSizes {
     
     syn Size FAbstractFunctionCall.sizeOfOutput(int i) = (i == 0) ? size() : Size.SCALAR;
-    eq InstVectorFunctionCall.sizeOfOutput(int i) = (i == 0) ? vectorizedSize() : super.sizeOfOutput(i);
-    eq FFunctionCall.sizeOfOutput(int i) = getSizes()[i];
-    eq InstFunctionCall.sizeOfOutput(int i) = myCallOutputs().get(i).size().createSizeFExp(this);
+    eq CommonFunctionCall.sizeOfOutput(int i)          = typeOfOutput(i).size();
     
     /**
      * Get array sizes of function argument.
@@ -260,6 +258,22 @@ aspect FunctionCallSizes {
 
     eq FArgumentExp.size() = copiedFExp().size();
     
+    public FType FType.createSizeFExp(InstFunctionCall call) {
+        FType res = treeCopy();
+        res.replaceSizeFExp(call);
+        return res;
+    }
+    
+    public void ASTNode.replaceSizeFExp(InstFunctionCall call) {
+        for (ASTNode n : this) {
+            n.replaceSizeFExp(call);
+        }
+    }
+    
+    public void FType.replaceSizeFExp(InstFunctionCall call) {
+        setSize(getSize().createSizeFExp(call));
+        super.replaceSizeFExp(call);
+    }
     
     public Size Size.createSizeFExp(InstFunctionCall call) {
         
@@ -307,6 +321,9 @@ aspect FunctionCallSizes {
     public void InstFunctionArgument.sizeOfOutput(Map<InstComponentDecl, InstFunctionArgument> varMap, InstComponentDecl icd) {
         getFExp().size();      // Cash sizes to prevent false circularity in nested function calls
         varMap.put(icd, this);
+    }
+    public void InstMissingArgument.sizeOfOutput(Map<InstComponentDecl, InstFunctionArgument> varMap, InstComponentDecl icd) {
+        
     }
     
     protected boolean FSizeExp.circ = false;

trunk/Compiler/ModelicaFlatTree/src/jastadd/TypeAnalysis.jrag
@@ -44,23 +44,7 @@ aspect FlatTypeAnalysis {
             type.addFEnumLiteralType(new FEnumLiteralType(Size.SCALAR, el));
         return type;
     }
-
-    // Re-evaluate record types for function variables
-    private boolean FFunctionVariable.typeRewritten = false;
-    rewrite FFunctionVariable { 
-        when (!typeRewritten) to FFunctionVariable {
-            if (getType().isRecord()) {
-                FRecordType type = (FRecordType) getType();
-                type.setFClass(myFClass());
-            } else if (getType().isFunction()) {
-                FFunctionType type = (FFunctionType) getType();
-                type.setFClass(myFClass());
-            }
-            typeRewritten = true;
-            return this;
-        }
-    }
-
+    
     syn FType FRecordVariable.getType() = myFRecordDecl().type().sizedType(size());
 
     syn FType FEnumVariable.getType() = myFEnumDecl().type().sizedType(size());
@@ -593,11 +577,29 @@ aspect FlatTypeAnalysis {
     eq FFunctionCallEquation.getLeft(int i).type() = getCall().typeOfOutput(i);
     eq FFunctionCallStmt.getLeft(int i).type()     = getCall().typeOfOutput(i);
 
-	syn FType FAbstractFunctionCall.typeOfOutput(int i) = (i == 0) ? type() : fUnknownType();
-    syn lazy FType InstFunctionCall.typeOfOutput(int i) = myCallOutputs().get(i).type().sizedType(sizeOfOutput(i));
-    syn lazy FType FFunctionCall.typeOfOutput(int i)    = myCallOutputs().get(i).type().sizedType(sizeOfOutput(i));
-    eq FSpatialDistExp.typeOfOutput(int i) = i == 1 ? type() : super.typeOfOutput(i);
-
+	syn FType FAbstractFunctionCall.typeOfOutput(int i) = typeOutput(i) ? type() : fUnknownType();
+    eq FFunctionCall.typeOfOutput(int i)                = getFType().component(i);
+    eq FPartialFunctionCall.typeOfOutput(int i)         = getFType();
+    eq InstFunctionCall.typeOfOutput(int i)             = getFType().component(i);
+    eq InstPartialFunctionCall.typeOfOutput(int i)      = getFType();
+    
+    syn boolean FAbstractFunctionCall.typeOutput(int i) = i == 0;
+    eq FSpatialDistExp.typeOutput(int i)                = i == 1 || super.typeOutput(i);
+    
+    syn FType FType.component(int i)  = fUnknownType();
+    eq FFunctionType.component(int i) = nullUnknown(getOutput(i)).vectorized(getSize());
+    
+    syn FType FType.nullUnknown(FRecordComponentType t) = t == null ? fUnknownType() : t.getFType();
+    
+    syn FType FType.vectorized(Size s) {
+        if (s == Size.SCALAR) {
+            return this;
+        }
+        MutableSize s2 = s.mutableClone();
+        s2.append(getSize());
+        return sizedType(s2);
+    }
+    
     eq FFunctionCall.type()    = hasOutputs() ? typeOfOutput(0) : fUnknownType();
     eq FMathematicalFunctionCall.type() = fRealType(size());
     eq FPartialFunctionCall.type() {

commit 4553fdab052f5d753b0f1469450a8658080f4b54

#4968 Fixes bug introduced in r9057
git-svn-id: https://svn.jmodelica.org@9062 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/jastadd/instance/InstTypeAnalysis.jrag
@@ -157,10 +157,12 @@ aspect InstTypeAnalysis {
 
     eq InstFunctionCall.type() = hasOutputs() ? typeOfOutput(0) : fUnknownType();
     
-    syn lazy FType InstFunctionCall.getFType() = myInstCallable().actualInstClassDecl().functionType().removeInputs().createSizeFExp(this);
-    eq InstVectorFunctionCall.getFType()       = myInstCallable().actualInstClassDecl().functionType().createSizeFExp(this).sizedType(vectorizedSize());
+    syn FFunctionType InstFunctionCall.declType() = myInstCallable().actualInstClassDecl().functionType();
+    
+    syn lazy FType InstFunctionCall.getFType() = declType().removeInputs().createSizeFExp(this);
+    eq InstVectorFunctionCall.getFType()       = declType().removeInputs().createSizeFExp(this).sizedType(vectorizedSize());
     eq InstPartialFunctionCall.getFType() {
-        FFunctionType type = (FFunctionType)myInstCallable().actualInstClassDecl().functionType().createSizeFExp(this);
+        FFunctionType type = declType();
         Set<String> s = new HashSet(namedArgs());
         List<FRecordComponentType> n = new List<FRecordComponentType>();
         for (FRecordComponentType frc : type.getInputs()) {

trunk/Compiler/ModelicaFrontEnd/test/modelica/TypeTests.mo
@@ -2433,6 +2433,82 @@ Error at line 2398, column 71, in file 'Compiler/ModelicaFrontEnd/test/modelica/
 ")})));
 end Functional7;
 
+model Functional8
+    partial function partFunc
+        input Real[:] x1;
+        output Real y;
+    end partFunc;
+    
+    partial function middleFunc
+        extends partFunc;
+        input Real x2;
+    end middleFunc;
+    
+    function fullFunc
+        extends middleFunc;
+        input Real x3;
+      algorithm
+        y := x1 + x2 + x3;
+    end fullFunc;
+    
+    function useMiddleFunc
+        input middleFunc mf;
+        input Real b;
+        input Real c;
+        output Real y = usePartFunc(function mf(x1={1},x2="string",x3=3,x4=4), c);
+        algorithm
+    end useMiddleFunc;
+    
+    function usePartFunc
+        input partFunc pf;
+        input Real c;
+        output Real y;
+      algorithm
+        y := pf(c);
+    end usePartFunc;
+
+    Real y = useMiddleFunc(function fullFunc(x3=time), time, time);
+    
+    annotation(__JModelica(UnitTesting(tests={
+        ComplianceErrorTestCase(
+            name="Functional8",
+            description="Type calculation with array sizes",
+            errorMessage="
+8 errors found:
+
+Error at line 2451, column 14, in file '...', TYPE_MISMATCH_IN_EXPRESSION:
+  Type error in expression: x1 + x2
+    type of 'x1' is Real[size(x1, 1)]
+    type of 'x2' is Real
+
+Compliance error at line 2454, column 26, in file '...', UNSUPPORTED_FUNCTIONAL_INPUT_COMPOSITE:
+  Functional input arguments with record/array inputs/outputs is currently not supported
+
+Error at line 2458, column 49, in file '...':
+  Creating functional input argument mf(): no input matching named argument x1 found
+
+Error at line 2458, column 56, in file '...':
+  Creating functional input argument mf(): types of named argument x2 and input x2 are not compatible
+    type of '\"string\"' is String
+    expected type is Real
+
+Error at line 2458, column 68, in file '...':
+  Creating functional input argument mf(): no input matching named argument x3 found
+
+Error at line 2458, column 73, in file '...':
+  Creating functional input argument mf(): no input matching named argument x4 found
+
+Compliance error at line 2462, column 24, in file '...', UNSUPPORTED_FUNCTIONAL_INPUT_COMPOSITE:
+  Functional input arguments with record/array inputs/outputs is currently not supported
+
+Error at line 2467, column 17, in file '...':
+  Calling function pf(): types of positional argument 1 and input x1 are not compatible
+    type of 'c' is Real
+    expected type is Real[:]
+
+")})));
+end Functional8;
+
 
 model Delay1
     Real x1 = sin(time);