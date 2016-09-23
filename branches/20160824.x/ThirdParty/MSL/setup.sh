f5ee89310f362ded368889975aa8bcae5827c1c7
jsten <jsten@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2016-09-23 12:07:33 +0000

    Stripped the MSL version in this branch. See ThirdParty/MSL/setup.sh for details on what's included. Ticket #5133.
    git-svn-id: https://svn.jmodelica.org@9251 7ae51c65-13d5-4921-9d80-6511ddd1419c

---
+++ branches/20160824.x/ThirdParty/MSL/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+# Based on https://svn.jmodelica.org/branches/20160331.x/ThirdParty/MSL/setup.sh
+
+svn co https://svn.modelica.org/projects/Modelica/tags/v3.2.2+build.3-release .
+#We have nothing to merge from trunk right now, but in the event that we have, this is how it is done!
+# svn merge --accept base -1234,1235 https://svn.modelica.org/projects/Modelica/trunk
+
+rm -r .svn
+rm -r ModelicaTest
+rm -r ModelicaReference
+rm ObsoleteModelica3.mo
+
+#Remove unwanted parts
+rm Modelica/Blocks/Noise.mo
+rm Modelica/Electrical/PowerConverters.mo
+rm -r Modelica/Magnetic/QuasiStatic
+rm Modelica/Math/Distributions.mo
+rm Modelica/Math/Random.mo
+rm Modelica/Math/Special.mo
+patch -i removeSubLibs.patch -p0
