svn2github/jmodelica

commit 15d2946750e535e5e39f3ad41f49926c4a0673b5

#5293 Fixes bug introduced in r9639. Some refactoring in code generation.
git-svn-id: https://svn.jmodelica.org@9642 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen/CCodeGen.jrag
@@ -3111,42 +3111,17 @@ aspect CCodeGen {
     }
 
     @Override
-	public void FEquation.genAssignment_C(CodePrinter p, CodeStream str, String indent) {
+    public void FEquation.genAssignment_C(CodePrinter p, CodeStream str, String indent) {
         p.printPreSteps(this, str, indent);
         FIdUseExp left = (FIdUseExp)getLeft().stripNegations();
-        FAbstractVariable fv = left.myFV();
-        if (left.myFV().isExternalObject()) {
+        FVariable fv = (FVariable)left.myFV();
+        if (fv.isExternalObject()) {
             FExternalObjectVariable eo = (FExternalObjectVariable) fv;
             eo.genDestructorCall_C(p, str, indent);
         }
-        str.print(indent);
-        if (left.type().isString()) {
-             str.print("JMI_ASG(STR_Z, ");
-             p.print(left.getFIdUse(), str, "");
-             str.print(", ");
-        } else {
-            p.print(left.getFIdUse(), str, "");
-            str.print(" = ");
-        }
-        if (getLeft().isNegated()) {
-            str.print("-");
-        }
-        str.print("(");
-        p.print(getRight(), str, "");
-        str.print(")");
-        if (left.myFV().isReal() && myOptions().getBooleanOption("enable_variable_scaling") && !inFunction()) {
-            str.print("/sf(");
-            str.print(left.myFV().valueReference());
-            str.print(")");
-        }
-        if (left.type().isString()) {
-            str.print(")");
-        } else {
-            str.print(";\n");
-        }
-        
+        fv.genAssignment_C(p, str, indent, fv.name_C(), getRight());
         p.printPostSteps(this, str, indent);
-	}
+    }
 
     public void FExternalObjectVariable.genDestructorCall_C(CodePrinter p, CodeStream str, String indent) {
         if (hasConstructorCall(Index.NULL)) {
@@ -3572,33 +3547,39 @@ aspect CCodeGen {
 
     public void FVariable.genStartValue_C(CodePrinter p, CodeStream str, String indent) {
         p.printPreSteps(this, str, indent);
+        FExp exp = useBindingExpAsStart() ? getBindingExp() : startAttributeExp();
+        boolean noExp = exp == null;
+        if (noExp)
+            setBindingExp(exp = type().zeroLiteral());
+        genAssignment_C(p, str, indent, name_C(), exp);
+        if (noExp)
+            setBindingExpOpt(new Opt());
+        p.printPostSteps(this, str, indent);
+    }
+
+    public void FVariable.genAssignment_C(CodePrinter p, CodeStream str, String indent, String name, FExp right) {
         str.print(indent);
         if (isString()) {
              str.print("JMI_ASG(STR_Z, ");
-             str.print(name_C());
+             str.print(name);
              str.print(", ");
         } else {
-            str.print(name_C());
+            str.print(name);
             str.print(" = ");
         }
-		FExp exp = useBindingExpAsStart() ? getBindingExp() : startAttributeExp();
-		boolean noExp = exp == null;
-		if (noExp)
-			setBindingExp(exp = type().zeroLiteral());
-		OptionRegistry opt = myOptions();
-		str.print("(");
-        p.print(exp, str, indent);
-		str.print(")");
-		if (isReal() && opt.getBooleanOption("enable_variable_scaling"))
-			str.format("/sf(%d)", valueReference());
+        str.print("(");
+        p.print(right, str, "");
+        str.print(")");
+        if (isReal() && myOptions().getBooleanOption("enable_variable_scaling")) {
+            str.print("/sf(");
+            str.print(valueReference());
+            str.print(")");
+        }
         if (isString()) {
             str.print(")");
         }
-		if (noExp)
-			setBindingExpOpt(new Opt());
-		str.print(";\n");
-        p.printPostSteps(this, str, indent);
-	}
+        str.print(";\n");
+    }
 
 	public void BLT.genOdeDerivativeBlocks(CodePrinter p, CodeStream str, String indent) {
 		for (AbstractEquationBlock block : this) {

trunk/Compiler/ModelicaFlatTree/src/jastadd/Sizes.jrag
@@ -555,7 +555,7 @@ aspect SizesUtil {
         return this;
     }
 
-    syn FExp FExp.cell(Index i) = i == Index.NULL ? this : getArray().get(i);
+    syn FExp FExp.cell(Index i) = i.ndims() > 0 ? getArray().get(i) : this;
     eq FDeferExp.cell(Index i)  = getFExp().cell(i);
 
     public FArraySubscripts FArraySubscripts.copyReplaceAccess(FExp.ContextReplacer crp) {

trunk/Compiler/ModelicaFrontEnd/test/modelica/ArrayTests.mo
@@ -1347,6 +1347,26 @@ end ArrayTests.General.ArrayTest48;
 ")})));
 end ArrayTest48;
 
+model ArrayTest49
+    record R
+        Real[:] x;
+    end R;
+    
+    R[:] r(x(start={1})) = {R({time})};
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="General_ArrayTest49",
+            description="",
+            flatModel="
+fclass ArrayTests.General.ArrayTest49
+ Real r[1].x[1](start = 1);
+equation
+ r[1].x[1] = time;
+end ArrayTests.General.ArrayTest49;
+")})));
+end ArrayTest49;
+
 end General;
 
 

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/scalarization/Scalarization.jrag
@@ -848,6 +848,9 @@ aspect Scalarization {
     public FExp FAttribute.scalarizeValue(Index i, Map<String,FExp> indexMap) {
         if (!hasValue()) 
             return null;
+        if (i.ndims() > getValue().ndims()) {
+            i = i.subIndex(i.ndims() - getValue().ndims());
+        }
         return getValue().cell(i).scalarize(indexMap);
     }
     

commit 03b355c5e04af1d04437527549b83422a30f2bd4

#5293 Fixes bug introduced in r9642.
git-svn-id: https://svn.jmodelica.org@9645 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen/CCodeGen.jrag
@@ -3119,7 +3119,7 @@ aspect CCodeGen {
             FExternalObjectVariable eo = (FExternalObjectVariable) fv;
             eo.genDestructorCall_C(p, str, indent);
         }
-        fv.genAssignment_C(p, str, indent, fv.name_C(), getRight());
+        fv.genAssignment_C(p, str, indent, fv.name_C(), getRight(), getLeft().isNegated());
         p.printPostSteps(this, str, indent);
     }
 
@@ -3551,13 +3551,14 @@ aspect CCodeGen {
         boolean noExp = exp == null;
         if (noExp)
             setBindingExp(exp = type().zeroLiteral());
-        genAssignment_C(p, str, indent, name_C(), exp);
+        genAssignment_C(p, str, indent, name_C(), exp, false);
         if (noExp)
             setBindingExpOpt(new Opt());
         p.printPostSteps(this, str, indent);
     }
 
-    public void FVariable.genAssignment_C(CodePrinter p, CodeStream str, String indent, String name, FExp right) {
+    public void FVariable.genAssignment_C(CodePrinter p, CodeStream str, String indent, String name, FExp right,
+            boolean neg) {
         str.print(indent);
         if (isString()) {
              str.print("JMI_ASG(STR_Z, ");
@@ -3566,6 +3567,9 @@ aspect CCodeGen {
         } else {
             str.print(name);
             str.print(" = ");
+        }
+        if (neg) {
+            str.print("-");
         }
         str.print("(");
         p.print(right, str, "");

trunk/Compiler/ModelicaFrontEnd/src/jastadd/flattening/Flattening.jrag
@@ -2093,10 +2093,17 @@ aspect FlatExpressions {
      */
     public FExp InstComponentDecl.flattenAccess(FQName prefix, FIdUseExp exp) {
         if (isEnumLiteral())
-            return new FIdUseExp(getFQName().fullCopy());
+            return createFIdUseExp(getFQName().fullCopy());
         InstAccess ia = exp.asInstAccess();
         FQName fqn = inOrIsInner() ? ia.flattenOuterAccess(prefix) : ia.flatten(prefix);
-        return new FIdUseExp(fqn);
+        return createFIdUseExp(fqn);
+    }
+    
+    /**
+     * Separate method for extensibility.
+     */
+    private FIdUseExp InstComponentDecl.createFIdUseExp(FQName name) {
+        return new FIdUseExp(name);
     }
 
     public FExp InstAssignable.flattenAccess(FQName prefix, FIdUseExp iae) {

trunk/Compiler/ModelicaCBackEnd/test/modelica/CCodeGenTests.mo
@@ -21983,4 +21983,26 @@ jmi_ad_var_t func_CCodeGenTests_ForOfUnknownSize3_f_exp0(jmi_ad_var_t n_v) {
 ")})));
 end ForOfUnknownSize3;
 
+model AliasNegParam1
+    parameter Real x = 1;
+    parameter Real y = x + 1;
+    parameter Real z = -y;
+    annotation(__JModelica(UnitTesting(tests={
+        CCodeGenTestCase(
+            name="AliasNegParam1",
+            description="",
+            eliminate_alias_parameters=true,
+            template="$C_DAE_initial_dependent_parameter_assignments$",
+            generatedCode="
+int model_init_eval_parameters_base(jmi_t* jmi) {
+    int ef = 0;
+    JMI_DYNAMIC_INIT()
+    _z_1 = - (_x_0 + 1);
+    JMI_DYNAMIC_FREE()
+    return ef;
+}
+")})));
+end AliasNegParam1;
+
+
 end CCodeGenTests;