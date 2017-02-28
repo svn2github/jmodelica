svn2github/jmodelica

commit d6974bf4d18b730593446748cccccf81e8dc838d

#5096 Direct access to z-vector for FSubscriptedExp. Also fixed bugs and other performance issues in code generation.
git-svn-id: https://svn.jmodelica.org@9664 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen/CCodeGenFunctionIO.jrag
@@ -400,6 +400,10 @@ aspect CodeFuncCallInOutArgPrinters {
 
 aspect CodeFuncCallInOutArg {
     
+    syn FExp FExp.fArrayCell(Index i, int d) = this;
+    eq FArray.fArrayCell(Index i, int d)     = getFExp(i.get(d) - 1).fArrayCell(i, d + 1);
+    eq FIdUseExp.fArrayCell(Index i, int d)  = cell(i);
+    
     public void FType.genFuncCallInOutArg_C(CodeFuncCallInOutArgPrinter p, CodeStream str, String indent, String name, FExp exp) {
         if (isArray()) {
             if (size().isUnknown()) {
@@ -410,10 +414,9 @@ aspect CodeFuncCallInOutArg {
             } else {
                 String type = isRecord() ? C_ARRAY_RECORD : p.arrayAccessConst();
                 String pre = String.format("jmi_array_%s_%d(%s, ", type, ndims(), name);
-                Array arr = exp.getArray();
-                for (Index i : arr.indices()) {
+                for (Index i : exp.indices()) {
                     String cellName = pre + i.toUnclosedString() + ")";
-                    genFuncCallCellInOutArg_C(p, str, indent, cellName, arr.get(i));
+                    genFuncCallCellInOutArg_C(p, str, indent, cellName, exp.fArrayCell(i, 0));
                 }
             }
         } else {

trunk/Compiler/ModelicaFrontEnd/test/modelica/RecordTests.mo
@@ -5545,7 +5545,7 @@ fclass RecordTests.RecordScalarize54
 initial equation 
  pre(temp_1) = 0;
 equation
- y.x[1] = ({1.0, 2.0})[temp_1];
+ y.x[1] = ({{1.0}, {2.0}})[temp_1,1];
  temp_1 = if time < pre(temp_1) or time >= pre(temp_1) + 1 or initial() then integer(time) else pre(temp_1);
 end RecordTests.RecordScalarize54;
 ")})));

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CADCodeGen/CADCodeGen.jrag
@@ -316,14 +316,11 @@ aspect CADCodeGen {
 
 	@Override
 	public void FExp.genTempVars_CAD(CodePrinter p, CodeStream str, String indent) {
-		if (isArray() && !size().isUnknown()) {
-			getArray().genTempVars_CAD(p, str, indent);
-		} else {
-			if (autoGenChildADStatements_CAD())
-				super.genTempVars_CAD(p, str, indent);
-			genADStatement_CAD(p , str, indent);
-		}
-	}
+        if (autoGenChildADStatements_CAD()) {
+            super.genTempVars_CAD(p, str, indent);
+        }
+        genADStatement_CAD(p , str, indent);
+    }
 	
 	/**
 	 * Determines if the expression type genereates ad statments
@@ -340,14 +337,16 @@ aspect CADCodeGen {
 	inh boolean FExp.inlineVar_CAD();
 	eq BaseNode.getChild().inlineVar_CAD() = true;
 	eq FExp.getChild().inlineVar_CAD()     = false;
-	
+    eq FArray.getChild().inlineVar_CAD()   = true;
+
 	/**
 	 * The der expression for this type will be inlined if true
 	 */
 	inh boolean FExp.inlineDer_CAD();
 	eq BaseNode.getChild().inlineDer_CAD() = true;
 	eq FExp.getChild().inlineDer_CAD()     = false;
-	
+    eq FArray.getChild().inlineDer_CAD()   = true;
+
 	/**
 	 * Generates var expression or prints reference to evaluated expression
 	 */
@@ -1543,7 +1542,12 @@ aspect CADCodeGen {
      */
     @Override
     public void FArray.genVarDecls_CAD(CodePrinter p, CodeStream str, String indent) {
-        getArray().genVarDecls_CAD(p, str, indent);
+        p.printVarDecls(getFExps(), str, indent);
+    }
+    
+    @Override
+    public void FArray.genTempVars_CAD(CodePrinter p, CodeStream str, String indent) {
+        p.printPreSteps(getFExps(), str, indent);
     }
     
     @Override

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen/CCodeGenExpressions.jrag
@@ -24,34 +24,10 @@ aspect CCodeGenExpressions {
      =========================================================================*/
 
     @Override
-    public void FExp.genVarDecls_C(CodePrinter p, CodeStream str, String indent) {
-        if (isArray() && !size().isUnknown())
-            getArray().genVarDecls_C(p, str, indent);
-        else
-            super.genVarDecls_C(p, str, indent);
-    }
-
-    @Override
-    public void FExp.genTempVars_C(CodePrinter p, CodeStream str, String indent) {
-        if (isArray() && !size().isUnknown())
-            p.printPreSteps(getArray(), str, indent);
-        else
-            super.genTempVars_C(p, str, indent);
-    }
-
-    @Override
     public void FAbsExp.prettyPrint_C(CodePrinter p, CodeStream str, String indent) {
         str.print("jmi_abs(");
         p.print(getFExp(), str, indent);
         str.print(")");
-    }
-
-    @Override
-    public void FExp.genTempFree_C(CodePrinter p, CodeStream str, String indent) {
-        if (isArray() && !size().isUnknown())
-            p.printPostSteps(getArray(), str, indent);
-        else
-            super.genTempFree_C(p, str, indent);
     }
 
     @Override
@@ -1507,29 +1483,81 @@ aspect CCodeGenExpressions {
     @Override
     public void FSubscriptedExp.genVarDecls_C(CodePrinter p, CodeStream str, String indent) {
         super.genVarDecls_C(p, str, indent);
-        getFExp().genTempInputDecl_C(p, str, indent);
+        if (needsTemp()) {
+            getFExp().genTempInputDecl_C(p, str, indent);
+        }
     }
 
     @Override
     public void FSubscriptedExp.genTempVars_C(CodePrinter p, CodeStream str, String indent) {
         super.genTempVars_C(p, str, indent);
-        getFExp().genTempInputAssigns_C(p, str, indent);
+        if (needsTemp()) {
+            getFExp().genTempInputAssigns_C(p, str, indent);
+        }
     }
 
     @Override
     public void FSubscriptedExp.genTempFree_C(CodePrinter p, CodeStream str, String indent) {
         super.genTempFree_C(p, str, indent);
-        if (isAssignedTo()) {
+        if (needsTemp() && isAssignedTo()) {
             p.printWriteBackAssignment(str, indent, this, null);
         }
     }
 
     @Override
     public void FSubscriptedExp.prettyPrint_C(CodePrinter p, CodeStream str, String indent) {
-        getFExp().genSubscriptedUse_C(p, str, indent, getFArraySubscripts());
+        if (needsTemp()) {
+            getFExp().genSubscriptedUse_C(p, str, indent, getFArraySubscripts());
+        } else {
+            getFExp().first().genDirectSubscriptedUse_C(p, str, indent, getFArraySubscripts());
+        }
+    }
+
+    syn FIdUseExp FExp.first() = null;
+    eq FArray.first()          = getFExp(0).first();
+    eq FIdUseExp.first()       = this;
+    
+    syn lazy boolean FSubscriptedExp.needsTemp() = (getFExp().consecutiveArray(null) == null);
+    
+    syn FAbstractVariable FExp.consecutiveArray(FAbstractVariable last) = null;
+    eq FArray.consecutiveArray(FAbstractVariable last) {
+        for (FExp exp : getFExps()) {
+            last = exp.consecutiveArray(last);
+            if (last == null) {
+                break;
+            }
+        }
+        return last;
+    }
+    eq FIdUseExp.consecutiveArray(FAbstractVariable last) {
+        if (last == null || last.isConsecutiveInZ(myFV())) {
+            return myFV();
+        }
+        return null;
     }
 
     public void FExp.genSubscriptedUse_C(CodePrinter p, CodeStream str, String indent, FArraySubscripts fas) {
         str.print(fas.genFunctionArrayUse_C(p, tempName_C(), C_ARRAY_VALUE));
+    }
+    
+    public void FIdUseExp.genDirectSubscriptedUse_C(CodePrinter p, CodeStream str, String indent, FArraySubscripts fas) {
+        str.print("(&");
+        p.print(this, str, indent);
+        str.print(")");
+        str.print("[(int)(");
+        Size s = fas.mySize();
+        for (int i = fas.getNumFSubscript() - 1; i >= 0; i--) {
+            p.print(fas.getFSubscript(i), str, indent);
+            str.print(" - 1");
+            if (i > 0) {
+                str.print(" + ");
+                str.print(s.get(i));
+                str.print(" * (");
+            }
+        }
+        for (int i = 0; i < fas.getNumFSubscript() - 1; i++) {
+            str.print(")");
+        }
+        str.print(")]");
     }
 }

trunk/Compiler/ModelicaCBackEnd/test/modelica/CADCodeGenTests.mo
@@ -2150,8 +2150,10 @@ void func_CADCodeGenTests_CADFunction10_F1_der_AD0(jmi_ad_var_t x_var_v, jmi_ad_
     jmi_array_ref_1(m_der_a, 8) = AD_WRAP_LITERAL(0);
     y_var_v = 0;
     y_der_v = AD_WRAP_LITERAL(0);
+    v_0 = jmi_divide_function(\"CADCodeGenTests.CADFunction10.F1\", 8,2,\"8 / 2\");
     i_0ie = v_0 + 1 / 2.0;
     for (i_0i = 1; i_0i < i_0ie; i_0i += 1) {
+        v_1 = jmi_divide_function(\"CADCodeGenTests.CADFunction10.F1\", 8,2,\"8 / 2\");
         j_1ie = v_1 + 1 / 2.0;
         for (j_1i = 1; j_1i < j_1ie; j_1i += 1) {
             if (COND_EXP_GT(jmi_array_val_1(m_var_a, floor(i_0i + j_1i * AD_WRAP_LITERAL(2))), x_var_v, JMI_TRUE, JMI_FALSE)) {

commit 1b7fb2cb799e5a62bc3dd946185c9465d8b5b951

#5096 Fixed problems introduced in r9664
git-svn-id: https://svn.jmodelica.org@9665 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen/CCodeGenFunctionIO.jrag
@@ -399,11 +399,10 @@ aspect CodeFuncCallInOutArgPrinters {
 }
 
 aspect CodeFuncCallInOutArg {
-    
-    syn FExp FExp.fArrayCell(Index i, int d) = this;
+
+    syn FExp FExp.fArrayCell(Index i, int d) = cell(i);
     eq FArray.fArrayCell(Index i, int d)     = getFExp(i.get(d) - 1).fArrayCell(i, d + 1);
-    eq FIdUseExp.fArrayCell(Index i, int d)  = cell(i);
-    
+
     public void FType.genFuncCallInOutArg_C(CodeFuncCallInOutArgPrinter p, CodeStream str, String indent, String name, FExp exp) {
         if (isArray()) {
             if (size().isUnknown()) {

trunk/Compiler/ModelicaFlatTree/src/jastadd/Sizes.jrag
@@ -555,7 +555,7 @@ aspect SizesUtil {
         return this;
     }
 
-    syn FExp FExp.cell(Index i) = i.ndims() > 0 ? getArray().get(i) : this;
+    syn FExp FExp.cell(Index i) = i.ndims() > 0 && ndims() > 0 ? getArray().get(i) : this;
     eq FDeferExp.cell(Index i)  = getFExp().cell(i);
 
     public FArraySubscripts FArraySubscripts.copyReplaceAccess(FExp.ContextReplacer crp) {

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/scalarization/Scalarization.jrag
@@ -1524,15 +1524,21 @@ aspect Scalarization {
     
     syn boolean FExp.scalarizeExpanded()       = 
         isComposite() && indexNames == null && !(size().isUnknown() || inUnknownSize()) && !(useTempVar && isForIndexExp());
-    eq FIdUseExp.scalarizeExpanded()           = !inFunction() && super.scalarizeExpanded();
+    eq FIdUseExp.scalarizeExpanded()           = super.scalarizeExpanded() && scalarizeExpandedContext();
     eq FFunctionCall.scalarizeExpanded()       = ((useTempVar && !inFunction()) || isVectorized()) && super.scalarizeExpanded();
     eq FArray.scalarizeExpanded()              = useTempVar && super.scalarizeExpanded();
     eq FRecordConstructor.scalarizeExpanded()  = useTempVar && super.scalarizeExpanded();
     eq FIfExp.scalarizeExpanded()              = false;
-    eq FSubscriptedExp.scalarizeExpanded()     = false;
     
     syn boolean FFunctionCall.isVectorized() = false;
     eq FVectorFunctionCall.isVectorized() = true;
+    
+    inh boolean FIdUseExp.scalarizeExpandedContext();
+    eq BaseNode.getChild().scalarizeExpandedContext()           = true;
+    eq FFunctionCall.getChild().scalarizeExpandedContext()      = !inFunction();
+    eq FFunctionCallLeft.getChild().scalarizeExpandedContext() = !inFunction();
+    eq FAssignStmt.getLeft().scalarizeExpandedContext()         = !inFunction();
+    eq FSizeExp.getChild().scalarizeExpandedContext()           = !inFunction();
     
 	/**
 	 * Scalarize expressions.

trunk/Compiler/ModelicaFrontEnd/test/modelica/FunctionTests.mo
@@ -7098,6 +7098,62 @@ end FunctionTests.ArrayExpInFunc46;
 ")})));
 end ArrayExpInFunc46;
 
+model ArrayExpInFunc47
+    function f1
+        input Real[:,:] x;
+        output Real y = sum(x);
+        algorithm
+        annotation(Inline=false);
+    end f1;
+    
+    function f2
+        input Real[2] x;
+        output Real y = f1({x,x});
+        algorithm
+        annotation(Inline=false);
+    end f2;
+    
+    Real y = f2({time,time});
+
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="ArrayExpInFunc47",
+            description="",
+            flatModel="
+fclass FunctionTests.ArrayExpInFunc47
+ Real y;
+equation
+ y = FunctionTests.ArrayExpInFunc47.f2({time, time});
+
+public
+ function FunctionTests.ArrayExpInFunc47.f2
+  input Real[:] x;
+  output Real y;
+ algorithm
+  y := FunctionTests.ArrayExpInFunc47.f1({{x[1], x[2]}, {x[1], x[2]}});
+  return;
+ annotation(Inline = false);
+ end FunctionTests.ArrayExpInFunc47.f2;
+
+ function FunctionTests.ArrayExpInFunc47.f1
+  input Real[:,:] x;
+  output Real y;
+  Real temp_1;
+ algorithm
+  temp_1 := 0.0;
+  for i1 in 1:size(x, 1) loop
+   for i2 in 1:size(x, 2) loop
+    temp_1 := temp_1 + x[i1,i2];
+   end for;
+  end for;
+  y := temp_1;
+  return;
+ annotation(Inline = false);
+ end FunctionTests.ArrayExpInFunc47.f1;
+
+end FunctionTests.ArrayExpInFunc47;
+")})));
+end ArrayExpInFunc47;
 
 
 model ArrayOutputScalarization1

trunk/Compiler/ModelicaFrontEnd/test/modelica/RecordTests.mo
@@ -5526,31 +5526,6 @@ end RecordTests.RecordScalarize53;
 ")})));
 end RecordScalarize53;
 
-model RecordScalarize54
-    record R
-        Real[1] x;
-    end R;
-    R x[2] = {R(1:1),R(2:2)};
-    R y = x[integer(time)];
-    annotation(__JModelica(UnitTesting(tests={
-        TransformCanonicalTestCase(
-            name="RecordScalarize54",
-            description="",
-            flatModel="
-fclass RecordTests.RecordScalarize54
- constant Real x[1].x[1] = 1;
- constant Real x[2].x[1] = 2;
- Real y.x[1];
- discrete Integer temp_1;
-initial equation 
- pre(temp_1) = 0;
-equation
- y.x[1] = ({{1.0}, {2.0}})[temp_1,1];
- temp_1 = if time < pre(temp_1) or time >= pre(temp_1) + 1 or initial() then integer(time) else pre(temp_1);
-end RecordTests.RecordScalarize54;
-")})));
-end RecordScalarize54;
-
 model RecordScalarize55
 
 function f