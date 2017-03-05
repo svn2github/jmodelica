svn2github/jmodelica

commit 174650068722788bb41801a316a5d4613321228f

#5096 Better solution for r9665
git-svn-id: https://svn.jmodelica.org@9670 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/scalarization/Scalarization.jrag
@@ -1340,10 +1340,16 @@ aspect Scalarization {
 		boolean last = dim == ndims() - 1;
 		for (int i = 0, n = size().get(dim); i < n; i++) {
 			FExp res;
-			if (last)
-				res = createRecordConstructorCell(indexMap, exp.extractArrayCell(indexMap, it.next()));
-			else
-				res = createRecordConstructorArray(indexMap, exp, it, dim + 1);
+            if (last) {
+                if (exp.type().isUnknown()) {
+                    // Work around for expanding FIdTempExp which doesnt know its type
+                    res = createRecordConstructorCell(indexMap, exp.extractArrayCell(indexMap, it.next()));
+                } else {
+                    res = createRecordConstructorCell(indexMap, exp.cell(it.next()));
+                }
+            } else {
+                res = createRecordConstructorArray(indexMap, exp, it, dim + 1);
+            }
 			arr.addFExp(res);
 		}
 		return arr;
@@ -1374,7 +1380,7 @@ aspect Scalarization {
 		FRecordConstructor rc = new FRecordConstructor(new FIdUse(getName()), new List());
 		for (FRecordComponentType comp : getComponents()) {
              FExp next = exp.scalarRecordFExp(indexMap, comp.getName());
-             if (exp.type().isUnknown()) {
+             if (exp.type().isUnknown() || exp.useTempVar) {
                  // Work around for expanding FIdTempExp which doesnt know its type
                  next = comp.getFType().createRecordConstructor(indexMap, exp.dynamicFExp(next));
              }
@@ -1524,21 +1530,12 @@ aspect Scalarization {
     
     syn boolean FExp.scalarizeExpanded()       = 
         isComposite() && indexNames == null && !(size().isUnknown() || inUnknownSize()) && !(useTempVar && isForIndexExp());
-    eq FIdUseExp.scalarizeExpanded()           = super.scalarizeExpanded() && scalarizeExpandedContext();
+    eq FIdUseExp.scalarizeExpanded()           = !inFunction() && super.scalarizeExpanded();
     eq FFunctionCall.scalarizeExpanded()       = ((useTempVar && !inFunction()) || isVectorized()) && super.scalarizeExpanded();
-    eq FArray.scalarizeExpanded()              = useTempVar && super.scalarizeExpanded();
-    eq FRecordConstructor.scalarizeExpanded()  = useTempVar && super.scalarizeExpanded();
     eq FIfExp.scalarizeExpanded()              = false;
     
     syn boolean FFunctionCall.isVectorized() = false;
     eq FVectorFunctionCall.isVectorized() = true;
-    
-    inh boolean FIdUseExp.scalarizeExpandedContext();
-    eq BaseNode.getChild().scalarizeExpandedContext()           = true;
-    eq FFunctionCall.getChild().scalarizeExpandedContext()      = !inFunction();
-    eq FFunctionCallLeft.getChild().scalarizeExpandedContext() = !inFunction();
-    eq FAssignStmt.getLeft().scalarizeExpandedContext()         = !inFunction();
-    eq FSizeExp.getChild().scalarizeExpandedContext()           = !inFunction();
     
 	/**
 	 * Scalarize expressions.

commit 527c375048d97b62017901e3d8f901c385610942

#5096 Fixes scalarization bug introduced in r9670
git-svn-id: https://svn.jmodelica.org@9672 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/scalarization/Scalarization.jrag
@@ -1124,8 +1124,9 @@ aspect Scalarization {
 	public FExp FQNameFull.scalarRecordFExpForCon(Map<String,FExp> indexMap, FRecordConstructor con) {
 		FQNamePart first = getFQNamePart(0);
         FExp arg = con.component(first.getName());
-		if (first.hasFArraySubscripts())
-		    arg = arg.extractArrayCell(indexMap, first.getFArraySubscripts().asIndex());
+        if (first.hasFArraySubscripts()) {
+            arg = arg.extractArrayCell(indexMap, first.getFArraySubscripts().asIndex());
+        }
 		return copySuffix().scalarRecordFExp(indexMap, arg);
 	}
 	
@@ -1139,7 +1140,7 @@ aspect Scalarization {
     
     
     public FExp FExp.extractArrayCell(Map<String,FExp> indexMap, Index i) {
-        if (size().isUnknown()) {
+        if (type().isUnknown() || size().isUnknown()) {
             int[] ind = i.index();
             FExp[] subs = new FExp[ind.length];
             for (int k = 0; k < ind.length; k++) {
@@ -1147,7 +1148,7 @@ aspect Scalarization {
             }
             return extractArrayCell(indexMap, subs);
         } else {
-            return dynamicFExp(getArray().get(i).scalarize(indexMap));
+            return cell(i);
         }
     }
     
@@ -1167,7 +1168,7 @@ aspect Scalarization {
         res.setElseExp(getElseExp().extractArrayCell(indexMap, i));
         return dynamicFExp(res);
     }
-	
+
 	/**
 	 * Scalarize equation and put all scalarized equations in list eqns.
 	 */
@@ -1341,12 +1342,7 @@ aspect Scalarization {
 		for (int i = 0, n = size().get(dim); i < n; i++) {
 			FExp res;
             if (last) {
-                if (exp.type().isUnknown()) {
-                    // Work around for expanding FIdTempExp which doesnt know its type
-                    res = createRecordConstructorCell(indexMap, exp.extractArrayCell(indexMap, it.next()));
-                } else {
-                    res = createRecordConstructorCell(indexMap, exp.cell(it.next()));
-                }
+                res = createRecordConstructorCell(indexMap, exp.extractArrayCell(indexMap, it.next()));
             } else {
                 res = createRecordConstructorArray(indexMap, exp, it, dim + 1);
             }
@@ -1845,7 +1841,7 @@ aspect Scalarization {
     public FExp FScalarExp.scalarizeExp(Map<String,FExp> indexMap) {
         int[] t = new int[getFExp().ndims()];
         Arrays.fill(t, 1);
-        return getFExp().extractArrayCell(indexMap, new Index(t));
+        return dynamicFExp(getFExp().extractArrayCell(indexMap, new Index(t))).scalarize(indexMap);
     }
     
 	public FExp FFillExp.scalarizeExp(Map<String,FExp> indexMap) {

trunk/Compiler/ModelicaMiddleEnd/test/modelica/AlgorithmTests.mo
@@ -873,4 +873,34 @@ end AlgorithmTests.UnusedBranch5;
 ")})));
 end UnusedBranch5;
 
+model VariableSubscriptAssign1
+    Real[2,2] y;
+algorithm
+    y[integer(time),:] := {time,time+1};
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="VariableSubscriptAssign1",
+            description="",
+            flatModel="
+fclass AlgorithmTests.VariableSubscriptAssign1
+ Real y[1,1];
+ Real y[1,2];
+ Real y[2,1];
+ Real y[2,2];
+ discrete Integer temp_1;
+ discrete Integer temp_2;
+initial equation 
+ pre(temp_1) = 0;
+ pre(temp_2) = 0;
+algorithm
+ temp_2 := if time < pre(temp_2) or time >= pre(temp_2) + 1 or initial() then integer(time) else pre(temp_2);
+ ({{y[1,1], y[1,2]}, {y[2,1], y[2,2]}})[temp_2,1] := time;
+ temp_1 := if time < pre(temp_1) or time >= pre(temp_1) + 1 or initial() then integer(time) else pre(temp_1);
+ ({{y[1,1], y[1,2]}, {y[2,1], y[2,2]}})[temp_1,2] := time + 1;
+end AlgorithmTests.VariableSubscriptAssign1;
+")})));
+end VariableSubscriptAssign1;
+
+
 end AlgorithmTests;