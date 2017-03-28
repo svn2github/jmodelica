svn2github/jmodelica

commit cea3af9fe7a9949dfe05c58bb791373dbdccbdf8

Fixed generation of components in expandable connectors to properly handle arrays of connectors, structural parameters, and modifications. (#5328)
git-svn-id: https://svn.jmodelica.org@9720 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/jastadd/util/Util.jrag
@@ -388,7 +388,7 @@ aspect NodeMethods {
     }
     
     public String InstComponentDecl.getNodeName() {
-        return dump() + ": '" + getComponentDecl().getClassName().qualifiedName() + " " + getComponentDecl().getName().getID() + "'";
+        return dump() + ": '" + getComponentDecl().getClassName().qualifiedName() + " " + name() + "'";
     }
 
     public class List {
@@ -1225,12 +1225,19 @@ aspect Visibility {
     syn boolean FVisibilityType.isTemporary() = false;
     eq FTemporaryVisibilityType.isTemporary() = true;
 
+    syn boolean FVisibilityType.isFromExpandableConnector()  = false;
+    eq FExpandableVisibilityType.isFromExpandableConnector() = true;
+
     syn boolean FVariable.isPublic()    = getFVisibilityType().isPublic();
     syn boolean FVariable.isProtected() = getFVisibilityType().isProtected();
 
     syn boolean FAbstractVariable.isTemporary() = false;
     eq FVariable.isTemporary()                  = getFVisibilityType().isTemporary();
-    
+
+    syn boolean FAbstractVariable.isFromExpandableConnector() = false;
+    eq FVariable.isFromExpandableConnector()                  = 
+        getFVisibilityType().isFromExpandableConnector();
+
 }
 
 

trunk/Compiler/ModelicaFlatTree/src/jastadd/Arrays.jrag
@@ -474,6 +474,12 @@ aspect Arrays {
 	eq InstComponentDecl.ndims()      = hasFArraySubscripts() ? getFArraySubscripts().ndims() : 0;
 	eq InstArrayComponentDecl.ndims() = retrieveArrayCompNdims();
 
+    /**
+     * The number of array dimensions declared locally on this component, i.e. not from the type.
+     */
+    syn int InstComponentDecl.localNdims() = 
+        hasLocalFArraySubscripts() ? getLocalFArraySubscripts().ndims() : 0;
+
     inh int InstArrayComponentDecl.retrieveArrayCompNdims();
     eq InstNode.getChild().retrieveArrayCompNdims()              = ndims() - 1;
     eq InstRecordConstructor.getChild().retrieveArrayCompNdims() = 0;

trunk/Compiler/ModelicaFlatTree/src/jastadd/Constructors.jrag
@@ -135,14 +135,14 @@ aspect Constructors {
     public FForClauseE.FForClauseE(List<FForIndex> indices, List<FAbstractEquation> eqns) {
         this(new FNormalEquation(), new List<FAttribute>(), new Opt<FStringComment>(), indices, eqns);
     }
-    
+
     /**
      * Constructs a for clause equation that is not initial.
      */
     public InstForClauseE.InstForClauseE(List<InstForIndex> indices, List<FAbstractEquation> eqns) {
         this(new FNormalEquation(), new List<FAttribute>(), new Opt<FStringComment>(), indices, eqns);
     }
-    
+
     /**
      * Constructs a for clause equation.
      */
@@ -151,30 +151,30 @@ aspect Constructors {
     }
 
     /**
-     * Constructor with only nessecary args. 
+     * Constructor with only necessary args. 
      */
     public InstCreateComponentDecl.InstCreateComponentDecl(ComponentDecl cd, InstLookupRedirectNode ln) {
-        this(cd.getClassName().newInstAccess(), new Opt(), cd, new Opt(), new Opt(), new Opt(), ln);
+        this(cd.name(), cd.getClassName().newInstAccess(), new Opt(), cd, new Opt(), new Opt(), new Opt(), ln);
     }
-    
+
     /**
-     * Constructor with only nessecary args. 
+     * Constructor with only necessary args. 
      */
     public InstCreateForIndexPrimitive.InstCreateForIndexPrimitive(ComponentDecl cd) {
         // This maneuver is pretty ugly, but we need to find an InstClassDecl
         // in the instance tree from the source tree...
-        this(cd.getClassName().newInstAccess(), new Opt(), cd, new Opt(), new Opt(), new Opt(), 
-                ((SourceRoot) cd.root()).getProgram().getInstProgramRoot());
+        this(cd.name(), cd.getClassName().newInstAccess(), new Opt(), cd, new Opt(), new Opt(), 
+                new Opt(), ((SourceRoot) cd.root()).getProgram().getInstProgramRoot());
     }
 
     /**
-     * Constructor with only nessecary args. 
+     * Constructor with only necessary args. 
      */
     public InstCreateReplacingComponentDecl.InstCreateReplacingComponentDecl(
             ComponentDecl cd, InstLookupRedirectNode ln, ComponentDecl ocd, 
             InstComponentRedeclare icr, InstComponentRedeclare cicr) {
-        this(cd.getClassName().newInstAccess(), new Opt(), cd, new Opt(), new Opt(), new Opt(), 
-                ln, ocd, icr, cicr);
+        this(cd.name(), cd.getClassName().newInstAccess(), new Opt(), cd, new Opt(), 
+                new Opt(), new Opt(), ln, ocd, icr, cicr);
     }
 
     /**
@@ -183,14 +183,14 @@ aspect Constructors {
     public FAlgorithm.FAlgorithm(List<FStatement> stmts) {
         this(new FNormalEquation(), new List<FAttribute>(), new Opt<FStringComment>(), stmts);
     }
-    
+
     /**
      * Constructs an algorithm block.
      */
     public FAlgorithm.FAlgorithm(FEquationType type, List<FStatement> stmts) {
         this(type, new List<FAttribute>(), new Opt<FStringComment>(), stmts);
     }
-    
+
     /**
      * Constructs a derivative algorithm block that is not initial.
      */

trunk/Compiler/ModelicaFlatTree/src/jastadd/ast/FlatModelica.ast
@@ -330,6 +330,13 @@ FProtectedVisibilityType : FVisibilityType;
 FTemporaryVisibilityType : FPublicVisibilityType;
 
 /**
+ * Visibility for variables in expandable connectors. 
+ *
+ * Used to mark temporary variables added during elaboration, e.g. to handle function calls returning array.
+ */
+FExpandableVisibilityType : FPublicVisibilityType;
+
+/**
  * Visibility for runtime option parameters. 
  */
 FRuntimeOptionVisibilityType : FPublicVisibilityType;

trunk/Compiler/ModelicaFrontEnd/src/jastadd/ast/InstanceTree.ast
@@ -96,7 +96,8 @@ InstConstrainingComponent : InstConstraining ::= <ComponentDecl:ComponentDecl> <
 
 // The class to expand may differ from the declared class of the component due to 
 // redeclaration of classes.
-abstract InstComponentDecl : InstBaseNode ::= ClassName:InstAccess 
+abstract InstComponentDecl : InstBaseNode ::= <Name:String>
+                                              ClassName:InstAccess 
                                               [LocalFArraySubscripts:FArraySubscripts] 
                                               <ComponentDecl:ComponentDecl> 
                                               [InstModification] 
@@ -118,7 +119,6 @@ UnknownInstComponentDecl      : InstComponentDecl;
 InstArrayComponentDecl        : InstComponentDecl ::= <Index:int>;
 InstExpandableConnectorDecl   : InstComposite;
 InstArrayExpandableConnector  : InstArrayComponentDecl;
-InstExpandableConnectorMember : InstComponentDecl ::= <CopyOf:InstComponentDecl> <Name:String>;
 
 InstReplacingComposite   : InstComposite ::= <OriginalDecl:ComponentDecl> 
                                              /OriginalInstComponent:InstComponentDecl/ 

trunk/Compiler/ModelicaFrontEnd/src/jastadd/errorcheck/ContentsCheck.jadd
@@ -228,7 +228,7 @@ aspect ContentCheck {
     public void InstComponentDecl.contentCheck(ErrorCheckType checkType) {
         if (!inFunction()) // Array sizes outside functions should be structural parameters
             size().forceVariability(checkType, fStructParameter());
-        checkQuotedIdentifier(getComponentDecl(), getComponentDecl().getName().getID());
+        checkQuotedIdentifier(getComponentDecl(), name());
     }
 
     public void InstAssignable.contentCheck(ErrorCheckType checkType) {
@@ -278,7 +278,7 @@ aspect ContentCheck {
             }
         }
         
-        checkQuotedIdentifier(getClassDecl(), getClassDecl().getName().getID());
+        checkQuotedIdentifier(getClassDecl(), name());
     }
     
     private static Set<String> AnnotationNode.buildExternalFunctionAnnotations() {

commit 022bef886a1c60fa2659e88c3345dfbdc8f15a73

Fixed bug introduced in r9720, where connecting inputs/outputs into expandable connectors on top level would be considered top level inputs/outputs. (#5328)
git-svn-id: https://svn.jmodelica.org@9730 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/jastadd/flattening/Connections.jrag
@@ -2430,6 +2430,20 @@ aspect ExpandableConnectors {
                 parent.expandConnector(memberList, templates);
             }
             
+            public String toString() {
+                StringBuilder buf = new StringBuilder("Connectors:");
+                for (InstComponentDecl c : connectors) {
+                    buf.append("\n  ");
+                    buf.append(c.qualifiedName());
+                }
+                buf.append("\nMembers:");
+                for (ConnectorMember m : members.values()) {
+                    buf.append("\n  ");
+                    buf.append(m.name);
+                }
+                return buf.toString();
+            }
+            
             private class ConnectorMember implements Comparable<ConnectorMember> {
                 
                 public final String name;

trunk/Compiler/ModelicaFrontEnd/src/jastadd/util/Util.jrag
@@ -1327,7 +1327,8 @@ aspect ComponentDeclMethods {
     /**
      * Check if this component or its class is declared input.
      */
-    syn boolean InstComponentDecl.isDeclaredInput() = isComponentDeclaredInput() || myInstClass().isInput();
+    syn boolean InstComponentDecl.isDeclaredInput() = 
+        (isComponentDeclaredInput() || myInstClass().isInput()) && !inExpandableConnector();
 
     /**
      * Check if this component is declared input.
@@ -1354,7 +1355,8 @@ aspect ComponentDeclMethods {
     /**
      * Check if this component is an output component.
      */
-    syn boolean InstComponentDecl.isOutput() = isDeclaredOutput() || inheritsOutput();
+    syn boolean InstComponentDecl.isOutput() = 
+        (isDeclaredOutput() || inheritsOutput()) && !inExpandableConnector();
     
     /**
      * Check if this component inherits output from the surrounding component.

trunk/Compiler/ModelicaFrontEnd/test/modelica/ExpandableConnectors.mo
@@ -2895,4 +2895,51 @@ end ExpandableConnectors.ConnectorArray7;
 ")})));
 end ConnectorArray7;
 
+
+model ExpandableWithInputOutput1
+    connector CI = input Real;
+    connector CO = output Real;
+    connector C = Real;
+
+    expandable connector EC
+    end EC;
+    
+    EC ec;
+    CI ci1;
+    CO co1;
+    input C ci2;
+    output C co2;
+equation
+    connect(ci1, ec.ci1);
+    connect(ci2, ec.ci2);
+    connect(co1, ec.co1);
+    connect(co2, ec.co2);
+
+    annotation(__JModelica(UnitTesting(tests={
+        FlatteningTestCase(
+            name="ExpandableWithInputOutput1",
+            description="Created variables in expandable connectors shouldn't be considered top-level input/outputs",
+            flatModel="
+fclass ExpandableConnectors.ExpandableWithInputOutput1
+ ExpandableConnectors.ExpandableWithInputOutput1.CI ec.ci1;
+ Real ec.ci2;
+ ExpandableConnectors.ExpandableWithInputOutput1.CO ec.co1;
+ Real ec.co2;
+ input ExpandableConnectors.ExpandableWithInputOutput1.CI ci1;
+ output ExpandableConnectors.ExpandableWithInputOutput1.CO co1;
+ input Real ci2;
+ output Real co2;
+equation
+ ci1 = ec.ci1;
+ ci2 = ec.ci2;
+ co1 = ec.co1;
+ co2 = ec.co2;
+
+public
+ type ExpandableConnectors.ExpandableWithInputOutput1.CI = Real;
+ type ExpandableConnectors.ExpandableWithInputOutput1.CO = Real;
+end ExpandableConnectors.ExpandableWithInputOutput1;
+")})));
+end ExpandableWithInputOutput1;
+
 end ExpandableConnectors;