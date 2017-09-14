svn2github/jmodelica

commit 4b97e9713e6d08050bba2fe55df27c73b6bb4892

#5312 Recommiting r10065 with a bug fix. "Evaluate" was interpreted as "Evaluate=false".
git-svn-id: https://svn.jmodelica.org@10078 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/jastadd/instance/InstVariability.jrag
@@ -68,16 +68,20 @@ aspect InstVariability {
             return fDiscrete();
         }
         if (v.parameterVariability() && !inRecordDecl()) {
-            
-            if (isSetAsInitial()) {
-                v = fInitialParameter();
-            } else if (!isRecord() && isEvalAnnotated()) {
-                v = fEvalParameter();
-            } else if (isFinalIndependent()) {
-                v = fFinalParameter();
+            if (!isRecord()) {
+                if (isSetAsInitial()) {
+                    v = fInitialParameter();
+                } else if (isEvalAnnotated(true)) {
+                    v = fEvalTrueParameter();
+                } else if (isEvalAnnotated(false)) {
+                    v = fEvalFalseParameter();
+                } else if (isFinalIndependent()) {
+                    v = fFinalParameter();
+                }
+                if (!(forcedVariability.testParameterVariability() && v.evalFalseParameterVariability())) {
+                    v = v.combineDown(forcedVariability);
+                }
             }
-            
-            v = v.combineDown(forcedVariability);
             v = bExpVariability().combinePropagate(v);
         }
         return v;
@@ -98,14 +102,27 @@ aspect InstVariability {
     syn boolean InstAssignable.isFinalIndependent() =
         hasBindingFExp() && myInstValueMod().isFinal() && getBindingFExp().isIndependentParameterExp();
 
-    syn boolean InstAssignable.isEvalAnnotated() = annotation().forPath("Evaluate").bool() || inEvalAnnotated();
-    inh boolean InstComponentDecl.inEvalAnnotated();
-    eq InstClassDecl.getChild().inEvalAnnotated()          = false;
-    eq InstComponentDecl.getChild().inEvalAnnotated()      = false;
-    eq InstAssignable.getChild().inEvalAnnotated()         = isEvalAnnotated();
-    eq InstArrayComponentDecl.getChild().inEvalAnnotated() = inEvalAnnotated();
-    eq InstRecordConstructor.getChild().inEvalAnnotated()  = false;
-    
+
+    syn boolean InstAssignable.isEvalAnnotated(boolean b) {
+        AnnotationNode an = evalAnnotation();
+        return an.exists() && an.isBoolValue() && (!b ^ an.bool());
+    }
+
+    syn AnnotationNode InstAssignable.evalAnnotation() {
+        AnnotationNode res = parentEvalAnnotation();
+        if (!res.exists()) {
+            res = annotation().forPath("Evaluate");
+        }
+        return res;
+    }
+
+    inh AnnotationNode InstComponentDecl.parentEvalAnnotation();
+    eq InstClassDecl.getChild().parentEvalAnnotation()          = AnnotationNode.NO_ANNOTATION;
+    eq InstComponentDecl.getChild().parentEvalAnnotation()      = AnnotationNode.NO_ANNOTATION;
+    eq InstAssignable.getChild().parentEvalAnnotation()         = evalAnnotation();
+    eq InstArrayComponentDecl.getChild().parentEvalAnnotation() = parentEvalAnnotation();
+    eq InstRecordConstructor.getChild().parentEvalAnnotation()  = AnnotationNode.NO_ANNOTATION;
+
     syn boolean InstAssignable.isSetAsInitial() = isPrimitive() && 
             attributeCValueSet(defaultVariableEvaluator(), FAttribute.FIXED) != null && 
             !fixedAttributeCValue().reduceBooleanAnd();
@@ -145,8 +162,10 @@ aspect InstVariability {
     eq FConstant.combinePropagate(FTypePrefixVariability other)             = other.combineDown(fFixedParameter());
     eq FParameter.combinePropagate(FTypePrefixVariability other)            = other.combineDown(this);
     eq FInitialParameter.combinePropagate(FTypePrefixVariability other)     = this;
-    eq FEvalParameter.combinePropagate(FTypePrefixVariability other)        =
+    eq FEvalTrueParameter.combinePropagate(FTypePrefixVariability other)    =
             fStructParameter().combinePropagate(other);
+    eq FEvalFalseParameter.combinePropagate(FTypePrefixVariability other)    =
+            other.testParameterVariability() ? this : super.combinePropagate(other);
     
     class FCompositeVariability {
         protected static Combiner propagateCombiner = new Combiner() {

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen/CCodeGen.jrag
@@ -780,11 +780,11 @@ aspect CCodeGen {
     syn VariabilityCausality_C FTypePrefixVariability.variabilityCausality_C(FVariable fv) {
         throw new UnsupportedOperationException();
     }
-    eq FConstant.variabilityCausality_C(FVariable fv)        = fv.isIndependentConstant()  ? VariabilityCausality_C.CI : VariabilityCausality_C.CD;
-    eq FParameter.variabilityCausality_C(FVariable fv)       = fv.isIndependentParameter() ? VariabilityCausality_C.PI : VariabilityCausality_C.PD;
-    eq FStructParameter.variabilityCausality_C(FVariable fv) = fv.isIndependentParameter() ? VariabilityCausality_C.PI : VariabilityCausality_C.PS;
-    eq FFinalParameter.variabilityCausality_C(FVariable fv)  = fv.isIndependentParameter() ? VariabilityCausality_C.PI : VariabilityCausality_C.PF;
-    eq FEvalParameter.variabilityCausality_C(FVariable fv)   = fv.isIndependentParameter() ? VariabilityCausality_C.PI : VariabilityCausality_C.PE;
+    eq FConstant.variabilityCausality_C(FVariable fv)          = fv.isIndependentConstant()  ? VariabilityCausality_C.CI : VariabilityCausality_C.CD;
+    eq FParameter.variabilityCausality_C(FVariable fv)         = fv.isIndependentParameter() ? VariabilityCausality_C.PI : VariabilityCausality_C.PD;
+    eq FStructParameter.variabilityCausality_C(FVariable fv)   = fv.isIndependentParameter() ? VariabilityCausality_C.PI : VariabilityCausality_C.PS;
+    eq FFinalParameter.variabilityCausality_C(FVariable fv)    = fv.isIndependentParameter() ? VariabilityCausality_C.PI : VariabilityCausality_C.PF;
+    eq FEvalTrueParameter.variabilityCausality_C(FVariable fv) = fv.isIndependentParameter() ? VariabilityCausality_C.PI : VariabilityCausality_C.PE;
     
 	syn String FAbstractVariable.funcArrayType(boolean assign) = 
 		isRecord() ? C_ARRAY_RECORD : (assign ? C_ARRAY_REFERENCE : C_ARRAY_VALUE);

trunk/Compiler/ModelicaFlatTree/src/jastadd/FlatAPI/FlatAPI.jrag
@@ -862,12 +862,12 @@ aspect FlatAPI {
      * variable.
      */
     syn FClass.ZVariability FTypePrefixVariability.zVariability(FVariable var) = null;
-    eq FConstant.zVariability(FVariable var)         = FClass.ZVariability.CONSTANT;
-    eq FFixedParameter.zVariability(FVariable var)   = FClass.ZVariability.REGULAR_PARAMETER;
-    eq FStructParameter.zVariability(FVariable var)  = FClass.ZVariability.STRUCTURAL_PARAMETER;
-    eq FFinalParameter.zVariability(FVariable var)   = FClass.ZVariability.FINAL_PARAMETER;
-    eq FEvalParameter.zVariability(FVariable var)    = FClass.ZVariability.EVAL_PARAMETER;
-    eq FInitialParameter.zVariability(FVariable var) = FClass.ZVariability.INITIAL_PARAMETER;
+    eq FConstant.zVariability(FVariable var)          = FClass.ZVariability.CONSTANT;
+    eq FFixedParameter.zVariability(FVariable var)    = FClass.ZVariability.REGULAR_PARAMETER;
+    eq FStructParameter.zVariability(FVariable var)   = FClass.ZVariability.STRUCTURAL_PARAMETER;
+    eq FFinalParameter.zVariability(FVariable var)    = FClass.ZVariability.FINAL_PARAMETER;
+    eq FEvalTrueParameter.zVariability(FVariable var) = FClass.ZVariability.EVAL_PARAMETER;
+    eq FInitialParameter.zVariability(FVariable var)  = FClass.ZVariability.INITIAL_PARAMETER;
     eq FDiscrete.zVariability(FVariable var) {
         if (var.isInput()) {
             return FClass.ZVariability.INPUT;

trunk/Compiler/ModelicaFlatTree/src/jastadd/PrettyPrint.jrag
@@ -2019,7 +2019,7 @@ aspect FlatPrettyPrint {
     eq FFixedParameter.toString()       = "parameter ";
     eq FInitialParameter.toString()     = "initial parameter ";
     eq FIndexParameter.toString()       = "index parameter";
-    eq FEvalParameter.toString()        = "eval parameter ";
+    eq FEvalTrueParameter.toString()    = "eval parameter ";
     eq FFinalParameter.toString()       = "final parameter ";
     eq FStructParameter.toString()      = "structural parameter ";
     eq FDiscrete.toString()             = "discrete ";
@@ -2030,13 +2030,13 @@ aspect FlatPrettyPrint {
         getVariability().toString() + " /* " + getName() + " */ ";
 
     syn String FTypePrefixVariability.toStringLiteral();
-    eq FParameter.toStringLiteral()       = "parameter";
-    eq FEvalParameter.toStringLiteral()   = "eval parameter";
-    eq FFinalParameter.toStringLiteral()  = "final parameter";
-    eq FStructParameter.toStringLiteral() = "structural parameter";
-    eq FDiscrete.toStringLiteral()        = "discrete-time";
-    eq FConstant.toStringLiteral()        = "constant";
-    eq FContinuous.toStringLiteral()      = "continuous-time";
+    eq FParameter.toStringLiteral()         = "parameter";
+    eq FEvalTrueParameter.toStringLiteral() = "eval parameter";
+    eq FFinalParameter.toStringLiteral()    = "final parameter";
+    eq FStructParameter.toStringLiteral()   = "structural parameter";
+    eq FDiscrete.toStringLiteral()          = "discrete-time";
+    eq FConstant.toStringLiteral()          = "constant";
+    eq FContinuous.toStringLiteral()        = "continuous-time";
     eq FCompositeVariability.toStringLiteral() = combine().toStringLiteral();
 
     syn String Restriction.toString();

trunk/Compiler/ModelicaFlatTree/src/jastadd/Variability.jrag
@@ -24,14 +24,18 @@ aspect Variability {
     eq FConstant.constantVariability() = true;	
     syn boolean FTypePrefixVariability.structParameterVariability() = false;
     eq FStructParameter.structParameterVariability() = true;
-    syn boolean FTypePrefixVariability.evalParameterVariability() = false;
-    eq FEvalParameter.evalParameterVariability() = true;
+    syn boolean FTypePrefixVariability.testParameterVariability() = false;
+    eq FTestParameter.testParameterVariability() = true;
+    syn boolean FTypePrefixVariability.evalTrueParameterVariability() = false;
+    eq FEvalTrueParameter.evalTrueParameterVariability() = true;
     syn boolean FTypePrefixVariability.finalParameterVariability() = false;
     eq FFinalParameter.finalParameterVariability() = true;
     syn boolean FTypePrefixVariability.knownParameterVariability() = false;
     eq FKnownParameter.knownParameterVariability() = true;
     syn boolean FTypePrefixVariability.indexParameterVariability() = false;
     eq FIndexParameter.indexParameterVariability() = true;
+    syn boolean FTypePrefixVariability.evalFalseParameterVariability() = false;
+    eq FEvalFalseParameter.evalFalseParameterVariability() = true;
     syn boolean FTypePrefixVariability.fixedParameterVariability()    = false;
     eq FFixedParameter.fixedParameterVariability()                    = true;
     syn boolean FTypePrefixVariability.initialParameterVariability() = false;
@@ -59,7 +63,7 @@ aspect Variability {
     syn boolean FTypePrefixVariability.lessOrEqual(FTypePrefixVariability other) = 
             compareTo(other) <= 0;
 	
-    syn boolean FTypePrefixVariability.evalOrLess()           = lessOrEqual(fEvalParameter());
+    syn boolean FTypePrefixVariability.evalOrLess()           = lessOrEqual(fEvalTrueParameter());
     syn boolean FTypePrefixVariability.knownParameterOrLess() = lessOrEqual(fFinalParameter());
     syn boolean FTypePrefixVariability.parameterOrLess()      = lessOrEqual(fInitialParameter());
     syn boolean FTypePrefixVariability.indexParameterOrLess() = lessOrEqual(fIndexParameter());
@@ -77,25 +81,29 @@ aspect Variability {
      * Also used to determine the behaviour of {@link #combine(FTypePrefixVariability)}.
      */
     abstract protected int FTypePrefixVariability.variabilityLevel();
-    protected int FConstant.variabilityLevel()        { return VARIABILITY_LEVEL; }
-    protected int FStructParameter.variabilityLevel() { return VARIABILITY_LEVEL; }
-    protected int FEvalParameter.variabilityLevel()   { return VARIABILITY_LEVEL; }
-    protected int FFinalParameter.variabilityLevel()  { return VARIABILITY_LEVEL; }
-    protected int FIndexParameter.variabilityLevel()  { return VARIABILITY_LEVEL; }
-    protected int FFixedParameter.variabilityLevel()  { return VARIABILITY_LEVEL; }
-    protected int FInitialParameter.variabilityLevel() { return VARIABILITY_LEVEL; }
-    protected int FDiscrete.variabilityLevel()        { return VARIABILITY_LEVEL; }
-    protected int FContinuous.variabilityLevel()      { return VARIABILITY_LEVEL; }
+    protected int FConstant.variabilityLevel()             { return VARIABILITY_LEVEL; }
+    protected int FStructParameter.variabilityLevel()      { return VARIABILITY_LEVEL; }
+    protected int FTestParameter.variabilityLevel()        { return VARIABILITY_LEVEL; }
+    protected int FEvalTrueParameter.variabilityLevel()    { return VARIABILITY_LEVEL; }
+    protected int FFinalParameter.variabilityLevel()       { return VARIABILITY_LEVEL; }
+    protected int FIndexParameter.variabilityLevel()       { return VARIABILITY_LEVEL; }
+    protected int FEvalFalseParameter.variabilityLevel()   { return VARIABILITY_LEVEL; }
+    protected int FFixedParameter.variabilityLevel()       { return VARIABILITY_LEVEL; }
+    protected int FInitialParameter.variabilityLevel()     { return VARIABILITY_LEVEL; }
+    protected int FDiscrete.variabilityLevel()             { return VARIABILITY_LEVEL; }
+    protected int FContinuous.variabilityLevel()           { return VARIABILITY_LEVEL; }
     protected int FCompositeVariability.variabilityLevel() { return combine().variabilityLevel(); }
-    protected static final int FConstant.VARIABILITY_LEVEL          = 0;
-    protected static final int FEvalParameter.VARIABILITY_LEVEL     = 3;
-    protected static final int FStructParameter.VARIABILITY_LEVEL   = 4;
-    protected static final int FFinalParameter.VARIABILITY_LEVEL    = 5;
-    protected static final int FIndexParameter.VARIABILITY_LEVEL    = 8;
-    protected static final int FFixedParameter.VARIABILITY_LEVEL    = 10;
-    protected static final int FInitialParameter.VARIABILITY_LEVEL = 11;
-    protected static final int FDiscrete.VARIABILITY_LEVEL          = 20;
-    protected static final int FContinuous.VARIABILITY_LEVEL        = 30;
+    protected static final int FConstant.VARIABILITY_LEVEL           = 0;
+    protected static final int FEvalTrueParameter.VARIABILITY_LEVEL  = 3;
+    protected static final int FStructParameter.VARIABILITY_LEVEL    = 4;
+    protected static final int FTestParameter.VARIABILITY_LEVEL      = 5;
+    protected static final int FFinalParameter.VARIABILITY_LEVEL     = 6;
+    protected static final int FIndexParameter.VARIABILITY_LEVEL     = 8;
+    protected static final int FEvalFalseParameter.VARIABILITY_LEVEL = 9;
+    protected static final int FFixedParameter.VARIABILITY_LEVEL     = 10;
+    protected static final int FInitialParameter.VARIABILITY_LEVEL   = 11;
+    protected static final int FDiscrete.VARIABILITY_LEVEL           = 20;
+    protected static final int FContinuous.VARIABILITY_LEVEL         = 30;
     
     /**
      * Combines (up) component variabilities to a single primitive variability.
@@ -631,15 +639,17 @@ aspect Variability {
 
 aspect VariabilitySingletons {
 
-    public static final FContinuous      FContinuous.instance      = new FContinuous();
-    public static final FDiscrete        FDiscrete.instance        = new FDiscrete();
-    public static final FInitialParameter FInitialParameter.instance = new FInitialParameter();
-    public static final FFixedParameter  FFixedParameter.instance  = new FFixedParameter();
-    public static final FIndexParameter  FIndexParameter.instance  = new FIndexParameter();
-    public static final FFinalParameter  FFinalParameter.instance  = new FFinalParameter();
-    public static final FEvalParameter   FEvalParameter.instance   = new FEvalParameter();
-    public static final FStructParameter FStructParameter.instance = new FStructParameter();
-    public static final FConstant        FConstant.instance        = new FConstant();
+    public static final FContinuous         FContinuous.instance         = new FContinuous();
+    public static final FDiscrete           FDiscrete.instance           = new FDiscrete();
+    public static final FInitialParameter   FInitialParameter.instance   = new FInitialParameter();
+    public static final FFixedParameter     FFixedParameter.instance     = new FFixedParameter();
+    public static final FEvalFalseParameter FEvalFalseParameter.instance = new FEvalFalseParameter();
+    public static final FIndexParameter     FIndexParameter.instance     = new FIndexParameter();
+    public static final FFinalParameter     FFinalParameter.instance     = new FFinalParameter();
+    public static final FEvalTrueParameter  FEvalTrueParameter.instance  = new FEvalTrueParameter();
+    public static final FStructParameter    FStructParameter.instance    = new FStructParameter();
+    public static final FTestParameter      FTestParameter.instance      = new FTestParameter();
+    public static final FConstant           FConstant.instance           = new FConstant();
 
     public static FContinuous ASTNode.fContinuous() {
         return FContinuous.instance;
@@ -664,6 +674,10 @@ aspect VariabilitySingletons {
         return FFixedParameter.instance;
     }
     
+    public static FEvalFalseParameter ASTNode.fEvalFalseParameter() {
+        return FEvalFalseParameter.instance;
+    }
+    
     public static FParameter ASTNode.fIndexParameter() {
         return FIndexParameter.instance;
     }
@@ -672,12 +686,16 @@ aspect VariabilitySingletons {
         return FFinalParameter.instance;
     }
     
-    public static FEvalParameter ASTNode.fEvalParameter() {
-        return FEvalParameter.instance;
+    public static FEvalTrueParameter ASTNode.fEvalTrueParameter() {
+        return FEvalTrueParameter.instance;
     }
     
     public static FStructParameter ASTNode.fStructParameter() {
         return FStructParameter.instance;
+    }
+    
+    public static FTestParameter ASTNode.fTestParameter() {
+        return FTestParameter.instance;
     }
     
     public static FConstant ASTNode.fConstant() {

trunk/Compiler/ModelicaFlatTree/src/jastadd/ast/FlatModelica.ast
@@ -163,6 +163,11 @@ FFixedParameter : FParameter;
 FIndexParameter : FParameter;
 
 /**
+ * Evaluate false parameter variability.
+ */
+FEvalFalseParameter : FFixedParameter;
+
+/**
  * Parameter with known value variability
  */
 abstract FKnownParameter : FParameter;
@@ -173,9 +178,15 @@ abstract FKnownParameter : FParameter;
 FStructParameter : FKnownParameter;
 
 /**
+ * Used to force variability for if equation tests
+ * Is canceled by setting Evaluate=false
+ */
+FTestParameter   : FStructParameter;
+
+/**
  * Evaluate true parameter variability.
  */
-FEvalParameter : FKnownParameter;
+FEvalTrueParameter : FKnownParameter;
 
 /**
  * Final parameter variability.

commit 51647b7654440f4f86025fbce49414f030709674

#5312 Fixed bug introduced in r10078. For expressions in binding expressions on variables with Evaluate=false annotation did not flatten properly.
git-svn-id: https://svn.jmodelica.org@10137 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/jastadd/instance/InstVariability.jrag
@@ -115,6 +115,7 @@ aspect InstVariability {
         }
         return res;
     }
+    eq InstForIndexPrimitive.evalAnnotation() = AnnotationNode.NO_ANNOTATION;
 
     inh AnnotationNode InstComponentDecl.parentEvalAnnotation();
     eq InstClassDecl.getChild().parentEvalAnnotation()          = AnnotationNode.NO_ANNOTATION;

trunk/Compiler/ModelicaFrontEnd/test/modelica/VariabilityTests.mo
@@ -739,6 +739,20 @@ end VariabilityTests.IfEquations.EvaluateAnnotation4;
 ")})));
 end EvaluateAnnotation4;
 
+model EvaluateAnnotation5
+    parameter Real[:] p = {i for i in 1:1} annotation(Evaluate=false);
+
+    annotation(__JModelica(UnitTesting(tests={
+        FlatteningTestCase(
+            name="EvaluateAnnotation5",
+            description="For index in evaluate false",
+            flatModel="
+fclass VariabilityTests.IfEquations.EvaluateAnnotation5
+ parameter Real p[1] = {1};
+end VariabilityTests.IfEquations.EvaluateAnnotation5;
+")})));
+end EvaluateAnnotation5;
+
 model EvaluateAnnotationUnbalanced1
     parameter Boolean p = false annotation(Evaluate=false);
     Real x;