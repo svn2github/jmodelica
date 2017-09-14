dbe07c43047d6371f659cf06e74eaa346af4c0d9
jsten <jsten@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2017-09-14 10:24:43 +0000

    Creating beta 5 of 2.1.x branch.
    git-svn-id: https://svn.jmodelica.org@10136 7ae51c65-13d5-4921-9d80-6511ddd1419c

---
+++ tags/2.1b5/Compiler/ModelicaCBackEnd/templates/fmi2_functions_cs_template.c
@@ -0,0 +1,68 @@
+/*
+    Copyright (C) 2013 Modelon AB
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* FMI 2.0 functions specific for CS.*/
+
+FMI2_Export fmi2Status fmi2SetRealInputDerivatives(fmi2Component c, 
+                                                   const fmi2ValueReference vr[],
+                                                   size_t nvr, const fmi2Integer order[],
+                                                   const fmi2Real value[]) {
+	return fmi2_set_real_input_derivatives(c, vr, nvr, order, value);
+}
+
+FMI2_Export fmi2Status fmi2GetRealOutputDerivatives(fmi2Component c,
+                                                    const fmi2ValueReference vr[],
+                                                    size_t nvr, const fmi2Integer order[],
+                                                    fmi2Real value[]) {
+	return fmi2_get_real_output_derivatives(c, vr, nvr, order, value);
+}
+
+FMI2_Export fmi2Status fmi2DoStep(fmi2Component c, fmi2Real currentCommunicationPoint,
+                                  fmi2Real    communicationStepSize,
+                                  fmi2Boolean noSetFMUStatePriorToCurrentPoint) {
+	return fmi2_do_step(c, currentCommunicationPoint, communicationStepSize,
+                        noSetFMUStatePriorToCurrentPoint);
+}
+
+FMI2_Export fmi2Status fmi2CancelStep(fmi2Component c) {
+	return fmi2_cancel_step(c);
+}
+
+FMI2_Export fmi2Status fmi2GetStatus(fmi2Component c, const fmi2StatusKind s,
+                                     fmi2Status* value) {
+	return fmi2_get_status(c, s, value);
+}
+
+FMI2_Export fmi2Status fmi2GetRealStatus(fmi2Component c, const fmi2StatusKind s,
+                                         fmi2Real* value) {
+	return fmi2_get_real_status(c, s, value);
+}
+
+FMI2_Export fmi2Status fmi2GetIntegerStatus(fmi2Component c, const fmi2StatusKind s,
+                                            fmi2Integer* values) {
+	return fmi2_get_integer_status(c, s, values);
+}
+
+FMI2_Export fmi2Status fmi2GetBooleanStatus(fmi2Component c, const fmi2StatusKind s,
+                                            fmi2Boolean* value) {
+	return fmi2_get_boolean_status(c, s, value);
+}
+
+FMI2_Export fmi2Status fmi2GetStringStatus(fmi2Component c, const fmi2StatusKind s,
+                                           fmi2String* value) {
+	return fmi2_get_string_status(c, s, value);
+	
+}
