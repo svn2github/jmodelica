svn2github/jmodelica

commit 04d34f49e073d17eed8f85a1741471123cb94d34

#5193 merged FEquation.genResidual, normalizing FFunctionCallEquation.genResidual
git-svn-id: https://svn.jmodelica.org@10197 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen/CCodeGen.jrag
@@ -174,14 +174,25 @@ aspect CCodeGen {
     @Override
     public void FFunctionCall.genTempVars_C(CodePrinter p, CodeStream str, String indent) {
         getArgs().genTempArg(p.PreStep, str, indent);
+        if (isFunctionCallArg()) {
+            type().print(p.createInitPrinter(str), indent, tempName_C(), this, false);
+        }
         if (isStringExp()) {
             str.print(indent);
             str.print(tempName_C());
             str.print(" = ");
             myFCallable().prettyPrintCall_C(p, str, indent, this);
             str.println(";");
+        } else if (!functionCallIsExp()) {
+            p.print(this, str, indent);
+            getArgs().genTempArg(p.PostStep, str, indent);
         }
     }
+
+	
+	
+    @Override
+    public void FArraySubscripts.genTempVars_C(CodePrinter p, CodeStream str, String indent) {}
 
     @Override
     public void FFunctionCall.prettyPrint_C(CodePrinter p, CodeStream str, String indent) {
@@ -1611,11 +1622,9 @@ aspect CCodeGen {
 	public void FFunctionCallStmt.prettyPrint_C(CodePrinter p, CodeStream str, String indent) {
 		if (!getCall().isIgnored()) {
 			p.printPreSteps(this, str, indent);
-			p.print(getCall(), str, indent);
 			for (FFunctionCallLeft l : getLefts()) {
 				l.genWriteBackAssignment_C(str, indent, null);
 			}
-            p.printPostSteps(this, str, indent);
 		}
 	}
 	
@@ -1623,8 +1632,6 @@ aspect CCodeGen {
 	public void FFunctionCallEquation.prettyPrint_C(CodePrinter p, CodeStream str, String indent) {
 		if (!getCall().isIgnored()) {
 			p.printPreSteps(this, str, indent);
-			p.print(getCall(), str, indent);
-            p.printPostSteps(this, str, indent);
 		}
 	}
 	
@@ -1634,6 +1641,7 @@ aspect CCodeGen {
         if (isWhen() && p.inInitialSystem() && !getTest().isInitialGuard()) {
             return;
         }
+        p.printPreSteps(this, str, indent);
         genIfStart_C(p, str, indent);
         for (FAbstractEquation equation : getFAbstractEquations())
             p.print(equation, str, p.indent(indent));
@@ -2857,8 +2865,6 @@ aspect CCodeGen {
         if (!getCall().isIgnored()) {
             if (firstVisit) {
                 p.printPreSteps(this, str, indent);
-                p.print(getCall(), str, indent);
-                p.printPostSteps(this, str, indent);
             }
         }
     }
@@ -2966,6 +2972,20 @@ aspect CCodeGen {
     }
     
     
+    public class CStatement {
+        public static class Residual {
+            public static void print(FEquation eqn, CodePrinter p, CodeStream str, String indent, Enumerator enumerator) {
+        		p.printPreSteps(eqn, str, indent);
+				str.print(indent + "(*res)[" + enumerator.next() + "] = ");
+				p.print(eqn.getRight(), str, "");
+				str.print(" - (");
+				p.print(eqn.getLeft(), str, "");
+				str.print(");\n");
+        		p.printPostSteps(eqn, str, indent);
+            }
+        }
+    }
+
     
     /**
      * Generate residual code for equations
@@ -2979,13 +2999,7 @@ aspect CCodeGen {
     @Override
     public void FEquation.genResidual_C(CodePrinter p, CodeStream str, String indent, Enumerator enumerator, Set<FAbstractEquation> visited, Set<Integer> forIndices) {
         boolean firstVisit = visited == null || visited.add(this);
-        p.printPreSteps(this, str, indent);
-        str.print(indent + "(*res)[" + enumerator.next() + "] = ");
-        p.print(getRight(), str, "");
-        str.print(" - (");
-        p.print(getLeft(), str, "");
-        str.print(");\n");
-        p.printPostSteps(this, str, indent);
+        CStatement.Residual.print(this, p, str, indent, enumerator);
     }
 
     @Override
@@ -2994,12 +3008,10 @@ aspect CCodeGen {
         if (!getCall().isIgnored()) {
             if (firstVisit) {
                 p.printPreSteps(this, str, indent);
-                p.print(getCall(), str, indent);
-                p.printPostSteps(this, str, indent);
             }
             Enumerator indexCounter = new Enumerator();
             for (FFunctionCallLeft l : getLefts())
-                l.genWriteBackResidual_C(str, indent, enumerator, forIndices, indexCounter);
+                p.printWriteBackResidual(str, indent, l, enumerator, forIndices, indexCounter);
         }
     }
 

trunk/Compiler/ModelicaCBackEnd/test/modelica/CCodeGenTests.mo
@@ -7124,9 +7124,6 @@ int model_ode_initialize_base(jmi_t* jmi) {
     if (jmi->atInitial || jmi->atEvent) {
         _sw(1) = jmi_turn_switch_time(jmi, _time - ((pre__sampleItr_1_4 + AD_WRAP_LITERAL(1)) * jmi_divide_equation(jmi, AD_WRAP_LITERAL(2),AD_WRAP_LITERAL(3),\"(2 / 3)\")), _sw(1), JMI_REL_LT);
     }
-    if (jmi->atInitial || jmi->atEvent) {
-        _sw(1) = jmi_turn_switch_time(jmi, _time - ((pre__sampleItr_1_4 + AD_WRAP_LITERAL(1)) * jmi_divide_equation(jmi, AD_WRAP_LITERAL(2),AD_WRAP_LITERAL(3),\"(2 / 3)\")), _sw(1), JMI_REL_LT);
-    }
     if (_sw(1) == JMI_FALSE) {
         jmi_assert_failed(\"Too long time steps relative to sample interval.\", JMI_ASSERT_ERROR);
     }
@@ -7139,9 +7136,6 @@ int model_ode_initialize_base(jmi_t* jmi) {
         _sw(2) = jmi_turn_switch_time(jmi, _time - (pre__sampleItr_2_6 * jmi_divide_equation(jmi, 1,3,\"(1 / 3)\")), _sw(2), JMI_REL_GEQ);
     }
     _temp_2_5 = LOG_EXP_AND(LOG_EXP_NOT(_atInitial), _sw(2));
-    if (jmi->atInitial || jmi->atEvent) {
-        _sw(3) = jmi_turn_switch_time(jmi, _time - ((pre__sampleItr_2_6 + AD_WRAP_LITERAL(1)) * jmi_divide_equation(jmi, AD_WRAP_LITERAL(1),AD_WRAP_LITERAL(3),\"(1 / 3)\")), _sw(3), JMI_REL_LT);
-    }
     if (jmi->atInitial || jmi->atEvent) {
         _sw(3) = jmi_turn_switch_time(jmi, _time - ((pre__sampleItr_2_6 + AD_WRAP_LITERAL(1)) * jmi_divide_equation(jmi, AD_WRAP_LITERAL(1),AD_WRAP_LITERAL(3),\"(1 / 3)\")), _sw(3), JMI_REL_LT);
     }
@@ -7175,9 +7169,6 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
         x[0] = 536870921;
     } else if (evaluation_mode & JMI_BLOCK_EVALUATE || evaluation_mode & JMI_BLOCK_WRITE_BACK) {
         if ((evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) == 0) {
-        }
-        if (evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) {
-            _sw(3) = jmi_turn_switch_time(jmi, _time - ((pre__sampleItr_2_6 + AD_WRAP_LITERAL(1)) * jmi_divide_equation(jmi, AD_WRAP_LITERAL(1),AD_WRAP_LITERAL(3),\"(1 / 3)\")), _sw(3), JMI_REL_LT);
         }
         if (evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) {
             _sw(3) = jmi_turn_switch_time(jmi, _time - ((pre__sampleItr_2_6 + AD_WRAP_LITERAL(1)) * jmi_divide_equation(jmi, AD_WRAP_LITERAL(1),AD_WRAP_LITERAL(3),\"(1 / 3)\")), _sw(3), JMI_REL_LT);
@@ -7218,9 +7209,6 @@ static int dae_block_1(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
         x[0] = 536870920;
     } else if (evaluation_mode & JMI_BLOCK_EVALUATE || evaluation_mode & JMI_BLOCK_WRITE_BACK) {
         if ((evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) == 0) {
-        }
-        if (evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) {
-            _sw(1) = jmi_turn_switch_time(jmi, _time - ((pre__sampleItr_1_4 + AD_WRAP_LITERAL(1)) * jmi_divide_equation(jmi, AD_WRAP_LITERAL(2),AD_WRAP_LITERAL(3),\"(2 / 3)\")), _sw(1), JMI_REL_LT);
         }
         if (evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) {
             _sw(1) = jmi_turn_switch_time(jmi, _time - ((pre__sampleItr_1_4 + AD_WRAP_LITERAL(1)) * jmi_divide_equation(jmi, AD_WRAP_LITERAL(2),AD_WRAP_LITERAL(3),\"(2 / 3)\")), _sw(1), JMI_REL_LT);
@@ -7324,9 +7312,6 @@ int model_ode_initialize_base(jmi_t* jmi) {
     if (jmi->atInitial || jmi->atEvent) {
         _sw(1) = jmi_turn_switch_time(jmi, _time - ((pre__sampleItr_1_12 + AD_WRAP_LITERAL(1)) * _h_11), _sw(1), JMI_REL_LT);
     }
-    if (jmi->atInitial || jmi->atEvent) {
-        _sw(1) = jmi_turn_switch_time(jmi, _time - ((pre__sampleItr_1_12 + AD_WRAP_LITERAL(1)) * _h_11), _sw(1), JMI_REL_LT);
-    }
     if (_sw(1) == JMI_FALSE) {
         jmi_assert_failed(\"Too long time steps relative to sample interval.\", JMI_ASSERT_ERROR);
     }
@@ -7354,9 +7339,6 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
         x[0] = 536870927;
     } else if (evaluation_mode & JMI_BLOCK_EVALUATE || evaluation_mode & JMI_BLOCK_WRITE_BACK) {
         if ((evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) == 0) {
-        }
-        if (evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) {
-            _sw(1) = jmi_turn_switch_time(jmi, _time - ((pre__sampleItr_1_12 + AD_WRAP_LITERAL(1)) * _h_11), _sw(1), JMI_REL_LT);
         }
         if (evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) {
             _sw(1) = jmi_turn_switch_time(jmi, _time - ((pre__sampleItr_1_12 + AD_WRAP_LITERAL(1)) * _h_11), _sw(1), JMI_REL_LT);
@@ -7484,9 +7466,6 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
         if (evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) {
             _sw(1) = jmi_turn_switch_time(jmi, _time - ((pre__sampleItr_1_13 + AD_WRAP_LITERAL(1)) * _h_11), _sw(1), JMI_REL_LT);
         }
-        if (evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) {
-            _sw(1) = jmi_turn_switch_time(jmi, _time - ((pre__sampleItr_1_13 + AD_WRAP_LITERAL(1)) * _h_11), _sw(1), JMI_REL_LT);
-        }
         if (_sw(1) == JMI_FALSE) {
             jmi_assert_failed(\"Too long time steps relative to sample interval.\", JMI_ASSERT_ERROR);
         }
@@ -7549,9 +7528,6 @@ int model_ode_initialize_base(jmi_t* jmi) {
         _sw(0) = jmi_turn_switch_time(jmi, _time - (pre__sampleItr_1_13 * _h_11), _sw(0), JMI_REL_GEQ);
     }
     _sampleTrigger_0 = LOG_EXP_AND(LOG_EXP_NOT(_atInitial), _sw(0));
-    if (jmi->atInitial || jmi->atEvent) {
-        _sw(1) = jmi_turn_switch_time(jmi, _time - ((pre__sampleItr_1_13 + AD_WRAP_LITERAL(1)) * _h_11), _sw(1), JMI_REL_LT);
-    }
     if (jmi->atInitial || jmi->atEvent) {
         _sw(1) = jmi_turn_switch_time(jmi, _time - ((pre__sampleItr_1_13 + AD_WRAP_LITERAL(1)) * _h_11), _sw(1), JMI_REL_LT);
     }
@@ -7629,9 +7605,6 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
         if (evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) {
             _sw(1) = jmi_turn_switch_time(jmi, _time - (pre__sampleItr_1_3 + AD_WRAP_LITERAL(1)), _sw(1), JMI_REL_LT);
         }
-        if (evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) {
-            _sw(1) = jmi_turn_switch_time(jmi, _time - (pre__sampleItr_1_3 + AD_WRAP_LITERAL(1)), _sw(1), JMI_REL_LT);
-        }
         if (_sw(1) == JMI_FALSE) {
             jmi_assert_failed(\"Too long time steps relative to sample interval.\", JMI_ASSERT_ERROR);
         }
@@ -7692,9 +7665,6 @@ int model_ode_initialize_base(jmi_t* jmi) {
         _sw(0) = jmi_turn_switch_time(jmi, _time - (pre__sampleItr_1_3), _sw(0), JMI_REL_GEQ);
     }
     _temp_1_2 = LOG_EXP_AND(LOG_EXP_NOT(_atInitial), _sw(0));
-    if (jmi->atInitial || jmi->atEvent) {
-        _sw(1) = jmi_turn_switch_time(jmi, _time - (pre__sampleItr_1_3 + AD_WRAP_LITERAL(1)), _sw(1), JMI_REL_LT);
-    }
     if (jmi->atInitial || jmi->atEvent) {
         _sw(1) = jmi_turn_switch_time(jmi, _time - (pre__sampleItr_1_3 + AD_WRAP_LITERAL(1)), _sw(1), JMI_REL_LT);
     }
@@ -16207,10 +16177,10 @@ void func_CCodeGenTests_StringOperations8_f2_def0(jmi_real_t x_v, jmi_real_t* y_
     snprintf(JMI_STR_END(tmp_4), JMI_STR_LEFT(tmp_4), \"%-.*g\", (int) 6, x_v);
     snprintf(JMI_STR_END(tmp_4), JMI_STR_LEFT(tmp_4), \"%s\", \" |\");
     func_CCodeGenTests_StringOperations8_f1_def1(tmp_4, &tmp_1, NULL);
-    JMI_ASG(STR, s_v, tmp_1)
-    JMI_FREE(tmp_1)
     JMI_FREE(tmp_3)
     JMI_FREE(tmp_4)
+    JMI_ASG(STR, s_v, tmp_1)
+    JMI_FREE(tmp_1)
     func_Modelica_Utilities_Streams_print_def2(s_v, \"\");
     JMI_RET(GEN, y_o, y_v)
     JMI_DYNAMIC_FREE()
@@ -16619,9 +16589,6 @@ int model_ode_initialize_base(jmi_t* jmi) {
     if (jmi->atInitial || jmi->atEvent) {
         _sw(0) = jmi_turn_switch(jmi, _x_0 - (AD_WRAP_LITERAL(3)), _sw(0), JMI_REL_LT);
     }
-    if (jmi->atInitial || jmi->atEvent) {
-        _sw(0) = jmi_turn_switch(jmi, _x_0 - (AD_WRAP_LITERAL(3)), _sw(0), JMI_REL_LT);
-    }
     if (_sw(0) == JMI_FALSE) {
         jmi_assert_failed(\"x is too high.\", JMI_ASSERT_ERROR);
     }
@@ -16629,14 +16596,8 @@ int model_ode_initialize_base(jmi_t* jmi) {
     if (jmi->atInitial || jmi->atEvent) {
         _sw(1) = jmi_turn_switch(jmi, _y_1 - (AD_WRAP_LITERAL(4)), _sw(1), JMI_REL_LT);
     }
-    if (jmi->atInitial || jmi->atEvent) {
-        _sw(1) = jmi_turn_switch(jmi, _y_1 - (AD_WRAP_LITERAL(4)), _sw(1), JMI_REL_LT);
-    }
     if (_sw(1) == JMI_FALSE) {
         jmi_assert_failed(\"y is too high.\", JMI_ASSERT_ERROR);
-    }
-    if (jmi->atInitial || jmi->atEvent) {
-        _sw(2) = jmi_turn_switch(jmi, _x_0 + _y_1 - (AD_WRAP_LITERAL(5)), _sw(2), JMI_REL_LT);
     }
     if (jmi->atInitial || jmi->atEvent) {
         _sw(2) = jmi_turn_switch(jmi, _x_0 + _y_1 - (AD_WRAP_LITERAL(5)), _sw(2), JMI_REL_LT);
@@ -16657,23 +16618,14 @@ int model_ode_derivatives_base(jmi_t* jmi) {
     if (jmi->atInitial || jmi->atEvent) {
         _sw(0) = jmi_turn_switch(jmi, _x_0 - (AD_WRAP_LITERAL(3)), _sw(0), JMI_REL_LT);
     }
-    if (jmi->atInitial || jmi->atEvent) {
-        _sw(0) = jmi_turn_switch(jmi, _x_0 - (AD_WRAP_LITERAL(3)), _sw(0), JMI_REL_LT);
-    }
     if (_sw(0) == JMI_FALSE) {
         jmi_assert_failed(\"x is too high.\", JMI_ASSERT_ERROR);
     }
     if (jmi->atInitial || jmi->atEvent) {
         _sw(1) = jmi_turn_switch(jmi, _y_1 - (AD_WRAP_LITERAL(4)), _sw(1), JMI_REL_LT);
     }
-    if (jmi->atInitial || jmi->atEvent) {
-        _sw(1) = jmi_turn_switch(jmi, _y_1 - (AD_WRAP_LITERAL(4)), _sw(1), JMI_REL_LT);
-    }
     if (_sw(1) == JMI_FALSE) {
         jmi_assert_failed(\"y is too high.\", JMI_ASSERT_ERROR);
-    }
-    if (jmi->atInitial || jmi->atEvent) {
-        _sw(2) = jmi_turn_switch(jmi, _x_0 + _y_1 - (AD_WRAP_LITERAL(5)), _sw(2), JMI_REL_LT);
     }
     if (jmi->atInitial || jmi->atEvent) {
         _sw(2) = jmi_turn_switch(jmi, _x_0 + _y_1 - (AD_WRAP_LITERAL(5)), _sw(2), JMI_REL_LT);
@@ -16706,9 +16658,6 @@ int model_ode_derivatives_base(jmi_t* jmi) {
     int ef = 0;
     JMI_DYNAMIC_INIT()
     _x_0 = _time + 1;
-    if (jmi->atInitial || jmi->atEvent) {
-        _sw(0) = jmi_turn_switch(jmi, _x_0 - (AD_WRAP_LITERAL(5)), _sw(0), JMI_REL_LT);
-    }
     if (jmi->atInitial || jmi->atEvent) {
         _sw(0) = jmi_turn_switch(jmi, _x_0 - (AD_WRAP_LITERAL(5)), _sw(0), JMI_REL_LT);
     }
@@ -16771,9 +16720,6 @@ int model_ode_derivatives_base(jmi_t* jmi) {
     int ef = 0;
     JMI_DYNAMIC_INIT()
     _x_1 = _time + 1;
-    if (jmi->atInitial || jmi->atEvent) {
-        _sw(0) = jmi_turn_switch(jmi, _x_1 - (AD_WRAP_LITERAL(5)), _sw(0), JMI_REL_LT);
-    }
     if (jmi->atInitial || jmi->atEvent) {
         _sw(0) = jmi_turn_switch(jmi, _x_1 - (AD_WRAP_LITERAL(5)), _sw(0), JMI_REL_LT);
     }
@@ -19699,7 +19645,6 @@ int model_ode_derivatives_base(jmi_t* jmi) {
     return ef;
 ")})));
 end Delay6;
-
 
 model SpatialDist1
     Real x1,x2,x3,x4;

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CADCodeGen/CADCodeGen.jrag
@@ -109,7 +109,7 @@ aspect CADCodeGen {
         @Override
         public void genResidual(FAbstractEquation node, CodeStream str, String indent, Enumerator enumerator,
                 Set<FAbstractEquation> visited, Set<Integer> forIndices) {
-            node.genResidual_CAD(enumerator, indent, str, visited, forIndices);
+            node.genResidual_CAD(this, enumerator, indent, str, visited, forIndices);
         }
     }
 
@@ -854,26 +854,18 @@ aspect CADCodeGen {
      * @param visited     A list that contains all visisted equations.
      *    Used by FFunctionCallEquation so that only one function call is generated.
      */
-    public void FAbstractEquation.genResidual_CAD(Enumerator enumerator, String indent, CodeStream str, Set<FAbstractEquation> visited, Set<Integer> forIndices) {}
+    public void FAbstractEquation.genResidual_CAD(CodePrinter p, Enumerator enumerator, String indent, CodeStream str, Set<FAbstractEquation> visited, Set<Integer> forIndices) {}
 
     @Override
-    public void FEquation.genResidual_CAD(Enumerator enumerator, String indent, CodeStream str, Set<FAbstractEquation> visited, Set<Integer> forIndices) {
+    public void FEquation.genResidual_CAD(CodePrinter p, Enumerator enumerator, String indent, CodeStream str, Set<FAbstractEquation> visited, Set<Integer> forIndices) {
         boolean firstVisit = visited == null ? true : visited.add(this);
         if(!shouldBeDifferentiated()){
             genResidual_C(printer_C, str, indent, enumerator, null, null);
             return;
         }
-        genTempVars_CAD(printer_CAD, str, indent);
-        int index = enumerator.next();
+        int index = enumerator.peek();
         
-        str.print(indent);
-        str.print("(*res)[");
-        str.print(index);
-        str.print("] = ");
-        getRight().genVar_CAD(printer_CAD, str, indent);
-        str.print(" - (");
-        getLeft().genVar_CAD(printer_CAD, str, indent);
-        str.print(");\n");
+        CStatement.Residual.print(this, p, str, indent, enumerator);
         
         str.print(indent);
         str.print("(*dF)[");
@@ -891,14 +883,14 @@ aspect CADCodeGen {
      * has several outputs which need to be aware of which index in the residual and dF vector that they should fill.
      * The call is then made and the variables are set. Then a write back function, which prints the residual code is called.
      * 
-     * @see FAbstractEquation#genResidual_CAD(Enumerator, String, CodeStream, Set, Collection)
+     * @see FAbstractEquation#genResidual_CAD(CodePrinter p, Enumerator, String, CodeStream, Set, Collection)
      */
     @Override
-    public void FFunctionCallEquation.genResidual_CAD(Enumerator enumerator, String indent, CodeStream str, Set<FAbstractEquation> visited, Set<Integer> forIndices) {
+    public void FFunctionCallEquation.genResidual_CAD(CodePrinter p, Enumerator enumerator, String indent, CodeStream str, Set<FAbstractEquation> visited, Set<Integer> forIndices) {
         boolean firstVisit = visited == null ? true : visited.add(this);
         if (!getCall().isIgnored()) {
             if (firstVisit) {
-                genTempVars_CAD(printer_CAD, str, indent);
+                p.printPreSteps(this, str, indent);
             }
             Enumerator indexCounter = new Enumerator();
             for (FFunctionCallLeft l : getLefts())
@@ -907,7 +899,7 @@ aspect CADCodeGen {
     }
 
     @Override
-    public void FIfWhenElseEquation.genResidual_CAD(Enumerator enumerator, String indent, CodeStream str, Set<FAbstractEquation> visited, Set<Integer> forIndices) {
+    public void FIfWhenElseEquation.genResidual_CAD(CodePrinter p, Enumerator enumerator, String indent, CodeStream str, Set<FAbstractEquation> visited, Set<Integer> forIndices) {
         genIfStart_C(printer_CAD, str, indent);
         Iterator<Integer> fiIt = forIndices.iterator();
         int i = 0;
@@ -919,23 +911,23 @@ aspect CADCodeGen {
                 n = fiIt.hasNext() ? fiIt.next() : -1;
             }
             if (forIndices.size() == 0 || fis.size() > 0) {
-                equation.genResidual_CAD(enumerator, printer_CAD.indent(indent), str, visited, fis);
+                equation.genResidual_CAD(p, enumerator, printer_CAD.indent(indent), str, visited, fis);
             }
             i += equation.numScalarEquations();
         }
     }
 
     @Override
-    public void FIfWhenEquation.genResidual_CAD(Enumerator enumerator, String indent, CodeStream str, Set<FAbstractEquation> visited, Set<Integer> forIndices) {
+    public void FIfWhenEquation.genResidual_CAD(CodePrinter p, Enumerator enumerator, String indent, CodeStream str, Set<FAbstractEquation> visited, Set<Integer> forIndices) {
         if (!isElse()) {
             boolean firstVisit = visited == null ? true : visited.add(this);
             if (firstVisit)
                 genTempVars_CAD(printer_CAD, str, indent);
         }
         Enumerator enumCopy = enumerator.copy();
-        super.genResidual_CAD(enumerator, indent, str, visited, forIndices);
+        super.genResidual_CAD(p, enumerator, indent, str, visited, forIndices);
         if (hasElse())
-            getElse().genResidual_CAD(enumCopy, indent, str, visited, forIndices);
+            getElse().genResidual_CAD(p, enumCopy, indent, str, visited, forIndices);
         genIfEnd_C(printer_CAD, str, indent);
     }
 

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CADCodeGen/CADGenerator.jrag
@@ -76,7 +76,7 @@ public class CADGenerator extends CGenerator {
 				}
 				Enumerator enumerator = new Enumerator();
 				for (FAbstractEquation e : fclass.equations()) {
-					e.genResidual_CAD(enumerator, INDENT, genPrinter, null, new HashSet<Integer>());
+					e.genResidual_CAD(ASTNode.printer_CAD, enumerator, INDENT, genPrinter, null, new HashSet<Integer>());
 				}
 			}
 		}

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen/CCodeGenBlocks.jrag
@@ -236,9 +236,7 @@ aspect CCodeGenBlocks {
     @Override
     protected void MetaEquationBlock.genSolved(CodePrinter p, CodeStream str, String indent,
             boolean inResFunc, Set<FAbstractEquation> visited) {
-        p.printPreSteps(equation(), str, indent);
         p.print(equation(), str, indent);
-        p.printPostSteps(equation(), str, indent);
     }
     
     @Override

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen/CCodeGenExpressions.jrag
@@ -38,6 +38,8 @@ aspect CCodeGenExpressions {
     @Override
     public void FAssert.genTempVars_C(CodePrinter p, CodeStream str, String indent) {
         p.printPreSteps(getTest(), str, indent);
+		prettyPrint_C(p, str, indent);
+        p.printPostSteps(getTest(), str, indent);
     }
 
     @Override
@@ -332,6 +334,12 @@ aspect CCodeGenExpressions {
         str.print("jmi_flag_termination(jmi, ");
         p.print(getFExp(), str, indent);
         str.print(");\n");
+    }
+
+    @Override
+    public void FTerminate.genTempVars_C(CodePrinter p, CodeStream str, String indent) {
+        //super.genTempVars_C(p, str, indent); //CR: do we need this here (ASTNode, traverse all children)
+        p.print(this, str, indent);
     }
 
     @Override
@@ -647,6 +655,12 @@ aspect CCodeGenExpressions {
         str.format("%s%s = ", indent, tempName_C());
         p.print(getFExp(), str, indent);
         str.println(";");
+    }
+
+    @Override
+    public void FReinit.genTempVars_C(CodePrinter p, CodeStream str, String indent) {
+        super.genTempVars_C(p, str, indent);
+        p.print(this, str, indent);
     }
 
     /**
@@ -1443,6 +1457,9 @@ aspect CCodeGenExpressions {
         p.printPreSteps(getIn1(), str, indent);
         p.printPreSteps(getX(), str, indent);
         p.printPreSteps(getPositiveVelocity(), str, indent);
+        if (!functionCallIsExp()) {// printed later if part of expression
+            p.print(this, str, indent);
+        }
     }
 
     @Override

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen/CCodeGenFunctionIO.jrag
@@ -76,8 +76,6 @@ aspect CCodeGenFunctionIO {
     @Override
     public void FFunctionCall.genTempInputAssigns_C(CodePrinter p, CodeStream str, String indent) {
         if (isComposite()) {
-            type().print(p.createInitPrinter(str), indent, tempName_C(), this, false);
-            myFCallable().prettyPrintCall_C(p, str, indent, this);
         } else {
             super.genTempInputAssigns_C(p, str, indent);
         }
@@ -326,14 +324,16 @@ aspect CodeFuncCallInOutArgPrinters {
         @Override
         public void printWriteBackResidual(CodeStream str, String indent, FFunctionCallLeft left,
                 Enumerator enumerator, Set<Integer> forIndices, Enumerator indexCounter) {
-            WriteBackResidualPrinter p = new WriteBackResidualPrinter(enumerator, forIndices, indexCounter);
-            p.print(str, indent, left.getFExp().type(), left.name_C(), left.getFExp());
+            left.genWriteBackResidual_C(str, indent, enumerator, forIndices, indexCounter);
         }
+        //TODO printWriteBackResidual function with fiduseexp instead of ffunctioncallleft, se falgorithm.genResidual_C
     }
 
     public void FFunctionCallLeft.genWriteBackResidual_C(CodeStream str, String indent, Enumerator enumerator, Set<Integer> forIndices, Enumerator indexCounter) {
-        if (hasFExp()) 
-            printer_C.printWriteBackResidual(str, indent, this, enumerator, forIndices, indexCounter);
+        if (hasFExp()) {
+            CodePrinter.WriteBackResidualPrinter p = printer_C.new WriteBackResidualPrinter(enumerator, forIndices, indexCounter);
+            p.print(str, indent, getFExp().type(), name_C(), getFExp());
+        }
         if (replaceWithTemp()) {
             type().printFree_C(str, indent, false, getFExp().tempName_C());
         }

commit 5213327a6069d965cb4a4c17a467ad0760bd0262

#5468 Fixed bug introduced in r10197
git-svn-id: https://svn.jmodelica.org@10231 7ae51c65-13d5-4921-9d80-6511ddd1419c

branches/dev/directionalderivatives/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen/CCodeGen.jrag
@@ -189,11 +189,6 @@ aspect CCodeGen {
         }
     }
 
-	
-	
-    @Override
-    public void FArraySubscripts.genTempVars_C(CodePrinter p, CodeStream str, String indent) {}
-
     @Override
     public void FFunctionCall.prettyPrint_C(CodePrinter p, CodeStream str, String indent) {
         if (isStringExp()) {

branches/dev/directionalderivatives/Compiler/ModelicaCBackEnd/test/modelica/CCodeGenTests.mo
@@ -14678,6 +14678,40 @@ int model_ode_derivatives_base(jmi_t* jmi) {
 ")})));
 end VariableArrayIndex8;
 
+model VariableArrayIndex9
+    Integer table[:] = {42, 3};
+    Integer i = if time > 1 then 1 else 2;
+    Real x = table[table[i]];
+
+    annotation(__JModelica(UnitTesting(tests={
+        CCodeGenTestCase(
+            name="VariableArrayIndex9",
+            description="Test of nested variable array index",
+            template="$C_ode_derivatives$",
+            generatedCode="
+int model_ode_derivatives_base(jmi_t* jmi) {
+    int ef = 0;
+    JMI_DYNAMIC_INIT()
+    JMI_ARR(STAT, jmi_real_t, jmi_array_t, tmp_1, 2, 1)
+    JMI_ARR(STAT, jmi_real_t, jmi_array_t, tmp_2, 2, 1)
+    if (jmi->atInitial || jmi->atEvent) {
+        _sw(0) = jmi_turn_switch_time(jmi, _time - (AD_WRAP_LITERAL(1)), _sw(0), jmi->eventPhase ? (JMI_REL_GEQ) : (JMI_REL_GT));
+    }
+    _i_2 = COND_EXP_EQ(_sw(0), JMI_TRUE, AD_WRAP_LITERAL(1), AD_WRAP_LITERAL(2));
+    pre_i_2 = _i_2;
+    JMI_ARRAY_INIT_1(STAT, jmi_real_t, jmi_array_t, tmp_1, 2, 1, 2)
+    jmi_array_ref_1(tmp_1, 1) = 42;
+    jmi_array_ref_1(tmp_1, 2) = 3;
+    JMI_ARRAY_INIT_1(STAT, jmi_real_t, jmi_array_t, tmp_2, 2, 1, 2)
+    jmi_array_ref_1(tmp_2, 1) = 42;
+    jmi_array_ref_1(tmp_2, 2) = 3;
+    _x_3 = jmi_array_val_1(tmp_2, jmi_array_val_1(tmp_1, _i_2));
+    JMI_DYNAMIC_FREE()
+    return ef;
+}
+")})));
+end VariableArrayIndex9;
+
 model TestRelationalOp1
 Real v1(start=-1);
 Real v2(start=-1);