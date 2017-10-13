0f1eb19c58d8bd3117672533b37d04e5c3e134ca
amartensen <amartensen@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2017-10-13 07:26:31 +0000

    #5192 added println of multiple objects in CodeStream
    git-svn-id: https://svn.jmodelica.org@10249 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev/directionalderivatives/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/streams/CodeStream.java
+++ branches/dev/directionalderivatives/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/streams/CodeStream.java
@@ -132,6 +132,12 @@ public class CodeStream {
         println();
     }
 
+    public void println(Object... o) {
+        print(o);
+        println();
+    }
+
+
     public void formatln(String format, Object... args) {
         format(format, args);
         println();
