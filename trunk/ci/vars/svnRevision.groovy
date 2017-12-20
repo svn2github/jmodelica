362e884703aab3d454beb499f2de2d92aa3dc292
aramle <aramle@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2017-12-20 07:28:39 +0000

    #5525 Fixed missing 'set' in svnRevision.groovy.
    git-svn-id: https://svn.jmodelica.org@10490 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/ci/vars/svnRevision.groovy
+++ trunk/ci/vars/svnRevision.groovy
@@ -4,7 +4,7 @@ def call(path, sdk_home="C:\\JModelica.org-SDK-1.13\\") {
     print "Getting svn revision for path ${path}";
     def infoStr = bat returnStdout: true, script: """\
 @echo off
-SDK_HOME=${sdk_home}
+set SDK_HOME="${sdk_home}"
 %SDK_HOME%\\Subversion\\bin\\svn.exe info --xml "${path}"
 """;
     def revPattern = new Pattern(/^\s+revision="([0-9]+)">$/, Pattern.MULTILINE);
