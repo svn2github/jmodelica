374f2b8ac21d89a9def9f80759723bcf4c1d89cd
jsten <jsten@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-01-04 12:06:43 +0000

    Fixed bug in runMSYSWithEnv.groovy where it crashed if returnStdout was equal to true. Ticket #5525.
    git-svn-id: https://svn.jmodelica.org@10522 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/ci/vars/runMSYSWithEnv.groovy
+++ trunk/ci/vars/runMSYSWithEnv.groovy
@@ -15,7 +15,7 @@ call %SDK_HOME%\\setenv.bat
 """
     if (returnStdout) {
         // Need to split due to output of "Welcome to MSYS shell..."
-        String[] outputSplit = binaryName.trim().split("\\r?\\n", 2)
+        String[] outputSplit = output.trim().split("\\r?\\n", 2)
         if (outputSplit.lenght >= 2) {
             output = outputSplit[1]
         }
