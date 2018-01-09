4e5cf06c76451437cbb3b2423ea09de3fa94f8aa
amartensen <amartensen@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-01-09 09:23:42 +0000

    #5174 remove feature branch
    git-svn-id: https://svn.jmodelica.org@10547 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev/5174/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/problemHandling/ProblemOptionsProvider.java
+++
@@ -1,8 +0,0 @@
-package org.jmodelica.util.problemHandling;
-
-import org.jmodelica.util.OptionRegistry;
-
-public interface ProblemOptionsProvider {
-    public OptionRegistry getOptionRegistry();
-    public boolean filterThisWarning(String identifier);
-}
