3155d201a94b1e6c7894fc708d24bbb3b9fdbf1a
amartensen <amartensen@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-01-16 17:47:51 +0000

    #5543 created branch
    git-svn-id: https://svn.jmodelica.org@10576 7ae51c65-13d5-4921-9d80-6511ddd1419c

---
+++ branches/dev-5543/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/formatting/FormattingType.java
@@ -0,0 +1,12 @@
+package org.jmodelica.util.formatting;
+
+public enum FormattingType {
+    DEFAULT,
+    NON_BREAKING_WHITESPACE,
+    LINE_BREAK,
+    COMMENT,
+    VISIBILITY_INFO,
+    PARENTHESIS,
+    MIXED,
+    EMPTY;
+}
