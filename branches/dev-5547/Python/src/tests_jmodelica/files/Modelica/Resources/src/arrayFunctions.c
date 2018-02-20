1f21d6ecf8b48c1656a76443e7e7c46bb0a803dd
amartensen <amartensen@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-02-20 15:40:20 +0000

    #5547 added new external function for testing extFunc2
    git-svn-id: https://svn.jmodelica.org@10698 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev-5547/Python/src/tests_jmodelica/files/Modelica/Resources/src/arrayFunctions.c
+++ branches/dev-5547/Python/src/tests_jmodelica/files/Modelica/Resources/src/arrayFunctions.c
@@ -46,6 +46,27 @@ void extFunc1(double m, double* a, size_t a1, size_t a2, size_t a3, int* b, size
     }
 }
 
+void extFunc2(double m, double* a, size_t a1, size_t a2, size_t a3, int* b, size_t b1, size_t b2, size_t b3, int* c, size_t c1, size_t c2, double* sum, 
+    double* o, size_t o1, size_t o2, size_t o3, double* step, size_t step1)
+{
+    size_t i1,i2,i3,t1,t2,i;
+    *sum = 0;
+    i = 0;
+    for (i1 = 0; i1 < a1; i1++) {
+        for (i2 = 0; i2 < a2; i2++) {
+            t1 = i1*a2+i2;
+            for (i3 = 0; i3 < a3; i3++) {
+                t2 = t1*a3 + i3;
+                o[t2] = m*(exp(a[t2]) + exp(b[t2]));
+                if (c[t1])
+                    *sum += o[t2];
+                step[i] = *sum;
+                i = i + 1;
+            }
+        }
+    }
+}
+
 void copyBoolArray(int* bin, size_t bin_size, int* bout, size_t bout_size)
 {
     size_t i1;
