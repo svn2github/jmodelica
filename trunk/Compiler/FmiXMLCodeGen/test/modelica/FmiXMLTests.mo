svn2github/jmodelica

commit a6f1a54a8bbd9e92bd56e8bfbca9b899d8f0b748

#5563 Added start value xml attribute for some cases with fixed=true.
git-svn-id: https://svn.jmodelica.org@10706 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/FmiXMLCodeGen/src/jastadd/FmiXMLGenerator.jrag
@@ -405,14 +405,17 @@ public abstract class FmiXMLGenerator extends GenericXMLGenerator {
      */
     protected boolean generateStartAttribute(FVariable var) {
         FExp startExp = var.generateXMLStartExp();
-        return startExp != null && startExp.isConstantExp();
+        return startExp != null && startExp.isConstantExp() || 
+                (!var.variability().parameterOrLess() && var.fixedAttribute());
     }
 
     protected void generateStartAttributeIfSet(FVariable var, CodeStream genPrinter) {
         if (generateStartAttribute(var)) {
             FExp startExp = var.generateXMLStartExp();
-            String val = var.type().convert(startExp.ceval()).xmlValue();
-            generateAttribute(FAttribute.START, val, genPrinter);
+            CValue val = startExp != null ?
+                    var.type().convert(startExp.ceval()) :
+                    var.startAttributeCValue();
+            generateAttribute(FAttribute.START, val.xmlValue(), genPrinter);
         }
     }
 

trunk/Compiler/FmiXMLCodeGen/test/modelica/FmiXMLTests.mo
@@ -2091,4 +2091,82 @@ $modelStructure$
 ")})));
 end DummyDerivative2;
 
+model Start1
+    Real x(fixed=true);
+    Real y(start=0, fixed=true);
+equation
+    der(x) = 1;
+    der(y) = 1;
+
+    annotation(__JModelica(UnitTesting(tests={
+        FmiXMLCodeGenTestCase(
+            name="Start1",
+            description="XML name of dummy derivative",
+            fmi_version="1.0",
+            template="
+$modelVariables$
+",
+            generatedCode="
+<ModelVariables>
+    <ScalarVariable name=\"x\" valueReference=\"2\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
+        <Real relativeQuantity=\"false\" start=\"0.0\" />
+    </ScalarVariable>
+    <ScalarVariable name=\"der(x)\" valueReference=\"0\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
+        <Real relativeQuantity=\"false\" />
+    </ScalarVariable>
+    <ScalarVariable name=\"y\" valueReference=\"3\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
+        <Real relativeQuantity=\"false\" start=\"0.0\" />
+    </ScalarVariable>
+    <ScalarVariable name=\"der(y)\" valueReference=\"1\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
+        <Real relativeQuantity=\"false\" />
+    </ScalarVariable>
+</ModelVariables>
+")})));
+end Start1;
+
+model Start2
+    Real x(fixed=true);
+    Real y(start=0, fixed=true);
+equation
+    der(x) = 1;
+    der(y) = 1;
+
+    annotation(__JModelica(UnitTesting(tests={
+        FmiXMLCodeGenTestCase(
+            name="Start2",
+            description="XML name of dummy derivative",
+            fmi_version="2.0",
+            template="
+$modelVariables$
+$modelStructure$
+",
+            generatedCode="
+<ModelVariables>
+    <ScalarVariable name=\"x\" valueReference=\"2\" causality=\"local\" variability=\"continuous\" initial=\"exact\">
+        <Real relativeQuantity=\"false\" start=\"0.0\" />
+    </ScalarVariable>
+    <ScalarVariable name=\"der(x)\" valueReference=\"0\" causality=\"local\" variability=\"continuous\" initial=\"calculated\">
+        <Real relativeQuantity=\"false\" derivative=\"1\" />
+    </ScalarVariable>
+    <ScalarVariable name=\"y\" valueReference=\"3\" causality=\"local\" variability=\"continuous\" initial=\"exact\">
+        <Real relativeQuantity=\"false\" start=\"0.0\" />
+    </ScalarVariable>
+    <ScalarVariable name=\"der(y)\" valueReference=\"1\" causality=\"local\" variability=\"continuous\" initial=\"calculated\">
+        <Real relativeQuantity=\"false\" derivative=\"3\" />
+    </ScalarVariable>
+</ModelVariables>
+
+<ModelStructure>
+    <Derivatives>
+        <Unknown index=\"2\" dependencies=\"\" />
+        <Unknown index=\"4\" dependencies=\"\" />
+    </Derivatives>
+    <InitialUnknowns>
+        <Unknown index=\"2\" dependencies=\"\" />
+        <Unknown index=\"4\" dependencies=\"\" />
+    </InitialUnknowns>
+</ModelStructure>
+")})));
+end Start2;
+
 end FmiXMLTests;

commit 69a7a7f7a752b5f508593225e338f814bb7f36a9

#5563 Fixed bug for start attribute generation introduced in r10706
git-svn-id: https://svn.jmodelica.org@10712 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/FmiXMLCodeGen/src/jastadd/FmiXMLGenerator.jrag
@@ -405,9 +405,10 @@ public abstract class FmiXMLGenerator extends GenericXMLGenerator {
      */
     protected boolean generateStartAttribute(FVariable var) {
         FExp startExp = var.generateXMLStartExp();
-        return (startExp != null && startExp.isConstantExp() || 
-                (!var.variability().parameterOrLess() && var.fixedAttribute()))
-                && (generateAsConstant(var) || !var.isAlias());
+        if (startExp != null) {
+            return startExp.isConstantExp();
+        }
+        return !var.variability().parameterOrLess() && var.fixedAttribute() && !var.isAlias();
     }
 
     protected void generateStartAttributeIfSet(FVariable var, CodeStream genPrinter) {

trunk/Compiler/FmiXMLCodeGen/test/modelica/FmiXMLTests.mo
@@ -2217,4 +2217,33 @@ $modelVariables$
 ")})));
 end Start4;
 
+model Start5
+    parameter Real p = 1;
+    Real x(start=p, fixed=true);
+equation
+    der(x) = time;
+
+    annotation(__JModelica(UnitTesting(tests={
+        FmiXMLCodeGenTestCase(
+            name="Start5",
+            description="Start value with fixed, parameter start",
+            fmi_version="1.0",
+            template="
+$modelVariables$
+",
+            generatedCode="
+<ModelVariables>
+    <ScalarVariable name=\"p\" valueReference=\"0\" variability=\"parameter\" causality=\"internal\" alias=\"noAlias\">
+        <Real relativeQuantity=\"false\" start=\"1.0\" />
+    </ScalarVariable>
+    <ScalarVariable name=\"x\" valueReference=\"2\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
+        <Real relativeQuantity=\"false\" />
+    </ScalarVariable>
+    <ScalarVariable name=\"der(x)\" valueReference=\"1\" variability=\"continuous\" causality=\"internal\" alias=\"noAlias\">
+        <Real relativeQuantity=\"false\" />
+    </ScalarVariable>
+</ModelVariables>
+")})));
+end Start5;
+
 end FmiXMLTests;