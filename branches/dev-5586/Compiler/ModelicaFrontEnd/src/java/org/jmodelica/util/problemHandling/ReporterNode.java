dae8e3c204ee11ec6a5b6e541ec1dbba304cf0c9
amartensen <amartensen@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-03-23 13:39:53 +0000

    git-svn-id: https://svn.jmodelica.org@10833 7ae51c65-13d5-4921-9d80-6511ddd1419c

---
+++ branches/dev-5586/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/problemHandling/ReporterNode.java
@@ -0,0 +1,19 @@
+package org.jmodelica.util.problemHandling;
+
+import org.jmodelica.api.problemHandling.Problem;
+
+/**
+ * A interface that all problem reporting nodes should fulfill. In reality
+ * this is implemented by ASTNode and the interface is needed since ASTnode
+ * isn't visible from the utils package. It also means that references of this
+ * type shouldn't be saved in classes which are meant to be serialized!
+ */
+public interface ReporterNode {
+    public int lineNumber();
+    public int columnNumber();
+    public String fileName();
+    public String errorComponentName();
+    public void reportProblem(Problem problem);
+    public ProblemOptionsProvider myProblemOptionsProvider();
+    public boolean inDisabledComponent();
+}
