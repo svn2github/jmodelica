c3aa105d36706ec4f5bdc8a8c77d1f4fb09737c8
randersson <randersson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-05-07 12:26:43 +0000

    #5611 Removed some docker files.
    git-svn-id: https://svn.jmodelica.org@10956 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev-5611/Docker/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env sh
-
-. /build/config.sh
-
-apt-get remove --purge -y ${BUILD_PKGS} $(apt-mark showauto)
-
-apt-get install -y ${RUNTIME_PKGS}
-
-rm -rf /var/lib/apt/lists/*
--- branches/dev-5611/Docker/prepare.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env sh
-
-. /build/config.sh
-
-yum update
-yum --enablerepo=extras install -y epel-release
-yum install -y python-pip
-pip install ${BUILD_PKGS}
