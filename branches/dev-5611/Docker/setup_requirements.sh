857a9b8928c888129798759d3da7a3b10f1907bb
aelsheikh <aelsheikh@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-05-07 14:49:20 +0000

    first version distinguished packages for redhat & debian
    git-svn-id: https://svn.jmodelica.org@10959 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev-5611/Docker/setup_requirements.sh
+++ branches/dev-5611/Docker/setup_requirements.sh
@@ -17,3 +17,28 @@ BUILD_PKGS_JM_COMMON="cmake swig ant"
 BUILD_PKGS_JM_CENTOS="subversion-devel gcc-g++ gcc-gfortran python-ipython java-1.8.0-openjdk python-devel numpy scipy matplotlib Cython python-lxml python-nose python-jpype zlib-devel boost-devel"
 BUILD_PKGS_JM_UBUNTU="subversion g++ gfortran ipython openjdk-8-jdk python-dev python-numpy python-scipy python-matplotlib cython python-lxml python-nose python-jpype zlib1g-dev libboost-dev"
 
+LINUX_DISTRIBUTION=UNKNOWN
+
+if [ -f /etc/centos-release ]; then
+	LINUX_DISTRIBUTION=CENTOS
+elif [-f /etc/redhat-release ]; then 
+	LINUX_DISTRIBUTION=REDHAT
+elif [-f /etc/debian_version ]; then 
+	LINUX_RELEASE=DEBIAN
+else 
+	echo ERROR: current linux distribution not supported yet 
+fi
+
+if [[ "$LINUX_DISTRIBUTION" == CENTOS ]] || [[ "$LINUX_DISTRIBUTION" == CENTOS ]] ; then 
+	yum install $BUILD_PKGS_JM_COMMON 
+	yum install $BUILD_PKGS_JM_CENTOS 
+elif  [[ "$LINUX_DISTRIBUTION" == CENTOS ]]; then  
+	apt-get install $BUILD_PKGS_JM_COMMON
+	apt-get install $BUILD_PKGS_JM_UBUNTU
+fi
+
+
+
+
+
+
