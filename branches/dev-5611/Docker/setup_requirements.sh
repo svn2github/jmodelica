ed8d50fb00a85219bbc38abf46a218dac9fd179a
aelsheikh <aelsheikh@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-05-07 16:35:41 +0000

    #5611 aliasing and simplifying
    git-svn-id: https://svn.jmodelica.org@10962 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev-5611/Docker/setup_requirements.sh
+++ branches/dev-5611/Docker/setup_requirements.sh
@@ -13,25 +13,27 @@
 #    along with this program.  If not, see
 #     <http://www.ibm.com/developerworks/library/os-cpl.html/>.
 BUILD_PKGS_JM_COMMON="cmake swig ant wget tar which patch"
-BUILD_PKGS_JM_CENTOS="subversion-devel gcc-c++ gcc-gfortran python-ipython java-1.8.0-openjdk python-devel numpy scipy matplotlib Cython python-lxml python-nose python-jpype zlib-devel boost-devel"
-BUILD_PKGS_JM_UBUNTU="subversion g++ gfortran ipython openjdk-8-jdk python-dev python-numpy python-scipy python-matplotlib cython python-lxml python-nose python-jpype zlib1g-dev libboost-dev"
+BUILD_PKGS_JM_REDHAT="subversion-devel gcc-c++ gcc-gfortran python-ipython java-1.8.0-openjdk python-devel numpy scipy matplotlib Cython python-lxml python-nose python-jpype zlib-devel boost-devel"
+BUILD_PKGS_JM_DEBIAN="subversion g++ gfortran ipython openjdk-8-jdk python-dev python-numpy python-scipy python-matplotlib cython python-lxml python-nose python-jpype zlib1g-dev libboost-dev"
 
 LINUX_DISTRIBUTION=UNKNOWN
 
 if [ -f /etc/centos-release ]; then
 	LINUX_DISTRIBUTION=CENTOS
+	BUILD_PKGS_JM=$BUILD_PKGS_JM_REDHAT
+	alias pckinstall="yum -y install"
 elif [-f /etc/redhat-release ]; then 
 	LINUX_DISTRIBUTION=REDHAT
+	BUILD_PKGS_JM=$BUILD_PKGS_JM_REDHAT
+	alias pckinstall="yum -y install"
 elif [-f /etc/debian_version ]; then 
 	LINUX_DISTRIBUTION=DEBIAN
+	BUILD_PKGS_JM=$BUILD_PKGS_JM_DEBIAN
+	alias pckinstall="apt-get install"
 else 
 	echo ERROR: current linux distribution not supported yet 
 fi
 
-if [[ "$LINUX_DISTRIBUTION" == CENTOS ]] || [[ "$LINUX_DISTRIBUTION" == CENTOS ]] ; then 
-	yum install -y $BUILD_PKGS_JM_COMMON 
-	yum install -y $BUILD_PKGS_JM_CENTOS 
-elif  [[ "$LINUX_DISTRIBUTION" == CENTOS ]]; then  
-	apt-get install $BUILD_PKGS_JM_COMMON
-	apt-get install $BUILD_PKGS_JM_UBUNTU
-fi
+pckinstall $BUILD_PKGS_JM_COMMON
+pckinstall $BUILD_PKGS_JM
+
