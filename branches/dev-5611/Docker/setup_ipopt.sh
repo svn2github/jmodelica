10a748ead7e7c164616cee06e18f89f12c596c5a
randersson <randersson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-05-11 11:37:50 +0000

    #5611 Removed white-spaces from variable assignment
    git-svn-id: https://svn.jmodelica.org@11016 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev-5611/Docker/setup_ipopt.sh
+++ branches/dev-5611/Docker/setup_ipopt.sh
@@ -32,7 +32,7 @@ cd build
 #Test with absolute path due to errors
 mkdir ipopt-installation
 
-IPOPT_INSTALLATION_LOCATION = "$PWD"/ipopt-installation
+IPOPT_INSTALLATION_LOCATION="$PWD"/ipopt-installation
 export IPOPT_INSTALLATION_LOCATION
 
 ../configure --prefix=${IPOPT_INSTALLATION_LOCATION}
