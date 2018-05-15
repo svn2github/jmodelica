09ed50e4ff17e21b67750f97a3af6d94a0a6afc2
randersson <randersson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-05-15 06:00:47 +0000

    #5611 Added debug messages to see issues on jenkins
    git-svn-id: https://svn.jmodelica.org@11034 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev-5611/Docker/run_scripts.sh
+++ branches/dev-5611/Docker/run_scripts.sh
@@ -1,6 +1,12 @@
+echo "STAGE 1/6: SETTING UP REQUIREMENTS"
 . Docker/build/setup_requirements.sh
+echo "STAGE 2/6: SETTING UP PYTHON PACKAGES"
 . Docker/build/setup_python_packages.sh
+echo "STAGE 3/6: SETTING UP IPOPT"
 . Docker/build/setup_ipopt.sh
+echo "STAGE 4/6: ADDING JMODELICA"
 . Docker/build/get_jmodelica.sh
+echo "Stage 5/6: BUILDING"
 . Docker/build/build.sh
+echo "STAGE 6/6: BUILDING CASADI"
 . Docker/build/build_casadi.sh 
