6adbeb208090b09b40a546986510c46bb7b5e03b
randersson <randersson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-05-21 10:15:57 +0000

    #5611 Changed to relative paths for exeuting scripts
    git-svn-id: https://svn.jmodelica.org@11129 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev-5611/Docker/run_full_scripts.sh
+++ branches/dev-5611/Docker/run_full_scripts.sh
@@ -12,14 +12,7 @@
 #    along with this program.  If not, see
 #     <http://www.ibm.com/developerworks/library/os-cpl.html/>.
 . $1/Docker/build/settings.sh
-echo "ECHO OF USR_PATH"
-echo ${USR_PATH}
-echo "PWD is here"
-echo $PWD
-cd home
-ls -la
-cd jenkins
-ls -la
+
 echo "STAGE 0/3: FILES IN BUILD FOLDER"
 . Docker/build
 echo "STAGE 1/3: ADDING ASSIMULO"
