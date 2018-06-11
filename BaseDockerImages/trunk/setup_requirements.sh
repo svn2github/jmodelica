4a82b357b90d7ad6cc41aeccdea957dac65910af
randersson <randersson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-06-11 13:59:24 +0000

    #5611 Added additional python packages to install with pip
    git-svn-id: https://svn.jmodelica.org@11270 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- BaseDockerImages/trunk/setup_requirements.sh
+++ BaseDockerImages/trunk/setup_requirements.sh
@@ -19,9 +19,8 @@ set -e
 
 BUILD_PKGS_JM_COMMON="sudo cmake swig ant wget tar patch"
 BUILD_PKGS_JM_REDHAT="redhat-lsb ant-junit dos2unix python-pip bc make lucene which subversion gcc-c++ gcc-gfortran python-ipython java-1.8.0-openjdk python-devel numpy scipy python-matplotlib Cython python-lxml python-nose python-jpype zlib-devel boost-devel"
-BUILD_PYTHON_REDHAT=""
 BUILD_PKGS_JM_DEBIAN="dos2unix dc jcc python-lucene subversion g++ gfortran ipython openjdk-8-jdk python-dev python-numpy python-scipy python-matplotlib cython python-lxml python-nose python-jpype zlib1g-dev libboost-dev"
-BUILD_PYTHON_DEBIAN=""
+BUILD_PYTHON_PIP_PACKAGES="jupyter, colorama, nbformat, Jinja2, openpyxl, xlwings, mock, pythonnet, natsort, six"
 if [ "$LINUX_DISTRIBUTION" = "CENTOS" ]; then
 	BUILD_PKGS_JM=$BUILD_PKGS_JM_REDHAT
 	yum -y install epel-release  # for some python packages 
@@ -44,15 +43,13 @@ pckinstall $BUILD_PKGS_JM
 
 #Install package lsb separately because it conflicts with the installation above
 if [ "$LINUX_DISTRIBUTION" = "CENTOS" ]; then
-    echo "Running pip install jupyter"
-	pip install jupyter
-    echo "Installing colorama on centos"
-    pip install colorama
+    echo "Installing extra python packages with pip on CentOS"
+	pip install $BUILD_PYTHON_PIP_PACKAGES
 elif [ "$LINUX_DISTRIBUTION" = "DEBIAN" ]; then
     echo "Installing package lsb"
     apt-get -y install lsb
     echo "Installing jupyter packages"
     apt-get -y install python3-notebook jupyter-core python-ipykernel
-    echo "Installing colorama on Ubuntu"
-    pip install colorama
+    echo "Installing extra python packages with pip on Ubuntu"
+    pip install $BUILD_PYTHON_PIP_PACKAGES
 fi
