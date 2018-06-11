d43db16bf6085440ef6f98ad59a0293a00148e76
randersson <randersson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-06-11 14:05:55 +0000

    #5611 Passing full list to pip install while investigating how to pass a string of packages.
    git-svn-id: https://svn.jmodelica.org@11271 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- BaseDockerImages/trunk/setup_requirements.sh
+++ BaseDockerImages/trunk/setup_requirements.sh
@@ -44,12 +44,12 @@ pckinstall $BUILD_PKGS_JM
 #Install package lsb separately because it conflicts with the installation above
 if [ "$LINUX_DISTRIBUTION" = "CENTOS" ]; then
     echo "Installing extra python packages with pip on CentOS"
-	pip install $BUILD_PYTHON_PIP_PACKAGES
+	pip install jupyter, colorama, nbformat, Jinja2, openpyxl, xlwings, mock, pythonnet, natsort, six
 elif [ "$LINUX_DISTRIBUTION" = "DEBIAN" ]; then
     echo "Installing package lsb"
     apt-get -y install lsb
     echo "Installing jupyter packages"
     apt-get -y install python3-notebook jupyter-core python-ipykernel
     echo "Installing extra python packages with pip on Ubuntu"
-    pip install $BUILD_PYTHON_PIP_PACKAGES
+    pip install jupyter, colorama, nbformat, Jinja2, openpyxl, xlwings, mock, pythonnet, natsort, six
 fi
