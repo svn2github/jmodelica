c1df9c52e629740f987aae3fb3fa8fbd63401fd4
randersson <randersson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-06-12 05:37:08 +0000

    #5611 Added one more package and added string argument to pip
    git-svn-id: https://svn.jmodelica.org@11273 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- BaseDockerImages/trunk/setup_requirements.sh
+++ BaseDockerImages/trunk/setup_requirements.sh
@@ -20,7 +20,7 @@ set -e
 BUILD_PKGS_JM_COMMON="sudo cmake swig ant wget tar patch"
 BUILD_PKGS_JM_REDHAT="redhat-lsb ant-junit dos2unix python-pip bc make lucene which subversion gcc-c++ gcc-gfortran python-ipython java-1.8.0-openjdk python-devel numpy scipy python-matplotlib Cython python-lxml python-nose python-jpype zlib-devel boost-devel"
 BUILD_PKGS_JM_DEBIAN="dos2unix dc jcc python-lucene subversion g++ gfortran ipython openjdk-8-jdk python-dev python-numpy python-scipy python-matplotlib cython python-lxml python-nose python-jpype zlib1g-dev libboost-dev"
-BUILD_PYTHON_PIP_PACKAGES="jupyter, colorama, nbformat, Jinja2, openpyxl, xlwings, mock, pythonnet, natsort, six"
+BUILD_PYTHON_PIP_PACKAGES="jupyter colorama nbformat Jinja2 openpyxl xlwings mock pythonnet natsort six MarkupSafe"
 if [ "$LINUX_DISTRIBUTION" = "CENTOS" ]; then
 	BUILD_PKGS_JM=$BUILD_PKGS_JM_REDHAT
 	yum -y install epel-release  # for some python packages 
@@ -41,15 +41,13 @@ fi
 pckinstall $BUILD_PKGS_JM_COMMON
 pckinstall $BUILD_PKGS_JM
 
-#Install package lsb separately because it conflicts with the installation above
 if [ "$LINUX_DISTRIBUTION" = "CENTOS" ]; then
     echo "Installing extra python packages with pip on CentOS"
-	pip install jupyter colorama nbformat Jinja2 openpyxl mock natsort six
+	pip install $BUILD_PYTHON_PIP_PACKAGES
 elif [ "$LINUX_DISTRIBUTION" = "DEBIAN" ]; then
-    echo "Installing package lsb"
-    apt-get -y install lsb
-    echo "Installing jupyter packages"
-    apt-get -y install python3-notebook jupyter-core python-ipykernel
+    echo "Installing extra packages for Ubuntu"
+    #Install package lsb separately because it conflicts with the installation above
+    apt-get -y install lsb python3-notebook jupyter-core python-ipykernel
     echo "Installing extra python packages with pip on Ubuntu"
-    pip install jupyter colorama nbformat Jinja2 openpyxl mock natsort six
+    pip install $BUILD_PYTHON_PIP_PACKAGES
 fi
