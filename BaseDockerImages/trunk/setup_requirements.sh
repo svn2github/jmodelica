cf74db5aa1d877878c53a0360917a6111148cac1
aelsheikh <aelsheikh@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-06-15 06:21:17 +0000

    #5611 updated version of scipy & cython (centos)
    git-svn-id: https://svn.jmodelica.org@11296 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- BaseDockerImages/trunk/setup_requirements.sh
+++ BaseDockerImages/trunk/setup_requirements.sh
@@ -18,10 +18,10 @@ set -e
 . ${USR_PATH}/Docker/build/settings.sh
 
 BUILD_PKGS_JM_COMMON="vim sudo cmake swig ant wget tar patch"
-BUILD_PKGS_JM_REDHAT="redhat-lsb ant-junit dos2unix python-pip bc make lucene which subversion gcc-c++ gcc-gfortran python-ipython java-1.8.0-openjdk python-devel scipy Cython python-nose python-jpype zlib-devel boost-devel"
+BUILD_PKGS_JM_REDHAT="redhat-lsb ant-junit dos2unix python-pip bc make lucene which subversion gcc-c++ gcc-gfortran python-ipython java-1.8.0-openjdk python-devel python-nose python-jpype zlib-devel boost-devel"
 BUILD_PKGS_JM_DEBIAN="dos2unix dc jcc python-lucene subversion g++ gfortran ipython openjdk-8-jdk python-dev python-scipy cython python-nose python-jpype zlib1g-dev libboost-dev"
 
-BUILD_PYTHON_PIP_PACKAGES="jupyter colorama nbformat Jinja2 openpyxl mock natsort six MarkupSafe lxml==4.2.1 numpy==1.14.4"
+BUILD_PYTHON_PIP_PACKAGES="jupyter colorama nbformat Jinja2 openpyxl mock natsort six MarkupSafe lxml==4.2.1 numpy==1.14.4 matplotlib==2.0.2"
 
 if [ "$LINUX_DISTRIBUTION" = "CENTOS" ]; then
 	BUILD_PKGS_JM=$BUILD_PKGS_JM_REDHAT
@@ -44,15 +44,16 @@ fi
 pckinstall $BUILD_PKGS_JM_COMMON
 pckinstall $BUILD_PKGS_JM
 
+pip install --upgrade pip 
+
 if [ "$LINUX_DISTRIBUTION" = "CENTOS" ]; then
     echo "Installing extra python packages with pip on CentOS"
-	pip install $BUILD_PYTHON_PIP_PACKAGES
-    pip install matplotlib==2.0.2
+    pip install scipy 
+    pip install cython 
 elif [ "$LINUX_DISTRIBUTION" = "DEBIAN" ]; then
     echo "Installing extra packages for Ubuntu"
     #Install package lsb separately because it conflicts with the installation above
     apt-get -y install lsb python3-notebook jupyter-core python-ipykernel
     echo "Installing extra python packages with pip on Ubuntu"
-    pip install $BUILD_PYTHON_PIP_PACKAGES
-    pip install matplotlib==2.0.2 #due to issues with ImportError: No module named functools_lru_cache
 fi
+pip install $BUILD_PYTHON_PIP_PACKAGES
