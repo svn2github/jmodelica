dcf2f8237d7db06a77c2177bd8a7c83790c3fb26
jkampe <jkampe@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-06-25 14:52:00 +0000

    #2654 closing dev branch
    git-svn-id: https://svn.jmodelica.org@11341 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/NewJMILogging/EclipseIDE/org.jmodelica.ide/java/org/jmodelica/ide/editor/ICurrentClassListener.java
+++
@@ -1,9 +0,0 @@
-package org.jmodelica.ide.editor;
-
-import org.jmodelica.modelica.compiler.BaseClassDecl;
-
-public interface ICurrentClassListener {
-
-	void setCurrentClass(BaseClassDecl selected);
-
-}
--- branches/NewJMILogging/ThirdParty/CasADi/cmath.sed
+++
@@ -1,4 +0,0 @@
-29 i\
-%begin %{\
-#include <cmath> /* Added to fix problem with Python + MinGW. */\
-%}
