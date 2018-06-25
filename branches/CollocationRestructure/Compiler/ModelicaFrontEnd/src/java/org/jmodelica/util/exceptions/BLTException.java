5ad0a89dc2b6f0ad9b65b6d0d500524c41656d0a
jkampe <jkampe@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-06-25 14:31:02 +0000

    #3571 closing dev branch
    git-svn-id: https://svn.jmodelica.org@11334 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/CollocationRestructure/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/exceptions/BLTException.java
+++
@@ -1,9 +0,0 @@
-package org.jmodelica.util.exceptions;
-
-public class BLTException extends ModelicaException {
-    private static final long serialVersionUID = 1L;
-
-    public BLTException(String message) {
-        super(message);
-    }
-}
