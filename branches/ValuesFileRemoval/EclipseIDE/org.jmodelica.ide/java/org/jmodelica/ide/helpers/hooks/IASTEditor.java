7e1fb9517d4eb7eaefe64bb66e333358bc7c87c7
jkampe <jkampe@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-06-25 14:42:41 +0000

    #2002 closing dev branch
    git-svn-id: https://svn.jmodelica.org@11337 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/ValuesFileRemoval/EclipseIDE/org.jmodelica.ide/java/org/jmodelica/ide/helpers/hooks/IASTEditor.java
+++
@@ -1,9 +0,0 @@
-package org.jmodelica.ide.helpers.hooks;
-
-import org.jmodelica.modelica.compiler.ClassDecl;
-
-public interface IASTEditor {
-
-	public ClassDecl getClassContainingCursor();
-	
-}
--- branches/ValuesFileRemoval/EclipseIDE/org.jmodelica.ide/tests/testcases/LibraryNoTest.java
+++
@@ -1,5 +0,0 @@
-package testcases;
-
-public class LibraryNoTest {
-
-}
