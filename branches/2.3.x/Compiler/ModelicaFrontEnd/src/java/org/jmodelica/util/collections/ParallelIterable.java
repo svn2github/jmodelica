d12a3fabcfc6805de34265bc9bb0f1090341b849
amartensen <amartensen@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-07-02 18:02:51 +0000

    merge from trunk to branches/2.3.x: #5628: r11308 #5633: r11352 #5635: r11357
    git-svn-id: https://svn.jmodelica.org@11361 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/2.3.x/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/collections/ParallelIterable.java
+++ branches/2.3.x/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/collections/ParallelIterable.java
@@ -26,22 +26,24 @@ public class ParallelIterable<T> implements Iterable<T[]> {
 	private T[] elems;
 	private Iterable<? extends T>[] iters;
 	private boolean max;
-	
+
+    @SafeVarargs
 	public ParallelIterable(T[] res, Iterable<? extends T>... iterables) {
 		this(res, false, iterables);
 	}
-	
+
+    @SafeVarargs
 	public ParallelIterable(T[] res, boolean max, Iterable<? extends T>... iterables) {
 		iters = iterables;
 		elems = res;
 		this.max = max;
 	}
 
-	public Iterator<T[]> iterator() {
-		Iterator<? extends T>[] iterators = new Iterator[iters.length];
-		for (int i = 0; i < iters.length; i++)
-			iterators[i] = iters[i].iterator();
-		return new ParallelIterator(elems, max, iterators);
-	}
+    public Iterator<T[]> iterator() {
+        Iterator<? extends T>[] iterators = new Iterator[iters.length];
+        for (int i = 0; i < iters.length; i++)
+            iterators[i] = iters[i].iterator();
+        return new ParallelIterator<T>(elems, max, iterators);
+    }
 
 }
