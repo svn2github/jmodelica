2ff13608df68bfbac2ea31919bb410b2cd87a99a
molsson <molsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-08-01 08:59:55 +0000

    Replaced FVisibilityType with enum VisibilityType. Ticket #5646.
    git-svn-id: https://svn.jmodelica.org@11414 7ae51c65-13d5-4921-9d80-6511ddd1419c

---
+++ branches/dev-performance/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/common/ast/prefixes/VisibilityType.java
@@ -0,0 +1,31 @@
+package org.jmodelica.common.ast.prefixes;
+
+public enum VisibilityType {
+
+    PUBLIC,
+    PROTECTED,
+    TEMPORARY,
+    EXPANDABLE,
+    RUNTIME_OPTION;
+    
+    public boolean isPublic() {
+        return this == PUBLIC;
+    }
+
+    public boolean isProtected() {
+        return this == PROTECTED;
+    }
+    
+    public boolean isTemporary() {
+        return this == TEMPORARY;
+    }
+
+    public boolean isFromExpandableConnector() {
+        return this == EXPANDABLE;
+    }
+
+    public boolean isRuntimeOptionVisibility() {
+        return this == RUNTIME_OPTION;
+    }
+
+}
