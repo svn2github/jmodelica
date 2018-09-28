a0c2e14da73f6294ef95c07b0703d3afde01acaa
molsson <molsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-09-28 13:48:38 +0000

    #5682 Split C files are now printed in debug log.
    git-svn-id: https://svn.jmodelica.org@11688 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev-1715/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/streams/SplitFilesCodeStream.java
+++ branches/dev-1715/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/streams/SplitFilesCodeStream.java
@@ -17,18 +17,21 @@
 package org.jmodelica.util.streams;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class SplitFilesCodeStream extends CodeStream {
     private File file;
     private boolean debugGen;
     private int i = 0;
     private String header;
+    private ArrayList<File> files;
     
     public SplitFilesCodeStream(File file, boolean debugGen, String header) {
         super((CodeStream) null);
         this.file = file;
         this.debugGen = debugGen;
         this.header = header;
+        files = new ArrayList<>();
         switchParent(nextFileStream());
     }
     
@@ -38,7 +41,9 @@ public class SplitFilesCodeStream extends CodeStream {
     }
     
     protected CodeStream nextFileStream() {
-        return createCodeStream(nextFile());
+        File nextFile = nextFile();
+        files.add(nextFile);
+        return createCodeStream(nextFile);
     }
 
     protected File nextFile() {
@@ -52,6 +57,13 @@ public class SplitFilesCodeStream extends CodeStream {
 
     protected NotNullCodeStream createCodeStream(File nextFile) {
         return new NotNullCodeStream(createPrintStream(nextFile, debugGen));
+    }
+
+    /**
+     * All files from this used by this code stream including the original file.
+     */
+    public Iterable<File> files() {
+        return files;
     }
 
 }
