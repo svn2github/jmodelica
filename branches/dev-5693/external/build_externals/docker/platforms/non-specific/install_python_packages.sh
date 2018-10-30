3ae9f7631a14c585a526dfcccc7bbd7858a64bef
randersson <randersson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-10-30 14:47:56 +0000

    #5693 Updated shell scripts that installs dependencies and removed old shell scripts that are no longer used. Added script for installing python packages with pip.
    git-svn-id: https://svn.jmodelica.org@11801 7ae51c65-13d5-4921-9d80-6511ddd1419c

---
+++ branches/dev-5693/external/build_externals/docker/platforms/non-specific/install_python_packages.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -e
+
+pip install --upgrade pip
+pip install numpy wheel setuptools
--- branches/dev-5693/external/build_externals/docker/platforms/ubuntu/install_dependencies.sh
+++ branches/dev-5693/external/build_externals/docker/platforms/ubuntu/install_dependencies.sh
@@ -7,6 +7,7 @@ apt-get update
 apt-get install -y apt-utils dialog # to fix docker issues, perhaps add --no-install-recommends 
 apt-get install -y libssl-dev zlib1g-dev libbz2-dev libsqlite3-dev # for python installations
 apt-get install -y build-essential # for a C-compiler, needed to build Python
+apt-get install -y python-pip python-dev # for installing pip
 apt-get install -y wget
 
 #for centos
