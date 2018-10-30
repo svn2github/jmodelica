67f50971b7a7484cabf2c3b78b72562f08e39cbd
molsson <molsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-10-30 15:23:16 +0000

    #5694 Added tests for GUID generation.
    git-svn-id: https://svn.jmodelica.org@11803 7ae51c65-13d5-4921-9d80-6511ddd1419c

---
+++ branches/dev-1832/Compiler/ModelicaFrontEnd/test/junit/org/jmodelica/test/common/GUIDManagerTest.java
@@ -0,0 +1,80 @@
+package org.jmodelica.test.common;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.jmodelica.common.GUIDManager;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GUIDManagerTest {
+    
+    private GUIDManager guidManager;
+    
+    @Before
+    public void setup() {
+        guidManager = new GUIDManager();
+    }
+    
+    private void test(String source, String[] dependent, String[] expected) {
+        guidManager.setSourceString(source);
+        StringBuilder[] output = new StringBuilder[dependent.length];
+        for (int i = 0; i < dependent.length; i++) {
+            output[i] = new StringBuilder();
+            guidManager.addDependentString(dependent[i], output[i]);
+        }
+        guidManager.processDependentFiles();
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(ignoreWhitespace(expected[i]), ignoreWhitespace(output[i].toString()));
+        }
+    }
+    
+    private String ignoreWhitespace(String string) {
+        return string.trim().replaceAll("\\s", " ");
+    }
+    
+    @Test
+    public void testGuid() {
+        String[] dependent = {"guid=" + guidManager.getGuidToken()};
+        String[] expected = {"guid=fd3dbec9730101bff92acc820befc34"};
+        test("Test string", dependent, expected);
+    }
+    
+    @Test
+    public void testDate() {
+        String input = "guid=" + guidManager.getGuidToken() + ", date=" + guidManager.getDateToken();
+        String expected = "guid=dd4396b82020aef1ea5c015eb67ce94";
+        guidManager.setSourceString(input);
+        StringBuilder output = new StringBuilder();
+        guidManager.addDependentString(input, output);
+        guidManager.processDependentFiles();
+        String actual = output.toString();
+        assertEquals(expected, actual.substring(0, expected.length()));
+        assertFalse(input.equals(actual));
+        actual = actual.substring(expected.length() + 2);
+        assertTrue(actual + " does not match date pattern", actual.trim().matches("date=[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}"));
+    }
+    
+    @Test
+    public void twoGuidSameLine() {
+        test(guidManager.getGuidToken() + " " + guidManager.getGuidToken(),
+                new String[]{guidManager.getGuidToken() + " " + guidManager.getGuidToken()},
+                new String[]{"e177d94705dcb794cd9aa8c0ffdbbd99" + " " + guidManager.getGuidToken()});
+    }
+    
+    @Test
+    public void twoGuidDifferentLines() {
+        test(guidManager.getGuidToken() + "\n" + guidManager.getGuidToken(),
+                new String[]{guidManager.getGuidToken() + "\n" + guidManager.getGuidToken()},
+                new String[]{"c3436849d689f4c7c964c3893f62315b" + "\n" + guidManager.getGuidToken()});
+    }
+    
+    @Test
+    public void guidTwoDependentFiles() {
+        test("Test string",
+                new String[]{guidManager.getGuidToken(), "guid: " + guidManager.getGuidToken()},
+                new String[]{"fd3dbec9730101bff92acc820befc34", "guid: fd3dbec9730101bff92acc820befc34"});
+    }
+
+}
