5d2cee77d6b6ba7ac6941de90f2e1386d92f7b7d
randersson <randersson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-11-01 07:13:59 +0000

    #5693 Disabled upgrade pip because it causes a python error "from pip import main error"
    git-svn-id: https://svn.jmodelica.org@11822 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev-5693/external/build_externals/docker/platforms/non-specific/install_python_packages.sh
+++ branches/dev-5693/external/build_externals/docker/platforms/non-specific/install_python_packages.sh
@@ -2,5 +2,7 @@
 
 set -e
 
-pip install --upgrade pip
+# TODO: seems to be a bug with pip because upgrading it causes a python error
+# have to look further into this later
+#pip install --upgrade pip
 pip install numpy wheel setuptools
