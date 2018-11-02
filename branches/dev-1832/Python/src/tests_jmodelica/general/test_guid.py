82b5d618149b6566d7a63f8c2ba98929d0e522ae
molsson <molsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-11-02 09:50:38 +0000

    #5694 Fixed null pointer exception and a test case.
    git-svn-id: https://svn.jmodelica.org@11828 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev-1832/Python/src/tests_jmodelica/general/test_guid.py
+++ branches/dev-1832/Python/src/tests_jmodelica/general/test_guid.py
@@ -32,5 +32,5 @@ class TestGuid:
         mo_file = os.path.join(get_files_path(), 'Modelica', "BouncingBall.mo")
         fmu = load_fmu(compile_fmu("BouncingBall", [mo_file]))
         guid = fmu.get_guid()
-        assert guid == "e78566267bbdd256d5ac8b1db5cb1c08", "GUID was " + fmu.get_guid()
+        assert guid == "411e30851be51a660a33943c8c1adfc6", "GUID was " + fmu.get_guid()
        
