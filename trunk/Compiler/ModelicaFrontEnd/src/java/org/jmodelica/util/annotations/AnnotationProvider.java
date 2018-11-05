8a3a732c65822519c148de9bf12a453aa9106fba
mjohnsson <mjohnsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-11-05 16:02:50 +0000

    #5698 Add more javadoc for how the AnnotationProvider framework works.
    git-svn-id: https://svn.jmodelica.org@11869 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/annotations/AnnotationProvider.java
+++ trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/annotations/AnnotationProvider.java
@@ -20,7 +20,8 @@ import org.jmodelica.util.values.Evaluable;
 
 /**
  * Generic interface which all nodes that are supposed to be navigable by
- * {@code GenericAnnotationNode}.
+ * {@code GenericAnnotationNode}. This include modifications and annotations.
+ * Provides methods for traversing the tree structure and manipulate the data.
  * 
  * @param <N> The base node type which we deal with
  * @param <V> The value that is returned by the nodes
@@ -28,8 +29,22 @@ import org.jmodelica.util.values.Evaluable;
 public interface AnnotationProvider<N extends AnnotationProvider<N, V>, V extends Evaluable> {
     public Iterable<SubNodePair<N>> annotationSubNodes();
     public V annotationValue();
+
+    /**
+     * Change the value of this Annotation/modification if possible.
+     * @param newValue The new value
+     * @throws FailedToSetAnnotationValueException If the value cannot be changed.
+     */
     public void setAnnotationValue(V newValue) throws FailedToSetAnnotationValueException;
+    
+    /**
+     * Create a new subannotation for the given name
+     * @param name The name to create a subannotation for.
+     * @return The annotationProvider for the new subannotation.
+     * @throws AnnotationEditException If the subannotation cannot be created.
+     */
     public N addAnnotationSubNode(String name) throws AnnotationEditException;
+    
     public boolean isEach();
     public boolean isFinal();
     public String resolveURI(String str) throws URIException;
