234508310b012603e5c51a039a4f6d7eb6e5180c
aramle <aramle@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-11-05 13:24:28 +0000

    #5693 Fixing unixpath for install.
    git-svn-id: https://svn.jmodelica.org@11859 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev-5693/external/build_externals/ci/vars/buildFMILibrary.groovy
+++ branches/dev-5693/external/build_externals/ci/vars/buildFMILibrary.groovy
@@ -2,13 +2,14 @@ def call(JM_CHECKOUT_PATH, JM_BRANCH, INSTALL_PATH, bitness=["32", "64"], stash=
     if (JM_CHECKOUT_PATH != null) {
         checkoutJM(${JM_BRANCH})
     }
+    INSTALL_PATH_UNIX=unixpath("${INSTALL_PATH}")
     for (bit in bitness) {
         stage ("FMILibrary ${bit} bit") {
             runMSYSWithEnv("""\
             export JM_HOME="\$(pwd)/JModelica/"
             JENKINS_BUILD_DIR="\$(pwd)/build"
             cd \${JM_HOME}/external/build_externals/build/fmil
-            make install USER_CONFIG=\${JM_HOME}/external/build_externals/configurations/FMILibrary/windows/win${bit} BUILD_DIR=\${JENKINS_BUILD_DIR} FMIL_INSTALL=${INSTALL_PATH}/fmil_install${bit}
+            make install USER_CONFIG=\${JM_HOME}/external/build_externals/configurations/FMILibrary/windows/win${bit} BUILD_DIR=\${JENKINS_BUILD_DIR} FMIL_INSTALL=${INSTALL_PATH_UNIX}/fmil_install${bit}
             """);
             if (stash || archive) {
                 dir("${INSTALL_PATH}") {
--- branches/dev-5693/external/build_externals/ci/vars/buildPyFMI.groovy
+++ branches/dev-5693/external/build_externals/ci/vars/buildPyFMI.groovy
@@ -2,15 +2,16 @@ def call(JM_CHECKOUT_PATH, JM_BRANCH, INSTALL_PATH, TARGET, bitness=["32", "64"]
     if (JM_CHECKOUT_PATH != null) {
         checkoutJM(${JM_BRANCH})
     }
+    INSTALL_PATH_UNIX=unixpath("${INSTALL_PATH}")
     if (FMIL_HOME_BASE != null) {
-        FMIL_HOME_BASE="${INSTALL_PATH}/fmil_install"
+        FMIL_HOME_BASE="${INSTALL_PATH_UNIX}/fmil_install"
     }
     for (bit in bitness) {
         runMSYSWithEnv("""\
         export JM_HOME="\$(pwd)/JModelica/"
         JENKINS_BUILD_DIR="\$(pwd)/build"
         cd \${JM_HOME}/external/build_externals/build/pyfmi
-        make ${TARGET} USER_CONFIG=\${JM_HOME}/external/build_externals/configurations/PyFMI/windows/win${bit} BUILD_DIR=\${JENKINS_BUILD_DIR} FMIL_HOME=${FMIL_HOME_BASE}${bit} INSTALL_DIR_FOLDER=${INSTALL_PATH}/${TARGET}/Python_${bit}
+        make ${TARGET} USER_CONFIG=\${JM_HOME}/external/build_externals/configurations/PyFMI/windows/win${bit} BUILD_DIR=\${JENKINS_BUILD_DIR} FMIL_HOME=${FMIL_HOME_BASE}${bit} INSTALL_DIR_FOLDER=${INSTALL_PATH_UNIX}/${TARGET}/Python_${bit}
         """);
         if (stash || archive) {
             dir("${INSTALL_PATH}/${TARGET}") {
