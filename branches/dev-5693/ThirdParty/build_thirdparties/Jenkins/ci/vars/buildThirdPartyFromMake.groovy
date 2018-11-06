6bfff1082223f5536949604f8e22dd8a898c4278
aramle <aramle@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-11-06 08:07:19 +0000

    #5693 Adding clean_install tagret and fixed typo.
    git-svn-id: https://svn.jmodelica.org@11876 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev-5693/ThirdParty/build_thirdparties/Jenkins/ci/vars/buildThirdPartyFromMake.groovy
+++ branches/dev-5693/ThirdParty/build_thirdparties/Jenkins/ci/vars/buildThirdPartyFromMake.groovy
@@ -8,7 +8,7 @@ def call(JM_CHECKOUT_PATH, JM_BRANCH, INSTALL_PATH, module,bitness=["32", "64"],
             runMSYSWithEnv("""\
             export JM_HOME="\$(pwd)/JModelica/"
             JENKINS_BUILD_DIR="\$(pwd)/build"
-            cd \${JM_HOME}/ThirdParty/nuild_thirdparties/build/${module}
+            cd \${JM_HOME}/ThirdParty/build_thirdparties/build/${module}
             make clean_install USER_CONFIG=\${JM_HOME}/ThirdParty/build_thirdparties/configurations/${module}/windows/win${bit} BUILD_DIR=\${JENKINS_BUILD_DIR} BLAS_INSTALL_DIR=${INSTALL_PATH_UNIX}/${module}_install${bit}
             """);
             if (stash || archive) {
