f13690c4363d622d8415a4d9a363b16e4a3b4731
randersson <randersson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-11-09 07:00:04 +0000

    #5693 Merged parts for building PyFMI in Docker, updated makefile config, added README and exaxmple config. Added python_configs, for windows it is currently empty so doesn't affect it in anyway
    git-svn-id: https://svn.jmodelica.org@11942 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev-5693/external/build_externals/docker/platforms/non-specific/install_python_packages.sh
+++ branches/dev-5693/external/build_externals/docker/platforms/non-specific/install_python_packages.sh
@@ -5,4 +5,4 @@ set -e
 # TODO: seems to be a bug with pip because upgrading it causes a python error
 # have to look further into this later
 #pip install --upgrade pip
-pip install numpy wheel setuptools
+#pip install numpy wheel setuptools
