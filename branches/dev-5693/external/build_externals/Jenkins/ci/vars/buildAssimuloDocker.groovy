0cd99df41ec25016569e793d2732ba8f6e441933
randersson <randersson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-11-16 14:51:36 +0000

    #5693 Updated Jenkinsfile and groovy script to correctly build archive artifacts assimulo for centos and ubuntu.
    git-svn-id: https://svn.jmodelica.org@12014 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev-5693/external/build_externals/Jenkins/ci/vars/buildAssimuloDocker.groovy
+++ branches/dev-5693/external/build_externals/Jenkins/ci/vars/buildAssimuloDocker.groovy
@@ -1,3 +1,16 @@
-def call(TARGET, USER_CONFIG_NAME) {
-    echo "TARGET=${TARGET} USER_CONFIG=${USER_CONFIG_NAME}"
+def call(PLATFORM, TARGET, USER_CONFIG, ARTIFACT_FILE="${WORKSPACE}/JModelica/artifacts_assimulo") {
+    stage("Assimulo ${TARGET} ${PLATFORM}") {
+        dir ('JModelica/external/build_externals/docker/src/components/Assimulo') {
+            try{ 
+                sh "make docker_assimulo_${TARGET} USER_CONFIG=${USER_CONFIG} ARTIFACT_FILE=${ARTIFACT_FILE}"
+                dir("${WORKSPACE}/JModelica") {
+                    artifact_list = sh returnStdout: true, script: "cat ${ARTIFACT_FILE}"
+                    archiveArtifacts artifacts: artifact_list, fingerprint: false
+                    sh "rm ${ARTIFACT_FILE}"
+                }
+            } finally {
+                sh "make clean_in_docker USER_CONFIG=${USER_CONFIG}"
+            }
+        }
+    }
 }
