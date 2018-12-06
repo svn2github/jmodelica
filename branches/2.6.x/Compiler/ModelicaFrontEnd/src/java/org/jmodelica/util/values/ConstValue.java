2c3206bc307b681f569cac474a4dbfe294e34693
jkulla <jkulla@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-12-06 16:21:52 +0000

    #5713 Merge r12094 to release branch 2.6.x
    git-svn-id: https://svn.jmodelica.org@12103 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/2.6.x/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/values/ConstValue.java
+++ branches/2.6.x/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/values/ConstValue.java
@@ -310,4 +310,25 @@ public abstract class ConstValue {
     public boolean hasStringValue() {
         return false;
     }
+    
+    /**
+     * Checks if this value is an unknown caused by an unresolvable access.
+     */
+    public boolean isUnknownAccess() {
+        return false;
+    }
+    
+    /**
+     * If this value is an unknown access returns the access as a string, otherwise null.
+     */
+    public String access() {
+        throw new ConstantEvaluationException(this, "get access from");
+    }
+    
+    /**
+     * If this value is an unknown access returns the access as a string, otherwise null.
+     */
+    public String[] accessVector() {
+        throw new ConstantEvaluationException(this, "get access vector from");
+    }
 }
