c69190a802f1f7a02c1283bbf834cd35374637af
randersson <randersson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-12-14 12:42:04 +0000

    #5693 Updated shell script for test dependencies
    git-svn-id: https://svn.jmodelica.org@12150 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev-5693/build_environment/platforms/centos/install_test_dependencies.sh
+++ branches/dev-5693/build_environment/platforms/centos/install_test_dependencies.sh
@@ -11,5 +11,9 @@ sed -i "/^backend/c\\backend:Agg" $(python -c "import matplotlib;print(matplotli
 
 yum install -y make
 
+pip install colorama decorator jinja2 jpype1
+pip install --upgrade lxml
+yum install -y java-1.8.0-openjdk
+
 # Debug packages
 yum install -y tmux vim
