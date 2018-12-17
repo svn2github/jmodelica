ca09f9610f3c77a55264860fa9ef9f05cc344898
randersson <randersson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2018-12-17 10:14:44 +0000

    #5693 Updated order of dependencies as java overrides dependencies with older versions
    git-svn-id: https://svn.jmodelica.org@12161 7ae51c65-13d5-4921-9d80-6511ddd1419c

--- branches/dev-5693/build_environment/platforms/centos/install_test_dependencies.sh
+++ branches/dev-5693/build_environment/platforms/centos/install_test_dependencies.sh
@@ -3,17 +3,16 @@
 set -e
 
 
-pip install --upgrade setuptools
-pip install scipy lxml
+yum install -y java-1.8.0-openjdk
 yum install -y python-matplotlib
 yum install -y libgomp
+yum install -y tmux vim
+pip install --upgrade setuptools
+pip install scipy lxml
 sed -i "/^backend/c\\backend:Agg" $(python -c "import matplotlib;print(matplotlib.matplotlib_fname())")
 
-yum install -y make
 
 pip install colorama decorator jinja2 jpype1
 pip install --upgrade lxml
-yum install -y java-1.8.0-openjdk
 
 # Debug packages
-yum install -y tmux vim
